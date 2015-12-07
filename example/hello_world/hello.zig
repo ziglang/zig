5fd754c84a6018f19b595154752c61c1cff3f757
Andrew Kelley <superjoe30@gmail.com>
2015-12-06 22:01:05 -0700

    fix hello world example

--- example/hello_world/hello.zig
+++ example/hello_world/hello.zig
@@ -6,18 +6,7 @@ extern {
     fn exit(code: i32) -> unreachable;
 }
 
-fn loop(a : i32) {
-    if a == 0 {
-        goto done;
-    }
-    puts("loop");
-    loop(a - 1);
-
-done:
-    return;
-}
-
 export fn _start() -> unreachable {
-    loop(3);
+    puts("Hello, world!");
     exit(0);
 }
