347866f3a70078736f8b4fb36ff458b37231889a
Andrew Kelley <superjoe30@gmail.com>
2016-01-28 11:57:56 -0700

    add parseh test for multiple fn prototypes

--- test/run_tests.cpp
+++ test/run_tests.cpp
@@ -1797,6 +1797,7 @@ static void add_parseh_test_cases(void) {
     add_parseh_case("simple data types", R"SOURCE(
 #include <stdint.h>
 int foo(char a, unsigned char b, signed char c);
+int foo(char a, unsigned char b, signed char c); // test a duplicate prototype
 void bar(uint8_t a, uint16_t b, uint32_t c, uint64_t d);
 void baz(int8_t a, int16_t b, int32_t c, int64_t d);
     )SOURCE", R"OUTPUT(pub extern fn foo(a: u8, b: u8, c: i8) -> c_int;
