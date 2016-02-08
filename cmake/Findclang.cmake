62a689f7f534beb237c634683cee50da86c439f0
Josh Wolfe <thejoshwolfe@gmail.com>
2016-02-08 14:34:51 -0700

    fix some of the ubuntu build trouble

--- cmake/Findclang.cmake
+++ cmake/Findclang.cmake
@@ -6,11 +6,11 @@
 # CLANG_INCLUDE_DIRS
 # CLANG_LIBRARIES
 
-find_path(CLANG_INCLUDE_DIRS NAMES clang/Frontend/ASTUnit.h)
+find_path(CLANG_INCLUDE_DIRS NAMES clang/Frontend/ASTUnit.h PATHS /usr/lib/llvm-3.7/include)
 
 macro(FIND_AND_ADD_CLANG_LIB _libname_)
     string(TOUPPER ${_libname_} _prettylibname_)
-    find_library(CLANG_${_prettylibname_}_LIB NAMES ${_libname_})
+    find_library(CLANG_${_prettylibname_}_LIB NAMES ${_libname_} PATHS /usr/lib/llvm-3.7/lib)
     if(CLANG_${_prettylibname_}_LIB)
         set(CLANG_LIBRARIES ${CLANG_LIBRARIES} ${CLANG_${_prettylibname_}_LIB})
     endif()
