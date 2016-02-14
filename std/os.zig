01fda6199ee0e2b5f79e58cbd862bd4882615a4b
Andrew Kelley <superjoe30@gmail.com>
2016-02-13 22:59:49 -0700

    dummy implementation of os_get_random_bytes for windows

--- std/os.zig
+++ std/os.zig
@@ -5,13 +5,24 @@ pub error SigInterrupt;
 pub error Unexpected;
 
 pub fn os_get_random_bytes(buf: []u8) -> %void {
-    const amt_got = getrandom(buf.ptr, buf.len, 0);
-    if (amt_got < 0) {
-        return switch (-amt_got) {
-            EINVAL => unreachable{},
-            EFAULT => unreachable{},
-            EINTR  => error.SigInterrupt,
-            else   => error.Unexpected,
-        }
+    switch (@compile_var("os")) {
+        linux => {
+            const amt_got = getrandom(buf.ptr, buf.len, 0);
+            if (amt_got < 0) {
+                return switch (-amt_got) {
+                    EINVAL => unreachable{},
+                    EFAULT => unreachable{},
+                    EINTR  => error.SigInterrupt,
+                    else   => error.Unexpected,
+                }
+            }
+        },
+        windows => {
+            // TODO
+            for (buf) |_, i| {
+                buf[i] = 4;
+            }
+        },
+        else => unreachable{},
     }
 }
