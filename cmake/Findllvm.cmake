3664e8e4e2478b129f128dfa49ae13d376651500
Andrew Kelley <superjoe30@gmail.com>
2016-02-15 15:50:06 -0700

    use llvm-config to find llvm include dirs

--- cmake/Findllvm.cmake
+++ cmake/Findllvm.cmake
@@ -3,13 +3,9 @@
 # See http://opensource.org/licenses/MIT
 
 # LLVM_FOUND
-# LLVM_INCLUDE_DIRS
+# LLVM_INCLUDE_DIR
 # LLVM_LIBRARIES
 # LLVM_LIBDIRS
-
-find_path(LLVM_C_INCLUDE_DIR NAMES llvm-c/Core.h PATHS /usr/include/llvm-c-3.7/)
-find_path(LLVM_INCLUDE_DIR NAMES llvm/IR/IRBuilder.h PATHS /usr/include/llvm-3.7/)
-set(LLVM_INCLUDE_DIRS ${LLVM_C_INCLUDE_DIR} ${LLVM_INCLUDE_DIR})
 
 find_program(LLVM_CONFIG_EXE NAMES llvm-config llvm-config-3.7)
 
@@ -28,10 +24,16 @@ execute_process(
     OUTPUT_VARIABLE LLVM_LIBDIRS
     OUTPUT_STRIP_TRAILING_WHITESPACE)
 
+execute_process(
+    COMMAND ${LLVM_CONFIG_EXE} --includedir
+    OUTPUT_VARIABLE LLVM_INCLUDE_DIR
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+
 set(LLVM_LIBRARIES ${LLVM_LIBRARIES} ${LLVM_SYSTEM_LIBS})
 
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LLVM DEFAULT_MSG LLVM_LIBRARIES LLVM_INCLUDE_DIRS)
+find_package_handle_standard_args(LLVM DEFAULT_MSG LLVM_LIBRARIES LLVM_INCLUDE_DIR)
 
-mark_as_advanced(LLVM_INCLUDE_DIRS LLVM_LIBRARIES LLVM_LIBDIRS)
+mark_as_advanced(LLVM_INCLUDE_DIR LLVM_LIBRARIES LLVM_LIBDIRS)
