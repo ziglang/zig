0c88b1ce73a926e60464c96a5a3f8ac8d9f84a8c
Andrew Kelley <superjoe30@gmail.com>
2017-02-05 16:09:33 -0500

    use printf in test runner code

--- std/test_runner.zig
+++ std/test_runner.zig
@@ -9,20 +9,10 @@ extern var zig_test_fn_list: []TestFn;
 
 pub fn runTests() -> %void {
     for (zig_test_fn_list) |testFn, i| {
-        // TODO: print var args
-        %%io.stderr.write("Test ");
-        %%io.stderr.printInt(@typeOf(i), i + 1);
-        %%io.stderr.write("/");
-        %%io.stderr.printInt(@typeOf(zig_test_fn_list.len), zig_test_fn_list.len);
-        %%io.stderr.write(" ");
-        %%io.stderr.write(testFn.name);
-        %%io.stderr.write("...");
-        %%io.stderr.flush();
+        %%io.stderr.printf("Test {}/{} {}...", i + 1, zig_test_fn_list.len, testFn.name);
 
         testFn.func();
 
-
-        %%io.stderr.write("OK\n");
-        %%io.stderr.flush();
+        %%io.stderr.printf("OK\n");
     }
 }
