403eb92a626e44ab456099fe48fa221639e2e2f5
Andrew Kelley <superjoe30@gmail.com>
2017-04-11 06:44:45 -0400

    try harder to find lld

--- cmake/Findlld.cmake
+++ cmake/Findlld.cmake
@@ -11,28 +11,32 @@ find_path(LLD_INCLUDE_DIRS NAMES lld/Driver/Driver.h
         /usr/lib/llvm-4.0/include
         /mingw64/include)
 
+find_library(LLD_LIBRARY NAMES lld-4.0 lld PATHS /usr/lib/llvm-4.0/lib)
+if(LLD_LIBRARY)
+    set(LLD_LIBRARIES ${LLD_LIBRARY})
+else()
     macro(FIND_AND_ADD_LLD_LIB _libname_)
-    string(TOUPPER ${_libname_} _prettylibname_)
-    find_library(LLD_${_prettylibname_}_LIB NAMES ${_libname_}
-        PATHS
-            /usr/lib/llvm-4.0/lib
-            /mingw64/lib)
-        if(LLD_${_prettylibname_}_LIB)
-            set(LLD_LIBRARIES ${LLD_LIBRARIES} ${LLD_${_prettylibname_}_LIB})
-    endif()
-endmacro(FIND_AND_ADD_LLD_LIB)
+        string(TOUPPER ${_libname_} _prettylibname_)
+        find_library(LLD_${_prettylibname_}_LIB NAMES ${_libname_}
+            PATHS
+                /usr/lib/llvm-4.0/lib
+                /mingw64/lib)
+            if(LLD_${_prettylibname_}_LIB)
+                set(LLD_LIBRARIES ${LLD_LIBRARIES} ${LLD_${_prettylibname_}_LIB})
+        endif()
+    endmacro(FIND_AND_ADD_LLD_LIB)
 
-FIND_AND_ADD_LLD_LIB(lldDriver)
-FIND_AND_ADD_LLD_LIB(lldELF)
-FIND_AND_ADD_LLD_LIB(lldCOFF)
-FIND_AND_ADD_LLD_LIB(lldMachO)
-FIND_AND_ADD_LLD_LIB(lldReaderWriter)
-FIND_AND_ADD_LLD_LIB(lldCore)
-FIND_AND_ADD_LLD_LIB(lldYAML)
-FIND_AND_ADD_LLD_LIB(lldConfig)
+    FIND_AND_ADD_LLD_LIB(lldDriver)
+    FIND_AND_ADD_LLD_LIB(lldELF)
+    FIND_AND_ADD_LLD_LIB(lldCOFF)
+    FIND_AND_ADD_LLD_LIB(lldMachO)
+    FIND_AND_ADD_LLD_LIB(lldReaderWriter)
+    FIND_AND_ADD_LLD_LIB(lldCore)
+    FIND_AND_ADD_LLD_LIB(lldYAML)
+    FIND_AND_ADD_LLD_LIB(lldConfig)
+endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LLD DEFAULT_MSG LLD_LIBRARIES LLD_INCLUDE_DIRS)
 
 mark_as_advanced(LLD_INCLUDE_DIRS LLD_LIBRARIES)
-
