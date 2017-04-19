35d60eb4e65c38a3c81e3a249de7447017616d02
Andrew Kelley <superjoe30@gmail.com>
2017-04-19 04:28:59 -0400

    remove unused test code

--- build.zig
+++ build.zig
@@ -5,11 +5,6 @@ pub fn build(b: &Builder) {
     const test_filter = b.option([]const u8, "test-filter", "Skip tests that do not match filter");
     const test_step = b.step("test", "Run all the tests");
 
-    const run_tests_exe = b.addExecutable("run_tests", "test/run_tests.zig");
-
-    const run_tests_cmd = b.addCommand(b.out_dir, b.env_map, "./run_tests", [][]const u8{});
-    run_tests_cmd.step.dependOn(&run_tests_exe.step);
-
     const self_hosted_tests = b.step("test-self-hosted", "Run the self-hosted tests");
     test_step.dependOn(self_hosted_tests);
     for ([]bool{false, true}) |release| {
@@ -39,8 +34,6 @@ pub fn build(b: &Builder) {
             std_lib_tests.dependOn(&these_tests.step);
         }
     }
-
-    //test_step.dependOn(&run_tests_cmd.step);
 
     test_step.dependOn(tests.addCompareOutputTests(b, test_filter));
     test_step.dependOn(tests.addBuildExampleTests(b, test_filter));
--- test/run_tests.zig
+++
@@ -1,5 +0,0 @@
-const io = @import("std").io;
-
-pub fn main() -> %void {
-    %%io.stderr.printf("TODO run tests\n");
-}
