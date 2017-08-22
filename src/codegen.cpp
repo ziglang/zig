5dddb45ec71ae93750fddda932ef8b7d2ffa293d
Andrew Kelley <superjoe30@gmail.com>
2017-08-22 11:54:58 -0400

    fix compiler test errors
    closes #428
    
    Thanks Marc Tiehuis for diagnosing this bug

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -3951,6 +3951,7 @@ static void do_code_gen(CodeGen *g) {
             assert(const_val->special != ConstValSpecialRuntime);
             TypeTableEntry *var_type = g->builtin_types.entry_f128;
             ConstExprValue coerced_value;
+            coerced_value.special = ConstValSpecialStatic;
             coerced_value.type = var_type;
             coerced_value.data.x_f128 = bigfloat_to_f128(&const_val->data.x_bigfloat);
             LLVMValueRef init_val = gen_const_val(g, &coerced_value);
