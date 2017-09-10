9dfbdeace6fd6fbeaea38bd48757f0ca0aee09a9
Andrew Kelley <superjoe30@gmail.com>
2017-09-09 22:54:50 -0400

    add import test

--- test/cases/import.zig
+++ test/cases/import.zig
@@ -4,3 +4,7 @@ const a_namespace = @import("import/a_namespace.zig");
 test "call fn via namespace lookup" {
     assert(a_namespace.foo() == 1234);
 }
+
+test "importing the same thing gives the same import" {
+    assert(@import("std") == @import("std"));
+}
