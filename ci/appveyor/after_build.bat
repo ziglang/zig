38a947c68307cd4bb44efff58cac1897ee1b4197
Andrew Kelley <superjoe30@gmail.com>
2017-10-16 13:01:34 -0400

    appveyor: update version to match zig version

--- ci/appveyor/after_build.bat
+++ ci/appveyor/after_build.bat
@@ -3,6 +3,7 @@ cd %APPVEYOR_BUILD_FOLDER%
 
 build-msvc-release\bin\zig.exe version >version.txt
 set /p ZIGVERSION=<version.txt
+appveyor UpdateBuild -Version "%ZIGVERSION%"
 SET "RELEASEDIR=zig-%ZIGVERSION%"
 mkdir "%RELEASEDIR%"
 move build-msvc-release\bin\zig.exe "%RELEASEDIR%"
