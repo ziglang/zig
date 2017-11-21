afbbdb2c67127985cadae7244348665ece8b2f25
Josh Wolfe <thejoshwolfe@gmail.com>
2017-11-20 21:36:18 -0700

    move base64 functions into structs

--- example/mix_o_files/base64.zig
+++ example/mix_o_files/base64.zig
@@ -3,7 +3,8 @@ const base64 = @import("std").base64;
 export fn decode_base_64(dest_ptr: &u8, dest_len: usize, source_ptr: &const u8, source_len: usize) -> usize {
     const src = source_ptr[0..source_len];
     const dest = dest_ptr[0..dest_len];
-    const decoded_size = base64.calcDecodedSizeExactUnsafe(src, base64.standard_pad_char);
-    base64.decodeExactUnsafe(dest[0..decoded_size], src, base64.standard_alphabet_unsafe);
+    const base64_decoder = base64.standard_decoder_unsafe;
+    const decoded_size = base64_decoder.calcSize(src);
+    base64_decoder.decode(dest[0..decoded_size], src);
     return decoded_size;
 }
