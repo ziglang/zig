617655b0a82afb306e56d4c35da6715bed7bcb3b
Marc Tiehuis <marctiehuis@gmail.com>
2018-01-17 19:25:00 +1300

    Add throughput test program
    Blake performance numbers for reference:
    
    ```
    Cpu: Intel(R) Core(TM) i5-6500 CPU @ 3.20GHz
    ```
    
    -- Blake2s
    
    ```
    Zig --release-fast
        485 Mb/s
    Zig --release-safe
        377 Mb/s
    Zig
        11 Mb/s
    ```
    
    -- Blake2b
    ```
    Zig --release-fast
        616 Mb/s
    Zig --release-safe
        573 Mb/s
    Zig
        18 Mb/s
    ```

--- std/crypto/sha1.zig
+++ std/crypto/sha1.zig
@@ -16,6 +16,8 @@ fn Rp(a: usize, b: usize, c: usize, d: usize, e: usize, i: u32) -> RoundParam {
 
 pub const Sha1 = struct {
     const Self = this;
+    const block_size = 64;
+    const digest_size = 20;
 
     s: [5]u32,
     // Streaming Cache
