2c697e50db36e4f9c0af8b1a9d357deeb8f80026
Andrew Kelley <superjoe30@gmail.com>
2018-02-09 01:15:17 -0500

    appveyor: don't try to build for mingw
    pacman is giving me:
    :: msys2-runtime and catgets are in conflict.
    Remove catgets? [y/N] error: unresolvable package conflicts detected
    error: failed to prepare transaction (conflicting dependencies)

--- ci/appveyor/build_script.bat
+++ ci/appveyor/build_script.bat
@@ -23,17 +23,6 @@ cd %ZIGBUILDDIR%
 cmake.exe .. -Thost=x64 -G"Visual Studio 14 2015 Win64" "-DCMAKE_INSTALL_PREFIX=%ZIGBUILDDIR%" "-DCMAKE_PREFIX_PATH=%ZIGPREFIXPATH%" -DCMAKE_BUILD_TYPE=Release "-DZIG_LIBC_INCLUDE_DIR=C:\Program Files (x86)\Windows Kits\10\Include\10.0.10240.0\ucrt" "-DZIG_LIBC_LIB_DIR=C:\Program Files (x86)\Windows Kits\10\bin\x64\ucrt" "-DZIG_LIBC_STATIC_LIB_DIR=C:\Program Files (x86)\Windows Kits\10\Lib\10.0.10240.0\ucrt\x64" || exit /b
 msbuild /p:Configuration=Release INSTALL.vcxproj || exit /b
 
-bin\zig.exe build --build-file ..\build.zig test -Dverbose-link || exit /b
+bin\zig.exe build --build-file ..\build.zig test || exit /b
 
-@echo "MSVC build succeeded, proceeding with MinGW build"
-cd %APPVEYOR_BUILD_FOLDER%
-SET "PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;%PATH%"
-SET "MSYSTEM=MINGW64"
-
-bash -lc "yes | pacman -Syu --needed --noconfirm"
-bash -lc "yes | pacman -Su --needed --noconfirm"
-
-bash -lc "yes | pacman -S --needed --noconfirm make mingw64/mingw-w64-x86_64-make mingw64/mingw-w64-x86_64-cmake mingw64/mingw-w64-x86_64-clang mingw64/mingw-w64-x86_64-llvm mingw64/mingw-w64-x86_64-lld mingw64/mingw-w64-x86_64-gcc"
-
-bash -lc "cd ${APPVEYOR_BUILD_FOLDER} && mkdir build && cd build && cmake .. -G""MSYS Makefiles"" -DCMAKE_INSTALL_PREFIX=$(pwd) -DZIG_LIBC_LIB_DIR=$(dirname $(cc -print-file-name=crt1.o)) -DZIG_LIBC_INCLUDE_DIR=$(echo -n | cc -E -x c - -v 2>&1 | grep -B1 ""End of search list."" | head -n1 | cut -c 2- | sed ""s/ .*//"") -DZIG_LIBC_STATIC_LIB_DIR=$(dirname $(cc -print-file-name=crtbegin.o)) && make && make install"
-
+@echo "MSVC build succeeded"
