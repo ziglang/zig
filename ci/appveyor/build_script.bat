73a306e2fa11c146b5f79a41953e0d19a82f17a5
Andrew Kelley <superjoe30@gmail.com>
2018-03-03 17:44:41 -0500

    fix conflict artifact accidentally in appveyor script

--- ci/appveyor/build_script.bat
+++ ci/appveyor/build_script.bat
@@ -17,7 +17,6 @@ SET "ZIGPREFIXPATH=%APPVEYOR_BUILD_FOLDER%\llvm+clang-6.0.0-win64-msvc-release"
 
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
->>>>>>> origin/master
 
 mkdir %ZIGBUILDDIR%
 cd %ZIGBUILDDIR%
