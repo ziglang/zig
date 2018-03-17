c2c34c09b9afd8a5370dce771d5e98af5b3303a1
Ben Noordhuis <info@bnoordhuis.nl>
2018-03-17 18:39:58 +0100

    add LLVM_LIBDIRS to link directories
    This seems to be the only way to get the zig build to link against llvm
    libraries in a non-standard location.

--- cmake/Findllvm.cmake
+++ cmake/Findllvm.cmake
@@ -66,7 +66,7 @@ if(NOT LLVM_LIBRARIES)
 endif()
 
 link_directories("${CMAKE_PREFIX_PATH}/lib")
-
+link_directories("${LLVM_LIBDIRS}")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LLVM DEFAULT_MSG LLVM_LIBRARIES LLVM_INCLUDE_DIRS)
