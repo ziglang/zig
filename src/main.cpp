ziglang/zig

commit 1999f0daad505f414f97845ecde0a56b3c2fedfd

fix undefined behavior triggered by fn inline test
LLVM destroys the string that we use to test if LLVM deleted the
inlined function.

Also fixed forgetting to initialize a buffer in std lib path detection.

src/main.cpp
@@ -179,6 +179,7 @@ static int find_zig_lib_dir(Buf *out_path) {
     Buf self_exe_path = BUF_INIT;
     if (!(err = os_self_exe_path(&self_exe_path))) {
         Buf *cur_path = &self_exe_path;
+        buf_resize(cur_path, 0);
 
         for (;;) {
             Buf *test_dir = buf_alloc();

src/codegen.cpp
@@ -467,7 +467,7 @@ static LLVMValueRef fn_llvm_value(CodeGen *g, FnTableEntry *fn_table_entry) {
                     fn_table_entry->llvm_value, buf_ptr(&fn_export->name));
         }
     }
-    fn_table_entry->llvm_name = LLVMGetValueName(fn_table_entry->llvm_value);
+    fn_table_entry->llvm_name = strdup(LLVMGetValueName(fn_table_entry->llvm_value));
 
     switch (fn_table_entry->fn_inline) {
         case FnInlineAlways:

commit 0509414dfecc1e82ed86b21a1c24aa978c2e810f

fix regression with zig install dir
introduced in 1999f0daad505

src/main.cpp
@@ -176,9 +176,9 @@ static int find_zig_lib_dir(Buf *out_path) {
     int err;
 
     Buf self_exe_path = BUF_INIT;
+    buf_resize(&self_exe_path, 0);
     if (!(err = os_self_exe_path(&self_exe_path))) {
         Buf *cur_path = &self_exe_path;
-        buf_resize(cur_path, 0);
 
         for (;;) {
             Buf *test_dir = buf_alloc();