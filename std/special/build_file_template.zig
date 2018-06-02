7b386ea24285db2fbc40233a232cb294dddf9879
Andrew Kelley <superjoe30@gmail.com>
2018-06-01 21:51:54 -0400

    fix build file template
    See #1035

--- std/special/build_file_template.zig
+++ std/special/build_file_template.zig
@@ -5,6 +5,6 @@ pub fn build(b: *Builder) void {
     const exe = b.addExecutable("YOUR_NAME_HERE", "src/main.zig");
     exe.setBuildMode(mode);
 
-    b.default_step.dependOn(*exe.step);
+    b.default_step.dependOn(&exe.step);
     b.installArtifact(exe);
 }
