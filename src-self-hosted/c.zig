10cc49db1ca1f9b3ac63277c0742e05f6412f3c6
Andrew Kelley <superjoe30@gmail.com>
2018-07-09 21:42:05 -0400

    define c macros before importing llvm h files
    Seems to matter on Ubuntu 16.04.
    
    closes #1196

--- src-self-hosted/c.zig
+++ src-self-hosted/c.zig
@@ -1,4 +1,6 @@
 pub use @cImport({
+    @cDefine("__STDC_CONSTANT_MACROS", "");
+    @cDefine("__STDC_LIMIT_MACROS", "");
     @cInclude("inttypes.h");
     @cInclude("config.h");
     @cInclude("zig_llvm.h");
