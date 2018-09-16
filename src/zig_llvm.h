1702a7dcaa8e8fc84bef6e26085af5f3d43ef829
kristopher tate <kris.tate+github@gmail.com>
2018-09-16 19:43:41 +0900

    issue #1530: guard int bitwidth from ZigLLVM_MIN_BITS to ZigLLVM_MAX_BITS bits;

--- src/zig_llvm.h
+++ src/zig_llvm.h
@@ -403,4 +403,9 @@ ZIG_EXTERN_C void ZigLLVMGetNativeTarget(enum ZigLLVM_ArchType *arch_type, enum 
         enum ZigLLVM_VendorType *vendor_type, enum ZigLLVM_OSType *os_type, enum ZigLLVM_EnvironmentType *environ_type,
         enum ZigLLVM_ObjectFormatType *oformat);
 
+enum ZigLLVM_BitWidth {
+    ZigLLVM_MIN_INT_BITS = 1,
+    ZigLLVM_MAX_INT_BITS = (1<<24)-1, //llvm::IntegerType::MAX_INT_BITS
+};
+
 #endif
