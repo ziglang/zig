68c1d059178a36add751e97e11cee004cfdf612e
emekoi <emekankurumeh@outlook.com>
2018-09-17 23:13:17 -0500

    compiling on mingw is now supported (#1542)
    * compiles on mingw-w64
    * fixed error in os_file_overwrite on windows
    * fixed windows hello_world example

--- example/hello_world/hello_windows.zig
+++ example/hello_world/hello_windows.zig
@@ -1,5 +1,7 @@
 use @import("std").os.windows;
 
+extern "user32" stdcallcc fn MessageBoxA(hWnd: ?HANDLE, lpText: ?LPCTSTR, lpCaption: ?LPCTSTR, uType: UINT) c_int;
+
 export fn WinMain(hInstance: HINSTANCE, hPrevInstance: HINSTANCE, lpCmdLine: PWSTR, nCmdShow: INT) INT {
     _ = MessageBoxA(null, c"hello", c"title", 0);
     return 0;
