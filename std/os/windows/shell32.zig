31469daca397b7cdb8c348932a7a3c9e27354fa5
emekoi <emekankurumeh@outlook.com>
2018-10-04 00:23:02 -0500

    removed unneeded dll extension

--- std/os/windows/shell32.zig
+++ std/os/windows/shell32.zig
@@ -1,4 +1,4 @@
 use @import("index.zig");
 
-pub extern "shell32.dll" stdcallcc fn SHGetKnownFolderPath(rfid: *const KNOWNFOLDERID, dwFlags: DWORD, hToken: ?HANDLE, ppszPath: *[*]WCHAR) HRESULT;
+pub extern "shell32" stdcallcc fn SHGetKnownFolderPath(rfid: *const KNOWNFOLDERID, dwFlags: DWORD, hToken: ?HANDLE, ppszPath: *[*]WCHAR) HRESULT;
 
