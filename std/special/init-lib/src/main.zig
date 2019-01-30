cfeceb6fd1cee1edf75dc881861a3b8b90fae9de
Kelly Wilson <wilsonk@cpsc.ucalgary.ca>
2019-01-30 16:27:06 -0700

    Fix for #1878 - zig init-lib on MacOS fails with __mh_execute_header not found

--- std/special/init-lib/src/main.zig
+++ std/special/init-lib/src/main.zig
@@ -1,6 +1,14 @@
 const std = @import("std");
 const assertOrPanic = std.debug.assertOrPanic;
 
+comptime {
+    const builtin = @import("builtin");
+    if (builtin.os == builtin.Os.macosx) {
+        @export("__mh_execute_header", _mh_execute_header, builtin.GlobalLinkage.Weak);
+    }
+}
+var _mh_execute_header = extern struct {x: usize}{.x = 0};
+
 export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
