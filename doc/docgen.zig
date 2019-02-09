ziglang/zig

commit c2db077574be841da586fa62d67619c901dd535d

std.debug.assert: remove special case for test builds
Previously, std.debug.assert would `@panic` in test builds,
if the assertion failed. Now, it's always `unreachable`.

This makes release mode test builds more accurately test
the actual code that will be run.

However this requires tests to call `std.testing.expect`
rather than `std.debug.assert` to make sure output is correct.

Here is the explanation of when to use either one, copied from
the assert doc comments:

Inside a test block, it is best to use the `std.testing` module
rather than assert, because assert may not detect a test failure
in ReleaseFast and ReleaseSafe mode. Outside of a test block, assert
is the correct function to use.

closes #1304

doc/docgen.zig
@@ -4,7 +4,7 @@ const io = std.io;
 const os = std.os;
 const warn = std.debug.warn;
 const mem = std.mem;
-const assert = std.debug.assert;
+const testing = std.testing;
 
 const max_doc_file_size = 10 * 1024 * 1024;
 
@@ -620,7 +620,7 @@ const TermState = enum {
 test "term color" {
     const input_bytes = "A\x1b[32;1mgreen\x1b[0mB";
     const result = try termColor(std.debug.global_allocator, input_bytes);
-    assert(mem.eql(u8, result, "A<span class=\"t32\">green</span>B"));
+    testing.expectEqualSlices(u8, "A<span class=\"t32\">green</span>B", result));
 }
 
 fn termColor(allocator: *mem.Allocator, input: []const u8) ![]u8 {

commit d6f2af378abe7467657f6fbf8133b995e5f55142

fix docs
broken by c2db077574be8

doc/docgen.zig
@@ -620,7 +620,7 @@ const TermState = enum {
 test "term color" {
     const input_bytes = "A\x1b[32;1mgreen\x1b[0mB";
     const result = try termColor(std.debug.global_allocator, input_bytes);
-    testing.expectEqualSlices(u8, "A<span class=\"t32\">green</span>B", result));
+    testing.expectEqualSlices(u8, "A<span class=\"t32\">green</span>B", result);
 }
 
 fn termColor(allocator: *mem.Allocator, input: []const u8) ![]u8 {

doc/langref.html.in
@@ -4627,7 +4627,7 @@ test "fibonacci" {
       <p>
       What if we fix the base case, but put the wrong value in the {#syntax#}assert{#endsyntax#} line?
       </p>
-      {#code_begin|test_err|encountered @panic at compile-time#}
+      {#code_begin|test_err|unable to evaluate constant expression#}
 const assert = @import("std").debug.assert;
 
 fn fibonacci(index: i32) i32 {