c221b29c9d3c269326b3d901ce5a8b5f50263feb
Jimmi Holst Christensen <jhc@liab.dk>
2019-02-13 11:31:13 +0100

    We already support vector on floats, so let's test it

--- test/stage1/behavior/vector.zig
+++ test/stage1/behavior/vector.zig
@@ -15,3 +15,17 @@ test "vector wrap operators" {
     S.doTheTest();
     comptime S.doTheTest();
 }
+
+test "vector float operators" {
+    const S = struct {
+        fn doTheTest() void {
+            const v: @Vector(4, f32) = [4]f32{ 10, 20, 30, 40 };
+            const x: @Vector(4, f32) = [4]f32{ 1, 2, 3, 4 };
+            expect(mem.eql(f32, ([4]f32)(v + x), [4]f32{ 11, 22, 33, 44 }));
+            expect(mem.eql(f32, ([4]f32)(v - x), [4]f32{ 9, 18, 27, 36 }));
+            expect(mem.eql(f32, ([4]f32)(v * x), [4]f32{ 10, 40, 90, 160 }));
+        }
+    };
+    S.doTheTest();
+    comptime S.doTheTest();
+}
