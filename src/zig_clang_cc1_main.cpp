bcca76b3c4c6dcb24435db7a3a10d2319ef4c3aa
Andrew Kelley <andrew@ziglang.org>
2019-02-24 13:37:41 -0500

    zig cc: remove "polly" which was an undefined symbol on macos

--- src/zig_clang_cc1_main.cpp
+++ src/zig_clang_cc1_main.cpp
@@ -67,12 +67,6 @@ static void LLVMErrorHandler(void *UserData, const std::string &Message,
   exit(GenCrashDiag ? 70 : 1);
 }
 
-#ifdef LINK_POLLY_INTO_TOOLS
-namespace polly {
-void initializePollyPasses(llvm::PassRegistry &Registry);
-}
-#endif
-
 #ifdef CLANG_HAVE_RLIMITS
 #if defined(__linux__) && defined(__PIE__)
 static size_t getCurrentStackAllocation() {
@@ -180,11 +174,6 @@ int cc1_main(ArrayRef<const char *> Argv, const char *Argv0, void *MainAddr) {
   llvm::InitializeAllTargetMCs();
   llvm::InitializeAllAsmPrinters();
   llvm::InitializeAllAsmParsers();
-
-#ifdef LINK_POLLY_INTO_TOOLS
-  llvm::PassRegistry &Registry = *llvm::PassRegistry::getPassRegistry();
-  polly::initializePollyPasses(Registry);
-#endif
 
   // Buffer diagnostics from argument parsing so that we can output them using a
   // well formed diagnostic object.
