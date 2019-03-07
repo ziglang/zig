3d2c48e4c9c0f766f8e18e6932cf5d8e22096582
Andrew Kelley <andrew@ziglang.org>
2019-03-07 16:11:00 -0500

    disable some tests until coroutine rewrite is finished

--- std/event/loop.zig
+++ std/event/loop.zig
@@ -864,7 +864,7 @@ pub const Loop = struct {
 
 test "std.event.Loop - basic" {
     // https://github.com/ziglang/zig/issues/1908
-    if (builtin.single_threaded) return error.SkipZigTest;
+    if (builtin.single_threaded or builtin.os != builtin.Os.linux) return error.SkipZigTest;
 
     var da = std.heap.DirectAllocator.init();
     defer da.deinit();
@@ -880,7 +880,7 @@ test "std.event.Loop - basic" {
 
 test "std.event.Loop - call" {
     // https://github.com/ziglang/zig/issues/1908
-    if (builtin.single_threaded) return error.SkipZigTest;
+    if (builtin.single_threaded or builtin.os != builtin.Os.linux) return error.SkipZigTest;
 
     var da = std.heap.DirectAllocator.init();
     defer da.deinit();
