6a9c32f7597d37592d79afa27441e7caf9a30529
Andrew Kelley <andrew@ziglang.org>
2019-03-23 19:01:33 -0400

    add regression tests for a bug fixed by an older commit
    closes #1914

---
+++ test/stage1/behavior/bugs/1914.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+
+const A = struct {
+    b_list_pointer: *const []B,
+};
+const B = struct {
+    a_pointer: *const A,
+};
+
+const b_list: []B = []B{};
+const a = A{ .b_list_pointer = &b_list };
+
+test "segfault bug" {
+    const assert = std.debug.assert;
+    const obj = B{ .a_pointer = &a };
+    assert(obj.a_pointer == &a); // this makes zig crash
+}
+
+const A2 = struct {
+    pointer: *B,
+};
+
+pub const B2 = struct {
+    pointer_array: []*A2,
+};
+
+var b_value = B2{ .pointer_array = []*A2{} };
+
+test "basic stuff" {
+    std.debug.assert(&b_value == &b_value);
+}
