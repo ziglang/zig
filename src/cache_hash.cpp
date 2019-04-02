ziglang/zig

commit 27e31f04758d95cb7cf51a74e2a3903553ce3bc5

fix fd leak in stage1 cacheing code

src/cache_hash.cpp
@@ -437,6 +437,7 @@ Error cache_hit(CacheHash *ch, Buf *out_digest) {
                 return ErrorCacheUnavailable;
             }
         }
+        os_file_close(ch->manifest_file);
         return return_code;
     }
     // Cache Hit

commit 2f96c55095514de35a25ec2cf84a79e6c5e3a646

fix cache hash regression
fixes "warning: unexpected seek failure"

fix regression introduced by 27e31f04758d95cb7cf51a74e2a3903553ce3bc5

the fd should be closed only if returning with an error

src/cache_hash.cpp
@@ -437,7 +437,9 @@ Error cache_hit(CacheHash *ch, Buf *out_digest) {
                 return ErrorCacheUnavailable;
             }
         }
-        os_file_close(ch->manifest_file);
+        if (return_code != ErrorNone) {
+            os_file_close(ch->manifest_file);
+        }
         return return_code;
     }
     // Cache Hit