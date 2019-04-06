d44abc4459a099ec15fd46ceae9593f9e956f9df
Shawn Landden <shawn@git.icu>
2019-04-05 21:05:41 -0500

    use optimized charToDigit in bigint code
    unless I am missing something it appears that the self-hosted compiler
    was not compliant as it did not take upper case hex digits

--- std/math/big/int.zig
+++ std/math/big/int.zig
@@ -4,6 +4,7 @@ const debug = std.debug;
 const testing = std.testing;
 const math = std.math;
 const mem = std.mem;
+const fmt = std.fmt;
 const Allocator = mem.Allocator;
 const ArrayList = std.ArrayList;
 const maxInt = std.math.maxInt;
@@ -281,16 +282,6 @@ pub const Int = struct {
         }
     }
 
-    fn charToDigit(ch: u8, base: u8) !u8 {
-        const d = switch (ch) {
-            '0'...'9' => ch - '0',
-            'a'...'f' => (ch - 'a') + 0xa,
-            else => return error.InvalidCharForDigit,
-        };
-
-        return if (d < base) d else return error.DigitTooLargeForBase;
-    }
-
     fn digitToChar(d: u8, base: u8) !u8 {
         if (d >= base) {
             return error.DigitTooLargeForBase;
@@ -326,7 +317,7 @@ pub const Int = struct {
 
         try self.set(0);
         for (value[i..]) |ch| {
-            const d = try charToDigit(ch, base);
+            const d = try fmt.charToDigit(ch, base);
             d_fba.end_index = 0;
             const d_ap = try Int.initSet(d_al, d);
 
@@ -423,7 +414,7 @@ pub const Int = struct {
     /// TODO make this non-allocating
     pub fn format(
         self: Int,
-        comptime fmt: []const u8,
+        comptime fmtstr: []const u8,
         context: var,
         comptime FmtError: type,
         output: fn (@typeOf(context), []const u8) FmtError!void,
