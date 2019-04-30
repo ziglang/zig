9dfd24a3345c83c9cd32d45bef3076bb94bc0eaa
Andrew Kelley <andrew@ziglang.org>
2019-04-30 14:17:16 -0400

    add test case to cover solved bug
    closes #2114

---
+++ test/stage1/behavior/bugs/2114.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const expect = std.testing.expect;
+const math = std.math;
+
+fn ctz(x: var) usize {
+    return @ctz(x);
+}
+
+test "fixed" {
+    testClz();
+    comptime testClz();
+}
+
+fn testClz() void {
+    expect(ctz(u128(0x40000000000000000000000000000000)) == 126);
+    expect(math.rotl(u128, u128(0x40000000000000000000000000000000), u8(1)) == u128(0x80000000000000000000000000000000));
+    expect(ctz(u128(0x80000000000000000000000000000000)) == 127);
+    expect(ctz(math.rotl(u128, u128(0x40000000000000000000000000000000), u8(1))) == 127);
+}
--- test/stage1/behavior/bugs/2346.zig
+++ test/stage1/behavior/bugs/2346.zig
@@ -1,4 +1,4 @@
-test "" {
+test "fixed" {
     const a: *void = undefined;
     const b: *[1]void = a;
     const c: *[0]u8 = undefined;
