5de07ab721380c39130a0bd3277a272673e3d644
Andrew Kelley <andrew@ziglang.org>
2019-05-27 15:48:31 -0400

    revert hello world examples. macos tests passing

--- example/hello_world/hello_libc.zig
+++ example/hello_world/hello_libc.zig
@@ -2,9 +2,12 @@ const c = @cImport({
     // See https://github.com/ziglang/zig/issues/515
     @cDefine("_NO_CRT_STDIO_INLINE", "1");
     @cInclude("stdio.h");
+    @cInclude("string.h");
 });
 
-export fn main(argc: c_int, argv: [*]?[*]u8) c_int {
-    _ = c.fprintf(c.stderr, c"Hello, world!\n");
+const msg = c"Hello, world!\n";
+
+export fn main(argc: c_int, argv: **u8) c_int {
+    if (c.printf(msg) != @intCast(c_int, c.strlen(msg))) return -1;
     return 0;
 }
