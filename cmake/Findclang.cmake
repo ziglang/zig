681ec91fbc6218b77ab832d03a6f07e71243b108
Fangrui Song <i@maskray.me>
2019-08-31 22:40:39 -0700

    cmake: rework LLVM/Clang cmake
    1. Use {LLVM,CLANG}_INCLUDE_DIRS from {LLVM,Clang}Config.cmake
      This makes it possible to use an LLVM/Clang source tree.
    
    2. Delete ZIG_STATIC_LLVM
    
    Building both libLLVM*.a and libLLVM*.so is unsupported in LLVM. The
    LLVM libraries can be configured in 3 ways:
    
    * -DLLVM_LINK_LLVM_DYLIB=ON => link against libLLVM.so
    * -DLLVM_LINK_LLVM_DYLIB=OFF -DBUILD_SHARED_LIBS=OFF => link against libLLVM*.a
    * -DLLVM_LINK_LLVM_DYLIB=OFF -DBUILD_SHARED_LIBS=ON => link against libLLVM*.so
    
    If libLLVM*.a are unavailable, ZIG_STATIC_LLVM will not work.
    If libLLVM*.a are available, linking against libLLVM*.a is automatic.
    So ZIG_STATIC_LLVM is not useful and can only be confusing.
    
    3. If CLANG_LINK_CLANG_DYLIB (available since clang 9) is ON,
    libclang-cpp.so instead of libclang*.so will be linked. It currently
    does not work because zig_cpp/libembedded_lld_lib.a has global
    constructors that conflict with libclang-cpp.so

--- cmake/Findclang.cmake
+++ cmake/Findclang.cmake
@@ -5,11 +5,12 @@
 # CLANG_FOUND
 # CLANG_INCLUDE_DIRS
 # CLANG_LIBRARIES
-# CLANG_LIBDIRS
 
-if(MSVC)
-  find_package(CLANG REQUIRED CONFIG)
+find_package(Clang REQUIRED)
 
+if(CLANG_LINK_CLANG_DYLIB)
+  set(CLANG_LIBRARIES clang-cpp)
+else()
   set(CLANG_LIBRARIES
       clangFrontendTool
       clangCodeGen
@@ -33,59 +34,5 @@ if(MSVC)
       clangRewrite
       clangCrossTU
       clangIndex
-  )
-
-else()
-  find_path(CLANG_INCLUDE_DIRS NAMES clang/Frontend/ASTUnit.h
-      PATHS
-          /usr/lib/llvm/8/include
-          /usr/lib/llvm-8/include
-          /usr/lib/llvm-8.0/include
-          /usr/local/llvm80/include
-          /mingw64/include)
-
-  macro(FIND_AND_ADD_CLANG_LIB _libname_)
-      string(TOUPPER ${_libname_} _prettylibname_)
-      find_library(CLANG_${_prettylibname_}_LIB NAMES ${_libname_}
-          PATHS
-              ${CLANG_LIBDIRS}
-              /usr/lib/llvm/8/lib
-              /usr/lib/llvm-8/lib
-              /usr/lib/llvm-8.0/lib
-              /usr/local/llvm80/lib
-              /mingw64/lib
-              /c/msys64/mingw64/lib
-              c:\\msys64\\mingw64\\lib)
-      if(CLANG_${_prettylibname_}_LIB)
-          set(CLANG_LIBRARIES ${CLANG_LIBRARIES} ${CLANG_${_prettylibname_}_LIB})
-      endif()
-  endmacro(FIND_AND_ADD_CLANG_LIB)
-
-  FIND_AND_ADD_CLANG_LIB(clangFrontendTool)
-  FIND_AND_ADD_CLANG_LIB(clangCodeGen)
-  FIND_AND_ADD_CLANG_LIB(clangFrontend)
-  FIND_AND_ADD_CLANG_LIB(clangDriver)
-  FIND_AND_ADD_CLANG_LIB(clangSerialization)
-  FIND_AND_ADD_CLANG_LIB(clangSema)
-  FIND_AND_ADD_CLANG_LIB(clangStaticAnalyzerFrontend)
-  FIND_AND_ADD_CLANG_LIB(clangStaticAnalyzerCheckers)
-  FIND_AND_ADD_CLANG_LIB(clangStaticAnalyzerCore)
-  FIND_AND_ADD_CLANG_LIB(clangAnalysis)
-  FIND_AND_ADD_CLANG_LIB(clangASTMatchers)
-  FIND_AND_ADD_CLANG_LIB(clangAST)
-  FIND_AND_ADD_CLANG_LIB(clangParse)
-  FIND_AND_ADD_CLANG_LIB(clangSema)
-  FIND_AND_ADD_CLANG_LIB(clangBasic)
-  FIND_AND_ADD_CLANG_LIB(clangEdit)
-  FIND_AND_ADD_CLANG_LIB(clangLex)
-  FIND_AND_ADD_CLANG_LIB(clangARCMigrate)
-  FIND_AND_ADD_CLANG_LIB(clangRewriteFrontend)
-  FIND_AND_ADD_CLANG_LIB(clangRewrite)
-  FIND_AND_ADD_CLANG_LIB(clangCrossTU)
-  FIND_AND_ADD_CLANG_LIB(clangIndex)
+      )
 endif()
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(CLANG DEFAULT_MSG CLANG_LIBRARIES CLANG_INCLUDE_DIRS)
-
-mark_as_advanced(CLANG_INCLUDE_DIRS CLANG_LIBRARIES CLANG_LIBDIRS)
