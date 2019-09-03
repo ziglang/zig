9d6f236728cf433b44048e21f81efdd167fe0098
Sahnvour <sahnvour@pm.me>
2019-09-03 23:56:04 +0200

    add fastpath for std.mem.eql and simplify std.hash_map.eqlString
    this should also be friendlier to the optimizer

--- std/mem.zig
+++ std/mem.zig
@@ -339,6 +339,7 @@ test "mem.lessThan" {
 /// Compares two slices and returns whether they are equal.
 pub fn eql(comptime T: type, a: []const T, b: []const T) bool {
     if (a.len != b.len) return false;
+    if (a.ptr == b.ptr) return true;
     for (a) |item, index| {
         if (b[index] != item) return false;
     }
