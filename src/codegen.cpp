8223aca09bc93279a88d1439d6dfc437144ec578
Andrew Kelley <andrew@ziglang.org>
2019-09-13 14:46:22 -0400

    no-stack-arg-probe only for UEFI

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -517,7 +517,7 @@ static LLVMValueRef make_fn_llvm_value(CodeGen *g, ZigFn *fn) {
         }
         if (g->have_stack_probing && !fn->def_scope->safety_off) {
             addLLVMFnAttrStr(llvm_fn, "probe-stack", "__zig_probe_stack");
-        } else {
+        } else if (g->zig_target->os == OsUefi) {
             addLLVMFnAttrStr(llvm_fn, "no-stack-arg-probe", "");
         }
     } else {
