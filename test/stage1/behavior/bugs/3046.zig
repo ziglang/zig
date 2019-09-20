c9a5a6b83abaeef232ce3f0d2407c59f8769e9e5
Andrew Kelley <andrew@ziglang.org>
2019-09-20 13:19:06 -0400

    add test case for already fixed bug
    closes #3046

---
+++ test/stage1/behavior/bugs/3046.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+const SomeStruct = struct {
+    field: i32,
+};
+
+fn couldFail() anyerror!i32 {
+    return 1;
+}
+
+var some_struct: SomeStruct = undefined;
+
+test "fixed" {
+    some_struct = SomeStruct{
+        .field = couldFail() catch |_| i32(0),
+    };
+    expect(some_struct.field == 1);
+}
