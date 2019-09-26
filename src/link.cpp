c8e4108c5b822af0cc99d35baea108c1738d55ff
LemonBoy <thatlemon@gmail.com>
2019-09-26 17:13:57 +0200

    Export _start as __start for MIPS targets

--- src/link.cpp
+++ src/link.cpp
@@ -1813,14 +1813,9 @@ static void construct_linker_job_elf(LinkJob *lj) {
         lj->args.append("--allow-shlib-undefined");
     }
 
-    // MIPS entry point name is __start instead of _start, force the linker to
-    // use the latter
-    if (target_is_mips(g->zig_target) || g->zig_target->os == OsZen) {
+    if (g->zig_target->os == OsZen) {
         lj->args.append("-e");
         lj->args.append("_start");
-    }
-
-    if (g->zig_target->os == OsZen) {
         lj->args.append("--image-base=0x10000000");
     }
 }
