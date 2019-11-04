8416793da94d05c8e141f0ee5b470a2fc616c161
daurnimator <quae@daurnimator.com>
2019-11-05 02:15:52 +1100

    std: Add definitions for (deprecated) futimes and utimes C functions

--- lib/std/c.zig
+++ lib/std/c.zig
@@ -149,6 +149,10 @@ pub extern "c" fn realloc(?*c_void, usize) ?*c_void;
 pub extern "c" fn free(*c_void) void;
 pub extern "c" fn posix_memalign(memptr: **c_void, alignment: usize, size: usize) c_int;
 
+// Deprecated
+pub extern "c" fn futimes(fd: fd_t, times: *[2]timeval) c_int;
+pub extern "c" fn utimes(path: [*]const u8, times: *[2]timeval) c_int;
+
 pub extern "c" fn utimensat(dirfd: fd_t, pathname: [*]const u8, times: *[2]timespec, flags: u32) c_int;
 pub extern "c" fn futimens(fd: fd_t, times: *const [2]timespec) c_int;
 
