8c4784f9c181a13eae36d7a1ac57f278cf5fcd72
Andrew Kelley <andrew@ziglang.org>
2019-11-14 12:38:12 -0500

    remove no longer correct comment

--- test/standalone/hello_world/hello.zig
+++ test/standalone/hello_world/hello.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // If this program is run without stdout attached, exit with an error.
     const stdout_file = std.io.getStdOut();
     // If this program encounters pipe failure when printing to stdout, exit
     // with an error.
