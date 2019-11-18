21aed86a4a33b51857d82e4d8ed12ba3d4183de2
Andrew Kelley <andrew@ziglang.org>
2019-11-18 17:31:35 -0500

    add regression test case
    closes #3097

--- test/compile_errors.zig
+++ test/compile_errors.zig
@@ -3,6 +3,25 @@ const builtin = @import("builtin");
 
 pub fn addCases(cases: *tests.CompileErrorContext) void {
     cases.add(
+        "asigning to struct or union fields that are not optionals with a function that returns an optional",
+        \\fn maybe(is: bool) ?u8 {
+        \\    if (is) return @as(u8, 10) else return null;
+        \\}
+        \\const U = union {
+        \\    Ye: u8,
+        \\};
+        \\const S = struct {
+        \\    num: u8,
+        \\};
+        \\export fn entry() void {
+        \\    var u = U{ .Ye = maybe(false) };
+        \\    var s = S{ .num = maybe(false) };
+        \\}
+    ,
+        "tmp.zig:11:27: error: expected type 'u8', found '?u8'",
+    );
+
+    cases.add(
         "missing result type for phi node",
         \\fn foo() !void {
         \\    return anyerror.Foo;
