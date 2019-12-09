3326dd30528088ec31aa37c03ff4a09ef498c47e
Heppokoyuki <yuki@uefi.jp>
2019-12-09 14:26:13 +0900

    fix con_in difinition and add EFI_SIMPLE_TEXT_INPUT_PROTOCOL difinition

---
+++ lib/std/os/uefi/protocols/simple_text_input_protocol.zig
@@ -0,0 +1,30 @@
+const uefi = @import("std").os.uefi;
+const Event = uefi.Event;
+const Guid = uefi.Guid;
+
+/// Character input devices, e.g. Keyboard
+pub const SimpleTextInputProtocol = extern struct {
+    _reset: extern fn (*const SimpleTextInputProtocol, bool) usize,
+    _read_key_stroke: extern fn (*const SimpleTextInputProtocol, *uefi.protocols.InputKey) usize,
+    wait_for_key: Event,
+
+    /// Resets the input device hardware.
+    pub fn reset(self: *const SimpleTextInputProtocol, verify: bool) usize {
+        return self._reset(self, verify);
+    }
+
+    /// Reads the next keystroke from the input device.
+    pub fn readKeyStroke(self: *const SimpleTextInputProtocol, input_key: *uefi.protocols.InputKey) usize {
+        return self._read_key_stroke(self, input_key);
+    }
+
+    pub const guid align(8) = Guid{
+        .time_low = 0x387477c1,
+        .time_mid = 0x69c7,
+        .time_high_and_version = 0x11d2,
+        .clock_seq_high_and_reserved = 0x8e,
+        .clock_seq_low = 0x39,
+        .node = [_]u8{ 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b },
+    };
+};
+
