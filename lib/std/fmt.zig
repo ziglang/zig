44b202e84632552d8694fef99a4d2a673a4a91cb
data-man <datamanrb@gmail.com>
2019-12-15 21:19:13 +0500

    Disable test for musleabihf

--- lib/std/fmt.zig
+++ lib/std/fmt.zig
@@ -1737,6 +1737,11 @@ test "positional/alignment/width/precision" {
 }
 
 test "vector" {
+    if (builtin.os == .linux and builtin.arch == .arm and builtin.abi == .musleabihf) {
+        // TODO https://github.com/ziglang/zig/issues/3289
+        return error.SkipZigTest;
+    }
+
     const vbool: @Vector(4, bool) = [_]bool{ true, false, true, false };
     const vi64: @Vector(4, i64) = [_]i64{ -2, -1, 0, 1 };
     const vu64: @Vector(4, u64) = [_]u64{ 1000, 2000, 3000, 4000 };
