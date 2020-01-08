da56c84fb8cc087cca273c1c91b13ae3654377c3
Travis Staloch <twostepted@gmail.com>
2020-01-08 00:33:17 -0800

    cast long long to usize rather than c_uint

--- test/translate_c.zig
+++ test/translate_c.zig
@@ -1862,7 +1862,7 @@ pub fn addCases(cases: *tests.TranslateCContext) void {
         \\}
     });
 
-    cases.add("long long array index cast to unsigned",
+    cases.add("long long array index cast to usize",
         \\void foo() {
         \\  long long a[10], i = 0;
         \\  a[i] = 0;
@@ -1871,7 +1871,7 @@ pub fn addCases(cases: *tests.TranslateCContext) void {
         \\pub export fn foo() void {
         \\    var a: [10]c_longlong = undefined;
         \\    var i: c_longlong = @bitCast(c_longlong, @as(c_longlong, @as(c_int, 0)));
-        \\    a[@intCast(c_uint, i)] = @bitCast(c_longlong, @as(c_longlong, @as(c_int, 0)));
+        \\    a[@intCast(usize, i)] = @bitCast(c_longlong, @as(c_longlong, @as(c_int, 0)));
         \\}
     });
 
