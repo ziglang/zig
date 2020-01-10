fe10fde029d8784e79e36bc4233174a9a1572394
Terin Stock <terinjokes@gmail.com>
2020-01-10 02:03:56 -0800

    std: implement sendfile on linux
    This changset adds a `sendfile(2)` syscall bindings to the linux bits
    component. Where available, the `sendfile64(2)` syscall will be
    transparently called.
    
    A wrapping function has also been added to the std.os to transform
    errno returns to Zig errors.
    
    Change-Id: I86769fc4382c0771e3656e7b21137bafd99a4411

--- lib/std/c/freebsd.zig
+++ lib/std/c/freebsd.zig
@@ -8,6 +8,14 @@ pub extern "c" fn getdents(fd: c_int, buf_ptr: [*]u8, nbytes: usize) usize;
 pub extern "c" fn sigaltstack(ss: ?*stack_t, old_ss: ?*stack_t) c_int;
 pub extern "c" fn getrandom(buf_ptr: [*]u8, buf_len: usize, flags: c_uint) isize;
 
+pub const sf_hdtr = extern struct {
+    headers: [*]iovec_const,
+    hdr_cnt: c_int,
+    trailers: [*]iovec_const,
+    trl_cnt: c_int,
+};
+pub extern "c" fn sendfile(fd: c_int, s: c_int, offset: u64, nbytes: usize, sf_hdtr: ?*sf_hdtr, sbytes: ?*u64, flags: c_int) c_int;
+
 pub const dl_iterate_phdr_callback = extern fn (info: *dl_phdr_info, size: usize, data: ?*c_void) c_int;
 pub extern "c" fn dl_iterate_phdr(callback: dl_iterate_phdr_callback, data: ?*c_void) c_int;
 
