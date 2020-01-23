1a08c0d40b8c6e72532762281365a5105116dc00
Andrew Kelley <andrew@ziglang.org>
2020-01-23 00:46:44 -0500

    new test case

--- test/stage1/behavior/bitcast.zig
+++ test/stage1/behavior/bitcast.zig
@@ -167,3 +167,12 @@ test "nested bitcast" {
     S.foo(42);
     comptime S.foo(42);
 }
+
+test "bitcast passed as tuple element" {
+    const S = struct {
+        fn foo(args: var) void {
+            expect(args[0] == 1.00000e-09);
+        }
+    };
+    S.foo(.{@bitCast(f32, @as(u32, 814313563))});
+}
