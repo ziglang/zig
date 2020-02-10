4277f0199c2a091e09f1f7ede5c9306368681b72
Timon Kruiper <timonkruiper@gmail.com>
2020-02-10 23:42:07 +0100

    Add cast between [*c]T and ?[*:0]T on fn parameter
    Fixes #4176

--- test/stage1/behavior/cast.zig
+++ test/stage1/behavior/cast.zig
@@ -768,3 +768,17 @@ test "variable initialization uses result locations properly with regards to the
     const x: i32 = if (b) 1 else 2;
     expect(x == 1);
 }
+
+test "cast between [*c]T and ?[*:0]T on fn parameter" {
+    const S = struct {
+        const Handler = ?extern fn ([*c]const u8) void;
+        fn addCallback(handler: Handler) void {}
+
+        fn myCallback(cstr: ?[*:0]const u8) callconv(.C) void {}
+
+        fn doTheTest() void {
+            addCallback(myCallback);
+        }
+    };
+    S.doTheTest();
+}
