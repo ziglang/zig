d8fe8161b31544e612b28b190e653cd72d855339
Alexandros Naskos <alex_naskos@hotmail.com>
2020-03-04 17:41:13 +0200

    Duplicate fields of anonymous struct literals are now reported correctly. Added some tests.

--- test/stage1/behavior/tuple.zig
+++ test/stage1/behavior/tuple.zig
@@ -22,8 +22,36 @@ test "tuple initialization with structure initializer and constant expression" {
         state: u8,
     };
 
-    const tuple_with_struct = .{ TestStruct{ .state = 42 }, 0 };
-    expect(tuple_with_struct.len == 2);
-    expect(tuple_with_struct[0].state == 42);
-    expect(tuple_with_struct[1] == 0);
+    const S = struct {
+        fn doTheTest() void {
+            const tuple_with_struct = .{ TestStruct{ .state = 42 }, 0 };
+            expect(tuple_with_struct.len == 2);
+            expect(tuple_with_struct[0].state == 42);
+            expect(tuple_with_struct[1] == 0);
+        }
+    };
+    S.doTheTest();
+    comptime S.doTheTest();
+}
+
+test "passing tuples as comptime generic parameters" {
+    const S = struct {
+        fn expect_len(comptime pack: var, comptime len: usize) void {
+            expect(pack.len == len);
+        }
+
+        fn expect_first_element(comptime pack: var, comptime elem: var) void {
+            expect(pack[0] == elem);
+        }
+
+        fn doTheTest() void {
+            expect_len(.{}, 0);
+            expect_len(.{ 0 }, 1);
+            expect_first_element(.{ 0 }, 0);
+            expect_len(.{ u8, 1, "literal" }, 3);
+            expect_first_element(.{ u8, 1, "literal" }, u8);
+        }
+    };
+    S.doTheTest();
+    comptime S.doTheTest();
 }
