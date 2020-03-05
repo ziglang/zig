22a4e15f9f1d7cc5478595640ee8f88f98f185cc
Vexu <git@vexu.eu>
2020-03-06 00:04:24 +0200

    translate-c: default initialize non-extern variables to undefined

--- test/translate_c.zig
+++ test/translate_c.zig
@@ -337,10 +337,11 @@ pub fn addCases(cases: *tests.TranslateCContext) void {
     cases.add("variables",
         \\extern int extern_var;
         \\static const int int_var = 13;
+        \\int foo;
     , &[_][]const u8{
         \\pub extern var extern_var: c_int;
-    ,
         \\pub const int_var: c_int = 13;
+        \\pub export var foo: c_int = undefined;
     });
 
     cases.add("const ptr initializer",
