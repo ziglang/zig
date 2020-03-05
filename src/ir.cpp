debcc79d56a40f77b92e243b4e344fc9385bd405
Alexandros Naskos <alex_naskos@hotmail.com>
2020-03-05 19:23:06 +0200

    Allow constant struct val to reallocate its fields when resolving an inferred struct field with a comptime value.

--- src/ir.cpp
+++ src/ir.cpp
@@ -18477,6 +18477,15 @@ static IrInstGen *ir_resolve_result(IrAnalyze *ira, IrInst *suspend_source_instr
         IrInstGen *casted_ptr;
         if (isf->already_resolved) {
             field = find_struct_type_field(isf->inferred_struct_type, isf->field_name);
+
+            // If the value originates from another node than the original value's we overwrite the inferred struct's
+            // type so that the new result can be written successfully.
+            // The duplicate field will be detected and reported in 'ir_analyze_container_init_fields'
+            AstNode *decl_node = value ? value->base.source_node : suspend_source_instr->source_node;
+            if (decl_node != field->decl_node) {
+                field->type_entry = value_type;
+                field->type_val = create_const_type(ira->codegen, field->type_entry);
+            }
             casted_ptr = result_loc;
         } else {
             isf->already_resolved = true;
@@ -18493,15 +18502,6 @@ static IrInstGen *ir_resolve_result(IrAnalyze *ira, IrInst *suspend_source_instr
             field->type_val = create_const_type(ira->codegen, field->type_entry);
             field->src_index = old_field_count;
             field->decl_node = value ? value->base.source_node : suspend_source_instr->source_node;
-            if (value && instr_is_comptime(value)) {
-                ZigValue *val = ir_resolve_const(ira, value, UndefOk);
-                if (!val)
-                    return ira->codegen->invalid_inst_gen;
-                field->is_comptime = true;
-                field->init_val = ira->codegen->pass1_arena->create<ZigValue>();
-                copy_const_val(ira->codegen, field->init_val, val);
-                return result_loc;
-            }
 
             ZigType *struct_ptr_type = get_pointer_to_type(ira->codegen, isf->inferred_struct_type, false);
             if (instr_is_comptime(result_loc)) {
@@ -18530,6 +18530,16 @@ static IrInstGen *ir_resolve_result(IrAnalyze *ira, IrInst *suspend_source_instr
                     field_val->parent.data.p_struct.field_index = old_field_count;
                 }
             }
+        }
+
+        if (value && instr_is_comptime(value)) {
+            ZigValue *val = ir_resolve_const(ira, value, UndefOk);
+            if (!val)
+                return ira->codegen->invalid_inst_gen;
+            field->is_comptime = true;
+            field->init_val = ira->codegen->pass1_arena->create<ZigValue>();
+            copy_const_val(ira->codegen, field->init_val, val);
+            return result_loc;
         }
 
         result_loc = ir_analyze_struct_field_ptr(ira, suspend_source_instr, field, casted_ptr,
@@ -22957,6 +22967,9 @@ static IrInstGen *ir_analyze_container_init_fields(IrAnalyze *ira, IrInst *sourc
             first_non_const_instruction = result_loc;
         }
     }
+
+    heap::c_allocator.deallocate(field_assign_nodes, actual_field_count);
+
     if (any_missing)
         return ira->codegen->invalid_inst_gen;
 
@@ -22971,6 +22984,8 @@ static IrInstGen *ir_analyze_container_init_fields(IrAnalyze *ira, IrInst *sourc
             }
         }
     }
+
+    const_ptrs.deinit();
 
     IrInstGen *result = ir_get_deref(ira, source_instr, result_loc, nullptr);
 
