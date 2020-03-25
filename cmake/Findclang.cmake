c61c288bd16f8f6ae3fd45d91130a90fbda1f09b
Michael Dusan <michael.dusan@gmail.com>
2020-03-25 18:43:28 -0400

    cmake: address new warnings from cmake-3.17
    Match package-name case from CMakeLists.txt .
    
    New warning sample:
    
        The package name passed to `find_package_handle_standard_args` (LLVM) does
        not match the name of the calling package (llvm).  This can lead to
        problems in calling code that expects `find_package` result variables
        (e.g., `_FOUND`) to follow a certain pattern.

--- cmake/Findclang.cmake
+++ cmake/Findclang.cmake
@@ -56,6 +56,6 @@ FIND_AND_ADD_CLANG_LIB(clangCrossTU)
 FIND_AND_ADD_CLANG_LIB(clangIndex)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(CLANG DEFAULT_MSG CLANG_LIBRARIES CLANG_INCLUDE_DIRS)
+find_package_handle_standard_args(clang DEFAULT_MSG CLANG_LIBRARIES CLANG_INCLUDE_DIRS)
 
 mark_as_advanced(CLANG_INCLUDE_DIRS CLANG_LIBRARIES CLANG_LIBDIRS)
