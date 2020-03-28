98cf68c4ccb56a3cc739a9e3aec5b32f82892bec
Michael Raymond <raymond.michael@gmail.com>
2020-03-28 15:44:40 +0000

    std.zig.render: fix newlines before DocComments

--- lib/std/zig/parser_test.zig
+++ lib/std/zig/parser_test.zig
@@ -373,6 +373,35 @@ test "zig fmt: correctly move doc comments on struct fields" {
     );
 }
 
+test "zig fmt: correctly space struct fields with doc comments" {
+    try testTransform(
+        \\pub const S = struct {
+        \\    /// A
+        \\    a: u8,
+        \\    /// B
+        \\    /// B (cont)
+        \\    b: u8,
+        \\
+        \\
+        \\    /// C
+        \\    c: u8,
+        \\};
+        \\
+        ,
+        \\pub const S = struct {
+        \\    /// A
+        \\    a: u8,
+        \\    /// B
+        \\    /// B (cont)
+        \\    b: u8,
+        \\
+        \\    /// C
+        \\    c: u8,
+        \\};
+        \\
+    );
+}
+
 test "zig fmt: doc comments on param decl" {
     try testCanonical(
         \\pub const Allocator = struct {
