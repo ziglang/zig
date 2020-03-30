9b10617eacd9f79b03324f1672f3bbec72ec8930
Luna <git@l4.pm>
2020-03-30 17:23:46 -0300

    Replace C shorts by integer types

--- lib/std/os/bits/linux.zig
+++ lib/std/os/bits/linux.zig
@@ -1717,7 +1717,7 @@ pub const IFNAMESIZE = 16;
 pub const ifmap = extern struct {
     mem_start: c_ulong,
     mem_end: c_ulong,
-    base_addr: c_ushort,
+    base_addr: u16,
     irq: u8,
     dma: u8,
     port: u8,
@@ -1733,7 +1733,7 @@ pub const ifreq = extern struct {
         ifru_broadaddr: sockaddr,
         ifru_netmask: sockaddr,
         ifru_hwaddr: sockaddr,
-        ifru_flags: c_short,
+        ifru_flags: i16,
         ifru_ivalue: i32,
         ifru_mtu: i32,
         ifru_map: ifmap,
