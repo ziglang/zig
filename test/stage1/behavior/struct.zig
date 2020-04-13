1777dc3266259827f1038b375d340712f59b73c5
LemonBoy <thatlemon@gmail.com>
2020-04-13 12:49:42 +0200

    stage1: More fixes for BE targets
    * Fix packed struct alignment
    * Adjust some tests

--- test/stage1/behavior/struct.zig
+++ test/stage1/behavior/struct.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
+const builtin = std.builtin;
 const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 const expectEqualSlices = std.testing.expectEqualSlices;
-const builtin = @import("builtin");
 const maxInt = std.math.maxInt;
 const StructWithNoFields = struct {
     fn add(a: i32, b: i32) i32 {
@@ -407,7 +407,10 @@ const Bitfields = packed struct {
 };
 
 test "native bit field understands endianness" {
-    var all: u64 = 0x7765443322221111;
+    var all: u64 = if (builtin.endian != .Little)
+        0x1111222233445677
+    else
+        0x7765443322221111;
     var bytes: [8]u8 = undefined;
     @memcpy(&bytes, @ptrCast([*]u8, &all), 8);
     var bitfields = @ptrCast(*Bitfields, &bytes).*;
