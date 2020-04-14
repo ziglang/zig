ae4bf4c7672d5e20a1fbb8d988e3387c6b9365f8
Jakub <jakubshark@gmail.com>
2020-04-14 09:07:57 -0700

    Added gdi32.zig and More user32 definitions

---
+++ lib/std/os/windows/gdi32.zig
@@ -0,0 +1,45 @@
+usingnamespace @import("bits.zig");
+
+pub const PIXELFORMATDESCRIPTOR = extern struct {
+    nSize: WORD = @sizeOf(PIXELFORMATDESCRIPTOR),
+    nVersion: WORD,
+    dwFlags: DWORD,
+    iPixelType: BYTE,
+    cColorBits: BYTE,
+    cRedBits: BYTE,
+    cRedShift: BYTE,
+    cGreenBits: BYTE,
+    cGreenShift: BYTE,
+    cBlueBits: BYTE,
+    cBlueShift: BYTE,
+    cAlphaBits: BYTE,
+    cAlphaShift: BYTE,
+    cAccumBits: BYTE,
+    cAccumRedBits: BYTE,
+    cAccumGreenBits: BYTE,
+    cAccumBlueBits: BYTE,
+    cAccumAlphaBits: BYTE,
+    cDepthBits: BYTE,
+    cStencilBits: BYTE,
+    cAuxBuffers: BYTE,
+    iLayerType: BYTE,
+    bReserved: BYTE,
+    dwLayerMask: DWORD,
+    dwVisibleMask: DWORD,
+    dwDamageMask: DWORD,
+};
+
+pub extern "gdi32" fn SetPixelFormat(
+    hdc: ?HDC,
+    format: i32,
+    ppfd: ?*const PIXELFORMATDESCRIPTOR,
+) callconv(.Stdcall) bool;
+
+pub extern "gdi32" fn ChoosePixelFormat(
+    hdc: ?HDC,
+    ppfd: ?*const PIXELFORMATDESCRIPTOR,
+) callconv(.Stdcall) i32;
+
+pub extern "gdi32" fn SwapBuffers(hdc: ?HDC) callconv(.Stdcall) bool;
+pub extern "gdi32" fn wglCreateContext(hdc: ?HDC) callconv(.Stdcall) ?HGLRC;
+pub extern "gdi32" fn wglMakeCurrent(hdc: ?HDC, hglrc: ?HGLRC) callconv(.Stdcall) bool;
