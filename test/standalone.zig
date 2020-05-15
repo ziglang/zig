b747d4d1cedc20ec1224ee6b2e788c5627464e1f
yvt <i@yvt.jp>
2020-05-16 02:05:55 +0900

    Enable the test `standalone/global_linkage`
    This test was added to the source tree in c39d7a6, but has never been
    referenced from anywhere.

--- test/standalone.zig
+++ test/standalone.zig
@@ -11,6 +11,7 @@ pub fn addCases(cases: *tests.StandaloneContext) void {
     cases.addBuildFile("test/standalone/main_pkg_path/build.zig");
     cases.addBuildFile("test/standalone/shared_library/build.zig");
     cases.addBuildFile("test/standalone/mix_o_files/build.zig");
+    cases.addBuildFile("test/standalone/global_linkage/build.zig");
     cases.addBuildFile("test/standalone/static_c_lib/build.zig");
     cases.addBuildFile("test/standalone/issue_339/build.zig");
     cases.addBuildFile("test/standalone/issue_794/build.zig");
