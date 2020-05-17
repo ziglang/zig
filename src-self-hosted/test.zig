b0968abccbfb4072528c3b5e039bc03b27af89a1
Andrew Kelley <andrew@ziglang.org>
2020-05-17 13:49:22 -0400

    update ZIR compare output test to test incremental updates

--- src-self-hosted/test.zig
+++ src-self-hosted/test.zig
@@ -21,8 +21,8 @@ pub const TestContext = struct {
 
     pub const ZIRCompareOutputCase = struct {
         name: []const u8,
-        src: [:0]const u8,
-        expected_stdout: []const u8,
+        src_list: []const []const u8,
+        expected_stdout_list: []const []const u8,
     };
 
     pub const ZIRTransformCase = struct {
@@ -35,13 +35,13 @@ pub const TestContext = struct {
     pub fn addZIRCompareOutput(
         ctx: *TestContext,
         name: []const u8,
-        src: [:0]const u8,
-        expected_stdout: []const u8,
+        src_list: []const []const u8,
+        expected_stdout_list: []const []const u8,
     ) void {
         ctx.zir_cmp_output_cases.append(.{
             .name = name,
-            .src = src,
-            .expected_stdout = expected_stdout,
+            .src_list = src_list,
+            .expected_stdout_list = expected_stdout_list,
         }) catch unreachable;
     }
 
@@ -104,56 +104,68 @@ pub const TestContext = struct {
         var tmp = std.testing.tmpDir(.{});
         defer tmp.cleanup();
 
-        var prg_node = root_node.start(case.name, 2);
-        prg_node.activate();
-        defer prg_node.end();
-
         const tmp_src_path = "test-case.zir";
-        try tmp.dir.writeFile(tmp_src_path, case.src);
-
         const root_pkg = try Package.create(allocator, tmp.dir, ".", tmp_src_path);
         defer root_pkg.destroy();
 
-        {
-            var module = try Module.init(allocator, .{
-                .target = target,
-                .output_mode = .Exe,
-                .optimize_mode = .Debug,
-                .bin_file_dir = tmp.dir,
-                .bin_file_path = "a.out",
-                .root_pkg = root_pkg,
-            });
-            defer module.deinit();
+        var prg_node = root_node.start(case.name, case.src_list.len);
+        prg_node.activate();
+        defer prg_node.end();
 
-            var module_node = prg_node.start("parse,analysis,codegen", null);
-            module_node.activate();
+        var module = try Module.init(allocator, .{
+            .target = target,
+            .output_mode = .Exe,
+            .optimize_mode = .Debug,
+            .bin_file_dir = tmp.dir,
+            .bin_file_path = "a.out",
+            .root_pkg = root_pkg,
+        });
+        defer module.deinit();
+
+        for (case.src_list) |source, i| {
+            var src_node = prg_node.start("update", 2);
+            src_node.activate();
+            defer src_node.end();
+
+            try tmp.dir.writeFile(tmp_src_path, source);
+
+            var update_node = src_node.start("parse,analysis,codegen", null);
+            update_node.activate();
+            try module.makeBinFileWritable();
             try module.update();
-            module_node.end();
-        }
+            update_node.end();
 
-        var exec_result = x: {
-            var exec_node = prg_node.start("execute", null);
-            exec_node.activate();
-            defer exec_node.end();
+            var exec_result = x: {
+                var exec_node = src_node.start("execute", null);
+                exec_node.activate();
+                defer exec_node.end();
 
-            break :x try std.ChildProcess.exec(.{
-                .allocator = allocator,
-                .argv = &[_][]const u8{"./a.out"},
-                .cwd_dir = tmp.dir,
-            });
-        };
-        defer allocator.free(exec_result.stdout);
-        defer allocator.free(exec_result.stderr);
-        switch (exec_result.term) {
-            .Exited => |code| {
-                if (code != 0) {
-                    std.debug.warn("elf file exited with code {}\n", .{code});
-                    return error.BinaryBadExitCode;
-                }
-            },
-            else => return error.BinaryCrashed,
+                try module.makeBinFileExecutable();
+                break :x try std.ChildProcess.exec(.{
+                    .allocator = allocator,
+                    .argv = &[_][]const u8{"./a.out"},
+                    .cwd_dir = tmp.dir,
+                });
+            };
+            defer allocator.free(exec_result.stdout);
+            defer allocator.free(exec_result.stderr);
+            switch (exec_result.term) {
+                .Exited => |code| {
+                    if (code != 0) {
+                        std.debug.warn("elf file exited with code {}\n", .{code});
+                        return error.BinaryBadExitCode;
+                    }
+                },
+                else => return error.BinaryCrashed,
+            }
+            const expected_stdout = case.expected_stdout_list[i];
+            if (!std.mem.eql(u8, expected_stdout, exec_result.stdout)) {
+                std.debug.panic(
+                    "update index {}, mismatched stdout\n====Expected (len={}):====\n{}\n====Actual (len={}):====\n{}\n========\n",
+                    .{ i, expected_stdout.len, expected_stdout, exec_result.stdout.len, exec_result.stdout },
+                );
+            }
         }
-        std.testing.expectEqualSlices(u8, case.expected_stdout, exec_result.stdout);
     }
 
     fn runOneZIRTransformCase(
