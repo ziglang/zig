4771c29f3edeaa1a78aa78fa6abdfe3a4c5bb247
Sergey Poznyak <Sergeeeekg@gmail.com>
2020-05-21 07:55:01 +0300

    Added global cache dir resolution

--- src-self-hosted/print_info.zig
+++ src-self-hosted/print_info.zig
@@ -1,6 +1,8 @@
+const builtin = @import("builtin");
 const std = @import("std");
 const io = std.io;
 const fs = std.fs;
+const os = std.os;
 const json = std.json;
 const StringifyOptions = json.StringifyOptions;
 const Allocator = std.mem.Allocator;
@@ -25,18 +27,20 @@ pub const CompilerInfo = struct {
     pub fn init(allocator: *Allocator) !CompilerInfo {
         const zig_lib_dir = try introspect.resolveZigLibDir(allocator);
         const zig_std_dir = try fs.path.join(allocator, &[_][]const u8{zig_lib_dir, "std"});
+        const global_cache_dir = try getAppCacheDir(allocator, "zig");
         return CompilerInfo{
             .id = "test",
             .version = "0.7.0",
             .lib_dir = zig_lib_dir,
             .std_dir = zig_std_dir,
-            .global_cache_dir = "/global/"
+            .global_cache_dir = global_cache_dir,
         };
     }
 
     pub fn deinit(self: *CompilerInfo, allocator: *Allocator) void {
         allocator.free(self.lib_dir);
         allocator.free(self.std_dir);
+        allocator.free(self.global_cache_dir);
     }
 };
 
@@ -57,4 +61,57 @@ pub fn cmdInfo(allocator: *Allocator, stdout: var) !void {
 
     try bos_stream.writeByte('\n');
     try bos.flush();
+}
+
+pub const GetAppCacheDirError = error{
+    OutOfMemory,
+    AppCacheDirUnavailable,
+};
+
+// Copied from fs.getAppDataDir, but changed it to return .cache/ dir on linux.
+// This is the same behavior as the current zig compiler global cache resolution.
+fn getAppCacheDir(allocator: *Allocator, appname: []const u8) GetAppCacheDirError![]u8 {
+    switch (builtin.os.tag) {
+        .windows => {
+            var dir_path_ptr: [*:0]u16 = undefined;
+            switch (os.windows.shell32.SHGetKnownFolderPath(
+                &os.windows.FOLDERID_LocalAppData,
+                os.windows.KF_FLAG_CREATE,
+                null,
+                &dir_path_ptr,
+            )) {
+                os.windows.S_OK => {
+                    defer os.windows.ole32.CoTaskMemFree(@ptrCast(*c_void, dir_path_ptr));
+                    const global_dir = unicode.utf16leToUtf8Alloc(allocator, mem.spanZ(dir_path_ptr)) catch |err| switch (err) {
+                        error.UnexpectedSecondSurrogateHalf => return error.AppDataDirUnavailable,
+                        error.ExpectedSecondSurrogateHalf => return error.AppDataDirUnavailable,
+                        error.DanglingSurrogateHalf => return error.AppDataDirUnavailable,
+                        error.OutOfMemory => return error.OutOfMemory,
+                    };
+                    defer allocator.free(global_dir);
+                    return fs.path.join(allocator, &[_][]const u8{ global_dir, appname });
+                },
+                os.windows.E_OUTOFMEMORY => return error.OutOfMemory,
+                else => return error.AppCacheDirUnavailable,
+            }
+        },
+        .macosx => {
+            const home_dir = os.getenv("HOME") orelse {
+                // TODO look in /etc/passwd
+                return error.AppCacheDirUnavailable;
+            };
+            return fs.path.join(allocator, &[_][]const u8{ home_dir, "Library", "Application Support", appname });
+        },
+        .linux, .freebsd, .netbsd, .dragonfly => {
+            if (os.getenv("XDG_CACHE_HOME")) |cache_home| {
+                return fs.path.join(allocator, &[_][]const u8{ cache_home, appname });
+            }
+
+            const home_dir = os.getenv("HOME") orelse {
+                return error.AppCacheDirUnavailable;
+            };
+            return fs.path.join(allocator, &[_][]const u8{ home_dir, ".cache", appname });
+        },
+        else => @compileError("Unsupported OS"),
+    }
 }
