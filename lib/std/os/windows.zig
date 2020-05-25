44516890a84261a862826aa90f73c2e6fce0830b
StrangeBug <steve@ineedtojet.com>
2020-05-26 01:14:26 +0200

    os/windows: change error value when writing to an invalid stdout or stderr.
    "Unexpected" shouldn't be use for expected error.
    Returns BrokenPipe instead only for stdout and stderr, this behavior matcjes the POSIX one.

--- lib/std/os/windows.zig
+++ lib/std/os/windows.zig
@@ -567,7 +567,11 @@ pub fn WriteFile(
                 // https://github.com/ziglang/zig/issues/4196
                 // Prevents infinite loop when stderr/stdout is not available.
                 // TODO Should implement a NoOpOutStream but this is not possible for now because we cannot coerce or cast to File.OutStream
-                .INVALID_HANDLE => return error.Unexpected,
+                .INVALID_HANDLE => {
+                    if (std.io.getStdOut().handle == handle) return error.BrokenPipe;
+                    if (std.io.getStdErr().handle == handle) return error.BrokenPipe;
+                    unreachable;
+                },
                 else => |err| return unexpectedError(err),
             }
         }
