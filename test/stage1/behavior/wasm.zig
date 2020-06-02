057d97c093c81150a3761b50a1f646703b6cfd1b
Jakub Konka <kubkon@jakubkonka.com>
2020-06-02 19:05:21 +0200

    Return should be i32 due to error signaling in memory.grow
    Also, fix tests.

--- test/stage1/behavior/wasm.zig
+++ test/stage1/behavior/wasm.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const expect = std.testing.expect;
 
 test "memory size and grow" {
-    var prev = @wasmMemorySize();
-    expect(prev == @wasmMemoryGrow(1));
-    expect(prev + 1 == @wasmMemorySize());
+    var prev = @wasmMemorySize(0);
+    expect(prev == @wasmMemoryGrow(0, 1));
+    expect(prev + 1 == @wasmMemorySize(0));
 }
