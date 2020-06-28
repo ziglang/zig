415c6e6ab995f27a797fdf9d57f0045576dfd2f7
data-man <datamanrb@gmail.com>
2020-06-28 05:13:45 +0500

    New allocator corrections

--- lib/std/heap/logging_allocator.zig
+++ lib/std/heap/logging_allocator.zig
@@ -1,36 +1,36 @@
 const std = @import("../std.zig");
 const Allocator = std.mem.Allocator;
 
-/// This allocator is used in front of another allocator and logs to the provided stream
-/// on every call to the allocator. Stream errors are ignored.
+/// This allocator is used in front of another allocator and logs to the provided writer
+/// on every call to the allocator. Writer errors are ignored.
 /// If https://github.com/ziglang/zig/issues/2586 is implemented, this API can be improved.
-pub fn LoggingAllocator(comptime OutStreamType: type) type {
+pub fn LoggingAllocator(comptime WriterType: type) type {
     return struct {
         allocator: Allocator,
         parent_allocator: *Allocator,
-        out_stream: OutStreamType,
+        writer: WriterType,
 
         const Self = @This();
 
-        pub fn init(parent_allocator: *Allocator, out_stream: OutStreamType) Self {
+        pub fn init(parent_allocator: *Allocator, writer: WriterType) Self {
             return Self{
                 .allocator = Allocator{
                     .allocFn = alloc,
                     .resizeFn = resize,
                 },
                 .parent_allocator = parent_allocator,
-                .out_stream = out_stream,
+                .writer = writer,
             };
         }
 
         fn alloc(allocator: *Allocator, len: usize, ptr_align: u29, len_align: u29) error{OutOfMemory}![]u8 {
             const self = @fieldParentPtr(Self, "allocator", allocator);
-            self.out_stream.print("alloc : {}", .{len}) catch {};
+            self.writer.print("alloc : {}", .{len}) catch {};
             const result = self.parent_allocator.callAllocFn(len, ptr_align, len_align);
             if (result) |buff| {
-                self.out_stream.print(" success!\n", .{}) catch {};
+                self.writer.print(" success!\n", .{}) catch {};
             } else |err| {
-                self.out_stream.print(" failure!\n", .{}) catch {};
+                self.writer.print(" failure!\n", .{}) catch {};
             }
             return result;
         }
@@ -38,20 +38,20 @@ pub fn LoggingAllocator(comptime OutStreamType: type) type {
         fn resize(allocator: *Allocator, buf: []u8, new_len: usize, len_align: u29) error{OutOfMemory}!usize {
             const self = @fieldParentPtr(Self, "allocator", allocator);
             if (new_len == 0) {
-                self.out_stream.print("free  : {}\n", .{buf.len}) catch {};
+                self.writer.print("free  : {}\n", .{buf.len}) catch {};
             } else if (new_len <= buf.len) {
-                self.out_stream.print("shrink: {} to {}\n", .{buf.len, new_len}) catch {};
+                self.writer.print("shrink: {} to {}\n", .{buf.len, new_len}) catch {};
             } else {
-                self.out_stream.print("expand: {} to {}", .{ buf.len, new_len }) catch {};
+                self.writer.print("expand: {} to {}", .{ buf.len, new_len }) catch {};
             }
             if (self.parent_allocator.callResizeFn(buf, new_len, len_align)) |resized_len| {
                 if (new_len > buf.len) {
-                    self.out_stream.print(" success!\n", .{}) catch {};
+                    self.writer.print(" success!\n", .{}) catch {};
                 }
                 return resized_len;
             } else |e| {
                 std.debug.assert(new_len > buf.len);
-                self.out_stream.print(" failure!\n", .{}) catch {};
+                self.writer.print(" failure!\n", .{}) catch {};
                 return e;
             }
         }
@@ -60,9 +60,9 @@ pub fn LoggingAllocator(comptime OutStreamType: type) type {
 
 pub fn loggingAllocator(
     parent_allocator: *Allocator,
-    out_stream: var,
-) LoggingAllocator(@TypeOf(out_stream)) {
-    return LoggingAllocator(@TypeOf(out_stream)).init(parent_allocator, out_stream);
+    writer: var,
+) LoggingAllocator(@TypeOf(writer)) {
+    return LoggingAllocator(@TypeOf(writer)).init(parent_allocator, writer);
 }
 
 test "LoggingAllocator" {
@@ -71,7 +71,7 @@ test "LoggingAllocator" {
 
     var allocator_buf: [10]u8 = undefined;
     var fixedBufferAllocator = std.mem.validationWrap(std.heap.FixedBufferAllocator.init(&allocator_buf));
-    const allocator = &loggingAllocator(&fixedBufferAllocator.allocator, fbs.outStream()).allocator;
+    const allocator = &loggingAllocator(&fixedBufferAllocator.allocator, fbs.writer()).allocator;
 
     var a = try allocator.alloc(u8, 10);
     a.len = allocator.shrinkBytes(a, 5, 0);
