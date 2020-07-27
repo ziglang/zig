be6272cf5920a62e91fec9d85e33a924b54ebea2
Nathan Michaels <nathan@nmichaels.org>
2020-07-27 18:32:25 -0400

    Change std.debug.warn to std.debug.print in init-exe template.

--- lib/std/special/init-exe/src/main.zig
+++ lib/std/special/init-exe/src/main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
 
 pub fn main() anyerror!void {
-    std.debug.warn("All your codebase are belong to us.\n", .{});
+    std.debug.print("All your codebase are belong to us.\n", .{});
 }
