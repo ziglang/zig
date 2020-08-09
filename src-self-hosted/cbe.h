b59e2c1e009e8e2df2de02d61d8f4ef463a652be
Noam Preil <noam@pixelhero.dev>
2020-08-09 15:41:43 -0400

    CBE: improve noreturn definition, support noreturn on MSVC

--- src-self-hosted/cbe.h
+++ src-self-hosted/cbe.h
@@ -1,7 +1,9 @@
 #if __STDC_VERSION__ >= 201112L
 #define noreturn _Noreturn
-#elif __GNUC__ && !__STRICT_ANSI__
+#elif __GNUC__
 #define noreturn __attribute__ ((noreturn))
+#elif _MSC_VER
+#define noreturn __declspec(noreturn)
 #else
 #define noreturn
 #endif
