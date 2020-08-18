250276a5b2070198f0907e53809fcfcdaf812b5e
Nathan Michaels <nathan@nmichaels.org>
2020-08-18 14:36:27 -0400

    Update for new std.log API
    Co-authored-by: Andrew Kelley <andrew@ziglang.org>

--- lib/std/special/init-exe/src/main.zig
+++ lib/std/special/init-exe/src/main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
 
 pub fn main() anyerror!void {
-    std.log.info(.app, "All your codebase are belong to us.", .{});
+    std.log.info("All your codebase are belong to us.", .{});
 }
