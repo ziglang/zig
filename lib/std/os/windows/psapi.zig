239b5860f228df2c6a244d4a817f9fa4fa3c49e2
Andrew Kelley <andrew@ziglang.org>
2020-08-19 19:40:15 -0700

    add license header to all std lib files
    add SPDX license identifier
    copyright ownership is zig contributors

--- lib/std/os/windows/psapi.zig
+++ lib/std/os/windows/psapi.zig
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2015-2020 Zig Contributors
+// This file is part of [zig](https://ziglang.org/), which is MIT licensed.
+// The MIT license requires this copyright notice to be included in all copies
+// and substantial portions of the software.
 usingnamespace @import("bits.zig");
 
 pub extern "psapi" fn EmptyWorkingSet(hProcess: HANDLE) callconv(.Stdcall) BOOL;
--- lib/std/special/c.zig
+++ lib/std/special/c.zig
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2015-2020 Zig Contributors
+// This file is part of [zig](https://ziglang.org/), which is MIT licensed.
+// The MIT license requires this copyright notice to be included in all copies
+// and substantial portions of the software.
 // This is Zig's multi-target implementation of libc.
 // When builtin.link_libc is true, we need to export all the functions and
 // provide an entire C API.
