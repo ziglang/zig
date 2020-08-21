56ea04cb6d1b2b87e2a2dbde6de8279e035bec73
Andrew Kelley <andrew@ziglang.org>
2020-08-20 17:19:26 -0700

    stage2: don't test compile errors of ZIR

--- test/stage2/test.zig
+++ test/stage2/test.zig
@@ -1,7 +1,6 @@
 const TestContext = @import("../../src-self-hosted/test.zig").TestContext;
 
 pub fn addCases(ctx: *TestContext) !void {
-    try @import("compile_errors.zig").addCases(ctx);
     try @import("compare_output.zig").addCases(ctx);
     try @import("zir.zig").addCases(ctx);
     try @import("cbe.zig").addCases(ctx);
