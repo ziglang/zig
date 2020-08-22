f31801c55de88ce1fa7adf53f0341e5cc0b32f25
Matt Knight <mattnite@protonmail.com>
2020-08-21 18:12:48 -0700

    removed line

--- lib/std/os/linux/bpf.zig
+++ lib/std/os/linux/bpf.zig
@@ -3,7 +3,6 @@
 // This file is part of [zig](https://ziglang.org/), which is MIT licensed.
 // The MIT license requires this copyright notice to be included in all copies
 // and substantial portions of the software.
-//
 usingnamespace std.os;
 const std = @import("../../../std.zig");
 const expectEqual = std.testing.expectEqual;
