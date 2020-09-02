1b2154dfe2f9b5030f487e7c4be8c706ce6e59b5
Tadeo Kondrak <me@tadeo.ca>
2020-09-01 18:55:36 -0600

    builtin: Add TypeInfo.StructField.is_comptime

--- lib/std/builtin.zig
+++ lib/std/builtin.zig
@@ -261,6 +261,7 @@ pub const TypeInfo = union(enum) {
         name: []const u8,
         field_type: type,
         default_value: anytype,
+        is_comptime: bool,
     };
 
     /// This data structure is used by the Zig language code generation and
