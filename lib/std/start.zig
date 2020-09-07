0c53770ed2fc8c2f02c78984dee3efb6f909f26f
Peter Spiess-Knafl <dev@spiessknafl.at>
2020-09-07 17:57:45 +0200

    Fix compile when using EFI target (Fixes #6275)

--- lib/std/start.zig
+++ lib/std/start.zig
@@ -67,7 +67,7 @@ fn EfiMain(handle: uefi.Handle, system_table: *uefi.tables.SystemTable) callconv
     uefi.handle = handle;
     uefi.system_table = system_table;
 
-    switch (@typeInfo(@TypeOf(read)).Fn.return_type.?) {
+    switch (@typeInfo(@TypeOf(root.main)).Fn.return_type.?) {
         noreturn => {
             root.main();
         },
