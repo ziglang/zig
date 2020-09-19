09f2f4aeb366279f30256a5968a246369fafcc27
Joran Dirk Greef <joran@ronomon.com>
2020-09-19 15:14:47 +0200

    Fix std @import

--- lib/std/io_uring.zig
+++ lib/std/io_uring.zig
@@ -3,7 +3,7 @@
 // This file is part of [zig](https://ziglang.org/), which is MIT licensed.
 // The MIT license requires this copyright notice to be included in all copies
 // and substantial portions of the software.
-const std = @import("std");
+const std = @import("std.zig");
 const assert = std.debug.assert;
 const builtin = std.builtin;
 const os = std.os;
