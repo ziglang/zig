f3339436091c0734a815be6093685fdef6dc2fe4
Juha Syrjälä <juha.syrjala@iki.fi>
2020-09-29 20:56:30 +0300

    Add support for `zig build run -- arg1 arg1` in file created by init-exe

--- lib/std/special/init-exe/build.zig
+++ lib/std/special/init-exe/build.zig
@@ -18,6 +18,9 @@ pub fn build(b: *Builder) void {
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
