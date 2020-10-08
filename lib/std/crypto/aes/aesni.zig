60d1e675d2b9c45c88fc01790205a64db6a43627
Frank Denis <github@pureftpd.org>
2020-10-08 14:41:18 +0200

    aes/aesni is not based on a Go implementation, only aes/soft is
    Don't blame them for our bugs :)

--- lib/std/crypto/aes/aesni.zig
+++ lib/std/crypto/aes/aesni.zig
@@ -3,7 +3,6 @@
 // This file is part of [zig](https://ziglang.org/), which is MIT licensed.
 // The MIT license requires this copyright notice to be included in all copies
 // and substantial portions of the software.
-// Based on Go stdlib implementation
 
 const std = @import("../../std.zig");
 const mem = std.mem;
