a410792ef474ae78a0505ed789922c4d32c5de8d
g-w1 <jacoblevgw@gmail.com>
2020-10-10 11:19:24 -0400

    Fix: now check c files, zig files, and link files to be null/0 len before zig run

--- src/main.zig
+++ src/main.zig
@@ -1238,8 +1238,8 @@ fn buildOutputType(
         fatal("one zig source file is required to run `zig test`", .{});
     }
 
-    if (root_src_file == null and arg_mode == .run) {
-        fatal("one zig source file is required to run `zig run`", .{});
+    if (link_objects.items.len == 0 and root_src_file == null and c_source_files.items.len == 0 and arg_mode == .run) {
+        fatal("one source file is required to run `zig run`", .{});
     }
 
     const root_name = if (provided_name) |n| n else blk: {
