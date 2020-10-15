eb0f7423929dc309c43257977ae4e0186ddfbbb1
LemonBoy <thatlemon@gmail.com>
2020-10-15 10:16:20 +0200

    stage1: Off-by-one error in int to float conversion
    The base is 2^64 and not 2^64-1.
    
    Closes #6683

--- test/stage1/behavior/cast.zig
+++ test/stage1/behavior/cast.zig
@@ -375,6 +375,22 @@ test "comptime_int @intToFloat" {
         expect(@TypeOf(result) == f32);
         expect(result == 1234.0);
     }
+    {
+        const result = @intToFloat(f64, 1234);
+        expect(@TypeOf(result) == f64);
+        expect(result == 1234.0);
+    }
+    {
+        const result = @intToFloat(f128, 1234);
+        expect(@TypeOf(result) == f128);
+        expect(result == 1234.0);
+    }
+    // big comptime_int (> 64 bits) to f128 conversion
+    {
+        const result = @intToFloat(f128, 0x1_0000_0000_0000_0000);
+        expect(@TypeOf(result) == f128);
+        expect(result == 0x1_0000_0000_0000_0000.0);
+    }
 }
 
 test "@intCast i32 to u7" {
