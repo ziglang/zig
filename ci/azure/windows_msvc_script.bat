198d1438eac27ec9f211eda8d1243875a795cae3
Andrew Kelley <andrew@ziglang.org>
2020-10-22 01:18:55 -0700

    ci: fix regression from not building docs
    0e4f7a8e3f94f0e799280d7e7836d67d776cbca6 forgot to build the docs,
    causing CI failures

--- ci/azure/windows_msvc_script.bat
+++ ci/azure/windows_msvc_script.bat
@@ -37,6 +37,7 @@ msbuild /maxcpucount /p:Configuration=Release INSTALL.vcxproj || exit /b
 "%ZIGINSTALLDIR%\bin\zig.exe" build test-runtime-safety -Dskip-non-native || exit /b
 "%ZIGINSTALLDIR%\bin\zig.exe" build test-translate-c -Dskip-non-native || exit /b
 "%ZIGINSTALLDIR%\bin\zig.exe" build test-run-translated-c -Dskip-non-native || exit /b
+"%ZIGINSTALLDIR%\bin\zig.exe" build docs || exit /b
 
 set "PATH=%CD:~0,2%\msys64\usr\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem"
 SET "MSYSTEM=MINGW64"
