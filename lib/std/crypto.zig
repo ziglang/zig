1b4ab749cffeff146b21fb0f6d30a5b8d2186765
Frank Denis <github@pureftpd.org>
2020-10-24 22:40:20 +0200

    std/crypto: add the bcrypt password hashing function
    The bcrypt function intentionally requires quite a lot of CPU cycles
    to complete.
    
    In addition to that, not having its full state constantly in the
    CPU L1 cache causes a massive performance drop.
    
    These properties slow down brute-force attacks against low-entropy
    inputs (typically passwords), and GPU-based attacks get little
    to no advantages over CPUs.

--- lib/std/crypto.zig
+++ lib/std/crypto.zig
@@ -86,6 +86,7 @@ pub const onetimeauth = struct {
 ///
 /// Password hashing functions must be used whenever sensitive data has to be directly derived from a password.
 pub const pwhash = struct {
+    pub const bcrypt = @import("crypto/bcrypt.zig");
     pub const pbkdf2 = @import("crypto/pbkdf2.zig").pbkdf2;
 };
 
@@ -130,6 +131,7 @@ test "crypto" {
     }
 
     _ = @import("crypto/aes.zig");
+    _ = @import("crypto/bcrypt.zig");
     _ = @import("crypto/blake2.zig");
     _ = @import("crypto/blake3.zig");
     _ = @import("crypto/chacha20.zig");
