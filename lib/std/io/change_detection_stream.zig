74a1175d9db56e0c6c3603699027434c489f4518
Frank Denis <github@pureftpd.org>
2020-10-26 08:56:29 +0100

    std/*: add missing MIT license headers

--- lib/std/io/change_detection_stream.zig
+++ lib/std/io/change_detection_stream.zig
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2015-2020 Zig Contributors
+// This file is part of [zig](https://ziglang.org/), which is MIT licensed.
+// The MIT license requires this copyright notice to be included in all copies
+// and substantial portions of the software.
+
 const std = @import("../std.zig");
 const io = std.io;
 const mem = std.mem;
