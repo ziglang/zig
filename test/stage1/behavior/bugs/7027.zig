5872ae5111ae49f3e82e2d07f7c199d6550a1bd7
LemonBoy <thatlemon@gmail.com>
2020-11-08 18:25:59 +0100

    stage1: Fix crash in comptime struct generation
    Using the gen_index rather than the src_index is needed to handle
    structures containing zero-sized or comptime only types.
    
    Closes #7027

---
+++ test/stage1/behavior/bugs/7027.zig
@@ -0,0 +1,17 @@
+const Foobar = struct {
+    myTypes: [128]type,
+    str: [1024]u8,
+
+    fn foo() @This() {
+        comptime var foobar: Foobar = undefined;
+        foobar.str = [_]u8{'a'} ** 1024;
+        return foobar;
+    }
+};
+
+fn foo(arg: anytype) void {}
+
+test "" {
+    comptime var foobar = Foobar.foo();
+    foo(foobar.str[0..10]);
+}
