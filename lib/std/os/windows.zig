b4f1dc09891a5cb96bfb6cf81d88c9d05010ba27
Lee Cannon <leecannon@leecannon.xyz>
2020-11-08 18:06:45 +0000

    Move utf8->utf16 up one level into os.zig

--- lib/std/os/windows.zig
+++ lib/std/os/windows.zig
@@ -565,28 +565,25 @@ pub const SetCurrentDirectoryError = error{
     NotDir,
     AccessDenied,
     NoDevice,
+    BadPathName,
     Unexpected,
 };
 
-pub fn SetCurrentDirectory(path_name: []const u8) SetCurrentDirectoryError!void {
-    var path_space: PathSpace = undefined;
-    path_space.len = try std.unicode.utf8ToUtf16Le(path_space.data[0..], path_name);
-    if (path_space.len > path_space.data.len) return error.NameTooLong;
-    
-    const path_len_bytes = math.cast(u16, path_space.len * 2) catch |err| switch (err) {
+pub fn SetCurrentDirectory(path_name: []const u16) SetCurrentDirectoryError!void {
+    const path_len_bytes = math.cast(u16, path_name.len * 2) catch |err| switch (err) {
         error.Overflow => return error.NameTooLong,
     };
     
     var nt_name = UNICODE_STRING{
         .Length = path_len_bytes,
         .MaximumLength = path_len_bytes,
-        .Buffer = @intToPtr([*]u16, @ptrToInt(&path_space.data)),
+        .Buffer = @intToPtr([*]u16, @ptrToInt(path_name.ptr)),
     };
     
     const rc = ntdll.RtlSetCurrentDirectory_U(&nt_name);
     switch (rc) {
         .SUCCESS => {},
-        .OBJECT_NAME_INVALID => unreachable,
+        .OBJECT_NAME_INVALID => return error.BadPathName,
         .OBJECT_NAME_NOT_FOUND => return error.FileNotFound,
         .OBJECT_PATH_NOT_FOUND => return error.FileNotFound,
         .NO_MEDIA_IN_DEVICE => return error.NoDevice,
