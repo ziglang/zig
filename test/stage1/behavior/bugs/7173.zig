5872ed5b06278dd18f8553895b840dd926324b45
g-w1 <jacoblevgw@gmail.com>
2020-11-20 08:03:17 -0500

    make test test instead of main function

--- test/stage1/behavior/bugs/7173.zig
+++ test/stage1/behavior/bugs/7173.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const print = std.debug.print;
 const expect = std.testing.expect;
 
-pub fn main() !void {
+test "integer peer type resolution should take signedness into account" {
     var x: u8 = 10;
     var b: i32 = -10;
     var c: i32 = b + x;
