c80d196094de3e0258b4a1146cff56576ec8e95d
LemonBoy <thatlemon@gmail.com>
2020-11-29 11:53:08 +0100

    stage1: Add missing bitcast when rendering var ptr
    Some types require this extra bitcast, eg. structs or unions with extra
    padding fields inserted by the compiler.
    
    Fixes #7250

---
+++ test/stage1/behavior/bugs/7250.zig
@@ -0,0 +1,15 @@
+const nrfx_uart_t = extern struct {
+    p_reg: [*c]u32,
+    drv_inst_idx: u8,
+};
+
+pub fn nrfx_uart_rx(p_instance: [*c]const nrfx_uart_t) void {}
+
+threadlocal var g_uart0 = nrfx_uart_t{
+    .p_reg = 0,
+    .drv_inst_idx = 0,
+};
+
+test "reference a global threadlocal variable" {
+    _ = nrfx_uart_rx(&g_uart0);
+}
