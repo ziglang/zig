99ca0acf81e756a1b9c81b0f0ed272ff0eccc05c
daurnimator <quae@daurnimator.com>
2020-12-01 18:48:58 +1100

    Add sincosf function

--- lib/std/special/c.zig
+++ lib/std/special/c.zig
@@ -634,6 +634,11 @@ export fn cosf(a: f32) f32 {
     return math.cos(a);
 }
 
+export fn sincosf(a: f32, r_sin: *f32, r_cos: *f32) void {
+    r_sin.* = math.sin(a);
+    r_cos.* = math.cos(a);
+}
+
 export fn exp(a: f64) f64 {
     return math.exp(a);
 }
