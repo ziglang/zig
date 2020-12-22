4128eea00c86f2f62d6d9e987c51929f1722be24
xackus <14938807+xackus@users.noreply.github.com>
2020-12-22 12:49:38 +0100

    std.c: add fmemopen

--- lib/std/c.zig
+++ lib/std/c.zig
@@ -342,6 +342,8 @@ pub extern "c" fn prctl(option: c_int, ...) c_int;
 pub extern "c" fn getrlimit(resource: rlimit_resource, rlim: *rlimit) c_int;
 pub extern "c" fn setrlimit(resource: rlimit_resource, rlim: *const rlimit) c_int;
 
+pub extern "c" fn fmemopen(noalias buf: ?*c_void, size: usize, noalias mode: [*:0]const u8) ?*FILE;
+
 pub const max_align_t = if (std.Target.current.abi == .msvc)
     f64
 else if (std.Target.current.isDarwin())
