b2a3ee6038a5be98a7bab29819f05cd89ca3a1ea
Andrew Kelley <andrew@ziglang.org>
2021-01-08 00:35:19 -0700

    browser playground: hook up js/html to wasm

--- src/playground/playground.js
+++ src/playground/playground.js
@@ -1,20 +1,24 @@
 (function() {
-    var env = {
-        imports: { }
+    var options = {
+        env: {
+            wasmEval: function(code_ptr, code_len) {
+                console.log("code_ptr", code_ptr, "code_len", code_len);
+            },
+        },
     };
 
-    WebAssembly.instantiateStreaming(fetch("playground.wasm"), {env}).then(function(result) {
+    WebAssembly.instantiateStreaming(fetch("playground.wasm"), options).then(function(result) {
         callback(null, result);
     }, function(err) {
         callback(err, null);
     });
     function callback(err, result) {
         if (err) {
-            document.getElementById('status').innerText = "error: " + err;
+            document.getElementById('status').textContent = "error: " + err;
             return;
         }
         // for debugging
         window._wasm = result.instance;
-        console.log(result.instance.exports.eval());
+        console.log(result.instance.exports.zigEval());
     }
 })();
