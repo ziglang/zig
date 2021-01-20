57b2176e28fd336309922cc9182cffcf084c1879
Evan Haas <evan@lagerdata.com>
2021-01-19 23:42:48 -0800

    translate-c: Improve array support
    1. For incomplete arrays with initializer list (`int x[] = {1};`) use the
    initializer size as the array size.
    
    2. For arrays initialized with a string literal translate it as an array
    of character literals instead of `[*c]const u8`
    
    3. Don't crash if an empty initializer is used for an incomplete array.
    
    4. Add a test for multi-character character constants
    
    Additionally lay some groundwork for supporting wide string literals.
    
    fixes #4831 #7832 #7842

--- test/run_translated_c.zig
+++ test/run_translated_c.zig
@@ -746,4 +746,52 @@ pub fn addCases(cases: *tests.RunTranslatedCContext) void {
         \\    return 0;
         \\}
     , "1 2" ++ nl);
+
+    cases.add("multi-character character constant",
+        \\#include <stdlib.h>
+        \\int main(void) {
+        \\    int foo = 'abcd';
+        \\    switch (foo) {
+        \\        case 'abcd': break;
+        \\        default: abort();
+        \\    }
+        \\    return 0;
+        \\}
+    , "");
+
+    cases.add("Array initializers (string literals, incomplete arrays)",
+        \\#include <stdlib.h>
+        \\#include <string.h>
+        \\extern int foo[];
+        \\int global_arr[] = {1, 2, 3};
+        \\char global_string[] = "hello";
+        \\int main(int argc, char *argv[]) {
+        \\    if (global_arr[2] != 3) abort();
+        \\    if (strlen(global_string) != 5) abort();
+        \\    const char *const_str = "hello";
+        \\    if (strcmp(const_str, "hello") != 0) abort();
+        \\    char empty_str[] = "";
+        \\    if (strlen(empty_str) != 0) abort();
+        \\    char hello[] = "hello";
+        \\    if (strlen(hello) != 5 || sizeof(hello) != 6) abort();
+        \\    int empty[] = {};
+        \\    if (sizeof(empty) != 0) abort();
+        \\    int bar[] = {42};
+        \\    if (bar[0] != 42) abort();
+        \\    bar[0] = 43;
+        \\    if (bar[0] != 43) abort();
+        \\    int baz[] = {1, [42] = 123, 456};
+        \\    if (baz[42] != 123 || baz[43] != 456) abort();
+        \\    if (sizeof(baz) != sizeof(int) * 44) abort();
+        \\    const char *const names[] = {"first", "second", "third"};
+        \\    if (strcmp(names[2], "third") != 0) abort();
+        \\    char catted_str[] = "abc" "def";
+        \\    if (strlen(catted_str) != 6 || sizeof(catted_str) != 7) abort();
+        \\    char catted_trunc_str[2] = "abc" "def";
+        \\    if (sizeof(catted_trunc_str) != 2 || catted_trunc_str[0] != 'a' || catted_trunc_str[1] != 'b') abort();
+        \\    char big_array_utf8lit[10] = "💯";
+        \\    if (strcmp(big_array_utf8lit, "💯") != 0 || big_array_utf8lit[9] != 0) abort();
+        \\    return 0;
+        \\}
+    , "");
 }
