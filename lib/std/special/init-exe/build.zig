9f722f43ac449caa0e09c1b7bb1bad0581ef323d
Meghan <nektromc@gmail.com>
2021-03-05 19:13:05 -0800

    std/special: init-exe,lib make import(std) its own decl (#8160)
    std/special: init-exe,lib make import(std) its own decl

--- lib/std/special/init-exe/build.zig
+++ lib/std/special/init-exe/build.zig
@@ -1,6 +1,6 @@
-const Builder = @import("std").build.Builder;
+const std = @import("std");
 
-pub fn build(b: *Builder) void {
+pub fn build(b: *std.build.Builder) void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
