9ebdbca379fb71da7236e7534e1d3465f7fb151f
Sreehari S <sreeharisreedev1@gmail.com>
2021-04-08 21:00:53 -0700

    callconv: add SysV

--- src/translate_c.zig
+++ src/translate_c.zig
@@ -4381,6 +4381,7 @@ fn transCC(
         .X86ThisCall => return CallingConvention.Thiscall,
         .AAPCS => return CallingConvention.AAPCS,
         .AAPCS_VFP => return CallingConvention.AAPCSVFP,
+        .X86_64SysV => return CallingConvention.SysV,
         else => return fail(
             c,
             error.UnsupportedType,
