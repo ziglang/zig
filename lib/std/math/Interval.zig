76543862f665cd7794a21fd0511e69253ed8356f
Meghan Denny <hello@nektro.net>
2021-05-12 22:14:53 -0700

    std.math.Interval: snake_case union field names

--- lib/std/math/Interval.zig
+++ lib/std/math/Interval.zig
@@ -16,27 +16,27 @@ pub fn Interval(comptime T: type) type {
         to: Endpoint,
 
         pub const Endpoint = union(enum) {
-            Infinity,
-            Closed: T,
-            Open: T,
+            infinity,
+            closed: T,
+            open: T,
 
             pub fn n(self: Endpoint) ?T {
                 return switch (self) {
-                    .Infinity => null,
-                    .Closed => |x| x,
-                    .Open => |x| x,
+                    .infinity => null,
+                    .closed => |x| x,
+                    .open => |x| x,
                 };
             }
 
             pub fn eql(self: Endpoint, other: Endpoint) bool {
-                if (self == .Infinity and other == .Infinity) {
+                if (self == .infinity and other == .infinity) {
                     return true;
                 }
-                if (self == .Closed and other == .Closed) {
-                    return self.Closed == other.Closed;
+                if (self == .closed and other == .closed) {
+                    return self.closed == other.closed;
                 }
-                if (self == .Open and other == .Open) {
-                    return self.Open == other.Open;
+                if (self == .open and other == .open) {
+                    return self.open == other.open;
                 }
                 return false;
             }
@@ -60,26 +60,26 @@ pub fn Interval(comptime T: type) type {
                 // .ComptimeFloat => x + 2,
                 else => unreachable,
             };
-            if (self.from == .Infinity) {
-                if (self.to == .Infinity) {
+            if (self.from == .infinity) {
+                if (self.to == .infinity) {
                     // handle (-inf, +inf)
                     return true;
                 }
                 // handle (-inf,
                 const to = self.to.n() orelse max;
-                const right = if (self.to == .Open) x < to else x <= to;
+                const right = if (self.to == .open) x < to else x <= to;
                 return right;
             }
-            if (self.to == .Infinity) {
+            if (self.to == .infinity) {
                 // handle +inf)
                 const from = self.from.n() orelse min;
-                const left = if (self.from == .Open) x > from else x >= from;
+                const left = if (self.from == .open) x > from else x >= from;
                 return left;
             }
             const from = self.from.n() orelse min;
             const to = self.to.n() orelse max;
-            const left = if (self.from == .Open) x > from else x >= from;
-            const right = if (self.to == .Open) x < to else x <= to;
+            const left = if (self.from == .open) x > from else x >= from;
+            const right = if (self.to == .open) x < to else x <= to;
             return left and right;
         }
 
@@ -92,18 +92,18 @@ pub fn Interval(comptime T: type) type {
 fn runTest(comptime T: type, from: Interval(T).Endpoint, to: Interval(T).Endpoint) !void {
     var x = Interval(T){ .from = from, .to = to };
 
-    try std.testing.expectEqual(x.contains(0), x.from == .Infinity);
-    try std.testing.expectEqual(x.contains(1), x.from != .Open);
+    try std.testing.expectEqual(x.contains(0), x.from == .infinity);
+    try std.testing.expectEqual(x.contains(1), x.from != .open);
     try std.testing.expectEqual(x.contains(2), true);
-    try std.testing.expectEqual(x.contains(3), x.to != .Open);
-    try std.testing.expectEqual(x.contains(4), x.to == .Infinity);
+    try std.testing.expectEqual(x.contains(3), x.to != .open);
+    try std.testing.expectEqual(x.contains(4), x.to == .infinity);
 }
 
 fn runTestMatrix(left: bool, right: bool) !void {
     inline for (.{ usize, isize, f32, f64 }) |T| {
-        const l: Interval(T).Endpoint = if (left) .{ .Open = 1 } else .{ .Closed = 1 };
-        const r: Interval(T).Endpoint = if (right) .{ .Open = 3 } else .{ .Closed = 3 };
-        const i: Interval(T).Endpoint = .{ .Infinity = {} };
+        const l: Interval(T).Endpoint = if (left) .{ .open = 1 } else .{ .closed = 1 };
+        const r: Interval(T).Endpoint = if (right) .{ .open = 3 } else .{ .closed = 3 };
+        const i: Interval(T).Endpoint = .{ .infinity = {} };
         try runTest(T, l, r);
         try runTest(T, i, r);
         try runTest(T, l, i);
@@ -128,13 +128,13 @@ test "contains: open open" {
 }
 
 test "contains: u8 edge" {
-    const a = Interval(u8){ .from = .{ .Closed = 0 }, .to = .{ .Closed = 255 } };
+    const a = Interval(u8){ .from = .{ .closed = 0 }, .to = .{ .closed = 255 } };
     try std.testing.expect(a.contains(0));
     try std.testing.expect(a.contains(1));
     try std.testing.expect(a.contains(254));
     try std.testing.expect(a.contains(255));
 
-    const b = Interval(u8){ .from = .{ .Open = 0 }, .to = .{ .Open = 255 } };
+    const b = Interval(u8){ .from = .{ .open = 0 }, .to = .{ .open = 255 } };
     try std.testing.expect(!b.contains(0));
     try std.testing.expect(b.contains(1));
     try std.testing.expect(b.contains(254));
@@ -142,13 +142,13 @@ test "contains: u8 edge" {
 }
 
 test "contains: i8 edge" {
-    const a = Interval(i8){ .from = .{ .Closed = -128 }, .to = .{ .Closed = 127 } };
+    const a = Interval(i8){ .from = .{ .closed = -128 }, .to = .{ .closed = 127 } };
     try std.testing.expect(a.contains(-128));
     try std.testing.expect(a.contains(-127));
     try std.testing.expect(a.contains(126));
     try std.testing.expect(a.contains(127));
 
-    const b = Interval(i8){ .from = .{ .Open = -128 }, .to = .{ .Open = 127 } };
+    const b = Interval(i8){ .from = .{ .open = -128 }, .to = .{ .open = 127 } };
     try std.testing.expect(!b.contains(-128));
     try std.testing.expect(b.contains(-127));
     try std.testing.expect(b.contains(126));
@@ -156,58 +156,58 @@ test "contains: i8 edge" {
 }
 
 test "contains: u0 edge" {
-    const a = Interval(u0){ .from = .{ .Closed = 0 }, .to = .{ .Closed = 0 } };
+    const a = Interval(u0){ .from = .{ .closed = 0 }, .to = .{ .closed = 0 } };
     try std.testing.expect(a.contains(0));
 
-    const b = Interval(u0){ .from = .{ .Open = 0 }, .to = .{ .Closed = 0 } };
+    const b = Interval(u0){ .from = .{ .open = 0 }, .to = .{ .closed = 0 } };
     try std.testing.expect(!b.contains(0));
 
-    const c = Interval(u0){ .from = .{ .Closed = 0 }, .to = .{ .Open = 0 } };
+    const c = Interval(u0){ .from = .{ .closed = 0 }, .to = .{ .open = 0 } };
     try std.testing.expect(!c.contains(0));
 
-    const d = Interval(u0){ .from = .{ .Open = 0 }, .to = .{ .Open = 0 } };
+    const d = Interval(u0){ .from = .{ .open = 0 }, .to = .{ .open = 0 } };
     try std.testing.expect(!d.contains(0));
 }
 
 test "contains: u1 edge" {
-    const a = Interval(u1){ .from = .{ .Closed = 0 }, .to = .{ .Closed = 1 } };
+    const a = Interval(u1){ .from = .{ .closed = 0 }, .to = .{ .closed = 1 } };
     try std.testing.expect(a.contains(0));
     try std.testing.expect(a.contains(1));
 
-    const b = Interval(u1){ .from = .{ .Open = 0 }, .to = .{ .Closed = 1 } };
+    const b = Interval(u1){ .from = .{ .open = 0 }, .to = .{ .closed = 1 } };
     try std.testing.expect(!b.contains(0));
     try std.testing.expect(b.contains(1));
 
-    const c = Interval(u1){ .from = .{ .Closed = 0 }, .to = .{ .Open = 1 } };
+    const c = Interval(u1){ .from = .{ .closed = 0 }, .to = .{ .open = 1 } };
     try std.testing.expect(c.contains(0));
     try std.testing.expect(!c.contains(1));
 
-    const d = Interval(u1){ .from = .{ .Open = 0 }, .to = .{ .Open = 1 } };
+    const d = Interval(u1){ .from = .{ .open = 0 }, .to = .{ .open = 1 } };
     try std.testing.expect(!d.contains(0));
     try std.testing.expect(!d.contains(1));
 }
 
 test "contains: i1 edge" {
-    const a = Interval(i1){ .from = .{ .Closed = -1 }, .to = .{ .Closed = 0 } };
+    const a = Interval(i1){ .from = .{ .closed = -1 }, .to = .{ .closed = 0 } };
     try std.testing.expect(a.contains(-1));
     try std.testing.expect(a.contains(0));
 
-    const b = Interval(i1){ .from = .{ .Open = -1 }, .to = .{ .Closed = 0 } };
+    const b = Interval(i1){ .from = .{ .open = -1 }, .to = .{ .closed = 0 } };
     try std.testing.expect(!b.contains(-1));
     try std.testing.expect(b.contains(0));
 
-    const c = Interval(i1){ .from = .{ .Closed = -1 }, .to = .{ .Open = 0 } };
+    const c = Interval(i1){ .from = .{ .closed = -1 }, .to = .{ .open = 0 } };
     try std.testing.expect(c.contains(-1));
     try std.testing.expect(!c.contains(0));
 
-    const d = Interval(i1){ .from = .{ .Open = -1 }, .to = .{ .Open = 0 } };
+    const d = Interval(i1){ .from = .{ .open = -1 }, .to = .{ .open = 0 } };
     try std.testing.expect(!d.contains(-1));
     try std.testing.expect(!d.contains(0));
 }
 
 test "eql" {
-    const a = Interval(i1){ .from = .{ .Closed = -1 }, .to = .{ .Closed = 0 } };
-    const b = Interval(i1){ .from = .{ .Closed = -1 }, .to = .{ .Closed = 0 } };
+    const a = Interval(i1){ .from = .{ .closed = -1 }, .to = .{ .closed = 0 } };
+    const b = Interval(i1){ .from = .{ .closed = -1 }, .to = .{ .closed = 0 } };
 
     try std.testing.expect(a.eql(b));
 }
