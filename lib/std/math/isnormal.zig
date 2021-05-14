612ad779bb2df8e7e15ab20dd3df3a2900ff82f0
viri <viridianmeow@gmail.com>
2021-05-14 14:15:53 -0600

    std: rework math.scalbn (#8733)
    * std: fix overflow in math.scalbn32
    
    * std: rewrite math.scalbn to be generic
    
    * std: support f128 in math.isNormal
    
    * std: enable f128 tests in math.scalbn

--- lib/std/math/isnormal.zig
+++ lib/std/math/isnormal.zig
@@ -14,15 +14,19 @@ pub fn isNormal(x: anytype) bool {
     switch (T) {
         f16 => {
             const bits = @bitCast(u16, x);
-            return (bits + 1024) & 0x7FFF >= 2048;
+            return (bits + (1 << 10)) & (maxInt(u16) >> 1) >= (1 << 11);
         },
         f32 => {
             const bits = @bitCast(u32, x);
-            return (bits + 0x00800000) & 0x7FFFFFFF >= 0x01000000;
+            return (bits + (1 << 23)) & (maxInt(u32) >> 1) >= (1 << 24);
         },
         f64 => {
             const bits = @bitCast(u64, x);
             return (bits + (1 << 52)) & (maxInt(u64) >> 1) >= (1 << 53);
+        },
+        f128 => {
+            const bits = @bitCast(u128, x);
+            return (bits + (1 << 112)) & (maxInt(u128) >> 1) >= (1 << 113);
         },
         else => {
             @compileError("isNormal not implemented for " ++ @typeName(T));
@@ -34,10 +38,13 @@ test "math.isNormal" {
     try expect(!isNormal(math.nan(f16)));
     try expect(!isNormal(math.nan(f32)));
     try expect(!isNormal(math.nan(f64)));
+    try expect(!isNormal(math.nan(f128)));
     try expect(!isNormal(@as(f16, 0)));
     try expect(!isNormal(@as(f32, 0)));
     try expect(!isNormal(@as(f64, 0)));
+    try expect(!isNormal(@as(f128, 0)));
     try expect(isNormal(@as(f16, 1.0)));
     try expect(isNormal(@as(f32, 1.0)));
     try expect(isNormal(@as(f64, 1.0)));
+    try expect(isNormal(@as(f128, 1.0)));
 }
