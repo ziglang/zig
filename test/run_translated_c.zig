1273bc277f7274683db5041d8b41d91903e3045e
Evan Haas <evan@lagerdata.com>
2021-05-18 21:59:45 -0700

    translate-c: add support for __cleanup__ attribute
    Use a `defer` statement to implement the C __cleanup__ attribute.
    
    See https://gcc.gnu.org/onlinedocs/gcc/Common-Variable-Attributes.html

--- test/run_translated_c.zig
+++ test/run_translated_c.zig
@@ -1490,4 +1490,22 @@ pub fn addCases(cases: *tests.RunTranslatedCContext) void {
         \\    return 0;
         \\}
     , "");
+
+    cases.add("__cleanup__ attribute",
+        \\#include <stdlib.h>
+        \\static int cleanup_count = 0;
+        \\void clean_up(int *final_value) {
+        \\    if (*final_value != cleanup_count++) abort();
+        \\}
+        \\void doit(void) {
+        \\    int a __attribute__ ((__cleanup__(clean_up))) __attribute__ ((unused)) = 2;
+        \\    int b __attribute__ ((__cleanup__(clean_up))) __attribute__ ((unused)) = 1;
+        \\    int c __attribute__ ((__cleanup__(clean_up))) __attribute__ ((unused)) = 0;
+        \\}
+        \\int main(void) {
+        \\    doit();
+        \\    if (cleanup_count != 3) abort();
+        \\    return 0;
+        \\}
+    , "");
 }
