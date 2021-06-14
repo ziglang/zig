8c51682862f33359235183c343db4b8467487fa6
d18g <gootvilig.davidhai@gmail.com>
2021-06-14 15:55:44 +0300

    Adding a trailing comma
    Co-authored-by: LemonBoy <LemonBoy@users.noreply.github.com>

--- lib/std/target/x86.zig
+++ lib/std/target/x86.zig
@@ -2241,7 +2241,7 @@ pub const cpu = struct {
             .cx8,
             .slow_unaligned_mem_16,
             .vzeroupper,
-            .soft_float
+            .soft_float,
         }),
     };
     pub const nehalem = CpuModel{
