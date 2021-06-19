fbdc5154184b0752175dcafc5bfdc4ea6a0cfebf
Jakub Konka <kubkon@jakubkonka.com>
2021-06-19 10:45:56 +0200

    link: add basic TAPI parser for linkers
    Parser uses kubkon/zig-yaml gitrev c3eae1e40a02aedd44ad1171e5c8b259896cbda0

---
+++ src/link/tapi.zig
@@ -0,0 +1,67 @@
+const std = @import("std");
+const fs = std.fs;
+const mem = std.mem;
+const log = std.log.scoped(.tapi);
+
+const Allocator = mem.Allocator;
+const Yaml = @import("tapi/yaml.zig").Yaml;
+
+pub const LibStub = struct {
+    /// Underlying memory for stub's contents.
+    yaml: Yaml,
+
+    /// Typed contents of the tbd file.
+    inner: []Tbd,
+
+    const Tbd = struct {
+        tbd_version: u3,
+        targets: []const []const u8,
+        uuids: []const struct {
+            target: []const u8,
+            value: []const u8,
+        },
+        install_name: []const u8,
+        current_version: union(enum) {
+            string: []const u8,
+            int: u32,
+        },
+        reexported_libraries: ?[]const struct {
+            targets: []const []const u8,
+            libraries: []const []const u8,
+        },
+        parent_umbrella: ?[]const struct {
+            targets: []const []const u8,
+            umbrella: []const u8,
+        },
+        exports: []const struct {
+            targets: []const []const u8,
+            symbols: []const []const u8,
+        },
+        allowable_clients: ?[]const struct {
+            targets: []const []const u8,
+            clients: []const []const u8,
+        },
+        objc_classes: ?[]const []const u8,
+    };
+
+    pub fn loadFromFile(allocator: *Allocator, file_path: []const u8) !LibStub {
+        const file = try fs.cwd().openFile(file_path, .{});
+        defer file.close();
+
+        const source = try file.readToEndAlloc(allocator, std.math.maxInt(u32));
+        defer allocator.free(source);
+
+        var lib_stub = LibStub{
+            .yaml = try Yaml.load(allocator, source),
+            .inner = undefined,
+        };
+
+        lib_stub.inner = try lib_stub.yaml.parse([]Tbd);
+
+        return lib_stub;
+    }
+
+    pub fn deinit(self: *LibStub) void {
+        self.yaml.deinit();
+    }
+};
