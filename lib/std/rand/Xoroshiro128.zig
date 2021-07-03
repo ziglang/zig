3cb07d6e5b65299f376b657f0ee15ee5d5a25823
leesongun@naver.com <leesongun@naver.com>
2021-07-03 23:49:15 +0900

    Update Xoroshiro128 parameters

--- lib/std/rand/Xoroshiro128.zig
+++ lib/std/rand/Xoroshiro128.zig
@@ -33,8 +33,8 @@ fn next(self: *Xoroshiro128) u64 {
     const r = s0 +% s1;
 
     s1 ^= s0;
-    self.s[0] = math.rotl(u64, s0, @as(u8, 55)) ^ s1 ^ (s1 << 14);
-    self.s[1] = math.rotl(u64, s1, @as(u8, 36));
+    self.s[0] = math.rotl(u64, s0, @as(u8, 24)) ^ s1 ^ (s1 << 16);
+    self.s[1] = math.rotl(u64, s1, @as(u8, 37));
 
     return r;
 }
@@ -45,8 +45,8 @@ fn jump(self: *Xoroshiro128) void {
     var s1: u64 = 0;
 
     const table = [_]u64{
-        0xbeac0467eba5facb,
-        0xd86b048b86aa9922,
+        0xdf900294d8f554a5,
+        0x170865df4b3201fc,
     };
 
     inline for (table) |entry| {
@@ -105,11 +105,11 @@ test "xoroshiro sequence" {
 
     const seq1 = [_]u64{
         0xb0ba0da5bb600397,
-        0x18a08afde614dccc,
-        0xa2635b956a31b929,
-        0xabe633c971efa045,
-        0x9ac19f9706ca3cac,
-        0xf62b426578c1e3fb,
+        0x81001a1198a3c2d6,
+        0xac1d84b223f779f1,
+        0xebfc3dcd3497f9d1,
+        0xc2e2ebbeaf498205,
+        0x4c5850efa7c314cd,
     };
 
     for (seq1) |s| {
@@ -119,12 +119,12 @@ test "xoroshiro sequence" {
     r.jump();
 
     const seq2 = [_]u64{
-        0x95344a13556d3e22,
-        0xb4fb32dafa4d00df,
-        0xb2011d9ccdcfe2dd,
-        0x05679a9b2119b908,
-        0xa860a1da7c9cd8a0,
-        0x658a96efe3f86550,
+        0x2c172522b78cc43c,
+        0x4911df6309f1fb47,
+        0xfaf678fd1b4e4317,
+        0xfdb1577c26c1aa96,
+        0x7ed7faeb0d2abf2a,
+        0x8144aa8853fb6013,
     };
 
     for (seq2) |s| {
@@ -139,11 +139,11 @@ test "xoroshiro fill" {
 
     const seq = [_]u64{
         0xb0ba0da5bb600397,
-        0x18a08afde614dccc,
-        0xa2635b956a31b929,
-        0xabe633c971efa045,
-        0x9ac19f9706ca3cac,
-        0xf62b426578c1e3fb,
+        0x81001a1198a3c2d6,
+        0xac1d84b223f779f1,
+        0xebfc3dcd3497f9d1,
+        0xc2e2ebbeaf498205,
+        0x4c5850efa7c314cd,
     };
 
     for (seq) |s| {
