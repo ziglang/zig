e1ff3690e061e7f2b7c8834d8dbff5b105e2f390
Michael Dusan <michael.dusan@gmail.com>
2021-07-19 13:28:03 -0400

    dragonfly: port libc++
    Used https://github.com/DragonFlyBSD/DPorts/tree/master/devel/libc++/files
    for hints as to which patches were needed.

--- lib/libcxx/src/locale.cpp
+++ lib/libcxx/src/locale.cpp
@@ -1133,7 +1133,7 @@ ctype<char>::classic_table()  _NOEXCEPT
 const ctype<char>::mask*
 ctype<char>::classic_table()  _NOEXCEPT
 {
-#if defined(__APPLE__) || defined(__FreeBSD__)
+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__DragonFly__)
     return _DefaultRuneLocale.__runetype;
 #elif defined(__NetBSD__)
     return _C_ctype_tab_ + 1;
