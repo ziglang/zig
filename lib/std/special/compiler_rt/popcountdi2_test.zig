1ea650bb75e009bb37b5bd4e896a90e2a86f8583
Jan Philipp Hafer <jan.hafer@rwth-aachen.de>
2021-09-03 01:12:48 +0200

    compiler_rt: add __popcountsi2, __popcountdi2 and __popcountti2
    - apply simpler approach than LLVM for __popcountdi2
      taken from The Art of Computer Programming and generalized
    - rename popcountdi2.zig to popcount.zig
    - test cases derived from popcountdi2_test.zig
    - tests: compare naive approach 10_000 times with
      random numbers created from naive seed 42
    
        See #1290

--- lib/std/special/compiler_rt/popcountdi2_test.zig
+++ lib/std/special/compiler_rt/popcountdi2_test.zig
@@ -1,27 +1,34 @@
-const __popcountdi2 = @import("popcountdi2.zig").__popcountdi2;
+const popcount = @import("popcount.zig");
 const testing = @import("std").testing;
 
-fn naive_popcount(a_param: i64) i32 {
-    var a = a_param;
+fn popcountdi2Naive(a: i64) i32 {
+    var x = a;
     var r: i32 = 0;
-    while (a != 0) : (a = @bitCast(i64, @bitCast(u64, a) >> 1)) {
-        r += @intCast(i32, a & 1);
+    while (x != 0) : (x = @bitCast(i64, @bitCast(u64, x) >> 1)) {
+        r += @intCast(i32, x & 1);
     }
     return r;
 }
 
 fn test__popcountdi2(a: i64) !void {
-    const x = __popcountdi2(a);
-    const expected = naive_popcount(a);
-    try testing.expect(expected == x);
+    const x = popcount.__popcountdi2(a);
+    const expected = popcountdi2Naive(a);
+    try testing.expectEqual(expected, x);
 }
 
 test "popcountdi2" {
     try test__popcountdi2(0);
     try test__popcountdi2(1);
     try test__popcountdi2(2);
-    try test__popcountdi2(@bitCast(i64, @as(u64, 0xFFFFFFFFFFFFFFFD)));
-    try test__popcountdi2(@bitCast(i64, @as(u64, 0xFFFFFFFFFFFFFFFE)));
-    try test__popcountdi2(@bitCast(i64, @as(u64, 0xFFFFFFFFFFFFFFFF)));
-    // TODO some fuzz testing
+    try test__popcountdi2(@bitCast(i64, @as(u64, 0xffffffff_fffffffd)));
+    try test__popcountdi2(@bitCast(i64, @as(u64, 0xffffffff_fffffffe)));
+    try test__popcountdi2(@bitCast(i64, @as(u64, 0xffffffff_ffffffff)));
+
+    const RndGen = @import("std").rand.DefaultPrng;
+    var rnd = RndGen.init(42);
+    var i: u32 = 0;
+    while (i < 10_000) : (i += 1) {
+        var rand_num = rnd.random().int(i64);
+        try test__popcountdi2(rand_num);
+    }
 }
