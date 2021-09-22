e14fcd60cb11d731ca19f97e5b0b6247aa7cf07b
Coleman Broaddus <coleman.broaddus@gmail.com>
2021-09-22 05:09:16 -0400

    FIX resize() for non u8 element types. (#9806)

--- lib/std/mem.zig
+++ lib/std/mem.zig
@@ -147,6 +147,46 @@ test "mem.Allocator basics" {
     try testing.expectError(error.OutOfMemory, failAllocator.allocSentinel(u8, 1, 0));
 }
 
+test "Allocator.resize" {
+    const primitiveIntTypes = .{
+        i8,
+        u8,
+        i16,
+        u16,
+        i32,
+        u32,
+        i64,
+        u64,
+        i128,
+        u128,
+        isize,
+        usize,
+    };
+    inline for (primitiveIntTypes) |T| {
+        var values = try testing.allocator.alloc(T, 100);
+        defer testing.allocator.free(values);
+
+        for (values) |*v, i| v.* = @intCast(T, i);
+        values = try testing.allocator.resize(values, values.len + 10);
+        try testing.expect(values.len == 110);
+    }
+
+    const primitiveFloatTypes = .{
+        f16,
+        f32,
+        f64,
+        f128,
+    };
+    inline for (primitiveFloatTypes) |T| {
+        var values = try testing.allocator.alloc(T, 100);
+        defer testing.allocator.free(values);
+
+        for (values) |*v, i| v.* = @intToFloat(T, i);
+        values = try testing.allocator.resize(values, values.len + 10);
+        try testing.expect(values.len == 110);
+    }
+}
+
 /// Copy all of source into dest at position 0.
 /// dest.len must be >= source.len.
 /// If the slices overlap, dest.ptr must be <= src.ptr.
