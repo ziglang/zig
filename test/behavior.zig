59ca45af9a664b978f03bfa8a5d36014bd1f1aa9
Daniele Cocca <daniele.cocca@gmail.com>
2021-11-02 01:08:29 +0000

    CBE: mark bugs/6850.zig tests as passing

--- test/behavior.zig
+++ test/behavior.zig
@@ -14,6 +14,7 @@ test {
     _ = @import("behavior/bugs/4560.zig");
     _ = @import("behavior/bugs/4769_a.zig");
     _ = @import("behavior/bugs/4769_b.zig");
+    _ = @import("behavior/bugs/6850.zig");
     _ = @import("behavior/call.zig");
     _ = @import("behavior/defer.zig");
     _ = @import("behavior/enum.zig");
@@ -42,7 +43,6 @@ test {
         _ = @import("behavior/bugs/2692.zig");
         _ = @import("behavior/bugs/3112.zig");
         _ = @import("behavior/bugs/3586.zig");
-        _ = @import("behavior/bugs/6850.zig");
         _ = @import("behavior/cast.zig");
         _ = @import("behavior/error.zig");
         _ = @import("behavior/eval.zig");
