90546731ab028bbf2edf5f448f37733271e8a65c
Lewis Gaul <lewis.gaul@gmail.com>
2021-12-24 16:04:24 +0000

    Add infrastructure and tests for exp(), exp2()

---
+++ lib/std/math/test.zig
@@ -0,0 +1,101 @@
+const std = @import("../std.zig");
+const print = std.debug.print;
+
+var verbose = false;
+
+// Include all tests.
+comptime {
+    _ = @import("test/exp.zig");
+    _ = @import("test/exp2.zig");
+}
+
+// Used for the type signature.
+fn genericFloatInFloatOut(x: anytype) @TypeOf(x) {
+    return x;
+}
+
+pub fn Testcase(
+    comptime func: @TypeOf(genericFloatInFloatOut),
+    comptime name: []const u8,
+    comptime float_type: type,
+) type {
+    if (@typeInfo(float_type) != .Float) @compileError("Expected float type");
+
+    return struct {
+        const F: type = float_type;
+
+        input: F,
+        exp_output: F,
+
+        const Self = @This();
+
+        const bits = std.meta.bitCount(F);
+        const U: type = std.meta.Int(.unsigned, bits);
+
+        pub fn init(input: F, exp_output: F) Self {
+            return .{ .input = input, .exp_output = exp_output };
+        }
+
+        pub fn run(tc: Self) !void {
+            const hex_bits_fmt_size = comptime std.fmt.comptimePrint("{d}", .{bits / 4});
+            const hex_float_fmt_size = switch (bits) {
+                16 => "10",
+                32 => "16",
+                64 => "24",
+                128 => "40",
+                else => unreachable,
+            };
+            const input_bits = @bitCast(U, tc.input);
+            if (verbose) {
+                print(
+                    " IN:  0x{X:0>" ++ hex_bits_fmt_size ++ "}  " ++
+                        "{[1]x:<" ++ hex_float_fmt_size ++ "}  {[1]e}\n",
+                    .{ input_bits, tc.input },
+                );
+            }
+            const output = func(tc.input);
+            const output_bits = @bitCast(U, output);
+            if (verbose) {
+                print(
+                    "OUT:  0x{X:0>" ++ hex_bits_fmt_size ++ "}  " ++
+                        "{[1]x:<" ++ hex_float_fmt_size ++ "}  {[1]e}\n",
+                    .{ output_bits, output },
+                );
+            }
+            const exp_output_bits = @bitCast(U, tc.exp_output);
+            // Compare bits rather than values so that NaN compares correctly.
+            if (output_bits != exp_output_bits) {
+                if (verbose) {
+                    print(
+                        "EXP:  0x{X:0>" ++ hex_bits_fmt_size ++ "}  " ++
+                            "{[1]x:<" ++ hex_float_fmt_size ++ "}  {[1]e}\n",
+                        .{ exp_output_bits, tc.exp_output },
+                    );
+                }
+                print(
+                    "FAILURE: expected {s}({x})->{x}, got {x} ({d}-bit)\n",
+                    .{ name, tc.input, tc.exp_output, output, bits },
+                );
+                return error.TestExpectedEqual;
+            }
+        }
+    };
+}
+
+pub fn runTests(tests: anytype) !void {
+    var failures: usize = 0;
+    print("\n", .{});
+    for (tests) |tc| {
+        tc.run() catch {
+            failures += 1;
+        };
+        if (verbose) print("\n", .{});
+    }
+    if (verbose) {
+        print(
+            "Subtest summary: {d} passed; {d} failed\n",
+            .{ tests.len - failures, failures },
+        );
+    }
+    if (failures > 0) return error.Failure;
+}
---
+++ lib/std/math/test/exp2.zig
@@ -0,0 +1,131 @@
+const math = @import("../../math.zig");
+const Testcase = @import("../test.zig").Testcase;
+const runTests = @import("../test.zig").runTests;
+const inf32 = math.inf_f32;
+const inf64 = math.inf_f64;
+const nan32 = math.nan_f32;
+const nan64 = math.nan_f64;
+
+const Tc32 = Testcase(math.exp2, "exp2", f32);
+const tc32 = Tc32.init;
+
+const Tc64 = Testcase(math.exp2, "exp2", f64);
+const tc64 = Tc64.init;
+
+test "math.exp2_32() sanity" {
+    const cases = [_]Tc32{
+        // zig fmt: off
+        tc32(-0x1.0223a0p+3, 0x1.e8d134p-9),
+        tc32( 0x1.161868p+2, 0x1.453672p+4),
+        tc32(-0x1.0c34b4p+3, 0x1.890ca0p-9),
+        tc32(-0x1.a206f0p+2, 0x1.622d4ep-7),
+        tc32( 0x1.288bbcp+3, 0x1.340ecep+9),
+        tc32( 0x1.52efd0p-1, 0x1.950eeep+0),
+        tc32(-0x1.a05cc8p-2, 0x1.824056p-1),
+        tc32( 0x1.1f9efap-1, 0x1.79dfa2p+0),
+        tc32( 0x1.8c5db0p-1, 0x1.b5ceacp+0),
+        tc32(-0x1.5b86eap-1, 0x1.3fd8bap-1),
+        // zig fmt: on
+    };
+    try runTests(cases);
+}
+
+test "math.exp2_32() special" {
+    const cases = [_]Tc32{
+        // zig fmt: off
+        tc32( 0x0p+0, 0x1p+0),
+        tc32(-0x0p+0, 0x1p+0),
+        tc32( 0x1p+0, 0x1p+1),
+        tc32(-0x1p+0, 0x1p-1),
+        tc32( inf32,  inf32 ),
+        tc32(-inf32,  0x0p+0),
+        tc32( nan32,  nan32 ),
+        tc32(-nan32,  nan32 ),
+        tc32(@bitCast(f32, @as(u32, 0x7ff01234)), nan32),
+        tc32(@bitCast(f32, @as(u32, 0xfff01234)), nan32),
+        // zig fmt: on
+    };
+    try runTests(cases);
+}
+
+test "math.exp2_32() boundary" {
+    const cases = [_]Tc32{
+        // zig fmt: off
+        tc32( 0x1.fffffep+6, 0x1.ffff4ep+127), // The last value before the exp gets infinite
+        tc32( 0x1.ff999ap+6, 0x1.ddb6a2p+127),
+        tc32( 0x1p+7,        inf32          ), // The first value that gives infinite exp
+        tc32( 0x1.003334p+7, inf32          ),
+        tc32(-0x1.2bccccp+7, 0x1p-149       ), // The last value before the exp flushes to zero
+        tc32(-0x1.2ap+7,     0x1p-149       ),
+        tc32(-0x1.2cp+7,     0x0p+0         ), // The first value at which the exp flushes to zero
+        tc32(-0x1.2c3334p+7, 0x0p+0         ),
+        tc32(-0x1.f8p+6,     0x1p-126       ), // The last value before the exp flushes to subnormal
+        tc32(-0x1.f80002p+6, 0x1.ffff5p-127 ), // The first value for which exp flushes to subnormal
+        tc32(-0x1.fcp+6,     0x1p-127       ),
+        tc32( 0x1p-149,      0x1p+0         ), // Very close to zero
+        // zig fmt: on
+    };
+    try runTests(cases);
+}
+
+test "math.exp2_64() sanity" {
+    const cases = [_]Tc64{
+        // zig fmt: off
+        tc64(-0x1.02239f3c6a8f1p+3, 0x1.e8d13c396f452p-9),
+        tc64( 0x1.161868e18bc67p+2, 0x1.4536746bb6f12p+4),
+        tc64(-0x1.0c34b3e01e6e7p+3, 0x1.890ca0c00b9a2p-9),
+        tc64(-0x1.a206f0a19dcc4p+2, 0x1.622d4b0ebc6c1p-7),
+        tc64( 0x1.288bbb0d6a1e6p+3, 0x1.340ec7f3e607ep+9),
+        tc64( 0x1.52efd0cd80497p-1, 0x1.950eef4bc5451p+0),
+        tc64(-0x1.a05cc754481d1p-2, 0x1.824056efc687cp-1),
+        tc64( 0x1.1f9ef934745cbp-1, 0x1.79dfa14ab121ep+0),
+        tc64( 0x1.8c5db097f7442p-1, 0x1.b5cead2247372p+0),
+        tc64(-0x1.5b86ea8118a0ep-1, 0x1.3fd8ba33216b9p-1),
+        // zig fmt: on
+    };
+    try runTests(cases);
+}
+
+test "math.exp2_64() special" {
+    const cases = [_]Tc64{
+        // zig fmt: off
+        tc64( 0x0p+0, 0x1p+0),
+        tc64(-0x0p+0, 0x1p+0),
+        tc64( 0x1p+0, 0x1p+1),
+        tc64(-0x1p+0, 0x1p-1),
+        tc64( inf64,  inf64 ),
+        tc64(-inf64,  0x0p+0),
+        tc64( nan64,  nan64 ),
+        tc64(-nan64,  nan64 ),
+        tc64(@bitCast(f64, @as(u64, 0x7ff0123400000000)), nan64),
+        tc64(@bitCast(f64, @as(u64, 0xfff0123400000000)), nan64),
+        // zig fmt: on
+    };
+    try runTests(cases);
+}
+
+test "math.exp2_64() boundary" {
+    const cases = [_]Tc64{
+        // zig fmt: off
+        tc64( 0x1.fffffffffffffp+9,  0x1.ffffffffffd3ap+1023), // The last value before the exp gets infinite
+        tc64( 0x1.fff3333333333p+9,  0x1.ddb680117aa8ep+1023),
+        tc64( 0x1p+10,               inf64                  ), // The first value that gives infinite exp
+        tc64( 0x1.0006666666666p+10, inf64                  ),
+        tc64(-0x1.0cbffffffffffp+10, 0x1p-1074              ), // The last value before the exp flushes to zero
+        tc64(-0x1.0c8p+10,           0x1p-1074              ),
+        tc64(-0x1.0cap+10,           0x1p-1074              ),
+        tc64(-0x1.0ccp+10,           0x0p+0                 ), // The first value at which the exp flushes to zero
+        tc64(-0x1p+11,               0x0p+0                 ),
+        tc64(-0x1.ffp+9,             0x1p-1022              ), // The last value before the exp flushes to subnormal
+        tc64(-0x1.fef3333333333p+9,  0x1.125fbee2506b0p-1022),
+        tc64(-0x1.ff00000000001p+9,  0x1.ffffffffffd3ap-1023), // The first value for which exp flushes to subnormal
+        tc64(-0x1.ff0cccccccccdp+9,  0x1.ddb680117aa8ep-1023),
+        tc64(-0x1.ff4p+9,            0x1.6a09e667f3bccp-1023),
+        tc64(-0x1.ff8p+9,            0x1p-1023              ),
+        tc64(-0x1.ffcp+9,            0x1.6a09e667f3bccp-1024),
+        tc64(-0x1p+10,               0x1p-1024              ),
+        tc64( 0x1p-1074,             0x1p+0                 ), // Very close to zero
+        // zig fmt: on
+    };
+    try runTests(cases);
+}
