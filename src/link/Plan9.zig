ab400ad62406e5ed7660d01d7065dc14081b10f6
Jacob G-W <jacoblevgw@gmail.com>
2022-01-06 22:47:27 -0500

    Plan9: implement getDeclVAddr

--- src/link/Plan9.zig
+++ src/link/Plan9.zig
@@ -740,3 +740,26 @@ pub fn allocateDeclIndexes(self: *Plan9, decl: *Module.Decl) !void {
     _ = self;
     _ = decl;
 }
+pub fn getDeclVAddr(self: *Plan9, decl: *const Module.Decl) u64 {
+    if (decl.ty.zigTypeTag() == .Fn) {
+        var start = self.bases.text;
+        var it_file = self.fn_decl_table.iterator();
+        while (it_file.next()) |fentry| {
+            var symidx_and_submap = fentry.value_ptr;
+            var submap_it = symidx_and_submap.functions.iterator();
+            while (submap_it.next()) |entry| {
+                if (entry.key_ptr.* == decl) return start;
+                start += entry.value_ptr.code.len;
+            }
+        }
+        unreachable;
+    } else {
+        var start = self.bases.data + self.got_len * if (!self.sixtyfour_bit) @as(u32, 4) else 8;
+        var it = self.data_decl_table.iterator();
+        while (it.next()) |kv| {
+            if (decl == kv.key_ptr.*) return start;
+            start += kv.value_ptr.len;
+        }
+        unreachable;
+    }
+}
