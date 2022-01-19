6a42abc958f9dbb5651425ad9903084431736312
riverbl <94326797+riverbl@users.noreply.github.com>
2022-01-19 09:57:41 +0000

    Fix formatting in multi_array_list.zig

--- lib/std/multi_array_list.zig
+++ lib/std/multi_array_list.zig
@@ -639,16 +639,16 @@ test "0 sized struct field" {
     try testing.expectEqualSlices(f32, &[_]f32{}, list.items(.b));
 
     try list.append(ally, .{ .a = 0, .b = 42.0 });
-    try testing.expectEqualSlices(u0, &[_]u0{ 0 }, list.items(.a));
-    try testing.expectEqualSlices(f32, &[_]f32{ 42.0 }, list.items(.b));
+    try testing.expectEqualSlices(u0, &[_]u0{0}, list.items(.a));
+    try testing.expectEqualSlices(f32, &[_]f32{42.0}, list.items(.b));
 
     try list.insert(ally, 0, .{ .a = 0, .b = -1.0 });
     try testing.expectEqualSlices(u0, &[_]u0{ 0, 0 }, list.items(.a));
     try testing.expectEqualSlices(f32, &[_]f32{ -1.0, 42.0 }, list.items(.b));
 
     list.swapRemove(list.len - 1);
-    try testing.expectEqualSlices(u0, &[_]u0{ 0 }, list.items(.a));
-    try testing.expectEqualSlices(f32, &[_]f32{ -1.0 }, list.items(.b));
+    try testing.expectEqualSlices(u0, &[_]u0{0}, list.items(.a));
+    try testing.expectEqualSlices(f32, &[_]f32{-1.0}, list.items(.b));
 }
 
 test "0 sized struct" {
@@ -664,11 +664,11 @@ test "0 sized struct" {
     try testing.expectEqualSlices(u0, &[_]u0{}, list.items(.a));
 
     try list.append(ally, .{ .a = 0 });
-    try testing.expectEqualSlices(u0, &[_]u0{ 0 }, list.items(.a));
+    try testing.expectEqualSlices(u0, &[_]u0{0}, list.items(.a));
 
     try list.insert(ally, 0, .{ .a = 0 });
     try testing.expectEqualSlices(u0, &[_]u0{ 0, 0 }, list.items(.a));
 
     list.swapRemove(list.len - 1);
-    try testing.expectEqualSlices(u0, &[_]u0{ 0 }, list.items(.a));
+    try testing.expectEqualSlices(u0, &[_]u0{0}, list.items(.a));
 }
