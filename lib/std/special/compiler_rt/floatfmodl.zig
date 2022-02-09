112fa91ea1b059244b983f761f9d56a972e0cd62
Mateusz Radomski <radomski.main@protonmail.com>
2022-02-09 16:56:58 +0100

    Remove the union that does not work with stage2

--- lib/std/special/compiler_rt/floatfmodl.zig
+++ lib/std/special/compiler_rt/floatfmodl.zig
@@ -5,42 +5,29 @@ const std = @import("std");
 // Logic and flow heavily inspired by MUSL fmodl for 113 mantissa digits
 pub fn fmodl(a: f128, b: f128) callconv(.C) f128 {
     @setRuntimeSafety(builtin.is_test);
-    const ReinterpretUnion = packed union {
-        u64s: switch (builtin.cpu.arch.endian()) {
-            .Little => extern struct {
-                low: u64,
-                high: u64,
-            },
-            .Big => extern struct {
-                high: u64,
-                low: u64,
-            },
-        },
-
-        parts: switch (builtin.cpu.arch.endian()) {
-            .Little => extern struct {
-                mantissa_low: u64,
-                mantissa_mid: u32,
-                mantissa_high: u16,
-                exp_and_sign: u16,
-            },
-            .Big => extern struct {
-                exp_and_sign: u16,
-                mantissa_high: u16,
-                mantissa_mid: u32,
-                mantissa_low: u64,
-            },
-        },
-    };
-
     var amod = a;
     var bmod = b;
-    const aPtr = @ptrCast(*ReinterpretUnion, &amod);
-    const bPtr = @ptrCast(*ReinterpretUnion, &bmod);
+    const aPtr_u64 = @ptrCast([*]u64, &amod);
+    const bPtr_u64 = @ptrCast([*]u64, &bmod);
+    const aPtr_u16 = @ptrCast([*]u16, &amod);
+    const bPtr_u16 = @ptrCast([*]u16, &bmod);
 
-    const signA = aPtr.parts.exp_and_sign & 0x8000;
-    var expA = @intCast(i32, (aPtr.parts.exp_and_sign & 0x7fff));
-    var expB = bPtr.parts.exp_and_sign & 0x7fff;
+    const exp_and_sign_index = comptime switch (builtin.target.cpu.arch.endian()) {
+        .Little => 7,
+        .Big => 0,
+    };
+    const low_index = comptime switch (builtin.target.cpu.arch.endian()) {
+        .Little => 0,
+        .Big => 1,
+    };
+    const high_index = comptime switch (builtin.target.cpu.arch.endian()) {
+        .Little => 1,
+        .Big => 0,
+    };
+
+    const signA = aPtr_u16[exp_and_sign_index] & 0x8000;
+    var expA = @intCast(i32, (aPtr_u16[exp_and_sign_index] & 0x7fff));
+    var expB = bPtr_u16[exp_and_sign_index] & 0x7fff;
 
     // There are 3 cases where the answer is undefined, check for:
     //   - fmodl(val, 0)
@@ -57,8 +44,8 @@ pub fn fmodl(a: f128, b: f128) callconv(.C) f128 {
     }
 
     // Remove the sign from both
-    aPtr.parts.exp_and_sign = @bitCast(u16, @intCast(i16, expA));
-    bPtr.parts.exp_and_sign = @bitCast(u16, @intCast(i16, expB));
+    aPtr_u16[exp_and_sign_index] = @bitCast(u16, @intCast(i16, expA));
+    bPtr_u16[exp_and_sign_index] = @bitCast(u16, @intCast(i16, expB));
     if (amod <= bmod) {
         if (amod == bmod) {
             return 0 * a;
@@ -68,19 +55,19 @@ pub fn fmodl(a: f128, b: f128) callconv(.C) f128 {
 
     if (expA == 0) {
         amod *= 0x1p120;
-        expA = aPtr.parts.exp_and_sign -% 120;
+        expA = aPtr_u16[exp_and_sign_index] -% 120;
     }
 
     if (expB == 0) {
         bmod *= 0x1p120;
-        expB = bPtr.parts.exp_and_sign -% 120;
+        expB = bPtr_u16[exp_and_sign_index] -% 120;
     }
 
     // OR in extra non-stored mantissa digit
-    var highA: u64 = (aPtr.u64s.high & (std.math.maxInt(u64) >> 16)) | 1 << 48;
-    var highB: u64 = (bPtr.u64s.high & (std.math.maxInt(u64) >> 16)) | 1 << 48;
-    var lowA: u64 = aPtr.u64s.low;
-    var lowB: u64 = bPtr.u64s.low;
+    var highA: u64 = (aPtr_u64[high_index] & (std.math.maxInt(u64) >> 16)) | 1 << 48;
+    var highB: u64 = (bPtr_u64[high_index] & (std.math.maxInt(u64) >> 16)) | 1 << 48;
+    var lowA: u64 = aPtr_u64[low_index];
+    var lowB: u64 = bPtr_u64[low_index];
 
     while (expA > expB) : (expA -= 1) {
         var high = highA -% highB;
@@ -120,15 +107,15 @@ pub fn fmodl(a: f128, b: f128) callconv(.C) f128 {
     }
 
     // Overwrite the current amod with the values in highA and lowA
-    aPtr.u64s.high = highA;
-    aPtr.u64s.low = lowA;
+    aPtr_u64[high_index] = highA;
+    aPtr_u64[low_index] = lowA;
 
     // Combine the exponent with the sign, normalize if happend to be denormalized
     if (expA <= 0) {
-        aPtr.parts.exp_and_sign = @truncate(u16, @bitCast(u32, (expA +% 120))) | signA;
+        aPtr_u16[exp_and_sign_index] = @truncate(u16, @bitCast(u32, (expA +% 120))) | signA;
         amod *= 0x1p-120;
     } else {
-        aPtr.parts.exp_and_sign = @truncate(u16, @bitCast(u32, expA)) | signA;
+        aPtr_u16[exp_and_sign_index] = @truncate(u16, @bitCast(u32, expA)) | signA;
     }
 
     return amod;
