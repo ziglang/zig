09797d48c89d243755630cfe66e981dfd6e1f56e
Andrew Kelley <andrew@ziglang.org>
2022-02-14 13:03:48 -0700

    start the 0.9.2 release branch

--- build.zig
+++ build.zig
@@ -10,7 +10,7 @@ const fs = std.fs;
 const InstallDirectoryOptions = std.build.InstallDirectoryOptions;
 const assert = std.debug.assert;
 
-const zig_version = std.builtin.Version{ .major = 0, .minor = 9, .patch = 1 };
+const zig_version = std.builtin.Version{ .major = 0, .minor = 9, .patch = 2 };
 
 pub fn build(b: *Builder) !void {
     b.setPreferredReleaseMode(.ReleaseFast);
