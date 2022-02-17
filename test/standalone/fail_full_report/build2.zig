5aa35f62c391d34b92445388c5d645d87f9c4621
Andrew Kelley <andrew@ziglang.org>
2022-02-17 14:28:19 -0700

    Revert "reduce build error noise"
    This reverts commit baead472d7641bdd96130354bafadc1fb1ed223b.
    
    Let's go through the proposal process on this one. I want to push back
    on this. My position is that, at the very least, a full trace of command
    lines of sub-processes should be printed on failure, with the exception
    of opt-in flags such as `--prominent-compile-errors`.

--- test/standalone/fail_full_report/build2.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const Builder = std.build.Builder;
-
-pub fn build(b: *Builder) !void {
-    const bad_option = if (b.option(bool, "badoption", "Use this to emulator a bad build option")) |o| o else false;
-    if (bad_option)
-        std.build.fatalFullReport("got a bad build option!", .{});
-}
