8e4a8771f56f41a56264eb000ee347ddf7b71dc4
Jakub Konka <kubkon@jakubkonka.com>
2022-02-22 22:04:32 +0100

    Skip unsupported enum behavior tests

--- test/behavior/enum.zig
+++ test/behavior/enum.zig
@@ -987,6 +987,10 @@ test "enum literal casting to tagged union" {
 const Bar = enum { A, B, C, D };
 
 test "enum literal casting to error union with payload enum" {
+    if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest;
+    if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest;
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest;
+
     var bar: error{B}!Bar = undefined;
     bar = .B; // should never cast to the error set
 
