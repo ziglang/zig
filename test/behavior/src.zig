ab4d693cfc82465c42021ba6f18c65fdd5f969e6
Veikka Tuominen <git@vexu.eu>
2022-02-23 09:46:28 +0200

    re-enable `@src` behavior test

--- test/behavior/src.zig
+++ test/behavior/src.zig
@@ -1,21 +1,20 @@
-const std = @import("std");
-const builtin = @import("builtin");
-const expect = std.testing.expect;
-
-test "@src" {
-    // TODO why is this failing on stage1?
-    return error.SkipZigTest;
-
-    // try doTheTest();
-}
-
 fn doTheTest() !void {
-    const src = @src();
+    const src = @src(); // do not move
 
-    try expect(src.line == 9);
+    try expect(src.line == 2);
     try expect(src.column == 17);
     try expect(std.mem.endsWith(u8, src.fn_name, "doTheTest"));
     try expect(std.mem.endsWith(u8, src.file, "src.zig"));
     try expect(src.fn_name[src.fn_name.len] == 0);
     try expect(src.file[src.file.len] == 0);
+}
+
+const std = @import("std");
+const builtin = @import("builtin");
+const expect = std.testing.expect;
+
+test "@src" {
+    if (builtin.zig_backend != .stage1) return error.SkipZigTest;
+
+    try doTheTest();
 }
