6ac3f09344d251705f6e1a3cfa42bf07cf6a0f52
tecanec <asgera02@gmail.com>
2022-03-09 22:19:45 +0100

    Function renames, as per Snektron's recommendations

--- lib/std/simd.zig
+++ lib/std/simd.zig
@@ -50,15 +50,15 @@ pub fn VectorCount(comptime VectorType: type) type {
     return std.math.IntFittingRange(0, vectorLength(VectorType));
 }
 
-/// Returns a vector containing the integer sequence starting at 0.
-/// For example, countUp(i32, 8) will return a vector containing {0, 1, 2, 3, 4, 5, 6, 7}.
-pub fn countUp(comptime T: type, comptime len: usize) Vector(len, T) {
+/// Returns a vector containing the first 'len' integers in order from 0 to 'len'-1.
+/// For example, 'iota(i32, 8)' will return a vector containing '{0, 1, 2, 3, 4, 5, 6, 7}'.
+pub fn iota(comptime T: type, comptime len: usize) Vector(len, T) {
     var out: [len]T = undefined;
     for (out) |*element, i| {
         element.* = switch (@typeInfo(T)) {
             .Int => @intCast(T, i),
             .Float => @intToFloat(T, i),
-            else => @compileError("Can't use type " ++ @typeName(T) ++ " in countUp."),
+            else => @compileError("Can't use type " ++ @typeName(T) ++ " in iota."),
         };
     }
     return @as(Vector(len, T), out);
@@ -69,7 +69,7 @@ pub fn countUp(comptime T: type, comptime len: usize) Vector(len, T) {
 pub fn repeat(comptime len: usize, vec: anytype) Vector(len, std.meta.Child(@TypeOf(vec))) {
     const Child = std.meta.Child(@TypeOf(vec));
 
-    return @shuffle(Child, vec, undefined, countUp(i32, len) % @splat(len, @intCast(i32, vectorLength(@TypeOf(vec)))));
+    return @shuffle(Child, vec, undefined, iota(i32, len) % @splat(len, @intCast(i32, vectorLength(@TypeOf(vec)))));
 }
 
 /// Returns a vector containing all elements of the first vector followed by all elements of the second vector.
@@ -78,18 +78,18 @@ pub fn join(a: anytype, b: anytype) Vector(vectorLength(@TypeOf(a)) + vectorLeng
     const a_len = vectorLength(@TypeOf(a));
     const b_len = vectorLength(@TypeOf(b));
 
-    return @shuffle(Child, a, b, @as([a_len]i32, countUp(i32, a_len)) ++ @as([b_len]i32, ~countUp(i32, b_len)));
+    return @shuffle(Child, a, b, @as([a_len]i32, iota(i32, a_len)) ++ @as([b_len]i32, ~iota(i32, b_len)));
 }
 
 /// Returns a vector whose elements alternates between those of each input vector.
-/// For example, braid(.{[4]u32{11, 12, 13, 14}, [4]u32{21, 22, 23, 24}}) returns a vector containing {11, 21, 12, 22, 13, 23, 14, 24}.
-pub fn braid(vecs: anytype) Vector(vectorLength(@TypeOf(vecs[0])) * vecs.len, std.meta.Child(@TypeOf(vecs[0]))) {
-    // braid doesn't work on MIPS, for some reason.
+/// For example, interlace(.{[4]u32{11, 12, 13, 14}, [4]u32{21, 22, 23, 24}}) returns a vector containing {11, 21, 12, 22, 13, 23, 14, 24}.
+pub fn interlace(vecs: anytype) Vector(vectorLength(@TypeOf(vecs[0])) * vecs.len, std.meta.Child(@TypeOf(vecs[0]))) {
+    // interlace doesn't work on MIPS, for some reason.
     // Notes from earlier debug attempt:
     //  The indices are correct. The problem seems to be with the @shuffle builtin.
-    //  On MIPS, the test that braids small_base gives { 0, 2, 0, 0, 64, 255, 248, 200, 0, 0 }.
+    //  On MIPS, the test that interlaces small_base gives { 0, 2, 0, 0, 64, 255, 248, 200, 0, 0 }.
     //  Calling this with two inputs seems to work fine, but I'll let the compile error trigger for all inputs, just to be safe.
-    comptime if (builtin.cpu.arch.isMIPS()) @compileError("TODO: Find out why braid() doesn't work on MIPS");
+    comptime if (builtin.cpu.arch.isMIPS()) @compileError("TODO: Find out why interlace() doesn't work on MIPS");
 
     const VecType = @TypeOf(vecs[0]);
     const vecs_arr = @as([vecs.len]VecType, vecs);
@@ -100,43 +100,43 @@ pub fn braid(vecs: anytype) Vector(vectorLength(@TypeOf(vecs[0])) * vecs.len, st
     const a_vec_count = (1 + vecs_arr.len) >> 1;
     const b_vec_count = vecs_arr.len >> 1;
 
-    const a = braid(@ptrCast(*const [a_vec_count]VecType, vecs_arr[0..a_vec_count]).*);
-    const b = braid(@ptrCast(*const [b_vec_count]VecType, vecs_arr[a_vec_count..]).*);
+    const a = interlace(@ptrCast(*const [a_vec_count]VecType, vecs_arr[0..a_vec_count]).*);
+    const b = interlace(@ptrCast(*const [b_vec_count]VecType, vecs_arr[a_vec_count..]).*);
 
     const a_len = vectorLength(@TypeOf(a));
     const b_len = vectorLength(@TypeOf(b));
     const len = a_len + b_len;
 
     const indices = comptime blk: {
-        const count_up = countUp(i32, len);
+        const count_up = iota(i32, len);
         const cycle = @divFloor(count_up, @splat(len, @intCast(i32, vecs_arr.len)));
         const select_mask = repeat(len, join(@splat(a_vec_count, true), @splat(b_vec_count, false)));
         const a_indices = count_up - cycle * @splat(len, @intCast(i32, b_vec_count));
-        const b_indices = shiftElementsUp(count_up - cycle * @splat(len, @intCast(i32, a_vec_count)), a_vec_count, 0);
+        const b_indices = shiftElementsRight(count_up - cycle * @splat(len, @intCast(i32, a_vec_count)), a_vec_count, 0);
         break :blk @select(i32, select_mask, a_indices, ~b_indices);
     };
 
     return @shuffle(Child, a, b, indices);
 }
 
-/// The contents of braided is evenly split between vec_count vectors that are returned as an array. They "take turns",
-/// recieving one element from braided at a time.
-pub fn unbraid(
+/// The contents of interlaced is evenly split between vec_count vectors that are returned as an array. They "take turns",
+/// recieving one element from interlaced at a time.
+pub fn deinterlace(
     comptime vec_count: usize,
-    braided: anytype,
+    interlaced: anytype,
 ) [vec_count]Vector(
-    vectorLength(@TypeOf(braided)) / vec_count,
-    std.meta.Child(@TypeOf(braided)),
+    vectorLength(@TypeOf(interlaced)) / vec_count,
+    std.meta.Child(@TypeOf(interlaced)),
 ) {
-    const vec_len = vectorLength(@TypeOf(braided)) / vec_count;
-    const Child = std.meta.Child(@TypeOf(braided));
+    const vec_len = vectorLength(@TypeOf(interlaced)) / vec_count;
+    const Child = std.meta.Child(@TypeOf(interlaced));
 
     var out: [vec_count]Vector(vec_len, Child) = undefined;
 
     comptime var i: usize = 0; // for doesn't work, apparently.
     inline while (i < out.len) : (i += 1) {
-        const indices = comptime countUp(i32, vec_len) * @splat(vec_len, @intCast(i32, vec_count)) + @splat(vec_len, @intCast(i32, i));
-        out[i] = @shuffle(Child, braided, undefined, indices);
+        const indices = comptime iota(i32, vec_len) * @splat(vec_len, @intCast(i32, vec_count)) + @splat(vec_len, @intCast(i32, i));
+        out[i] = @shuffle(Child, interlaced, undefined, indices);
     }
 
     return out;
@@ -152,7 +152,7 @@ pub fn extract(
 
     std.debug.assert(@intCast(comptime_int, first) + @intCast(comptime_int, count) <= len);
 
-    return @shuffle(Child, vec, undefined, countUp(i32, count) + @splat(count, @intCast(i32, first)));
+    return @shuffle(Child, vec, undefined, iota(i32, count) + @splat(count, @intCast(i32, first)));
 }
 
 test "vector patterns" {
@@ -172,57 +172,57 @@ test "vector patterns" {
     try std.testing.expectEqual([2]u32{ 20, 30 }, extract(base, 1, 2));
 
     if (comptime !builtin.cpu.arch.isMIPS()) {
-        try std.testing.expectEqual([8]u32{ 10, 55, 20, 66, 30, 77, 40, 88 }, braid(.{ base, other_base }));
+        try std.testing.expectEqual([8]u32{ 10, 55, 20, 66, 30, 77, 40, 88 }, interlace(.{ base, other_base }));
 
-        const small_braid = braid(small_bases);
+        const small_braid = interlace(small_bases);
         try std.testing.expectEqual([10]u8{ 0, 2, 4, 6, 8, 1, 3, 5, 7, 9 }, small_braid);
-        try std.testing.expectEqual(small_bases, unbraid(small_bases.len, small_braid));
+        try std.testing.expectEqual(small_bases, deinterlace(small_bases.len, small_braid));
     }
 }
 
 /// Elements are shifted to higher indices. Elements that pass the top are discarded.
-pub fn shiftElementsUp(vec: anytype, comptime amount: VectorCount(@TypeOf(vec)), shift_in: std.meta.Child(@TypeOf(vec))) @TypeOf(vec) {
+pub fn shiftElementsRight(vec: anytype, comptime amount: VectorCount(@TypeOf(vec)), shift_in: std.meta.Child(@TypeOf(vec))) @TypeOf(vec) {
     return join(@splat(amount, shift_in), extract(vec, 0, vectorLength(@TypeOf(vec)) - amount));
 }
 
 /// Elements are shifted to lower indices. Elements that pass the bottom are discarded.
-pub fn shiftElementsDown(vec: anytype, comptime amount: VectorCount(@TypeOf(vec)), shift_in: std.meta.Child(@TypeOf(vec))) @TypeOf(vec) {
+pub fn shiftElementsLeft(vec: anytype, comptime amount: VectorCount(@TypeOf(vec)), shift_in: std.meta.Child(@TypeOf(vec))) @TypeOf(vec) {
     const len = vectorLength(@TypeOf(vec));
 
     return join(extract(vec, amount, len - amount), @splat(amount, shift_in));
 }
 
 /// Elements are shifted to lower indices. Elements that pass the bottom return to the top.
-pub fn rotateElementsDown(vec: anytype, comptime amount: VectorCount(@TypeOf(vec))) @TypeOf(vec) {
+pub fn rotateElementsLeft(vec: anytype, comptime amount: VectorCount(@TypeOf(vec))) @TypeOf(vec) {
     const Child = std.meta.Child(@TypeOf(vec));
     const len = vectorLength(@TypeOf(vec));
 
     std.debug.assert(amount <= len);
 
-    const indices = comptime @mod(countUp(i32, len) + @splat(len, @as(i32, amount)), @splat(len, @intCast(i32, len)));
+    const indices = comptime @mod(iota(i32, len) + @splat(len, @as(i32, amount)), @splat(len, @intCast(i32, len)));
 
     return @shuffle(Child, vec, undefined, indices);
 }
 
 /// Elements are shifted to higher indices. Elements that pass the top return to the bottom.
-pub fn rotateElementsUp(vec: anytype, comptime amount: VectorCount(@TypeOf(vec))) @TypeOf(vec) {
-    return rotateElementsDown(vec, vectorLength(@TypeOf(vec)) - amount);
+pub fn rotateElementsRight(vec: anytype, comptime amount: VectorCount(@TypeOf(vec))) @TypeOf(vec) {
+    return rotateElementsLeft(vec, vectorLength(@TypeOf(vec)) - amount);
 }
 
 pub fn reverseOrder(vec: anytype) @TypeOf(vec) {
     const Child = std.meta.Child(@TypeOf(vec));
     const len = vectorLength(@TypeOf(vec));
 
-    return @shuffle(Child, vec, undefined, @splat(len, @intCast(i32, len) - 1) - countUp(i32, len));
+    return @shuffle(Child, vec, undefined, @splat(len, @intCast(i32, len) - 1) - iota(i32, len));
 }
 
 test "vector shifting" {
     const base = Vector(4, u32){ 10, 20, 30, 40 };
 
-    try std.testing.expectEqual([4]u32{ 30, 40, 999, 999 }, shiftElementsDown(base, 2, 999));
-    try std.testing.expectEqual([4]u32{ 999, 999, 10, 20 }, shiftElementsUp(base, 2, 999));
-    try std.testing.expectEqual([4]u32{ 20, 30, 40, 10 }, rotateElementsDown(base, 1));
-    try std.testing.expectEqual([4]u32{ 40, 10, 20, 30 }, rotateElementsUp(base, 1));
+    try std.testing.expectEqual([4]u32{ 30, 40, 999, 999 }, shiftElementsLeft(base, 2, 999));
+    try std.testing.expectEqual([4]u32{ 999, 999, 10, 20 }, shiftElementsRight(base, 2, 999));
+    try std.testing.expectEqual([4]u32{ 20, 30, 40, 10 }, rotateElementsLeft(base, 1));
+    try std.testing.expectEqual([4]u32{ 40, 10, 20, 30 }, rotateElementsRight(base, 1));
     try std.testing.expectEqual([4]u32{ 40, 30, 20, 10 }, reverseOrder(base));
 }
 
@@ -233,7 +233,7 @@ pub fn firstTrue(vec: anytype) ?VectorIndex(@TypeOf(vec)) {
     if (!@reduce(.Or, vec)) {
         return null;
     }
-    const indices = @select(IndexInt, vec, countUp(IndexInt, len), @splat(len, ~@as(IndexInt, 0)));
+    const indices = @select(IndexInt, vec, iota(IndexInt, len), @splat(len, ~@as(IndexInt, 0)));
     return @reduce(.Min, indices);
 }
 
@@ -244,7 +244,7 @@ pub fn lastTrue(vec: anytype) ?VectorIndex(@TypeOf(vec)) {
     if (!@reduce(.Or, vec)) {
         return null;
     }
-    const indices = @select(IndexInt, vec, countUp(IndexInt, len), @splat(len, @as(IndexInt, 0)));
+    const indices = @select(IndexInt, vec, iota(IndexInt, len), @splat(len, @as(IndexInt, 0)));
     return @reduce(.Max, indices);
 }
 
@@ -283,10 +283,10 @@ test "vector searching" {
     try std.testing.expectEqual(@as(u4, 3), countElementsWithValue(base, 4));
 }
 
-/// Same as accum, but with a user-provided function that mathematically must be associative.
+/// Same as prefixScan, but with a user-provided function that mathematically must be associative.
 /// The function must also do nothing when called with identity as either argument.
 /// Set ErrorType to void if func doesn't return an error union.
-pub fn accumWithFunc(
+pub fn prefixScanWithFunc(
     comptime hop: isize,
     vec: anytype,
     comptime ErrorType: type,
@@ -294,7 +294,7 @@ pub fn accumWithFunc(
     comptime identity: std.meta.Child(@TypeOf(vec)),
 ) if (ErrorType == void) @TypeOf(vec) else ErrorType!@TypeOf(vec) {
     // I haven't debugged this, but it might be a cousin of sorts to what's going on with braid.
-    comptime if (builtin.cpu.arch.isMIPS()) @compileError("TODO: Find out why accum doesn't work on MIPS");
+    comptime if (builtin.cpu.arch.isMIPS()) @compileError("TODO: Find out why prefixScan doesn't work on MIPS");
 
     const len = vectorLength(@TypeOf(vec));
 
@@ -304,7 +304,7 @@ pub fn accumWithFunc(
     var acc = vec;
     comptime var i = 0;
     inline while ((abs_hop << i) < len) : (i += 1) {
-        const shifted = if (hop < 0) shiftElementsDown(acc, abs_hop << i, identity) else shiftElementsUp(acc, abs_hop << i, identity);
+        const shifted = if (hop < 0) shiftElementsLeft(acc, abs_hop << i, identity) else shiftElementsRight(acc, abs_hop << i, identity);
 
         acc = if (ErrorType == void) func(acc, shifted) else try func(acc, shifted);
     }
@@ -314,7 +314,7 @@ pub fn accumWithFunc(
 /// Returns a vector whose elements are the result of performing the specified operation on the corresponding
 /// element of the input vector and every hop'th element that came before it (or after, if hop is negative).
 /// Supports the same operations as the @reduce() builtin. Takes O(logN) to compute.
-pub fn accum(comptime op: std.builtin.ReduceOp, comptime hop: isize, vec: anytype) @TypeOf(vec) {
+pub fn prefixScan(comptime op: std.builtin.ReduceOp, comptime hop: isize, vec: anytype) @TypeOf(vec) {
     const VecType = @TypeOf(vec);
     const Child = std.meta.Child(VecType);
     const len = vectorLength(VecType);
@@ -323,7 +323,7 @@ pub fn accum(comptime op: std.builtin.ReduceOp, comptime hop: isize, vec: anytyp
         .Bool => switch (op) {
             .Or, .Xor => false,
             .And => true,
-            else => @compileError("Invalid accum operation " ++ @tagName(op) ++ " for vector of booleans."),
+            else => @compileError("Invalid prefixScan operation " ++ @tagName(op) ++ " for vector of booleans."),
         },
         .Int => switch (op) {
             .Max => std.math.minInt(Child),
@@ -336,9 +336,9 @@ pub fn accum(comptime op: std.builtin.ReduceOp, comptime hop: isize, vec: anytyp
             .Add => 0,
             .Mul => 1,
             .Min => std.math.inf(Child),
-            else => @compileError("Invalid accum operation " ++ @tagName(op) ++ " for vector of floats."),
+            else => @compileError("Invalid prefixScan operation " ++ @tagName(op) ++ " for vector of floats."),
         },
-        else => @compileError("Invalid type " ++ @typeName(VecType) ++ " for accum."),
+        else => @compileError("Invalid type " ++ @typeName(VecType) ++ " for prefixScan."),
     };
 
     const fn_container = struct {
@@ -360,10 +360,10 @@ pub fn accum(comptime op: std.builtin.ReduceOp, comptime hop: isize, vec: anytyp
         }
     };
 
-    return accumWithFunc(hop, vec, void, fn_container.opFn, identity);
+    return prefixScanWithFunc(hop, vec, void, fn_container.opFn, identity);
 }
 
-test "vector accum" {
+test "vector prefix scan" {
     if (comptime builtin.cpu.arch.isMIPS()) {
         return error.SkipZigTest;
     }
@@ -372,24 +372,24 @@ test "vector accum" {
     const float_base = Vector(4, f32){ 2, 0.5, -10, 6.54321 };
     const bool_base = Vector(4, bool){ true, false, true, false };
 
-    try std.testing.expectEqual(countUp(u8, 32) + @splat(32, @as(u8, 1)), accum(.Add, 1, @splat(32, @as(u8, 1))));
-    try std.testing.expectEqual(Vector(4, i32){ 11, 3, 1, 1 }, accum(.And, 1, int_base));
-    try std.testing.expectEqual(Vector(4, i32){ 11, 31, 31, -1 }, accum(.Or, 1, int_base));
-    try std.testing.expectEqual(Vector(4, i32){ 11, 28, 21, -2 }, accum(.Xor, 1, int_base));
-    try std.testing.expectEqual(Vector(4, i32){ 11, 34, 43, 22 }, accum(.Add, 1, int_base));
-    try std.testing.expectEqual(Vector(4, i32){ 11, 253, 2277, -47817 }, accum(.Mul, 1, int_base));
-    try std.testing.expectEqual(Vector(4, i32){ 11, 11, 9, -21 }, accum(.Min, 1, int_base));
-    try std.testing.expectEqual(Vector(4, i32){ 11, 23, 23, 23 }, accum(.Max, 1, int_base));
+    try std.testing.expectEqual(iota(u8, 32) + @splat(32, @as(u8, 1)), prefixScan(.Add, 1, @splat(32, @as(u8, 1))));
+    try std.testing.expectEqual(Vector(4, i32){ 11, 3, 1, 1 }, prefixScan(.And, 1, int_base));
+    try std.testing.expectEqual(Vector(4, i32){ 11, 31, 31, -1 }, prefixScan(.Or, 1, int_base));
+    try std.testing.expectEqual(Vector(4, i32){ 11, 28, 21, -2 }, prefixScan(.Xor, 1, int_base));
+    try std.testing.expectEqual(Vector(4, i32){ 11, 34, 43, 22 }, prefixScan(.Add, 1, int_base));
+    try std.testing.expectEqual(Vector(4, i32){ 11, 253, 2277, -47817 }, prefixScan(.Mul, 1, int_base));
+    try std.testing.expectEqual(Vector(4, i32){ 11, 11, 9, -21 }, prefixScan(.Min, 1, int_base));
+    try std.testing.expectEqual(Vector(4, i32){ 11, 23, 23, 23 }, prefixScan(.Max, 1, int_base));
 
     // Not testing add and mul because I'm not gonna try predicting those rounding errors.
-    try std.testing.expectEqual(Vector(4, f32){ 2, 0.5, -10, -10 }, accum(.Min, 1, float_base));
-    try std.testing.expectEqual(Vector(4, f32){ 2, 2, 2, 6.54321 }, accum(.Max, 1, float_base));
+    try std.testing.expectEqual(Vector(4, f32){ 2, 0.5, -10, -10 }, prefixScan(.Min, 1, float_base));
+    try std.testing.expectEqual(Vector(4, f32){ 2, 2, 2, 6.54321 }, prefixScan(.Max, 1, float_base));
 
-    try std.testing.expectEqual(Vector(4, bool){ true, true, false, false }, accum(.Xor, 1, bool_base));
-    try std.testing.expectEqual(Vector(4, bool){ true, true, true, true }, accum(.Or, 1, bool_base));
-    try std.testing.expectEqual(Vector(4, bool){ true, false, false, false }, accum(.And, 1, bool_base));
+    try std.testing.expectEqual(Vector(4, bool){ true, true, false, false }, prefixScan(.Xor, 1, bool_base));
+    try std.testing.expectEqual(Vector(4, bool){ true, true, true, true }, prefixScan(.Or, 1, bool_base));
+    try std.testing.expectEqual(Vector(4, bool){ true, false, false, false }, prefixScan(.And, 1, bool_base));
 
-    try std.testing.expectEqual(Vector(4, i32){ 11, 23, 20, 2 }, accum(.Add, 2, int_base));
-    try std.testing.expectEqual(Vector(4, i32){ 22, 11, -12, -21 }, accum(.Add, -1, int_base));
-    try std.testing.expectEqual(Vector(4, i32){ 11, 23, 9, -10 }, accum(.Add, 3, int_base));
+    try std.testing.expectEqual(Vector(4, i32){ 11, 23, 20, 2 }, prefixScan(.Add, 2, int_base));
+    try std.testing.expectEqual(Vector(4, i32){ 22, 11, -12, -21 }, prefixScan(.Add, -1, int_base));
+    try std.testing.expectEqual(Vector(4, i32){ 11, 23, 9, -10 }, prefixScan(.Add, 3, int_base));
 }
