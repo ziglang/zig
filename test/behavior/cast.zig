1f76b4c6b84c1fd7d370bde75b8e37822bd85086
Cody Tapscott <topolarity@tapscott.me>
2022-03-11 14:23:29 -0700

    stage2 llvm: Respect container type when lowering parent pointers
    We need to make sure that we bitcast our pointers correctly before
    we use get_element_ptr to compute the offset for the parent
    pointer.
    
    This also includes a small fix-up for a problem where ptrs to const
    i64/u64 were not using the correct type in >1-level decl chains
    (where we call lowerParentPtr recursively)

--- test/behavior/cast.zig
+++ test/behavior/cast.zig
@@ -887,7 +887,9 @@ test "peer cast *[N:x]T to *[N]T" {
 }
 
 test "peer cast [*:x]T to [*]T" {
-    if (builtin.zig_backend != .stage1) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest;
+    if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest;
+    if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest;
 
     const S = struct {
         fn doTheTest() !void {
