fd1ce329b3f978a4ac2ae272afee7f07b5841990
John Schmidt <john.schmidt.h@gmail.com>
2022-03-26 19:34:55 +0100

    stage2: add union compile error tests

---
+++ test/compile_errors/stage2/union_enum_field_missing.zig
@@ -0,0 +1,20 @@
+const E = enum {
+    a,
+    b,
+    c,
+};
+
+const U = union(E) {
+    a: i32,
+    b: f64,
+};
+
+export fn entry() usize {
+    return @sizeOf(U);
+}
+
+// enum field missing in union
+//
+// :7:1: error: enum field(s) missing in union
+// :4:5: note: field 'c' missing, declared here
+// :1:11: note: enum declared here
