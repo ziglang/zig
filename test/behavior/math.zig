77e70189f438316a8d4e48b2457be0b5eb5974f3
joachimschmidt557 <joachim.schmidt557@outlook.com>
2022-03-30 11:09:05 +0200

    stage2 ARM: implement shl_with_overflow for ints <= 32 bits

--- test/behavior/math.zig
+++ test/behavior/math.zig
@@ -719,7 +719,6 @@ test "@shlWithOverflow" {
     if (builtin.zig_backend == .stage2_c) return error.SkipZigTest; // TODO
     if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest; // TODO
     if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest; // TODO
-    if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest; // TODO
 
     var result: u16 = undefined;
     try expect(@shlWithOverflow(u16, 0b0010111111111111, 3, &result));
