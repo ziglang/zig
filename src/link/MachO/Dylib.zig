f034cef262ed050551bc5c36d710263950e5eb27
Andrew Kelley <andrew@ziglang.org>
2022-05-05 22:16:38 -0700

    link/MachO: use const instead of var and limit scope of vars

--- src/link/MachO/Dylib.zig
+++ src/link/MachO/Dylib.zig
@@ -214,12 +214,12 @@ fn parseSymbols(self: *Dylib, allocator: Allocator) !void {
     const index = self.symtab_cmd_index orelse return;
     const symtab_cmd = self.load_commands.items[index].symtab;
 
-    var symtab = try allocator.alloc(u8, @sizeOf(macho.nlist_64) * symtab_cmd.nsyms);
+    const symtab = try allocator.alloc(u8, @sizeOf(macho.nlist_64) * symtab_cmd.nsyms);
     defer allocator.free(symtab);
     _ = try self.file.preadAll(symtab, symtab_cmd.symoff + self.library_offset);
     const slice = @alignCast(@alignOf(macho.nlist_64), mem.bytesAsSlice(macho.nlist_64, symtab));
 
-    var strtab = try allocator.alloc(u8, symtab_cmd.strsize);
+    const strtab = try allocator.alloc(u8, symtab_cmd.strsize);
     defer allocator.free(strtab);
     _ = try self.file.preadAll(strtab, symtab_cmd.stroff + self.library_offset);
 
@@ -345,14 +345,16 @@ pub fn parseFromStub(
 
     const umbrella_lib = lib_stub.inner[0];
 
-    var id = try Id.default(allocator, umbrella_lib.installName());
-    if (umbrella_lib.currentVersion()) |version| {
-        try id.parseCurrentVersion(version);
+    {
+        var id = try Id.default(allocator, umbrella_lib.installName());
+        if (umbrella_lib.currentVersion()) |version| {
+            try id.parseCurrentVersion(version);
+        }
+        if (umbrella_lib.compatibilityVersion()) |version| {
+            try id.parseCompatibilityVersion(version);
+        }
+        self.id = id;
     }
-    if (umbrella_lib.compatibilityVersion()) |version| {
-        try id.parseCompatibilityVersion(version);
-    }
-    self.id = id;
 
     var umbrella_libs = std.StringHashMap(void).init(allocator);
     defer umbrella_libs.deinit();
