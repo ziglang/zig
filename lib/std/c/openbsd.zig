0c95267b4b6bc6aa98660654291af661d4ec10a3
aiotter <git@aiotter.com>
2022-05-07 10:08:57 +0900

    std.c: Fix incorrenct dirent structure

--- lib/std/c/openbsd.zig
+++ lib/std/c/openbsd.zig
@@ -353,7 +353,7 @@ pub const dirent = extern struct {
     d_type: u8,
     d_namlen: u8,
     __d_padding: [4]u8,
-    d_name: [MAXNAMLEN + 1]u8,
+    d_name: [MAXNAMLEN:0]u8,
 
     pub fn reclen(self: dirent) u16 {
         return self.d_reclen;
