2e30201ff5c452852e4804186c723cc742da3294
Robin Eklind <rnd0x00@gmail.com>
2022-05-10 19:51:25 +0200

    init-exe: add note about log_level in ReleaseSmall and ReleaseFast build mode
    As suggested in https://github.com/ziglang/zig/issues/9945#issuecomment-950114977 by @wizzard0.
    
    Fixes #9945.

--- lib/init-exe/src/main.zig
+++ lib/init-exe/src/main.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 
 pub fn main() anyerror!void {
+    // Note that info level log messages are by default printed only in Debug
+    // and ReleaseSafe build modes.
     std.log.info("All your codebase are belong to us.", .{});
 }
 
