3a64693db30d0a9d72dcc34d3ae053f25927f5ec
Meghan <hello@nektro.net>
2022-05-11 12:43:18 -0700

    std: add http definitions for Method and Status (#10661)

--- lib/std/std.zig
+++ lib/std/std.zig
@@ -65,6 +65,7 @@ pub const fs = @import("fs.zig");
 pub const hash = @import("hash.zig");
 pub const hash_map = @import("hash_map.zig");
 pub const heap = @import("heap.zig");
+pub const http = @import("http.zig");
 pub const io = @import("io.zig");
 pub const json = @import("json.zig");
 pub const leb = @import("leb128.zig");
