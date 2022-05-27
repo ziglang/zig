6b81d862b779aaa88b91225509b23c1f897b5c13
Jan Philipp Hafer <jan.hafer@rwth-aachen.de>
2022-05-27 16:50:10 +0200

    also check for leaks in child

--- test/standalone/childprocess_pipetest/child.zig
+++ test/standalone/childprocess_pipetest/child.zig
@@ -2,13 +2,11 @@ const std = @import("std");
 const builtin = @import("builtin");
 const windows = std.os.windows;
 pub fn main() !void {
-    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
-    defer std.debug.assert(!general_purpose_allocator.deinit());
-    const gpa = general_purpose_allocator.allocator();
+    const alloc = std.testing.allocator;
     const stdin = std.io.getStdIn();
     const stdin_reader = stdin.reader();
-    const stdin_cont = try stdin_reader.readUntilDelimiterAlloc(gpa, '\n', 2_000);
-    defer gpa.free(stdin_cont);
+    const stdin_cont = try stdin_reader.readUntilDelimiterAlloc(alloc, '\n', 2_000);
+    defer alloc.free(stdin_cont);
     var file_handle = file_handle: {
         if (builtin.target.os.tag == .windows) {
             var handle_int = try std.fmt.parseInt(usize, stdin_cont, 10);
@@ -18,20 +16,19 @@ pub fn main() !void {
         }
     };
     if (builtin.target.os.tag == .windows) {
-        // windows.HANDLE_FLAG_INHERIT is enabled
         var handle_flags = windows.DWORD;
         try windows.GetHandleInformation(file_handle, &handle_flags);
-        try std.testing.expect(handle_flags & windows.HANDLE_FLAG_INHERIT != 0);
+        std.debug.assert(handle_flags & windows.HANDLE_FLAG_INHERIT != 0);
+        try windows.SetHandleInformation(file_handle, windows.HANDLE_FLAG_INHERIT, 0);
     } else {
-        // FD_CLOEXEC is not set
         var fcntl_flags = try std.os.fcntl(file_handle, std.os.F.GETFD, 0);
         try std.testing.expect((fcntl_flags & std.os.FD_CLOEXEC) == 0);
+        _ = try std.os.fcntl(file_handle, std.os.F.SETFD, std.os.FD_CLOEXEC);
     }
-    try std.os.disableFileInheritance(file_handle);
     var extra_stream_in = std.fs.File{ .handle = file_handle };
     defer extra_stream_in.close();
     const extra_str_in_rd = extra_stream_in.reader();
-    const all_extra_str_in = try extra_str_in_rd.readUntilDelimiterAlloc(gpa, '\x17', 20_000);
-    defer gpa.free(all_extra_str_in);
+    const all_extra_str_in = try extra_str_in_rd.readUntilDelimiterAlloc(alloc, '\x17', 20_000);
+    defer alloc.free(all_extra_str_in);
     try std.testing.expectEqualSlices(u8, all_extra_str_in, "test123");
 }
