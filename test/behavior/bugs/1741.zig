94624893d88526d7a0f88f048e2a26abea6454c2
Veikka Tuominen <git@vexu.eu>
2022-06-01 02:24:03 +0300

    disable failing test

--- test/behavior/bugs/1741.zig
+++ test/behavior/bugs/1741.zig
@@ -2,9 +2,7 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 test "fixed" {
-    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest;
-    if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest;
-    if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest;
+    if (builtin.zig_backend != .stage1) return error.SkipZigTest;
     const x: f32 align(128) = 12.34;
     try std.testing.expect(@ptrToInt(&x) % 128 == 0);
 }
