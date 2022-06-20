50e964e5a3b604f38b72be269b484fda3b44960b
Jakub Konka <kubkon@jakubkonka.com>
2022-06-20 17:29:00 +0200

    link-tests: CheckFileStep to do grep test on the binary

--- test/link/dylib/build.zig
+++ test/link/dylib/build.zig
@@ -24,6 +24,13 @@ pub fn build(b: *Builder) void {
     run.cwd = b.pathFromRoot(".");
     run.expectStdOutEqual("Hello world");
 
+    const exp_dylib = std.macho.createLoadDylibCommand(b.allocator, "@rpath/liba.dylib", 2, 0x10000, 0x10000) catch unreachable;
+    var buf = std.ArrayList(u8).init(b.allocator);
+    defer buf.deinit();
+    exp_dylib.write(buf.writer()) catch unreachable;
+    const check_file = std.build.CheckFileStep.create(b, exe.getOutputSource(), &[_][]const u8{buf.items});
+
     test_step.dependOn(b.getInstallStep());
     test_step.dependOn(&run.step);
+    test_step.dependOn(&check_file.step);
 }
