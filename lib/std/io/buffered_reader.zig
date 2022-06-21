a0d531f162d2213a985581ad2987d9666b98c1fb
Igor Stojkovic <stojkovic.igor+git@gmail.com>
2022-06-21 21:54:18 +0200

    Added seeking support to BufferedReader

--- lib/std/io/buffered_reader.zig
+++ lib/std/io/buffered_reader.zig
@@ -6,37 +6,92 @@ const testing = std.testing;
 pub fn BufferedReader(comptime buffer_size: usize, comptime ReaderType: type) type {
     return struct {
         unbuffered_reader: ReaderType,
-        fifo: FifoType = FifoType.init(),
+        start: usize = 0,
+        end: usize = 0,
+        buffer: [buffer_size]u8 = undefined,
 
         pub const Error = ReaderType.Error;
-        pub const Reader = io.Reader(*Self, Error, read);
+
+        pub usingnamespace if (@hasDecl(ReaderType, "seek_interface_id")) struct {
+            pub const SeekError = ReaderType.SeekError;
+            pub const GetSeekPosError = ReaderType.GetSeekPosError;
+            pub const Reader = io.SeekableReader(*Self, Error, SeekError, GetSeekPosError, read);
+
+            pub fn seekTo(self: *Self, pos: u64) SeekError!void {
+                // We discard the buffer cause we can't know if we will end up within it
+                self.start = 0;
+                self.end = 0;
+                return self.unbuffered_reader.seekTo(pos);
+            }
+
+            pub fn seekBy(self: *Self, amt: i64) SeekError!void {
+                if (amt < 0) {
+                    const abs_amt = std.math.absCast(amt);
+                    const abs_amt_usize = std.math.cast(usize, abs_amt) orelse std.math.maxInt(usize);
+                    if (abs_amt_usize > self.start) {
+                        try self.unbuffered_reader.seekBy(amt + @intCast(i64, self.start));
+                        self.start = 0;
+                        self.end = 0;
+                    } else {
+                        self.start -= abs_amt_usize;
+                    }
+                } else {
+                    const amt_usize = std.math.cast(usize, amt) orelse std.math.maxInt(usize);
+                    const new_pos = self.start +| amt_usize;
+                    if (new_pos > self.end) {
+                        try self.unbuffered_reader.seekBy(@intCast(i64, new_pos - self.end));
+                        self.start = 0;
+                        self.end = 0;
+                    } else {
+                        self.start = new_pos;
+                    }
+                }
+            }
+
+            pub fn getEndPos(self: *Self) GetSeekPosError!u64 {
+                return self.unbuffered_reader.getEndPos();
+            }
+
+            pub fn getPos(self: *Self) GetSeekPosError!u64 {
+                return self.unbuffered_reader.getPos();
+            }
+
+            pub fn reader(self: *Self) Reader {
+                return .{ .context = self };
+            }
+        } else struct {
+            pub const Reader = io.Reader(*Self, Error, read);
+
+            pub fn reader(self: *Self) Reader {
+                return .{ .context = self };
+            }
+        };
 
         const Self = @This();
-        const FifoType = std.fifo.LinearFifo(u8, std.fifo.LinearFifoBufferType{ .Static = buffer_size });
 
         pub fn read(self: *Self, dest: []u8) Error!usize {
-            var dest_index: usize = 0;
-            while (dest_index < dest.len) {
-                const written = self.fifo.read(dest[dest_index..]);
-                if (written == 0) {
-                    // fifo empty, fill it
-                    const writable = self.fifo.writableSlice(0);
-                    assert(writable.len > 0);
-                    const n = try self.unbuffered_reader.read(writable);
-                    if (n == 0) {
-                        // reading from the unbuffered stream returned nothing
-                        // so we have nothing left to read.
-                        return dest_index;
-                    }
-                    self.fifo.update(n);
-                }
-                dest_index += written;
+            const size = dest.len;
+            const available = self.end - self.start;
+            if (available >= size) {
+                const endPos = self.start + size;
+                std.mem.copy(u8, dest[0..], self.buffer[self.start..endPos]);
+                self.start = endPos;
+                return size;
             }
-            return dest.len;
-        }
 
-        pub fn reader(self: *Self) Reader {
-            return .{ .context = self };
+            std.mem.copy(u8, dest[0..available], self.buffer[self.start..self.end]);
+            self.start = 0;
+            self.end = 0;
+            var remaining_size = size - available;
+            if (remaining_size >= self.buffer.len) {
+                return available + try self.unbuffered_reader.read(dest[available..]);
+            }
+
+            self.end = try self.unbuffered_reader.read(self.buffer[0..]);
+            if (remaining_size > self.end) remaining_size = self.end;
+            std.mem.copy(u8, dest[available..available + remaining_size], self.buffer[0..remaining_size]);
+            self.start = remaining_size;
+            return available + remaining_size;
         }
     };
 }
@@ -83,4 +138,28 @@ test "io.BufferedReader" {
     const res = try stream.readAllAlloc(testing.allocator, str.len + 1);
     defer testing.allocator.free(res);
     try testing.expectEqualSlices(u8, str, res);
+}
+
+test "io.SeekableBufferedReader" {
+    const str = "This is a test";
+    var stream = io.fixedBufferStream(str);
+
+    var buf_reader = bufferedReader(stream.reader());
+    const reader = buf_reader.reader();
+
+    var buffer: [str.len]u8 = undefined;
+    var res = try reader.readAll(buffer[0..]);
+    try testing.expectEqual(@as(usize, str.len), res);
+    try testing.expectEqualSlices(u8, str, buffer[0..]);
+    try reader.seekTo(0);
+    std.mem.set(u8, buffer[0..], 0);
+    res = try reader.readAll(buffer[0..4]);
+    try testing.expectEqual(@as(usize, 4), res);
+    try reader.seekBy(3);
+    res = try reader.readAll(buffer[4..]);
+    try testing.expectEqual(@as(usize, 7), res);
+    try reader.seekBy(-10);
+    res = try reader.readAll(buffer[11..]);
+    try testing.expectEqual(@as(usize, 3), res);
+    try testing.expectEqualSlices(u8, "This a test is", buffer[0..]);
 }
