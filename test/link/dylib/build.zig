0fa1996abf415b02f72fed51328a1382cca1ecb5
Jakub Konka <kubkon@jakubkonka.com>
2022-06-21 15:44:22 +0200

    link-tests: add CheckMachOStep
    CheckMachOStep specialises CheckFileStep into directed (surgical)
    MachO file fuzzy searches. This will be the building block for
    comprehensive MachO linker tests.

--- test/link/dylib/build.zig
+++ test/link/dylib/build.zig
@@ -5,12 +5,25 @@ pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
 
     const test_step = b.step("test", "Test");
+    test_step.dependOn(b.getInstallStep());
 
     const dylib = b.addSharedLibrary("a", null, b.version(1, 0, 0));
     dylib.setBuildMode(mode);
     dylib.addCSourceFile("a.c", &.{});
     dylib.linkLibC();
     dylib.install();
+
+    {
+        const check_macho = dylib.checkMachO();
+        check_macho.checkLoadCommand(.{
+            .cmd = std.macho.LC.ID_DYLIB,
+            .name = "@rpath/liba.dylib",
+            .timestamp = 2,
+            .current_version = 0x10000,
+            .compat_version = 0x10000,
+        });
+        test_step.dependOn(&check_macho.step);
+    }
 
     const exe = b.addExecutable("main", null);
     exe.setBuildMode(mode);
@@ -20,17 +33,28 @@ pub fn build(b: *Builder) void {
     exe.addLibraryPath(b.pathFromRoot("zig-out/lib/"));
     exe.addRPath(b.pathFromRoot("zig-out/lib"));
 
+    {
+        const check_macho = exe.checkMachO();
+        check_macho.checkLoadCommand(.{
+            .cmd = std.macho.LC.LOAD_DYLIB,
+            .name = "@rpath/liba.dylib",
+            .timestamp = 2,
+            .current_version = 0x10000,
+            .compat_version = 0x10000,
+        });
+        test_step.dependOn(&check_macho.step);
+    }
+    {
+        const check_macho = exe.checkMachO();
+        check_macho.checkLoadCommand(.{
+            .cmd = std.macho.LC.RPATH,
+            .name = b.pathFromRoot("zig-out/lib"),
+        });
+        test_step.dependOn(&check_macho.step);
+    }
+
     const run = exe.run();
     run.cwd = b.pathFromRoot(".");
     run.expectStdOutEqual("Hello world");
-
-    const exp_dylib = std.macho.createLoadDylibCommand(b.allocator, "@rpath/liba.dylib", 2, 0x10000, 0x10000) catch unreachable;
-    var buf = std.ArrayList(u8).init(b.allocator);
-    defer buf.deinit();
-    exp_dylib.write(buf.writer()) catch unreachable;
-    const check_file = std.build.CheckFileStep.create(b, exe.getOutputSource(), &[_][]const u8{buf.items});
-
-    test_step.dependOn(b.getInstallStep());
     test_step.dependOn(&run.step);
-    test_step.dependOn(&check_file.step);
 }
