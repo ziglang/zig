8f00bc9d231c0c686b2ffa3bd0b14181afa7a171
Jakub Konka <kubkon@jakubkonka.com>
2022-06-25 10:57:56 +0200

    link-tests: put macho search strategy tests into one test case

--- test/link/macho/search_paths_first/a.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-char world[] = "world";
-
-char* hello() {
-  return "Hello";
-}
--- test/link/macho/search_paths_first/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-char* hello();
-extern char world[];
-
-int main() {
-  printf("%s %s", hello(), world);
-  return 0;
-}
