b6422d7cdbbb4982b35ccd80d5b66f92962f0585
r00ster91 <r00ster91@proton.me>
2022-07-07 20:40:31 +0200

    std.log: give friendly error to freestanding users

--- test/standalone/issue_7030/main.zig
+++ test/standalone/issue_7030/main.zig
@@ -1,5 +1,17 @@
 const std = @import("std");
 
+pub fn log(
+    comptime message_level: std.log.Level,
+    comptime scope: @Type(.EnumLiteral),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    _ = message_level;
+    _ = scope;
+    _ = format;
+    _ = args;
+}
+
 pub fn main() anyerror!void {
     std.log.info("All your codebase are belong to us.", .{});
 }
