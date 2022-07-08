2263bba6cd544c31a6f5e1223503facb3d6c07e7
Koakuma <koachan@protonmail.com>
2022-07-08 20:53:46 +0700

    stage2: sparc64: Skip Sema-failing tests for now

--- test/behavior/error.zig
+++ test/behavior/error.zig
@@ -223,6 +223,7 @@ fn testErrorSetType() !void {
 test "explicit error set cast" {
     if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest; // TODO
     if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_sparc64) return error.SkipZigTest; // TODO
 
     try testExplicitErrorSetCast(Set1.A);
     comptime try testExplicitErrorSetCast(Set1.A);
