47c68849ac0ca8e154145bb4f50b06b5c481ffaf
Veikka Tuominen <git@vexu.eu>
2022-07-19 20:20:57 +0300

    Sema: better function parameter source location

--- test/cases/compile_errors/function_with_non-extern_non-packed_struct_parameter.zig
+++ test/cases/compile_errors/function_with_non-extern_non-packed_struct_parameter.zig
@@ -9,6 +9,6 @@ export fn entry(foo: Foo) void { _ = foo; }
 // backend=stage2
 // target=native
 //
-// :6:8: error: parameter of type 'tmp.Foo' not allowed in function with calling convention 'C'
-// :6:8: note: only structs with packed or extern layout are extern compatible
+// :6:17: error: parameter of type 'tmp.Foo' not allowed in function with calling convention 'C'
+// :6:17: note: only structs with packed or extern layout are extern compatible
 // :1:13: note: struct declared here
