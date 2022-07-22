1f748fe42662da3ee2c977ba638a714e15acb433
Veikka Tuominen <git@vexu.eu>
2022-07-22 19:48:58 +0300

    Sema: fix mutation of optional ptr represented as regular ptr

--- test/behavior/eval.zig
+++ test/behavior/eval.zig
@@ -1282,3 +1282,14 @@ test "optional pointer represented as a pointer value" {
         try expect(payload_ptr.*.* == 15);
     }
 }
+
+test "mutate through pointer-like optional at comptime" {
+    comptime {
+        var val: u8 = 15;
+        var opt_ptr: ?*const u8 = &val;
+
+        const payload_ptr = &opt_ptr.?;
+        payload_ptr.* = &@as(u8, 16);
+        try expect(payload_ptr.*.* == 16);
+    }
+}
