ziglang/zig

commit 2a990d69669c2a2cd16134e8ebbd2750060f8071

stage1: rework tokenizer to match stage2
 * Extracts AstGen logic from ir.cpp into astgen.cpp. Reduces the
   largest file of stage1 from 33,551 lines to 25,510.
 * tokenizer: rework it completely to match the stage2 tokenizer logic.
   They can now be maintained together; when one is changed, the other
   can be changed in the same way.
   - Each token now takes up 13 bytes instead of 64 bytes. The tokenizer
     does not parse char literals, string literals, integer literals,
     etc into meaningful data. Instead, that happens during parsing or
     astgen.
   - no longer store line offsets. Error messages scan source
     files to find the line/column as needed (same as stage2).
   - main loop: instead of checking the loop, handle a null byte
     explicitly in the switch statements. This is a nice improvement
     that we may want to backport to stage2.
   - delete some dead tokens, artifacts of past syntax that no longer
     exists.
 * Parser: fix a TODO by parsing builtin functions as tokens rather than
   `@` as a separate token. This is how stage2 does it.
 * Remove some debugging infrastructure. These will need to be redone,
   if at all, as the code migrates to match stage2.
   - remove the ast_render code.
   - remove the IR debugging stuff
   - remove teh token printing code

src/stage1/parser.hpp
@@ -12,14 +12,21 @@
 #include "tokenizer.hpp"
 #include "errmsg.hpp"
 
-ATTRIBUTE_PRINTF(2, 3)
-void ast_token_error(Token *token, const char *format, ...);
-
-
-AstNode * ast_parse(Buf *buf, ZigList<Token> *tokens, ZigType *owner, ErrColor err_color);
+AstNode * ast_parse(Buf *buf, ZigType *owner, ErrColor err_color);
 
 void ast_print(AstNode *node, int indent);
 
 void ast_visit_node_children(AstNode *node, void (*visit)(AstNode **, void *context), void *context);
+
+Buf *node_identifier_buf(AstNode *node);
+Buf *node_string_literal_buf(AstNode *node);
+
+Buf *token_identifier_buf(RootStruct *root_struct, TokenIndex token);
+Buf *token_string_literal_buf(RootStruct *root_struct, TokenIndex token);
+
+void token_number_literal_bigint(RootStruct *root_struct, BigInt *result, TokenIndex token);
+
+Error source_string_literal_buf(const char *source, Buf *out, size_t *bad_index);
+Error source_char_literal(const char *source, uint32_t *out, size_t *bad_index);
 
 #endif

CMakeLists.txt
@@ -292,7 +292,7 @@ set(ZIG0_SOURCES
 
 set(STAGE1_SOURCES
     "${CMAKE_SOURCE_DIR}/src/stage1/analyze.cpp"
-    "${CMAKE_SOURCE_DIR}/src/stage1/ast_render.cpp"
+    "${CMAKE_SOURCE_DIR}/src/stage1/astgen.cpp"
     "${CMAKE_SOURCE_DIR}/src/stage1/bigfloat.cpp"
     "${CMAKE_SOURCE_DIR}/src/stage1/bigint.cpp"
     "${CMAKE_SOURCE_DIR}/src/stage1/buffer.cpp"
@@ -307,11 +307,11 @@ set(STAGE1_SOURCES
     "${CMAKE_SOURCE_DIR}/src/stage1/os.cpp"
     "${CMAKE_SOURCE_DIR}/src/stage1/parser.cpp"
     "${CMAKE_SOURCE_DIR}/src/stage1/range_set.cpp"
+    "${CMAKE_SOURCE_DIR}/src/stage1/softfloat_ext.cpp"
     "${CMAKE_SOURCE_DIR}/src/stage1/stage1.cpp"
     "${CMAKE_SOURCE_DIR}/src/stage1/target.cpp"
     "${CMAKE_SOURCE_DIR}/src/stage1/tokenizer.cpp"
     "${CMAKE_SOURCE_DIR}/src/stage1/util.cpp"
-    "${CMAKE_SOURCE_DIR}/src/stage1/softfloat_ext.cpp"
 )
 set(OPTIMIZED_C_SOURCES
     "${CMAKE_SOURCE_DIR}/src/stage1/parse_f128.c"

lib/std/zig/string_literal.zig
@@ -107,6 +107,7 @@ pub fn parseAppend(buf: *std.ArrayList(u8), bytes: []const u8) error{OutOfMemory
                             const hex_str = slice[index + 2 .. index_end];
                             if (std.fmt.parseUnsigned(u32, hex_str, 16)) |uint| {
                                 if (uint <= 0x10ffff) {
+                                    // TODO this incorrectly depends on endianness
                                     try buf.appendSlice(std.mem.toBytes(uint)[0..]);
                                     state = State.Start;
                                     index = index_end; // loop-header increments

src/stage1.zig
@@ -252,6 +252,8 @@ const Error = extern enum {
     ZigIsTheCCompiler,
     FileBusy,
     Locked,
+    InvalidCharacter,
+    UnicodePointTooLarge,
 };
 
 // ABI warning

src/stage1/all_types.hpp
@@ -670,7 +670,7 @@ enum NodeType {
     NodeTypeIntLiteral,
     NodeTypeStringLiteral,
     NodeTypeCharLiteral,
-    NodeTypeSymbol,
+    NodeTypeIdentifier,
     NodeTypePrefixOpExpr,
     NodeTypePointerType,
     NodeTypeFnCallExpr,
@@ -710,6 +710,7 @@ enum NodeType {
     NodeTypeAwaitExpr,
     NodeTypeSuspend,
     NodeTypeAnyFrameType,
+    // main_token points to the identifier.
     NodeTypeEnumLiteral,
     NodeTypeAnyTypeField,
 };
@@ -733,7 +734,8 @@ struct AstNodeFnProto {
     AstNode *section_expr;
     // populated if the "callconv(S)" is present
     AstNode *callconv_expr;
-    Buf doc_comments;
+
+    TokenIndex doc_comments;
 
     // This is set based only on the existence of a noinline or inline keyword.
     // This is then resolved to an is_noinline bool and (potentially .Inline)
@@ -755,8 +757,8 @@ struct AstNodeFnDef {
 struct AstNodeParamDecl {
     Buf *name;
     AstNode *type;
-    Token *anytype_token;
-    Buf doc_comments;
+    TokenIndex doc_comments;
+    TokenIndex anytype_token;
     bool is_noalias;
     bool is_comptime;
     bool is_var_args;
@@ -799,9 +801,9 @@ struct AstNodeVariableDeclaration {
     AstNode *align_expr;
     // populated if the "section(S)" is present
     AstNode *section_expr;
-    Token *threadlocal_tok;
-    Buf doc_comments;
+    TokenIndex doc_comments;
 
+    TokenIndex threadlocal_tok;
     VisibMod visib_mod;
     bool is_const;
     bool is_comptime;
@@ -935,13 +937,14 @@ struct AstNodePrefixOpExpr {
 };
 
 struct AstNodePointerType {
-    Token *star_token;
+    TokenIndex star_token;
+    TokenIndex allow_zero_token;
+    TokenIndex bit_offset_start;
+    TokenIndex host_int_bytes;
+
     AstNode *sentinel;
     AstNode *align_expr;
-    BigInt *bit_offset_start;
-    BigInt *host_int_bytes;
     AstNode *op_expr;
-    Token *allow_zero_token;
     bool is_const;
     bool is_volatile;
 };
@@ -956,7 +959,7 @@ struct AstNodeArrayType {
     AstNode *sentinel;
     AstNode *child_type;
     AstNode *align_expr;
-    Token *allow_zero_token;
+    TokenIndex allow_zero_token;
     bool is_const;
     bool is_volatile;
 };
@@ -974,11 +977,11 @@ struct AstNodeIfBoolExpr {
 
 struct AstNodeTryExpr {
     Buf *var_symbol;
-    bool var_is_ptr;
     AstNode *target_node;
     AstNode *then_node;
     AstNode *else_node;
     Buf *err_symbol;
+    bool var_is_ptr;
 };
 
 struct AstNodeTestExpr {
@@ -993,12 +996,12 @@ struct AstNodeWhileExpr {
     Buf *name;
     AstNode *condition;
     Buf *var_symbol;
-    bool var_is_ptr;
     AstNode *continue_expr;
     AstNode *body;
     AstNode *else_node;
     Buf *err_symbol;
     bool is_inline;
+    bool var_is_ptr;
 };
 
 struct AstNodeForExpr {
@@ -1070,7 +1073,7 @@ struct AsmToken {
 };
 
 struct AstNodeAsmExpr {
-    Token *volatile_token;
+    TokenIndex volatile_token;
     AstNode *asm_template;
     ZigList<AsmOutput*> output_list;
     ZigList<AsmInput*> input_list;
@@ -1094,7 +1097,7 @@ struct AstNodeContainerDecl {
     AstNode *init_arg_expr; // enum(T), struct(endianness), or union(T), or union(enum(T))
     ZigList<AstNode *> fields;
     ZigList<AstNode *> decls;
-    Buf doc_comments;
+    TokenIndex doc_comments;
 
     ContainerKind kind;
     ContainerLayout layout;
@@ -1103,7 +1106,7 @@ struct AstNodeContainerDecl {
 };
 
 struct AstNodeErrorSetField {
-    Buf doc_comments;
+    TokenIndex doc_comments;
     AstNode *field_name;
 };
 
@@ -1118,28 +1121,8 @@ struct AstNodeStructField {
     AstNode *value;
     // populated if the "align(A)" is present
     AstNode *align_expr;
-    Buf doc_comments;
-    Token *comptime_token;
-};
-
-struct AstNodeStringLiteral {
-    Buf *buf;
-};
-
-struct AstNodeCharLiteral {
-    uint32_t value;
-};
-
-struct AstNodeFloatLiteral {
-    BigFloat *bigfloat;
-
-    // overflow is true if when parsing the number, we discovered it would not
-    // fit without losing data in a double
-    bool overflow;
-};
-
-struct AstNodeIntLiteral {
-    BigInt *bigint;
+    TokenIndex doc_comments;
+    TokenIndex comptime_token;
 };
 
 struct AstNodeStructValueField {
@@ -1156,19 +1139,6 @@ struct AstNodeContainerInitExpr {
     AstNode *type;
     ZigList<AstNode *> entries;
     ContainerInitKind kind;
-};
-
-struct AstNodeNullLiteral {
-};
-
-struct AstNodeUndefinedLiteral {
-};
-
-struct AstNodeThisLiteral {
-};
-
-struct AstNodeSymbolExpr {
-    Buf *symbol;
 };
 
 struct AstNodeBoolLiteral {
@@ -1188,13 +1158,6 @@ struct AstNodeContinueExpr {
     Buf *name;
 };
 
-struct AstNodeUnreachableExpr {
-};
-
-
-struct AstNodeErrorType {
-};
-
 struct AstNodeAwaitExpr {
     AstNode *expr;
 };
@@ -1207,16 +1170,10 @@ struct AstNodeAnyFrameType {
     AstNode *payload_type; // can be NULL
 };
 
-struct AstNodeEnumLiteral {
-    Token *period;
-    Token *identifier;
-};
-
 struct AstNode {
     enum NodeType type;
+    TokenIndex main_token;
     bool already_traced_this_node;
-    size_t line;
-    size_t column;
     ZigType *owner;
     union {
         AstNodeFnDef fn_def;
@@ -1252,30 +1209,19 @@ struct AstNode {
         AstNodePtrDerefExpr ptr_deref_expr;
         AstNodeContainerDecl container_decl;
         AstNodeStructField struct_field;
-        AstNodeStringLiteral string_literal;
-        AstNodeCharLiteral char_literal;
-        AstNodeFloatLiteral float_literal;
-        AstNodeIntLiteral int_literal;
         AstNodeContainerInitExpr container_init_expr;
         AstNodeStructValueField struct_val_field;
-        AstNodeNullLiteral null_literal;
-        AstNodeUndefinedLiteral undefined_literal;
-        AstNodeThisLiteral this_literal;
-        AstNodeSymbolExpr symbol_expr;
         AstNodeBoolLiteral bool_literal;
         AstNodeBreakExpr break_expr;
         AstNodeContinueExpr continue_expr;
-        AstNodeUnreachableExpr unreachable_expr;
         AstNodeArrayType array_type;
         AstNodeInferredArrayType inferred_array_type;
-        AstNodeErrorType error_type;
         AstNodeErrorSetDecl err_set_decl;
         AstNodeErrorSetField err_set_field;
         AstNodeResumeExpr resume_expr;
         AstNodeAwaitExpr await_expr;
         AstNodeSuspend suspend;
         AstNodeAnyFrameType anyframe_type;
-        AstNodeEnumLiteral enum_literal;
     } data;
 
     // This is a function for use in the debugger to print
@@ -1409,9 +1355,11 @@ struct ZigPackage {
 struct RootStruct {
     ZigPackage *package;
     Buf *path; // relative to root_package->root_src_dir
-    ZigList<size_t> *line_offsets;
     Buf *source_code;
     ZigLLVMDIFile *di_file;
+    size_t token_count;
+    TokenId *token_ids;
+    TokenLoc *token_locs;
 };
 
 enum StructSpecial {

src/stage1/analyze.hpp
@@ -12,7 +12,9 @@
 
 void semantic_analyze(CodeGen *g);
 ErrorMsg *add_node_error(CodeGen *g, AstNode *node, Buf *msg);
-ErrorMsg *add_token_error(CodeGen *g, ZigType *owner, Token *token, Buf *msg);
+ErrorMsg *add_token_error(CodeGen *g, ZigType *owner, TokenIndex token, Buf *msg);
+ErrorMsg *add_token_error_offset(CodeGen *g, ZigType *owner, TokenIndex token, Buf *msg,
+        uint32_t bad_index);
 ErrorMsg *add_error_note(CodeGen *g, ErrorMsg *parent_msg, const AstNode *node, Buf *msg);
 ZigType *new_type_table_entry(ZigTypeId id);
 ZigType *get_fn_frame_type(CodeGen *g, ZigFn *fn);
@@ -140,7 +142,7 @@ Scope *create_runtime_scope(CodeGen *g, AstNode *node, Scope *parent, IrInstSrc 
 Scope *create_typeof_scope(CodeGen *g, AstNode *node, Scope *parent);
 ScopeExpr *create_expr_scope(CodeGen *g, AstNode *node, Scope *parent);
 
-void init_const_str_lit(CodeGen *g, ZigValue *const_val, Buf *str);
+void init_const_str_lit(CodeGen *g, ZigValue *const_val, Buf *str, bool move_str);
 ZigValue *create_const_str_lit(CodeGen *g, Buf *str);
 
 void init_const_bigint(ZigValue *const_val, ZigType *type, const BigInt *bigint);

commit 624633d7efcca6181c04560c3c8ffc2d87c2b940

stage1: remove deadcode ast_print
Fixes: 2a990d696 ("stage1: rework tokenizer to match stage2")
Fixes: b6354ddd5 ("move AST rendering code to separate file")

Signed-off-by: Wei Fu <fuweid89@gmail.com>

src/stage1/parser.hpp
@@ -14,8 +14,6 @@
 
 AstNode * ast_parse(Buf *buf, ZigType *owner, ErrColor err_color);
 
-void ast_print(AstNode *node, int indent);
-
 void ast_visit_node_children(AstNode *node, void (*visit)(AstNode **, void *context), void *context);
 
 Buf *node_identifier_buf(AstNode *node);