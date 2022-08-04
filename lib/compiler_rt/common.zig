169ad1aac745857965db8fe7565aa45b8d8cea0f
Andrew Kelley <andrew@ziglang.org>
2022-08-04 14:55:35 -0700

    compiler_rt: update ABI for x86 float16 functions
    See https://github.com/llvm/llvm-project/issues/56854 for more details.

--- lib/compiler_rt/common.zig
+++ lib/compiler_rt/common.zig
@@ -77,6 +77,7 @@ pub fn panic(msg: []const u8, error_return_trace: ?*std.builtin.StackTrace) nore
 pub const F16T = switch (builtin.cpu.arch) {
     .aarch64, .aarch64_be, .aarch64_32 => f16,
     .riscv64 => if (builtin.zig_backend == .stage1) u16 else f16,
+    .i386, .x86_64 => f16,
     else => u16,
 };
 
