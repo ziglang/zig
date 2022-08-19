bd5fcdf5d6834c01b61515805ec0dba64c70817e
Jakub Konka <kubkon@jakubkonka.com>
2022-08-19 12:13:04 +0200

    coff: specify default base path for relative source paths in pdb
    PDB requires all file paths to be fully resolved, and it is really
    the linker's responsibility to canonicalize any path extracted from
    the CodeView in the object file. However, LLD-link has some very
    questionable defaults, and in particular, it purposely bakes in path
    separator of the host system it was built on rather the target's, or
    just throw an error. Thankfully, they have left a backdoor we can
    use via `-PDBSOURCEPATH` flag.

--- src/link/Coff/lld.zig
+++ src/link/Coff/lld.zig
@@ -181,6 +181,10 @@ pub fn linkWithLLD(self: *Coff, comp: *Compilation, prog_node: *std.Progress.Nod
         try argv.append("-NOLOGO");
         if (!self.base.options.strip) {
             try argv.append("-DEBUG");
+
+            if (self.base.options.pdb_source_path) |path| {
+                try argv.append(try std.fmt.allocPrint(arena, "-PDBSOURCEPATH:{s}", .{path}));
+            }
         }
         if (self.base.options.lto) {
             switch (self.base.options.optimize_mode) {
