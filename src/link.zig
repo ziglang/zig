1357790ec969bb6ee19ade6e8a348bd9d7cbbc4d
Jakub Konka <kubkon@jakubkonka.com>
2022-08-19 12:13:04 +0200

    win: combine PDB fixes into one changeset

--- src/link.zig
+++ src/link.zig
@@ -218,6 +218,10 @@ pub const Options = struct {
     /// (Darwin) remove dylibs that are unreachable by the entry point or exported symbols
     dead_strip_dylibs: bool = false,
 
+    /// (Windows) PDB source path prefix to instruct the linker how to resolve relative
+    /// paths when consolidating CodeView streams into a single PDB file.
+    pdb_source_path: ?[]const u8 = null,
+
     pub fn effectiveOutputMode(options: Options) std.builtin.OutputMode {
         return if (options.use_lld) .Obj else options.output_mode;
     }
