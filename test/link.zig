714b8ea018ea14da5cbea6f1112e6d5be23f6178
Luuk de Gram <luuk@degram.dev>
2022-08-20 15:46:39 +0200

    test/link: add wasm linker-test for archives
    Adds a test case that will pull-in compiler-rt symbols,
    and therefore link with the compiler-rt archive file.

--- test/link.zig
+++ test/link.zig
@@ -47,6 +47,11 @@ fn addWasmCases(cases: *tests.StandaloneContext) void {
         .build_modes = true,
         .requires_stage2 = true,
     });
+
+    cases.addBuildFile("test/link/wasm/archive/build.zig", .{
+        .build_modes = true,
+        .requires_stage2 = true,
+    });
 }
 
 fn addMachOCases(cases: *tests.StandaloneContext) void {
