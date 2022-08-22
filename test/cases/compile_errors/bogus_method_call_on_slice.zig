e141a3b4501482080067ff6a780ba2a23557cd83
Veikka Tuominen <git@vexu.eu>
2022-08-22 11:13:01 +0300

    Sema: fix fieldCallBind on tuples and anon structs
    Closes #12573

--- test/cases/compile_errors/bogus_method_call_on_slice.zig
+++ test/cases/compile_errors/bogus_method_call_on_slice.zig
@@ -3,9 +3,17 @@ fn f(m: []const u8) void {
     m.copy(u8, self[0..], m);
 }
 export fn entry() usize { return @sizeOf(@TypeOf(&f)); }
+pub export fn entry1() void {
+    .{}.bar();
+}
+pub export fn entry2() void {
+    .{ .foo = 1 }.bar();
+}
 
 // error
 // backend=stage2
 // target=native
 //
+// :7:8: error: no field or member function named 'bar' in '@TypeOf(.{})'
+// :10:18: error: no field or member function named 'bar' in 'struct{comptime foo: comptime_int = 1}'
 // :3:6: error: no field or member function named 'copy' in '[]const u8'
