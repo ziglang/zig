26e98391322645f2d09aaaaf36d94cae06424a18
Veikka Tuominen <git@vexu.eu>
2022-08-31 16:46:51 +0300

    disable test for non-llvm backends
    Follow up to fdb88708527742e450e4c566024d9d50ce61dd8d

--- test/behavior/translate_c_macros.zig
+++ test/behavior/translate_c_macros.zig
@@ -115,5 +115,11 @@ test "cast functions" {
 }
 
 test "large integer macro" {
+    if (builtin.zig_backend == .stage2_wasm) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_c) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest; // TODO
+
     try expectEqual(@as(c_ulonglong, 18446744073709550592), h.LARGE_INT);
 }
