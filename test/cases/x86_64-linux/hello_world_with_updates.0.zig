56a131d27a45af793b6adc0795bd240ed74d3634
Jakub Konka <kubkon@jakubkonka.com>
2022-09-04 14:55:02 +0200

    test-cases: fix compiler error locations for hello world with updates

--- test/cases/x86_64-linux/hello_world_with_updates.0.zig
+++ test/cases/x86_64-linux/hello_world_with_updates.0.zig
@@ -2,5 +2,5 @@
 // output_mode=Exe
 // target=x86_64-linux
 //
-// :107:9: error: struct 'tmp.tmp' has no member named 'main'
+// :109:9: error: struct 'tmp.tmp' has no member named 'main'
 // :7:1: note: struct declared here
