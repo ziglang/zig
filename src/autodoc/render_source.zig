a86c7b13a399f1e296a202c414a26ac062cf8b8b
Eric Joldasov <bratishkaerik@getgoogleoff.me>
2022-09-11 14:32:55 +0600

    docgen: add f80 to builtin types

--- src/autodoc/render_source.zig
+++ src/autodoc/render_source.zig
@@ -418,10 +418,11 @@ fn writeEscaped(out: anytype, input: []const u8) !void {
 }
 
 const builtin_types = [_][]const u8{
-    "f16",         "f32",      "f64",       "f128",     "c_longdouble", "c_short",
-    "c_ushort",    "c_int",    "c_uint",    "c_long",   "c_ulong",      "c_longlong",
-    "c_ulonglong", "c_char",   "anyopaque", "void",     "bool",         "isize",
-    "usize",       "noreturn", "type",      "anyerror", "comptime_int", "comptime_float",
+    "f16",          "f32",     "f64",        "f80",          "f128",
+    "c_longdouble", "c_short", "c_ushort",   "c_int",        "c_uint",
+    "c_long",       "c_ulong", "c_longlong", "c_ulonglong",  "c_char",
+    "anyopaque",    "void",    "bool",       "isize",        "usize",
+    "noreturn",     "type",    "anyerror",   "comptime_int", "comptime_float",
 };
 
 fn isType(name: []const u8) bool {
