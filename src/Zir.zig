0a89624d599cfcb884946dfb938945f399df085a
Andrew Kelley <andrew@ziglang.org>
2022-09-13 18:08:59 -0700

    stage2: support being built in ReleaseSafe mode

--- src/Zir.zig
+++ src/Zir.zig
@@ -2602,7 +2602,7 @@ pub const Inst = struct {
         // bigger than expected. Note that in Debug builds, Zig is allowed
         // to insert a secret field for safety checks.
         comptime {
-            if (builtin.mode != .Debug) {
+            if (builtin.mode != .Debug and builtin.mode != .ReleaseSafe) {
                 assert(@sizeOf(Data) == 8);
             }
         }
