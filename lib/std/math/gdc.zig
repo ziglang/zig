bb4d5f3aa7061c41b2b9dcd4f957bcbc8c76ff8a
Chris Boesch <48591413+chrboesch@users.noreply.github.com>
2022-09-25 22:34:03 +0200

    Addition of the function "Greatest common divisor" (gcd) to the math library.

---
+++ lib/std/math/gdc.zig
@@ -0,0 +1,26 @@
+/// Find the greatest common divisor (GCD) of two integers.
+const std = @import("std");
+const expect = std.testing.expect;
+
+// Greatest common divisor
+fn gcd(a: anytype, b: anytype) @TypeOf(a) {
+    if (a < 0 or b < 0) @compileError("gcd is only defined for positive numbers (integer)");
+
+    var x = a;
+    var y = b;
+    var m = a;
+
+    while (y != 0) {
+        m = x % y;
+        x = y;
+        y = m;
+    }
+    return x;
+}
+
+test "gcd" {
+    try expect(gcd(0, 5) == 5);
+    try expect(gcd(33, 77) == 11);
+    try expect(gcd(49865, 69811) == 9973);
+    try expect(gcd(300_000, 2_300_000) == 100_000);
+}
