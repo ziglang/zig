5183e3add1801a56cdc70e07369c2c19d1b80c10
Chris Boesch <48591413+chrboesch@users.noreply.github.com>
2022-09-26 17:50:55 +0200

    Some improvements after review.

--- lib/std/math/gdc.zig
+++ lib/std/math/gdc.zig
@@ -1,15 +1,20 @@
-/// Find the greatest common divisor (GCD) of two integers.
+/// Find the greatest common divisor (GCD) of two integers (uint), which are not all zero.
+/// For example, the GCD of 8 and 12 is 4, that is, gcd(8, 12) == 4.
 const std = @import("std");
 const expect = std.testing.expect;
 
-// Greatest common divisor
-pub fn gcd(a: anytype, b: anytype) @TypeOf(a) {
-    if (a < 0 or b < 0) @compileError("gcd is only defined for positive numbers (integer)");
+// Greatest common divisor (https://mathworld.wolfram.com/GreatestCommonDivisor.html)
+pub fn gcd(a: anytype, b: anytype) @TypeOf(a, b) {
 
-    var x = a;
-    var y = b;
-    var m = a;
+    // only integers are allowed and not both must be zero
+    std.debug.assert(a != 0 or b != 0);
+    std.debug.assert(a >= 0 and b >= 0);
 
+    var x: @TypeOf(a, b) = a;
+    var y: @TypeOf(a, b) = b;
+    var m: @TypeOf(a, b) = a;
+
+    // using the Euclidean algorithm (https://mathworld.wolfram.com/EuclideanAlgorithm.html)
     while (y != 0) {
         m = x % y;
         x = y;
@@ -19,7 +24,10 @@ pub fn gcd(a: anytype, b: anytype) @TypeOf(a) {
 }
 
 test "gcd" {
+    // try expect(gcd(0, 0) == 0);
     try expect(gcd(0, 5) == 5);
+    try expect(gcd(5, 0) == 5);
+    try expect(gcd(8, 12) == 4);
     try expect(gcd(33, 77) == 11);
     try expect(gcd(49865, 69811) == 9973);
     try expect(gcd(300_000, 2_300_000) == 100_000);
