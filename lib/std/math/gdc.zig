2efb6397d32c9027075dfbf60484ac717efea6ff
Chris Boesch <48591413+chrboesch@users.noreply.github.com>
2022-09-26 23:55:37 +0200

    Small improvement in performance.

--- lib/std/math/gdc.zig
+++ lib/std/math/gdc.zig
@@ -1,7 +1,7 @@
 /// Find the greatest common divisor (GCD) of two integers (uint), which are not all zero.
 /// For example, the GCD of 8 and 12 is 4, that is, gcd(8, 12) == 4.
 const std = @import("std");
-const expect = std.testing.expect;
+const expectEQ = std.testing.expectEqual;
 
 // Greatest common divisor (https://mathworld.wolfram.com/GreatestCommonDivisor.html)
 pub fn gcd(a: anytype, b: anytype) @TypeOf(a, b) {
@@ -10,6 +10,11 @@ pub fn gcd(a: anytype, b: anytype) @TypeOf(a, b) {
     std.debug.assert(a != 0 or b != 0);
     std.debug.assert(a >= 0 and b >= 0);
 
+    // if one of them is zero, the other is returned
+    if (a == 0) return b;
+    if (b == 0) return a;
+
+    // init vars
     var x: @TypeOf(a, b) = a;
     var y: @TypeOf(a, b) = b;
     var m: @TypeOf(a, b) = a;
@@ -24,11 +29,13 @@ pub fn gcd(a: anytype, b: anytype) @TypeOf(a, b) {
 }
 
 test "gcd" {
-    // try expect(gcd(0, 0) == 0);
-    try expect(gcd(0, 5) == 5);
-    try expect(gcd(5, 0) == 5);
-    try expect(gcd(8, 12) == 4);
-    try expect(gcd(33, 77) == 11);
-    try expect(gcd(49865, 69811) == 9973);
-    try expect(gcd(300_000, 2_300_000) == 100_000);
+    try expectEQ(gcd(0, 5), 5);
+    try expectEQ(gcd(5, 0), 5);
+    try expectEQ(gcd(8, 12), 4);
+    try expectEQ(gcd(12, 8), 4);
+    try expectEQ(gcd(33, 77), 11);
+    try expectEQ(gcd(77, 33), 11);
+    try expectEQ(gcd(49865, 69811), 9973);
+    try expectEQ(gcd(300_000, 2_300_000), 100_000);
+    try expectEQ(gcd(90000000_000_000_000_000_000, 2), 2);
 }
