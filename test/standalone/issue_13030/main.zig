9cad44770a15d189cddb749bc1b2fca9e24b858f
Jacob Young <jacobly0@users.noreply.github.com>
2022-10-03 10:28:30 -0400

    test/standalone: remove unneeded FnPtr
    The behavior of this test is not affected by an extra level of
    indirection.

--- test/standalone/issue_13030/main.zig
+++ test/standalone/issue_13030/main.zig
@@ -1,7 +1,7 @@
-fn b(comptime T: type) ?@import("std").meta.FnPtr(fn () error{}!T) {
+fn b(comptime T: type) ?*const fn () error{}!T {
     return null;
 }
 
-export fn c() void {
+export fn entry() void {
     _ = b(void);
 }
