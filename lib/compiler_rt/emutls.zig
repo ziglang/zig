ziglang/zig

commit f14cc753d8d9aa9df7736fb1857fb1017676e432

use @ptrCast to assigned generic type to default_value
If the type happens to be a pointer, then the double pointer will not
coerce implicitly.

lib/compiler_rt/emutls.zig
@@ -296,7 +296,7 @@ const emutls_control = extern struct {
             .size = @sizeOf(T),
             .alignment = @alignOf(T),
             .object = .{ .index = 0 },
-            .default_value = @ptrCast(?*anyopaque, default_value),
+            .default_value = @ptrCast(?*const anyopaque, default_value),
         };
     }
 

lib/std/enums.zig
@@ -16,7 +16,7 @@ pub fn EnumFieldStruct(comptime E: type, comptime Data: type, comptime field_def
         fields = fields ++ &[_]StructField{.{
             .name = field.name,
             .field_type = Data,
-            .default_value = if (field_default) |d| &d else null,
+            .default_value = if (field_default) |d| @ptrCast(?*const anyopaque, &d) else null,
             .is_comptime = false,
             .alignment = if (@sizeOf(Data) > 0) @alignOf(Data) else 0,
         }};
@@ -158,6 +158,20 @@ test "std.enums.directEnumArrayDefault" {
     try testing.expectEqual(true, array[4]);
     try testing.expectEqual(false, array[6]);
     try testing.expectEqual(false, array[2]);
+}
+
+test "std.enums.directEnumArrayDefault slice" {
+    const E = enum(i4) { a = 4, b = 6, c = 2 };
+    var runtime_b = "b";
+    const array = directEnumArrayDefault(E, []const u8, "default", 4, .{
+        .a = "a",
+        .b = runtime_b,
+    });
+
+    try testing.expectEqual([7][]const u8, @TypeOf(array));
+    try testing.expectEqualSlices(u8, "a", array[4]);
+    try testing.expectEqualSlices(u8, "b", array[6]);
+    try testing.expectEqualSlices(u8, "default", array[2]);
 }
 
 /// Cast an enum literal, value, or string to the enum value of type E

commit b086bdd026e90c6c6c6713ff3b9fb9d231d9e76c

emutls: add const to default_value field
Commit f14cc75 accidentally added a const when grepping for assignments
to `std.builtin.Type.StructField.default_value`, however when looking
into it further, I noticed that even though this default_value field is
emitted into the .data section, the value it points to is actually
emitted into the .rodata section, so it seems correct to use const here.

lib/compiler_rt/emutls.zig
@@ -141,7 +141,7 @@ const ObjectArray = struct {
 
             if (control.default_value) |value| {
                 // default value: copy the content to newly allocated object.
-                @memcpy(data, @ptrCast([*]u8, value), size);
+                @memcpy(data, @ptrCast([*]const u8, value), size);
             } else {
                 // no default: return zeroed memory.
                 @memset(data, 0, size);
@@ -236,7 +236,7 @@ const emutls_control = extern struct {
     },
 
     // null or non-zero initial value for the object
-    default_value: ?*anyopaque,
+    default_value: ?*const anyopaque,
 
     // global Mutex used to serialize control.index initialization.
     var mutex: std.c.pthread_mutex_t = std.c.PTHREAD_MUTEX_INITIALIZER;
@@ -291,7 +291,7 @@ const emutls_control = extern struct {
     }
 
     /// Simple helper for testing purpose.
-    pub fn init(comptime T: type, default_value: ?*T) emutls_control {
+    pub fn init(comptime T: type, default_value: ?*const T) emutls_control {
         return emutls_control{
             .size = @sizeOf(T),
             .alignment = @alignOf(T),
@@ -362,7 +362,7 @@ test "__emutls_get_address zeroed" {
 test "__emutls_get_address with default_value" {
     if (!builtin.link_libc or builtin.os.tag != .openbsd) return error.SkipZigTest;
 
-    var value: usize = 5678; // default value
+    const value: usize = 5678; // default value
     var ctl = emutls_control.init(usize, &value);
     try expect(ctl.object.index == 0);
 
@@ -384,8 +384,7 @@ test "test default_value with differents sizes" {
 
     const testType = struct {
         fn _testType(comptime T: type, value: T) !void {
-            var def: T = value;
-            var ctl = emutls_control.init(T, &def);
+            var ctl = emutls_control.init(T, &value);
             var x = ctl.get_typed_pointer(T);
             try expect(x.* == value);
         }