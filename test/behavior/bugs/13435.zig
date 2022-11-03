ff0a9dcb2eb646de9c5d459c80bbde15a8c6f28b
Micah Switzer <micah.switzer.dev@outlook.com>
2022-11-03 19:56:23 -0400

    Sema: resolve lazy align in zirReify for union fields
    Closes #13435

---
+++ test/behavior/bugs/13435.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+
+fn CreateUnion(comptime T: type) type {
+    return @Type(.{
+        .Union = .{
+            .layout = .Auto,
+            .tag_type = null,
+            .fields = &[_]std.builtin.Type.UnionField{
+                .{
+                    .name = "field",
+                    .field_type = T,
+                    .alignment = @alignOf(T),
+                },
+            },
+            .decls = &[_]std.builtin.Type.Declaration{},
+        },
+    });
+}
+
+test {
+    _ = CreateUnion(struct {});
+}
