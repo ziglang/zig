0dd5bb0714181838227729a6cdc19def7f35e0b0
Veikka Tuominen <git@vexu.eu>
2022-11-18 14:45:46 +0200

    skip more self-hosted targets

--- test/behavior/bugs/12891.zig
+++ test/behavior/bugs/12891.zig
@@ -29,26 +29,44 @@ test "inf >= 1" {
     try std.testing.expect(f >= i);
 }
 test "isNan(nan * 1)" {
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest; // TODO
+
     const nan_times_one = comptime std.math.nan(f64) * 1;
     try std.testing.expect(std.math.isNan(nan_times_one));
 }
 test "runtime isNan(nan * 1)" {
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest; // TODO
+
     const nan_times_one = std.math.nan(f64) * 1;
     try std.testing.expect(std.math.isNan(nan_times_one));
 }
 test "isNan(nan * 0)" {
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest; // TODO
+
     const nan_times_zero = comptime std.math.nan(f64) * 0;
     try std.testing.expect(std.math.isNan(nan_times_zero));
     const zero_times_nan = 0 * comptime std.math.nan(f64);
     try std.testing.expect(std.math.isNan(zero_times_nan));
 }
 test "isNan(inf * 0)" {
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest; // TODO
+
     const inf_times_zero = comptime std.math.inf(f64) * 0;
     try std.testing.expect(std.math.isNan(inf_times_zero));
     const zero_times_inf = 0 * comptime std.math.inf(f64);
     try std.testing.expect(std.math.isNan(zero_times_inf));
 }
 test "runtime isNan(nan * 0)" {
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest; // TODO
     if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest; // TODO
 
     const nan_times_zero = std.math.nan(f64) * 0;
@@ -57,6 +75,8 @@ test "runtime isNan(nan * 0)" {
     try std.testing.expect(std.math.isNan(zero_times_nan));
 }
 test "runtime isNan(inf * 0)" {
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest; // TODO
     if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest; // TODO
 
     const inf_times_zero = std.math.inf(f64) * 0;
