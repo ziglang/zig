e21b6a712d7e5c7acb19a7d522406a705223d325
Andrew Kelley <andrew@ziglang.org>
2022-11-21 20:15:22 -0700

    CI: switch to GitHub Actions

--- ci/linux/build-tarballs.sh
+++ ci/linux/build-tarballs.sh
@@ -12,7 +12,7 @@ WORKDIR="$(pwd)"
 ARCH="$(uname -m)"
 TARGET="$ARCH-linux-musl"
 MCPU="baseline"
-CACHE_BASENAME="zig+llvm+lld+clang-$TARGET-0.10.0-dev.4560+828735ac0"
+CACHE_BASENAME="zig+llvm+lld+clang-$TARGET-0.11.0-dev.256+271cc52a1"
 PREFIX="$HOME/deps/$CACHE_BASENAME"
 ZIG="$PREFIX/bin/zig" 
 
@@ -45,7 +45,6 @@ unset CXX
 
 ninja install
 
-# Make sure our ZIG_VERSION below is correct.
 ZIG_VERSION="$(stage3-release/bin/zig version)"
 BOOTSTRAP_SRC="$WORKDIR/zig-bootstrap"
 TARBALLS_DIR="$WORKDIR/tarballs"
@@ -56,29 +55,46 @@ rm -rf zig
 cp -r "$ZIGDIR" ./
 sed -i "/^ZIG_VERSION=\".*\"\$/c\\ZIG_VERSION=\"$ZIG_VERSION\"" build
 
-CMAKE_GENERATOR=Ninja ./build aarch64-linux-musl baseline
-CMAKE_GENERATOR=Ninja ./build x86_64-linux-musl baseline
-#CMAKE_GENERATOR=Ninja ./build x86-linux-musl baseline
-#CMAKE_GENERATOR=Ninja ./build riscv64-linux-musl baseline
-#CMAKE_GENERATOR=Ninja ./build arm-linux-musleabihf generic+v7a
-CMAKE_GENERATOR=Ninja ./build x86_64-macos-none baseline
-CMAKE_GENERATOR=Ninja ./build aarch64-macos-none apple_a14
-CMAKE_GENERATOR=Ninja ./build x86_64-windows-gnu baseline
-#CMAKE_GENERATOR=Ninja ./build x86-windows-gnu baseline
-#CMAKE_GENERATOR=Ninja ./build aarch64-windows-gnu baseline
+# Unfortunately Debian's cmake (3.14.4) is too old for zig-bootstrap.
+PATH="$HOME/local/bin:$PATH" CMAKE_GENERATOR=Ninja ./build aarch64-linux-musl baseline
+PATH="$HOME/local/bin:$PATH" CMAKE_GENERATOR=Ninja ./build x86_64-linux-musl baseline
+#PATH="$HOME/local/bin:$PATH" CMAKE_GENERATOR=Ninja ./build x86-linux-musl baseline
+#PATH="$HOME/local/bin:$PATH" CMAKE_GENERATOR=Ninja ./build riscv64-linux-musl baseline
+#PATH="$HOME/local/bin:$PATH" CMAKE_GENERATOR=Ninja ./build arm-linux-musleabihf generic+v7a
+PATH="$HOME/local/bin:$PATH" CMAKE_GENERATOR=Ninja ./build x86_64-macos-none baseline
+PATH="$HOME/local/bin:$PATH" CMAKE_GENERATOR=Ninja ./build aarch64-macos-none apple_a14
+PATH="$HOME/local/bin:$PATH" CMAKE_GENERATOR=Ninja ./build x86_64-windows-gnu baseline
+#PATH="$HOME/local/bin:$PATH" CMAKE_GENERATOR=Ninja ./build x86-windows-gnu baseline
+#PATH="$HOME/local/bin:$PATH" CMAKE_GENERATOR=Ninja ./build aarch64-windows-gnu baseline
 
 ZIG="$BOOTSTRAP_SRC/out/host/bin/zig"
 
-cd "$ZIGDIR"
-SRC_TARBALL="$TARBALLS_DIR/zig-$ZIG_VERSION.tar.xz"
-git archive --format=tar.xz -o "$SRC_TARBALL" --prefix=zig-$ZIG_VERSION/ $ZIG_VERSION
+rm -rf "$TARBALLS_DIR"
+mkdir "$TARBALLS_DIR"
+cd "$TARBALLS_DIR"
+
+cp -r "$ZIGDIR" ./
+rm -rf \
+  "zig/.github" \
+  "zig/.gitignore" \
+  "zig/.git" \
+  "zig/ci" \
+  "zig/CODE_OF_CONDUCT.md" \
+  "zig/CONTRIBUTING.md" \
+  "zig/.builds" \
+  "zig/build" \
+  "zig/build-release" \
+  "zig/build-debug" \
+  "zig/zig-cache"
+mv zig "zig-$ZIG_VERSION"
+tar cfJ "zig-$ZIG_VERSION.tar.xz" "zig-$ZIG_VERSION"
 
 cd "$BOOTSTRAP_SRC"
 "$ZIG" build --build-file zig/build.zig docs
 LANGREF_HTML="$BOOTSTRAP_SRC/zig/zig-cache/langref.html"
+# Look for HTML errors.
+tidy --drop-empty-elements no -qe "$LANGREF_HTML"
 
-rm -rf "$TARBALLS_DIR"
-mkdir "$TARBALLS_DIR"
 cd "$TARBALLS_DIR"
 
 cp -r $BOOTSTRAP_SRC/out/zig-aarch64-linux-musl-baseline zig-linux-aarch64-$ZIG_VERSION/
@@ -166,17 +182,17 @@ mkdir zig-macos-x86_64-$ZIG_VERSION/doc/
 #mkdir zig-freebsd-x86_64-$ZIG_VERSION/doc/
 
 cd $REL_SRC_BUILD
-stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-linux-aarch64 -target aarch64-linux-musl -fno-emit-bin 
-#stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-linux-armv7a -target arm-linux-musl -mcpu=generic+v7a -fno-emit-bin 
-#stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-linux-x86 -target x86-linux-musl -fno-emit-bin 
-#stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-linux-riscv64 -target riscv64-linux-musl -fno-emit-bin 
-stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-linux-x86_64 -target x86_64-linux-musl -fno-emit-bin 
-#stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-windows-x86 -target x86-windows-gnu -fno-emit-bin 
-stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-windows-x86_64 -target x86_64-windows-gnu -fno-emit-bin 
-#stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-windows-aarch64 -target aarch64-windows-gnu -fno-emit-bin 
-stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-macos-x86_64 -target x86_64-macos -fno-emit-bin 
-stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-macos-aarch64 -target aarch64-macos -fno-emit-bin 
-#stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-freebsd-x86_64 -target x86_64-freebsd -fno-emit-bin 
+stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-linux-aarch64 -target aarch64-linux-musl -fno-emit-bin --zig-lib-dir ../lib
+#stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-linux-armv7a -target arm-linux-musl -mcpu=generic+v7a -fno-emit-bin --zig-lib-dir ../lib
+#stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-linux-x86 -target x86-linux-musl -fno-emit-bin --zig-lib-dir ../lib
+#stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-linux-riscv64 -target riscv64-linux-musl -fno-emit-bin --zig-lib-dir ../lib
+stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-linux-x86_64 -target x86_64-linux-musl -fno-emit-bin --zig-lib-dir ../lib
+#stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-windows-x86 -target x86-windows-gnu -fno-emit-bin --zig-lib-dir ../lib
+stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-windows-x86_64 -target x86_64-windows-gnu -fno-emit-bin --zig-lib-dir ../lib
+#stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-windows-aarch64 -target aarch64-windows-gnu -fno-emit-bin --zig-lib-dir ../lib
+stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-macos-x86_64 -target x86_64-macos -fno-emit-bin --zig-lib-dir ../lib
+stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-macos-aarch64 -target aarch64-macos -fno-emit-bin --zig-lib-dir ../lib
+#stage3-release/bin/zig test ../lib/std/std.zig -femit-docs=doc-freebsd-x86_64 -target x86_64-freebsd -fno-emit-bin --zig-lib-dir ../lib
 
 cd "$TARBALLS_DIR"
 cp -r $REL_SRC_BUILD/doc-linux-aarch64 zig-linux-aarch64-$ZIG_VERSION/doc/std
@@ -202,18 +218,6 @@ cp $LANGREF_HTML zig-windows-x86_64-$ZIG_VERSION/doc/
 cp $LANGREF_HTML zig-macos-aarch64-$ZIG_VERSION/doc/
 cp $LANGREF_HTML zig-macos-x86_64-$ZIG_VERSION/doc/
 #cp $LANGREF_HTML zig-freebsd-x86_64-$ZIG_VERSION/doc/
-
-#rm -rf zig-linux-aarch64-$ZIG_VERSION/doc/std/src
-#rm -rf zig-linux-armv7a-$ZIG_VERSION/doc/std/src
-#rm -rf zig-linux-x86-$ZIG_VERSION/doc/std/src
-#rm -rf zig-linux-riscv64-$ZIG_VERSION/doc/std/src
-#rm -rf zig-linux-x86_64-$ZIG_VERSION/doc/std/src
-##rm -rf zig-windows-x86-$ZIG_VERSION/doc/std/src
-#rm -rf zig-windows-x86_64-$ZIG_VERSION/doc/std/src
-#rm -rf zig-windows-aarch64-$ZIG_VERSION/doc/std/src
-#rm -rf zig-macos-aarch64-$ZIG_VERSION/doc/std/src
-#rm -rf zig-macos-x86_64-$ZIG_VERSION/doc/std/src
-#rm -rf zig-freebsd-x86_64-$ZIG_VERSION/doc/std/src
 
 tar cJf zig-linux-aarch64-$ZIG_VERSION.tar.xz zig-linux-aarch64-$ZIG_VERSION/
 #tar cJf zig-linux-armv7a-$ZIG_VERSION.tar.xz zig-linux-armv7a-$ZIG_VERSION/
@@ -241,8 +245,9 @@ s3cmd put -P --add-header="cache-control: public, max-age=31536000, immutable" z
 s3cmd put -P --add-header="cache-control: public, max-age=31536000, immutable" zig-macos-x86_64-$ZIG_VERSION.tar.xz s3://ziglang.org/builds/$ZIG_VERSION/
 #s3cmd put -P --add-header="cache-control: public, max-age=31536000, immutable" zig-freebsd-x86_64-$ZIG_VERSION.tar.xz s3://ziglang.org/builds/$ZIG_VERSION/
 
-export SRC_SHASUM=$(sha256sum $SRC_TARBALL | cut '-d ' -f1)
-export SRC_BYTESIZE=$(wc -c < $SRC_TARBALL)
+export SRC_TARBALL="zig-$ZIG_VERSION.tar.xz"
+export SRC_SHASUM=$(sha256sum "zig-$ZIG_VERSION.tar.xz" | cut '-d ' -f1)
+export SRC_BYTESIZE=$(wc -c < "zig-$ZIG_VERSION.tar.xz")
 
 export X86_64_WINDOWS_TARBALL="zig-windows-x86_64-$ZIG_VERSION.zip"
 export X86_64_WINDOWS_BYTESIZE=$(wc -c < "zig-windows-x86_64-$ZIG_VERSION.zip")
@@ -260,9 +265,9 @@ export X86_64_LINUX_TARBALL="zig-linux-x86_64-$ZIG_VERSION.tar.xz"
 export X86_64_LINUX_BYTESIZE=$(wc -c < "zig-linux-x86_64-$ZIG_VERSION.tar.xz")
 export X86_64_LINUX_SHASUM="$(sha256sum "zig-linux-x86_64-$ZIG_VERSION.tar.xz" | cut '-d ' -f1)"
 
-#export AARCH64_LINUX_TARBALL="zig-linux-aarch64-$ZIG_VERSION.tar.xz"
-#export AARCH64_LINUX_BYTESIZE=$(wc -c < "zig-linux-aarch64-$ZIG_VERSION.tar.xz")
-#export AARCH64_LINUX_SHASUM="$(sha256sum "zig-linux-aarch64-$ZIG_VERSION.tar.xz" | cut '-d ' -f1)"
+export AARCH64_LINUX_TARBALL="zig-linux-aarch64-$ZIG_VERSION.tar.xz"
+export AARCH64_LINUX_BYTESIZE=$(wc -c < "zig-linux-aarch64-$ZIG_VERSION.tar.xz")
+export AARCH64_LINUX_SHASUM="$(sha256sum "zig-linux-aarch64-$ZIG_VERSION.tar.xz" | cut '-d ' -f1)"
 
 #export X86_64_FREEBSD_TARBALL="zig-freebsd-x86_64-$ZIG_VERSION.tar.xz"
 #export X86_64_FREEBSD_BYTESIZE=$(wc -c < "zig-freebsd-x86_64-$ZIG_VERSION.tar.xz")
@@ -276,9 +281,9 @@ export MASTER_DATE="$(date +%Y-%m-%d)"
 export MASTER_VERSION="$ZIG_VERSION"
 
 # Create index.json and update the website repo.
-CIDIR="$ZIGDIR/ci"
-cd "$CIDIR"
+cd "$ZIGDIR/ci"
 "$ZIG" run update-download-page.zig
+mv out "$TARBALLS_DIR/out"
 
 cd "$WORKDIR/www.ziglang.org"
 # This is the user when pushing to the website repo.
@@ -286,7 +291,7 @@ git config user.email "ziggy@ziglang.org"
 git config user.name "Ziggy"
 git pull
 
-cp "$CIDIR/out/index.json" data/releases.json
+cp "$TARBALLS_DIR/out/index.json" data/releases.json
 git add data/releases.json
 git commit -m "CI: update releases"
 git push origin master
@@ -304,7 +309,7 @@ git push origin master
 # * Figure out how to adjust the Cloudfront settings to increase the max size for
 #   auto-compressed objects.
 # * Migrate to fastly.
-DOCDIR="$REL_SRC_BUILD/doc-linux-aarch64"
+DOCDIR="$TARBALLS_DIR/zig-linux-aarch64-$ZIG_VERSION/doc"
 s3cmd put -P --no-mime-magic \
   --add-header="Cache-Control: max-age=0, must-revalidate" \
   "$LANGREF_HTML" s3://ziglang.org/documentation/master/index.html
