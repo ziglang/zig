cf543199cad033b9739ce1b623d95fd30c973791
Jakub Konka <kubkon@jakubkonka.com>
2022-12-02 10:29:36 +0100

    mix_c_files: skip build modes on aarch64-windows due to bug

--- test/standalone.zig
+++ test/standalone.zig
@@ -31,7 +31,8 @@ pub fn addCases(cases: *tests.StandaloneContext) void {
     cases.addBuildFile("test/standalone/shared_library/build.zig", .{});
     cases.addBuildFile("test/standalone/mix_o_files/build.zig", .{});
     cases.addBuildFile("test/standalone/mix_c_files/build.zig", .{
-        .build_modes = true,
+        // https://github.com/ziglang/zig/issues/13738
+        .build_modes = !(builtin.os.tag == .windows and builtin.cpu.arch == .aarch64),
         .cross_targets = true,
     });
     cases.addBuildFile("test/standalone/global_linkage/build.zig", .{});
