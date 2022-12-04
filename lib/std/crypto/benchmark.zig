ee5f9997234b12052f2122c2c47d1ede0ae12e73
Frank Denis <github@pureftpd.org>
2022-12-05 00:57:29 +0100

    std.crypto benchmark: don't use a relative path to import std

--- lib/std/crypto/benchmark.zig
+++ lib/std/crypto/benchmark.zig
@@ -1,6 +1,6 @@
 // zig run -O ReleaseFast --zig-lib-dir ../.. benchmark.zig
 
-const std = @import("../std.zig");
+const std = @import("std");
 const builtin = @import("builtin");
 const mem = std.mem;
 const time = std.time;
