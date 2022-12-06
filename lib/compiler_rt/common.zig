c4dc8515b6321828e1bfa404657f9a5f33aa1346
Jacob Young <jacobly0@users.noreply.github.com>
2022-12-06 00:22:39 -0500

    compiler_rt: don't use the llvm windows v2u64 abi with the C backend

--- lib/compiler_rt/common.zig
+++ lib/compiler_rt/common.zig
@@ -20,7 +20,7 @@ pub const want_ppc_abi = builtin.cpu.arch.isPPC() or builtin.cpu.arch.isPPC64();
 
 // Libcalls that involve u128 on Windows x86-64 are expected by LLVM to use the
 // calling convention of @Vector(2, u64), rather than what's standard.
-pub const want_windows_v2u64_abi = builtin.os.tag == .windows and builtin.cpu.arch == .x86_64;
+pub const want_windows_v2u64_abi = builtin.os.tag == .windows and builtin.cpu.arch == .x86_64 and @import("builtin").object_format != .c;
 
 /// This governs whether to use these symbol names for f16/f32 conversions
 /// rather than the standard names:
