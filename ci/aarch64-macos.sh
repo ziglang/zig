f46567e6bf80c4f5b21608383bf99cca8a77c3b0
Andrew Kelley <andrew@ziglang.org>
2022-12-05 21:00:01 -0700

    CI: update tarballs
    In particular, these two changes are relevant:
    
     * zig cc: support -stack in addition to --stack for linker arg
       - Fixes stack overflow when running zig2 on aarch64-macos.
     * compiler_rt: avoid using weak aliases
       - Fixes duplicate symbol when linking zig2 on aarch64-linux.

--- ci/aarch64-macos.sh
+++ ci/aarch64-macos.sh
@@ -9,7 +9,7 @@ set -e
 ZIGDIR="$(pwd)"
 TARGET="$ARCH-macos-none"
 MCPU="baseline"
-CACHE_BASENAME="zig+llvm+lld+clang-$TARGET-0.11.0-dev.448+e6e459e9e"
+CACHE_BASENAME="zig+llvm+lld+clang-$TARGET-0.11.0-dev.534+b0b1cc356"
 PREFIX="$HOME/$CACHE_BASENAME"
 ZIG="$PREFIX/bin/zig"
 
