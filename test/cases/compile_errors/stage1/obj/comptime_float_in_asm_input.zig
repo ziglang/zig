41913ddb1a94591c305870ad2854ecff67ac3242
Veikka Tuominen <git@vexu.eu>
2022-12-11 19:03:42 +0200

    remove no longer relevant stage1 compile errors

--- test/cases/compile_errors/stage1/obj/comptime_float_in_asm_input.zig
+++
@@ -1,9 +0,0 @@
-export fn foo() void {
-    asm volatile ("" : : [bar]"r"(3.17) : "");
-}
-
-// error
-// backend=stage1
-// target=native
-//
-// tmp.zig:2:35: error: expected sized integer or sized float, found comptime_float
