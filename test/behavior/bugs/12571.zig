b5d1df091b3c2b8943a1bdece5d48a4355489a4e
Evin Yulo <yujiri@disroot.org>
2022-12-14 18:43:48 -0500

    Add test for #12571
    Closes #12571

---
+++ test/behavior/bugs/12571.zig
@@ -0,0 +1,22 @@
+const builtin = @import("builtin");
+const std = @import("std");
+const expect = std.testing.expect;
+
+const Frame = packed struct {
+    num: u20,
+};
+
+const Entry = packed struct {
+    other: u12,
+    frame: Frame,
+};
+
+test {
+    if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest; // TODO
+
+    const frame = Frame{ .num = 0x7FDE };
+    var entry = Entry{ .other = 0, .frame = .{ .num = 0xFFFFF } };
+    entry.frame = frame;
+    try expect(entry.frame.num == 0x7FDE);
+}
