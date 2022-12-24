eaa61e39ed6866e6359126c785524c8bba7336f8
Jacob Young <jacobly0@users.noreply.github.com>
2022-12-23 19:00:20 -0500

    behavior: disable tests that trigger an llvm 15 bug and assertion
    ```
    LLVM Emit Object... zig: llvm/include/llvm/ADT/APInt.h:840: void llvm::APInt::lshrInPlace(unsigned int): Assertion `ShiftAmt <= BitWidth && "Invalid shift amount"' failed.
    Aborted
    ```
    
    Tracked by #13782

--- test/behavior/bugs/12169.zig
+++ test/behavior/bugs/12169.zig
@@ -7,6 +7,12 @@ test {
     if (builtin.zig_backend == .stage2_x86) return error.SkipZigTest; // TODO
     if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest; // TODO
     if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest; // TODO
+
+    if (comptime builtin.zig_backend == .stage2_llvm and builtin.cpu.arch.endian() == .Big) {
+        // https://github.com/ziglang/zig/issues/13782
+        return error.SkipZigTest;
+    }
+
     const a = @Vector(2, bool){ true, true };
     const b = @Vector(1, bool){true};
     try std.testing.expect(@reduce(.And, a));
