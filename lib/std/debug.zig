2f83764c3f38574ce1e202c285b2417989e9f80f
Brendan Burns <bburns@microsoft.com>
2023-01-03 23:31:18 +0000

    Remove hasEnvVarConstant

--- lib/std/debug.zig
+++ lib/std/debug.zig
@@ -110,25 +110,31 @@ pub fn getSelfDebugInfo() !*DebugInfo {
 }
 
 pub fn detectTTYConfig(file: std.fs.File) TTY.Config {
-    if (process.hasEnvVarConstant("ZIG_DEBUG_COLOR")) {
-        return .escape_codes;
-    } else if (process.hasEnvVarConstant("NO_COLOR")) {
+    if (builtin.os.tag == .wasi) {
+        // Per https://github.com/WebAssembly/WASI/issues/162 ANSI codes
+        // aren't currently supported.
         return .no_color;
     } else {
-        if (file.supportsAnsiEscapeCodes()) {
+        if (process.hasEnvVarConstant("ZIG_DEBUG_COLOR")) {
             return .escape_codes;
-        } else if (native_os == .windows and file.isTty()) {
-            var info: windows.CONSOLE_SCREEN_BUFFER_INFO = undefined;
-            if (windows.kernel32.GetConsoleScreenBufferInfo(file.handle, &info) != windows.TRUE) {
-                // TODO: Should this return an error instead?
+        } else if (process.hasEnvVarConstant("NO_COLOR")) {
+            return .no_color;
+        } else {
+            if (file.supportsAnsiEscapeCodes()) {
+                return .escape_codes;
+            } else if (native_os == .windows and file.isTty()) {
+                var info: windows.CONSOLE_SCREEN_BUFFER_INFO = undefined;
+                if (windows.kernel32.GetConsoleScreenBufferInfo(file.handle, &info) != windows.TRUE) {
+                    // TODO: Should this return an error instead?
+                    return .no_color;
+                }
+                return .{ .windows_api = .{
+                    .handle = file.handle,
+                    .reset_attributes = info.wAttributes,
+                } };
+            } else {
                 return .no_color;
             }
-            return .{ .windows_api = .{
-                .handle = file.handle,
-                .reset_attributes = info.wAttributes,
-            } };
-        } else {
-            return .no_color;
         }
     }
 }
