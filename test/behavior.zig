868cbb334c749951f8462f5144c81f4b3d06f576
Michael Dusan <michael.dusan@gmail.com>
2023-01-04 15:18:17 -0500

    llvm codegen: fix f16,f32,f64 nan bitcasts
    @bitCast from integer NaN representation to float NaN resulted in
    changed bits in float. This only happened with signaled NaN.
    
    - added test for signaled NaN
    - added tests for quiet NaN (for completeness)
    
    closes #14198

--- test/behavior.zig
+++ test/behavior.zig
@@ -240,6 +240,7 @@ test {
     {
         _ = @import("behavior/bugs/13063.zig");
         _ = @import("behavior/bugs/11227.zig");
+        _ = @import("behavior/bugs/14198.zig");
         _ = @import("behavior/export.zig");
     }
 
