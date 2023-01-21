f85c01d4c7520d2242626f4c0684ab97e47af373
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2023-01-21 15:47:31 -0800

    Implement gzip header CRC check.
    From RFC 1952:
    
    > If FHCRC is set, a CRC16 for the gzip header is present,
    > immediately before the compressed data. The CRC16 consists
    > of the two least significant bytes of the CRC32 for all
    > bytes of the gzip header up to and not including the CRC16.

--- lib/std/compress.zig
+++ lib/std/compress.zig
@@ -4,6 +4,36 @@ pub const deflate = @import("compress/deflate.zig");
 pub const gzip = @import("compress/gzip.zig");
 pub const zlib = @import("compress/zlib.zig");
 
+pub fn HashedReader(
+    comptime ReaderType: anytype,
+    comptime HasherType: anytype,
+) type {
+    return struct {
+        child_reader: ReaderType,
+        hasher: HasherType,
+
+        pub const Error = ReaderType.Error;
+        pub const Reader = std.io.Reader(*@This(), Error, read);
+
+        pub fn read(self: *@This(), buf: []u8) Error!usize {
+            const amt = try self.child_reader.read(buf);
+            self.hasher.update(buf);
+            return amt;
+        }
+
+        pub fn reader(self: *@This()) Reader {
+            return .{ .context = self };
+        }
+    };
+}
+
+pub fn hashedReader(
+    reader: anytype,
+    hasher: anytype,
+) HashedReader(@TypeOf(reader), @TypeOf(hasher)) {
+    return .{ .child_reader = reader, .hasher = hasher };
+}
+
 test {
     _ = deflate;
     _ = gzip;
