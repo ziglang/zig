db6908c41e1898dad48341b56e00aeec4cf9629a
John Schmidt <john.schmidt.h@gmail.com>
2023-02-05 22:13:23 +0100

    Fix pointer diff test case on Windows

--- test/translate_c.zig
+++ test/translate_c.zig
@@ -3917,18 +3917,35 @@ pub fn addCases(cases: *tests.TranslateCContext) void {
         \\}
     });
 
-    cases.add("Pointer subtraction with typedef",
-        \\typedef char* S;
-        \\void foo() {
-        \\    S a, b;
-        \\    long c = a - b;
-        \\}
-    , &[_][]const u8{
-        \\pub export fn foo() void {
-        \\    var a: S = undefined;
-        \\    var b: S = undefined;
-        \\    var c: c_long = @divExact(@bitCast(c_long, @ptrToInt(a) -% @ptrToInt(b)), @sizeOf(u8));
-        \\    _ = @TypeOf(c);
-        \\}
-    });
+    if (builtin.os.tag == .windows) {
+        cases.add("Pointer subtraction with typedef",
+            \\typedef char* S;
+            \\void foo() {
+            \\    S a, b;
+            \\    long long c = a - b;
+            \\}
+        , &[_][]const u8{
+            \\pub export fn foo() void {
+            \\    var a: S = undefined;
+            \\    var b: S = undefined;
+            \\    var c: c_longlong = @divExact(@bitCast(c_longlong, @ptrToInt(a) -% @ptrToInt(b)), @sizeOf(u8));
+            \\    _ = @TypeOf(c);
+            \\}
+        });
+    } else {
+        cases.add("Pointer subtraction with typedef",
+            \\typedef char* S;
+            \\void foo() {
+            \\    S a, b;
+            \\    long c = a - b;
+            \\}
+        , &[_][]const u8{
+            \\pub export fn foo() void {
+            \\    var a: S = undefined;
+            \\    var b: S = undefined;
+            \\    var c: c_long = @divExact(@bitCast(c_long, @ptrToInt(a) -% @ptrToInt(b)), @sizeOf(u8));
+            \\    _ = @TypeOf(c);
+            \\}
+        });
+    }
 }
