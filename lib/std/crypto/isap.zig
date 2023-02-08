f62e3b8c0dd232bddb559405b00c1c1c4815f359
Frank Denis <github@pureftpd.org>
2023-02-08 14:23:48 +0100

    std.crypto: add the Ascon permutation
    Ascon has been selected as new standard for lightweight cryptography
    in the NIST Lightweight Cryptography competition.
    
    Ascon won over Gimli and Xoodoo.
    
    The permutation is unlikely to change. However, NIST may tweak
    the constructions (XOF, hash, authenticated encryption) before
    standardizing them. For that reason, implementations of those
    are better maintained outside the standard library for now.
    
    In fact, we already had an Ascon implementation in Zig:
    `std.crypto.aead.isap` is based on it. While the implementation was
    here, there was no public API to access it directly.
    
    So:
    
    - The Ascon permutation is now available as `std.crypto.core.Ascon`,
    with everything needed to use it in AEADs and other Ascon-based
    constructions
    - The ISAP implementation now uses std.crypto.core.Ascon instead of
    keeping a private copy
    - The default CSPRNG replaces Xoodoo with Ascon. And instead of an
    ad-hoc construction, it's using the XOFa mode of the NIST submission.

--- lib/std/crypto/isap.zig
+++ lib/std/crypto/isap.zig
@@ -1,9 +1,11 @@
 const std = @import("std");
+const crypto = std.crypto;
 const debug = std.debug;
 const mem = std.mem;
 const math = std.math;
 const testing = std.testing;
-const AuthenticationError = std.crypto.errors.AuthenticationError;
+const Ascon = crypto.core.Ascon(.Big);
+const AuthenticationError = crypto.errors.AuthenticationError;
 
 /// ISAPv2 is an authenticated encryption system hardened against side channels and fault attacks.
 /// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/round-2/spec-doc-rnd2/isap-spec-round2.pdf
@@ -25,90 +27,26 @@ pub const IsapA128A = struct {
     const iv2 = [_]u8{ 0x02, 0x80, 0x40, 0x01, 0x0c, 0x01, 0x06, 0x0c };
     const iv3 = [_]u8{ 0x03, 0x80, 0x40, 0x01, 0x0c, 0x01, 0x06, 0x0c };
 
-    const Block = [5]u64;
-
-    block: Block,
-
-    fn round(isap: *IsapA128A, rk: u64) void {
-        var x = &isap.block;
-        x[2] ^= rk;
-        x[0] ^= x[4];
-        x[4] ^= x[3];
-        x[2] ^= x[1];
-        var t = x.*;
-        x[0] = t[0] ^ ((~t[1]) & t[2]);
-        x[2] = t[2] ^ ((~t[3]) & t[4]);
-        x[4] = t[4] ^ ((~t[0]) & t[1]);
-        x[1] = t[1] ^ ((~t[2]) & t[3]);
-        x[3] = t[3] ^ ((~t[4]) & t[0]);
-        x[1] ^= x[0];
-        t[1] = x[1];
-        x[1] = math.rotr(u64, x[1], 39);
-        x[3] ^= x[2];
-        t[2] = x[2];
-        x[2] = math.rotr(u64, x[2], 1);
-        t[4] = x[4];
-        t[2] ^= x[2];
-        x[2] = math.rotr(u64, x[2], 5);
-        t[3] = x[3];
-        t[1] ^= x[1];
-        x[3] = math.rotr(u64, x[3], 10);
-        x[0] ^= x[4];
-        x[4] = math.rotr(u64, x[4], 7);
-        t[3] ^= x[3];
-        x[2] ^= t[2];
-        x[1] = math.rotr(u64, x[1], 22);
-        t[0] = x[0];
-        x[2] = ~x[2];
-        x[3] = math.rotr(u64, x[3], 7);
-        t[4] ^= x[4];
-        x[4] = math.rotr(u64, x[4], 34);
-        x[3] ^= t[3];
-        x[1] ^= t[1];
-        x[0] = math.rotr(u64, x[0], 19);
-        x[4] ^= t[4];
-        t[0] ^= x[0];
-        x[0] = math.rotr(u64, x[0], 9);
-        x[0] ^= t[0];
-    }
-
-    fn p12(isap: *IsapA128A) void {
-        const rks = [12]u64{ 0xf0, 0xe1, 0xd2, 0xc3, 0xb4, 0xa5, 0x96, 0x87, 0x78, 0x69, 0x5a, 0x4b };
-        inline for (rks) |rk| {
-            isap.round(rk);
-        }
-    }
-
-    fn p6(isap: *IsapA128A) void {
-        const rks = [6]u64{ 0x96, 0x87, 0x78, 0x69, 0x5a, 0x4b };
-        inline for (rks) |rk| {
-            isap.round(rk);
-        }
-    }
-
-    fn p1(isap: *IsapA128A) void {
-        isap.round(0x4b);
-    }
+    st: Ascon,
 
     fn absorb(isap: *IsapA128A, m: []const u8) void {
-        var block = &isap.block;
         var i: usize = 0;
         while (true) : (i += 8) {
             const left = m.len - i;
             if (left >= 8) {
-                block[0] ^= mem.readIntBig(u64, m[i..][0..8]);
-                isap.p12();
+                isap.st.addBytes(m[i..][0..8]);
+                isap.st.permute();
                 if (left == 8) {
-                    block[0] ^= 0x8000000000000000;
-                    isap.p12();
+                    isap.st.addByte(0x80, 0);
+                    isap.st.permute();
                     break;
                 }
             } else {
                 var padded = [_]u8{0} ** 8;
                 mem.copy(u8, padded[0..left], m[i..]);
                 padded[left] = 0x80;
-                block[0] ^= mem.readIntBig(u64, padded[0..]);
-                isap.p12();
+                isap.st.addBytes(&padded);
+                isap.st.permute();
                 break;
             }
         }
@@ -116,65 +54,59 @@ pub const IsapA128A = struct {
 
     fn trickle(k: [16]u8, iv: [8]u8, y: []const u8, comptime out_len: usize) [out_len]u8 {
         var isap = IsapA128A{
-            .block = Block{
+            .st = Ascon.initFromWords(.{
                 mem.readIntBig(u64, k[0..8]),
                 mem.readIntBig(u64, k[8..16]),
                 mem.readIntBig(u64, iv[0..8]),
                 0,
                 0,
-            },
+            }),
         };
-        isap.p12();
+        isap.st.permute();
 
         var i: usize = 0;
         while (i < y.len * 8 - 1) : (i += 1) {
             const cur_byte_pos = i / 8;
             const cur_bit_pos = @truncate(u3, 7 - (i % 8));
-            const cur_bit = @as(u64, ((y[cur_byte_pos] >> cur_bit_pos) & 1) << 7);
-            isap.block[0] ^= cur_bit << 56;
-            isap.p1();
+            const cur_bit = ((y[cur_byte_pos] >> cur_bit_pos) & 1) << 7;
+            isap.st.addByte(cur_bit, 0);
+            isap.st.permuteR(1);
         }
-        const cur_bit = @as(u64, (y[y.len - 1] & 1) << 7);
-        isap.block[0] ^= cur_bit << 56;
-        isap.p12();
+        const cur_bit = (y[y.len - 1] & 1) << 7;
+        isap.st.addByte(cur_bit, 0);
+        isap.st.permute();
 
         var out: [out_len]u8 = undefined;
-        var j: usize = 0;
-        while (j < out_len) : (j += 8) {
-            mem.writeIntBig(u64, out[j..][0..8], isap.block[j / 8]);
-        }
-        std.crypto.utils.secureZero(u64, &isap.block);
+        isap.st.extractBytes(&out);
+        isap.st.secureZero();
         return out;
     }
 
     fn mac(c: []const u8, ad: []const u8, npub: [16]u8, key: [16]u8) [16]u8 {
         var isap = IsapA128A{
-            .block = Block{
+            .st = Ascon.initFromWords(.{
                 mem.readIntBig(u64, npub[0..8]),
                 mem.readIntBig(u64, npub[8..16]),
                 mem.readIntBig(u64, iv1[0..]),
                 0,
                 0,
-            },
+            }),
         };
-        isap.p12();
+        isap.st.permute();
 
         isap.absorb(ad);
-        isap.block[4] ^= 1;
+        isap.st.addByte(1, Ascon.block_bytes - 1);
         isap.absorb(c);
 
         var y: [16]u8 = undefined;
-        mem.writeIntBig(u64, y[0..8], isap.block[0]);
-        mem.writeIntBig(u64, y[8..16], isap.block[1]);
+        isap.st.extractBytes(&y);
         const nb = trickle(key, iv2, y[0..], 16);
-        isap.block[0] = mem.readIntBig(u64, nb[0..8]);
-        isap.block[1] = mem.readIntBig(u64, nb[8..16]);
-        isap.p12();
+        isap.st.setBytes(&nb);
+        isap.st.permute();
 
         var tag: [16]u8 = undefined;
-        mem.writeIntBig(u64, tag[0..8], isap.block[0]);
-        mem.writeIntBig(u64, tag[8..16], isap.block[1]);
-        std.crypto.utils.secureZero(u64, &isap.block);
+        isap.st.extractBytes(&tag);
+        isap.st.secureZero();
         return tag;
     }
 
@@ -183,34 +115,31 @@ pub const IsapA128A = struct {
 
         const nb = trickle(key, iv3, npub[0..], 24);
         var isap = IsapA128A{
-            .block = Block{
+            .st = Ascon.initFromWords(.{
                 mem.readIntBig(u64, nb[0..8]),
                 mem.readIntBig(u64, nb[8..16]),
                 mem.readIntBig(u64, nb[16..24]),
                 mem.readIntBig(u64, npub[0..8]),
                 mem.readIntBig(u64, npub[8..16]),
-            },
+            }),
         };
-        isap.p6();
+        isap.st.permuteR(6);
 
         var i: usize = 0;
         while (true) : (i += 8) {
             const left = in.len - i;
             if (left >= 8) {
-                mem.writeIntNative(u64, out[i..][0..8], mem.bigToNative(u64, isap.block[0]) ^ mem.readIntNative(u64, in[i..][0..8]));
+                isap.st.xorBytes(out[i..][0..8], in[i..][0..8]);
                 if (left == 8) {
                     break;
                 }
-                isap.p6();
+                isap.st.permuteR(6);
             } else {
-                var pad = [_]u8{0} ** 8;
-                mem.copy(u8, pad[0..left], in[i..][0..left]);
-                mem.writeIntNative(u64, pad[i..][0..8], mem.bigToNative(u64, isap.block[0]) ^ mem.readIntNative(u64, pad[i..][0..8]));
-                mem.copy(u8, out[i..][0..left], pad[0..left]);
+                isap.st.xorBytes(out[i..], in[i..]);
                 break;
             }
         }
-        std.crypto.utils.secureZero(u64, &isap.block);
+        isap.st.secureZero();
     }
 
     pub fn encrypt(c: []u8, tag: *[tag_length]u8, m: []const u8, ad: []const u8, npub: [nonce_length]u8, key: [key_length]u8) void {
@@ -220,12 +149,9 @@ pub const IsapA128A = struct {
 
     pub fn decrypt(m: []u8, c: []const u8, tag: [tag_length]u8, ad: []const u8, npub: [nonce_length]u8, key: [key_length]u8) AuthenticationError!void {
         var computed_tag = mac(c, ad, npub, key);
-        var acc: u8 = 0;
-        for (computed_tag) |_, j| {
-            acc |= (computed_tag[j] ^ tag[j]);
-        }
-        std.crypto.utils.secureZero(u8, &computed_tag);
-        if (acc != 0) {
+        const res = crypto.utils.timingSafeEql([tag_length]u8, computed_tag, tag);
+        crypto.utils.secureZero(u8, &computed_tag);
+        if (!res) {
             return error.AuthenticationFailed;
         }
         xor(m, c, npub, key);
