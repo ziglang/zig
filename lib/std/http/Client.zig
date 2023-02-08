6afde631ad9d3f823c92e5efca3cb372c70ca887
Gaëtan <blaxoujunior@gmail.com>
2023-02-08 17:05:53 +0100

    Don't add the connection header for other versions that 1.0 and 1.1
    As per the mdn docs:
    
    Warning: Connection-specific header fields such as Connection and Keep-Alive are prohibited in HTTP/2 and HTTP/3. Chrome and Firefox ignore them in HTTP/2 responses, but Safari conforms to the HTTP/2 spec requirements and does not load any response that contains them.

--- lib/std/http/Client.zig
+++ lib/std/http/Client.zig
@@ -907,10 +907,11 @@ pub fn request(client: *Client, uri: Uri, headers: Request.Headers, options: Req
         try h.appendSlice(@tagName(headers.version));
         try h.appendSlice("\r\nHost: ");
         try h.appendSlice(host);
-        try h.appendSlice(switch (headers.version) {
-            .@"HTTP/1.0" => "\r\nConnection: close\r\n\r\n",
-            .@"HTTP/1.1" => "\r\nConnection: keep-alive\r\n\r\n",
-        });
+        switch (headers.version) {
+            .@"HTTP/1.0" => try h.appendSlice("\r\nConnection: close\r\n\r\n"),
+            .@"HTTP/1.1" => try h.appendSlice("\r\nConnection: keep-alive\r\n\r\n"),
+            else => {},
+        }
         const header_bytes = h.slice();
         try req.connection.writeAll(header_bytes);
     }
