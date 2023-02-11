84de2fcb3476f0d32aa5998f0b9d36646924ae7c
flexicoding <>
2023-02-11 15:15:18 +0100

    compiler_rt: Fixed unexported functions #14615

--- lib/compiler_rt/gehf2.zig
+++ lib/compiler_rt/gehf2.zig
@@ -6,8 +6,13 @@ const comparef = @import("./comparef.zig");
 pub const panic = common.panic;
 
 comptime {
-    @export(__gehf2, .{ .name = "__gehf2", .linkage = common.linkage, .visibility = common.visibility });
-    @export(__gthf2, .{ .name = "__gthf2", .linkage = common.linkage, .visibility = common.visibility });
+    if (common.want_aeabi) {
+        @export(__aeabi_hcmpge, .{ .name = "__aeabi_hcmpge", .linkage = common.linkage, .visibility = common.visibility });
+        @export(__aeabi_hcmpgt, .{ .name = "__aeabi_hcmpgt", .linkage = common.linkage, .visibility = common.visibility });
+    } else {
+        @export(__gehf2, .{ .name = "__gehf2", .linkage = common.linkage, .visibility = common.visibility });
+        @export(__gthf2, .{ .name = "__gthf2", .linkage = common.linkage, .visibility = common.visibility });
+    }
 }
 
 /// "These functions return a value greater than or equal to zero if neither
@@ -22,10 +27,10 @@ pub fn __gthf2(a: f16, b: f16) callconv(.C) i32 {
     return __gehf2(a, b);
 }
 
-fn __aeabi_fcmpge(a: f16, b: f16) callconv(.AAPCS) i32 {
+fn __aeabi_hcmpge(a: f16, b: f16) callconv(.AAPCS) i32 {
     return @boolToInt(comparef.cmpf2(f16, comparef.GE, a, b) != .Less);
 }
 
-fn __aeabi_fcmpgt(a: f16, b: f16) callconv(.AAPCS) i32 {
+fn __aeabi_hcmpgt(a: f16, b: f16) callconv(.AAPCS) i32 {
     return @boolToInt(comparef.cmpf2(f16, comparef.LE, a, b) == .Greater);
 }
