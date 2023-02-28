5be2951002dffa7a74b9965b111f611cda9badbb
John Schmidt <john.schmidt.h@gmail.com>
2023-02-28 11:27:56 +0100

    astgen: do not discard result location in for/while loops
    If we use the discard result location any break with a value will be
    ignored and not checked for usage.
    
    Closes https://github.com/ziglang/zig/issues/14684.

---
+++ test/cases/compile_errors/for_loop_break_value_ignored.zig
@@ -0,0 +1,15 @@
+fn returns() usize {
+    return 2;
+}
+
+export fn f1() void {
+    for ("hello") |_| {
+        break returns();
+    }
+}
+
+// error
+// backend=stage2
+// target=native
+//
+// :6:5: error: incompatible types: 'usize' and 'void'
