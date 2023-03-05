8ea1c1932e7bd869ec77a161da7876d171d4ef1d
Jacob Young <jacobly0@users.noreply.github.com>
2023-03-05 04:25:04 -0500

    behavior: disable failing tests

--- test/behavior/slice.zig
+++ test/behavior/slice.zig
@@ -749,6 +749,11 @@ test "slice decays to many pointer" {
 }
 
 test "write through pointer to optional slice arg" {
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest;
+    if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest;
+    if (builtin.zig_backend == .stage2_sparc64) return error.SkipZigTest;
+    if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest;
+
     const S = struct {
         fn bar(foo: *?[]const u8) !void {
             foo.* = try baz();
