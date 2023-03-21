e8b7b9b66a5c1b8d25ad1a022b7d10da910c74a5
Hashi364 <49736221+Kiyoshi364@users.noreply.github.com>
2023-03-21 14:35:11 -0300

    Add SetConsoleMode to windows.kernel32

--- lib/std/os/windows/kernel32.zig
+++ lib/std/os/windows/kernel32.zig
@@ -165,6 +165,7 @@ pub extern "kernel32" fn GetCommandLineA() callconv(WINAPI) LPSTR;
 pub extern "kernel32" fn GetCommandLineW() callconv(WINAPI) LPWSTR;
 
 pub extern "kernel32" fn GetConsoleMode(in_hConsoleHandle: HANDLE, out_lpMode: *DWORD) callconv(WINAPI) BOOL;
+pub extern "kernel32" fn SetConsoleMode(in_hConsoleHandle: HANDLE, in_dwMode: DWORD) callconv(WINAPI) BOOL;
 
 pub extern "kernel32" fn GetConsoleOutputCP() callconv(WINAPI) UINT;
 
