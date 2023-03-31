a7ab55e935ed1c67dc5c297021c966391a67564b
Chris Boesch <48591413+chrboesch@users.noreply.github.com>
2023-03-31 15:41:00 +0200

    removed deprecated exe.run()

--- lib/init-exe/build.zig
+++ lib/init-exe/build.zig
@@ -32,7 +32,7 @@ pub fn build(b: *std.Build) void {
     // This *creates* a RunStep in the build graph, to be executed when another
     // step is evaluated that depends on it. The next line below will establish
     // such a dependency.
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
 
     // By making the run step depend on the install step, it will be run from the
     // installation directory rather than directly from within the cache directory.
