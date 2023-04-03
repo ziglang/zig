c91929090db337810aa3b55f9bd26f5fd0af4fff
Jacob Young <jacobly0@users.noreply.github.com>
2023-04-03 14:17:00 -0400

    start: disable extra start logic on various x86_64 subtargets

--- test/cases/x86_64-linux/hello_world_with_updates.0.zig
+++ test/cases/x86_64-linux/hello_world_with_updates.0.zig
@@ -2,4 +2,7 @@
 // output_mode=Exe
 // target=x86_64-linux
 //
-// :109:9: error: root struct of file 'tmp' has no member named 'main'
+// :604:45: error: root struct of file 'tmp' has no member named 'main'
+// :553:12: note: called from here
+// :503:36: note: called from here
+// :466:17: note: called from here
--- test/cases/x86_64-windows/hello_world_with_updates.0.zig
+++ test/cases/x86_64-windows/hello_world_with_updates.0.zig
@@ -2,4 +2,6 @@
 // output_mode=Exe
 // target=x86_64-windows
 //
-// :130:9: error: root struct of file 'tmp' has no member named 'main'
+// :604:45: error: root struct of file 'tmp' has no member named 'main'
+// :553:12: note: called from here
+// :389:65: note: called from here
