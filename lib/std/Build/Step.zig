869385c78e89853fc1155ee837a41a6fe7166a3b
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2023-05-02 02:01:05 -0400

    show errorReturnTrace when Step.makeFn errors

--- lib/std/Build/Step.zig
+++ lib/std/Build/Step.zig
@@ -183,7 +183,19 @@ pub fn make(s: *Step, prog_node: *std.Progress.Node) error{ MakeFailed, MakeSkip
         error.MakeFailed => return error.MakeFailed,
         error.MakeSkipped => return error.MakeSkipped,
         else => {
-            s.result_error_msgs.append(arena, @errorName(err)) catch @panic("OOM");
+            var msg_buffer = arena.alloc(u8, 4096) catch @panic("OOM");
+            var stream = std.io.fixedBufferStream(msg_buffer);
+            const writer = stream.writer();
+
+            writer.writeAll("step failed with error ") catch @panic("msg_buffer OOM");
+            writer.writeAll(@errorName(err)) catch @panic("msg_buffer OOM");
+
+            if (@errorReturnTrace()) |trace| {
+                writer.writeAll(":") catch @panic("msg_buffer OOM");
+                trace.format("", .{}, writer) catch @panic("msg_buffer OOM");
+            }
+
+            s.result_error_msgs.append(arena, msg_buffer[0..stream.pos]) catch @panic("OOM");
             return error.MakeFailed;
         },
     };
