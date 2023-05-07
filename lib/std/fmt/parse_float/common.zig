4c76b6f01ba06f4a4d56c39bc9cf2aec6ce988a7
jvyden <jvyden@jvyden.xyz>
2023-05-06 22:30:59 -0400

    Remove modification of MantissaType

--- lib/std/fmt/parse_float/common.zig
+++ lib/std/fmt/parse_float/common.zig
@@ -86,6 +86,6 @@ pub fn mantissaType(comptime T: type) type {
     return switch (T) {
         f16, f32, f64 => u64,
         f128 => u128,
-        else => @compileError("Invalid type for Mantissa - should be a floating-point type."),
+        else => unreachable,
     };
 }
