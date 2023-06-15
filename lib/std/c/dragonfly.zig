4054d001445098cdb72a02243e13180f1fba6d79
Michael Dusan <michael.dusan@gmail.com>
2023-06-15 14:48:20 -0400

    dragonfly: fix std.c.getdents
    - fix getdents return type usize → c_int

--- lib/std/c/dragonfly.zig
+++ lib/std/c/dragonfly.zig
@@ -9,7 +9,7 @@ pub fn _errno() *c_int {
     return &errno;
 }
 
-pub extern "c" fn getdents(fd: c_int, buf_ptr: [*]u8, nbytes: usize) usize;
+pub extern "c" fn getdents(fd: c_int, buf_ptr: [*]u8, nbytes: usize) c_int;
 pub extern "c" fn sigaltstack(ss: ?*stack_t, old_ss: ?*stack_t) c_int;
 pub extern "c" fn getrandom(buf_ptr: [*]u8, buf_len: usize, flags: c_uint) isize;
 pub extern "c" fn pipe2(fds: *[2]fd_t, flags: u32) c_int;
