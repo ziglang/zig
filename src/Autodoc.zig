28ad74e8a6ed3484d632a6a615f5d0935c03676a
Loris Cro <kappaloris@gmail.com>
2023-07-03 19:18:44 +0200

    autodoc: wire in js tokenizer to frontend

--- src/Autodoc.zig
+++ src/Autodoc.zig
@@ -435,6 +435,7 @@ pub fn generateZirData(self: *Autodoc) !void {
     var docs_dir = try self.comp_module.comp.zig_lib_directory.handle.openDir("docs", .{});
     defer docs_dir.close();
     try docs_dir.copyFile("main.js", output_dir, "main.js", .{});
+    try docs_dir.copyFile("ziglexer.js", output_dir, "ziglexer.js", .{});
     try docs_dir.copyFile("commonmark.js", output_dir, "commonmark.js", .{});
     try docs_dir.copyFile("index.html", output_dir, "index.html", .{});
 }
