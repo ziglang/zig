56767e57faa812782449d1d25564c43269f24ae2
r00ster91 <r00ster91@proton.me>
2023-07-21 03:22:59 +0200

    Sema: improve error set/union discard/ignore errors
    Previously the error had a note suggesting to use `try`, `catch`, or
    `if`, even for error sets where none of those work.
    Instead, in case of an error set the way you can handle the error
    depends very much on the specific case. For example you might be in a
    `catch` where you are discarding or ignoring the error set capture
    value, in which case one way to handle the error might be to `return`
    the error.
    So, in that case, we do not attach that error note.
    
    Additionally, this makes the error tell you what kind of an error it is:
    is it an error set or an error union? This distinction is very relevant
    in how to handle the error.

--- test/cases/compile_errors/ignored_deferred_function_call.zig
+++ test/cases/compile_errors/ignored_deferred_function_call.zig
@@ -5,9 +5,17 @@ fn bar() anyerror!i32 {
     return 0;
 }
 
+export fn foo2() void {
+    defer bar2();
+}
+fn bar2() anyerror {
+    return error.a;
+}
+
 // error
 // backend=stage2
 // target=native
 //
-// :2:14: error: error is ignored
+// :2:14: error: error union is ignored
 // :2:14: note: consider using 'try', 'catch', or 'if'
+// :9:15: error: error set is ignored
--- test/cases/compile_errors/ignored_expression_in_while_continuation.zig
+++ test/cases/compile_errors/ignored_expression_in_while_continuation.zig
@@ -15,13 +15,21 @@ fn bad() anyerror!void {
     return error.Bad;
 }
 
+export fn d() void {
+    while (true) : (bad2()) {}
+}
+fn bad2() anyerror {
+    return error.Bad;
+}
+
 // error
 // backend=stage2
 // target=native
 //
-// :2:24: error: error is ignored
+// :2:24: error: error union is ignored
 // :2:24: note: consider using 'try', 'catch', or 'if'
-// :7:25: error: error is ignored
+// :7:25: error: error union is ignored
 // :7:25: note: consider using 'try', 'catch', or 'if'
-// :12:25: error: error is ignored
+// :12:25: error: error union is ignored
 // :12:25: note: consider using 'try', 'catch', or 'if'
+// :19:25: error: error set is ignored
