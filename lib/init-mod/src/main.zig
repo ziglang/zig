99daa8a4187a7be5945d0f15e64463143cc93a18
cactusbento <cactusbento@outlook.com>
2023-07-25 13:16:16 -0400

    Cleaned up template

--- lib/init-mod/src/main.zig
+++ lib/init-mod/src/main.zig
@@ -1,16 +1,11 @@
 const std = @import("std");
-const mem = std.mem;
 const testing = std.testing;
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-pub fn leftpad(alloc: mem.Allocator, str: []const u8, len: usize, ch: u8) ![]const u8 {
+pub fn leftpad(alloc: std.mem.Allocator, str: []const u8, len: usize, ch: u8) ![]const u8 {
     const pad: []u8 = try alloc.alloc(u8, len);
     defer alloc.free(pad);
     @memset(pad, ch);
-    const padded = mem.concat(alloc, u8, &[_][]const u8{
+    const padded = std.mem.concat(alloc, u8, &[_][]const u8{
         pad, str,
     });
 
