1cf07c7571fac3dc380c53410f7b6f7d54ff7ec7
Lewis Gaul <lewis.gaul@gmail.com>
2023-08-17 21:59:27 +0100

    Adjust docstrings and tentatively add/activate testcases

--- lib/std/math/isnan.zig
+++ lib/std/math/isnan.zig
@@ -1,19 +1,20 @@
 const std = @import("../std.zig");
+const builtin = @import("builtin");
 const math = std.math;
 const meta = std.meta;
 const expect = std.testing.expect;
 
-/// Returns whether x is a NaN.
 pub fn isNan(x: anytype) bool {
     return x != x;
 }
 
-/// Returns whether x is a signalling NaN.
+/// TODO: LLVM is known to miscompile on some architectures to quiet NaN -
+///       this is tracked by https://github.com/ziglang/zig/issues/14366
 pub fn isSignalNan(x: anytype) bool {
     const T = @TypeOf(x);
     const U = meta.Int(.unsigned, @bitSizeOf(T));
-    const signal_bit_mask = 1 << (math.floatFractionalBits(T) - 1);
-    return isNan(x) and (@as(U, @bitCast(x)) & signal_bit_mask == 0);
+    const quiet_signal_bit_mask = 1 << (math.floatFractionalBits(T) - 1);
+    return isNan(x) and (@as(U, @bitCast(x)) & quiet_signal_bit_mask == 0);
 }
 
 test "math.isNan" {
@@ -27,10 +28,11 @@ test "math.isNan" {
 }
 
 test "math.isSignalNan" {
+    // TODO: https://github.com/ziglang/zig/issues/14366
+    if (builtin.zig_backend == .stage2_llvm and comptime builtin.cpu.arch.isArmOrThumb()) return error.SkipZigTest;
+
     inline for ([_]type{ f16, f32, f64, f80, f128, c_longdouble }) |T| {
-        // TODO: Signalling NaN values get converted to quiet NaN values in
-        //       some cases where they shouldn't such that this can fail.
-        // try expect(isSignalNan(math.snan(T)));
+        try expect(isSignalNan(math.snan(T)));
         try expect(!isSignalNan(math.nan(T)));
         try expect(!isSignalNan(@as(T, 1.0)));
         try expect(!isSignalNan(math.inf(T)));
