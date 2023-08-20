19e7f33bff76c2faa507d24c617e158c1315c880
riverbl <94326797+riverbl@users.noreply.github.com>
2023-08-20 08:39:38 +0100

    Implement @divCeil lowering code for wasm

--- src/arch/wasm/CodeGen.zig
+++ src/arch/wasm/CodeGen.zig
@@ -6813,8 +6813,120 @@ fn airDivFloor(func: *CodeGen, inst: Air.Inst.Index) InnerError!void {
 }
 
 fn airDivCeil(func: *CodeGen, inst: Air.Inst.Index) InnerError!void {
-    _ = inst;
-    return func.fail("TODO: implement `@divCeil` for {}", .{func.target.cpu.arch});
+    const bin_op = func.air.instructions.items(.data)[inst].bin_op;
+
+    const mod = func.bin_file.base.options.module.?;
+    const ty = func.typeOfIndex(inst);
+    const lhs = try func.resolveInst(bin_op.lhs);
+    const rhs = try func.resolveInst(bin_op.rhs);
+
+    if (ty.isUnsignedInt(mod)) {
+        const int_bits = ty.intInfo(mod).bits;
+        const wasm_bits = toWasmBits(int_bits) orelse {
+            return func.fail("TODO: `@divCeil` for unsigned integers larger than 64 bits ({d} bits requested)", .{int_bits});
+        };
+
+        if (wasm_bits > 64) {
+            return func.fail("TODO: `@divCeil` for unsigned integers larger than 64 bits ({d} bits requested)", .{int_bits});
+        }
+
+        _ = try func.binOp(lhs, rhs, ty, .div);
+        _ = try func.binOp(lhs, rhs, ty, .rem);
+
+        switch (wasm_bits) {
+            32 => {
+                _ = try func.cmp(.stack, WValue{ .imm32 = 0 }, ty, .neq);
+                try func.addTag(.i32_add);
+            },
+            64 => {
+                _ = try func.cmp(.stack, WValue{ .imm64 = 0 }, ty, .neq);
+                try func.addTag(.i64_extend_i32_u);
+                try func.addTag(.i64_add);
+            },
+            else => @panic("Unexpected wasm integer width"),
+        }
+    } else if (ty.isSignedInt(mod)) {
+        const int_bits = ty.intInfo(mod).bits;
+        const wasm_bits = toWasmBits(int_bits) orelse {
+            return func.fail("TODO: `@divCeil` for signed integers larger than 64 bits ({d} bits requested)", .{int_bits});
+        };
+
+        if (wasm_bits > 64) {
+            return func.fail("TODO: `@divCeil` for signed integers larger than 64 bits ({d} bits requested)", .{int_bits});
+        }
+
+        const lhs_wasm = if (wasm_bits != int_bits)
+            try (try func.signAbsValue(lhs, ty)).toLocal(func, ty)
+        else
+            lhs;
+        const rhs_wasm = if (wasm_bits != int_bits)
+            try (try func.signAbsValue(rhs, ty)).toLocal(func, ty)
+        else
+            rhs;
+
+        const zero = switch (wasm_bits) {
+            32 => WValue{ .imm32 = 0 },
+            64 => WValue{ .imm64 = 0 },
+            else => @panic("Unexpected wasm integer width"),
+        };
+
+        _ = try func.binOp(lhs_wasm, rhs_wasm, ty, .div);
+
+        // 1 if lhs and rhs have the same sign, 0 otherwise.
+        _ = try func.binOp(lhs_wasm, rhs_wasm, ty, .xor);
+        _ = try func.cmp(.stack, zero, ty, .gte);
+
+        _ = try func.binOp(lhs_wasm, rhs_wasm, ty, .rem);
+        _ = try func.cmp(.stack, zero, ty, .neq);
+
+        try func.addTag(.i32_and);
+
+        // Comparisons produce 32 bit integers, which must be extended in the 64 bit case.
+        if (wasm_bits == 64) {
+            try func.addTag(.i64_extend_i32_u);
+        }
+
+        _ = try func.binOp(.stack, .stack, ty, .add);
+
+        // We need to zero the high bits because N bit comparisons consider all 32 or 64 bits, and
+        // expect all but the lowest N bits to be 0.
+        // TODO: Should we be zeroing the high bits here or should we be ignoring the high bits
+        // when performing comparisons?
+        if (wasm_bits != int_bits) {
+            _ = try func.wrapOperand(.stack, ty);
+        }
+    } else {
+        const float_bits = ty.floatBits(func.target);
+        if (float_bits > 64) {
+            return func.fail("TODO: `@divCeil` for floats larger than 64 bits ({d} bits requested)", .{float_bits});
+        }
+        const is_f16 = float_bits == 16;
+        const lhs_wasm = if (is_f16) try func.fpext(lhs, Type.f16, Type.f32) else lhs;
+        const rhs_wasm = if (is_f16) try func.fpext(rhs, Type.f16, Type.f32) else rhs;
+
+        try func.emitWValue(lhs_wasm);
+        try func.emitWValue(rhs_wasm);
+
+        switch (float_bits) {
+            16, 32 => {
+                try func.addTag(.f32_div);
+                try func.addTag(.f32_ceil);
+            },
+            64 => {
+                try func.addTag(.f64_div);
+                try func.addTag(.f64_ceil);
+            },
+            else => @panic("Unexpected float width"),
+        }
+
+        if (is_f16) {
+            _ = try func.fptrunc(.stack, Type.f32, Type.f16);
+        }
+    }
+
+    const result = try func.allocLocal(ty);
+    try func.addLabel(.local_set, result.local.value);
+    func.finishAir(inst, result, &.{ bin_op.lhs, bin_op.rhs });
 }
 
 fn divSigned(func: *CodeGen, lhs: WValue, rhs: WValue, ty: Type) InnerError!WValue {
