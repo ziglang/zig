ziglang/zig

commit a31748b29e2e980ac4bf08cf10ee0d6ece1bd9e7

std.os.uefi: reorganize namespaces
This is a breaking change.

This commit applies the following rules to std.os.uefi:
* avoid redundant names in the namespace such as "protocol.FooProtocol"
* don't initialize struct field to undefined. do that at the
  initialization site if you want that, or create a named constant that
  sets all the fields to undefined.
* avoid the word "data", "info", "context", "state", "details", or
  "config" in the type name, especially if a word from that category is
  already in the type name.
* embrace tree structure

After following these rules, `usingnamespace` disappeared naturally.
This commit eliminates 26/53 (49%) instances of `usingnamespace` in the
standard library. All these uses were due to not understanding how
to properly use namespaces.

I did not test this commit. The standard library UEFI code is
experimental and pull requests have been accepted with minimal vetting.
Users of std.os.uefi will need to submit follow-up pull requests to fix
up whatever regressions this commit introduces, this time without
abusing namespaces (pun intended).

lib/std/os/uefi/protocol.zig
@@ -1,0 +1,37 @@
+pub const LoadedImage = @import("protocol/loaded_image.zig").LoadedImage;
+pub const DevicePath = @import("protocol/device_path.zig").DevicePath;
+pub const Rng = @import("protocol/rng.zig").Rng;
+pub const ShellParameters = @import("protocol/shell_parameters.zig").ShellParameters;
+
+pub const SimpleFileSystem = @import("protocol/simple_file_system.zig").SimpleFileSystem;
+pub const File = @import("protocol/file.zig").File;
+pub const BlockIo = @import("protocol/block_io.zig").BlockIo;
+
+pub const SimpleTextInput = @import("protocol/simple_text_input.zig").SimpleTextInput;
+pub const SimpleTextInputEx = @import("protocol/simple_text_input_ex.zig").SimpleTextInputEx;
+pub const SimpleTextOutput = @import("protocol/simple_text_output.zig").SimpleTextOutput;
+
+pub const SimplePointer = @import("protocol/simple_pointer.zig").SimplePointer;
+pub const AbsolutePointer = @import("protocol/absolute_pointer.zig").AbsolutePointer;
+
+pub const GraphicsOutput = @import("protocol/graphics_output.zig").GraphicsOutput;
+
+pub const edid = @import("protocol/edid.zig");
+
+pub const SimpleNetwork = @import("protocol/simple_network.zig").SimpleNetwork;
+pub const ManagedNetwork = @import("protocol/managed_network.zig").ManagedNetwork;
+
+pub const Ip6ServiceBinding = @import("protocol/ip6_service_binding.zig").Ip6ServiceBinding;
+pub const Ip6 = @import("protocol/ip6.zig").Ip6;
+pub const Ip6Config = @import("protocol/ip6_config.zig").Ip6Config;
+
+pub const Udp6ServiceBinding = @import("protocol/udp6_service_binding.zig").Udp6ServiceBinding;
+pub const Udp6 = @import("protocol/udp6.zig").Udp6;
+
+pub const HiiDatabase = @import("protocol/hii_database.zig").HiiDatabase;
+pub const HiiPopup = @import("protocol/hii_popup.zig").HiiPopup;
+
+test {
+    @setEvalBranchQuota(2000);
+    @import("std").testing.refAllDeclsRecursive(@This());
+}

lib/std/os/uefi/protocol/hii_popup.zig
@@ -1,0 +1,46 @@
+const std = @import("std");
+const uefi = std.os.uefi;
+const Guid = uefi.Guid;
+const Status = uefi.Status;
+const hii = uefi.hii;
+const cc = uefi.cc;
+
+/// Display a popup window
+pub const HIIPopup = extern struct {
+    revision: u64,
+    _create_popup: *const fn (*const HIIPopup, PopupStyle, PopupType, hii.HIIHandle, u16, ?*PopupSelection) callconv(cc) Status,
+
+    /// Displays a popup window.
+    pub fn createPopup(self: *const HIIPopup, style: PopupStyle, popup_type: PopupType, handle: hii.HIIHandle, msg: u16, user_selection: ?*PopupSelection) Status {
+        return self._create_popup(self, style, popup_type, handle, msg, user_selection);
+    }
+
+    pub const guid align(8) = Guid{
+        .time_low = 0x4311edc0,
+        .time_mid = 0x6054,
+        .time_high_and_version = 0x46d4,
+        .clock_seq_high_and_reserved = 0x9e,
+        .clock_seq_low = 0x40,
+        .node = [_]u8{ 0x89, 0x3e, 0xa9, 0x52, 0xfc, 0xcc },
+    };
+
+    pub const PopupStyle = enum(u32) {
+        Info,
+        Warning,
+        Error,
+    };
+
+    pub const PopupType = enum(u32) {
+        Ok,
+        Cancel,
+        YesNo,
+        YesNoCancel,
+    };
+
+    pub const PopupSelection = enum(u32) {
+        Ok,
+        Cancel,
+        Yes,
+        No,
+    };
+};

lib/std/os/uefi/protocol/ip6.zig
@@ -1,0 +1,146 @@
+const std = @import("std");
+const uefi = std.os.uefi;
+const Guid = uefi.Guid;
+const Event = uefi.Event;
+const Status = uefi.Status;
+const MacAddress = uefi.protocol.MacAddress;
+const ManagedNetworkConfigData = uefi.protocol.ManagedNetworkConfigData;
+const SimpleNetworkMode = uefi.protocol.SimpleNetworkMode;
+const cc = uefi.cc;
+
+pub const Ip6 = extern struct {
+    _get_mode_data: *const fn (*const Ip6, ?*Mode, ?*ManagedNetworkConfigData, ?*SimpleNetworkMode) callconv(cc) Status,
+    _configure: *const fn (*const Ip6, ?*const Config) callconv(cc) Status,
+    _groups: *const fn (*const Ip6, bool, ?*const Address) callconv(cc) Status,
+    _routes: *const fn (*const Ip6, bool, ?*const Address, u8, ?*const Address) callconv(cc) Status,
+    _neighbors: *const fn (*const Ip6, bool, *const Address, ?*const MacAddress, u32, bool) callconv(cc) Status,
+    _transmit: *const fn (*const Ip6, *CompletionToken) callconv(cc) Status,
+    _receive: *const fn (*const Ip6, *CompletionToken) callconv(cc) Status,
+    _cancel: *const fn (*const Ip6, ?*CompletionToken) callconv(cc) Status,
+    _poll: *const fn (*const Ip6) callconv(cc) Status,
+
+    /// Gets the current operational settings for this instance of the EFI IPv6 Protocol driver.
+    pub fn getModeData(self: *const Ip6, ip6_mode_data: ?*Mode, mnp_config_data: ?*ManagedNetworkConfigData, snp_mode_data: ?*SimpleNetworkMode) Status {
+        return self._get_mode_data(self, ip6_mode_data, mnp_config_data, snp_mode_data);
+    }
+
+    /// Assign IPv6 address and other configuration parameter to this EFI IPv6 Protocol driver instance.
+    pub fn configure(self: *const Ip6, ip6_config_data: ?*const Config) Status {
+        return self._configure(self, ip6_config_data);
+    }
+
+    /// Joins and leaves multicast groups.
+    pub fn groups(self: *const Ip6, join_flag: bool, group_address: ?*const Address) Status {
+        return self._groups(self, join_flag, group_address);
+    }
+
+    /// Adds and deletes routing table entries.
+    pub fn routes(self: *const Ip6, delete_route: bool, destination: ?*const Address, prefix_length: u8, gateway_address: ?*const Address) Status {
+        return self._routes(self, delete_route, destination, prefix_length, gateway_address);
+    }
+
+    /// Add or delete Neighbor cache entries.
+    pub fn neighbors(self: *const Ip6, delete_flag: bool, target_ip6_address: *const Address, target_link_address: ?*const MacAddress, timeout: u32, override: bool) Status {
+        return self._neighbors(self, delete_flag, target_ip6_address, target_link_address, timeout, override);
+    }
+
+    /// Places outgoing data packets into the transmit queue.
+    pub fn transmit(self: *const Ip6, token: *CompletionToken) Status {
+        return self._transmit(self, token);
+    }
+
+    /// Places a receiving request into the receiving queue.
+    pub fn receive(self: *const Ip6, token: *CompletionToken) Status {
+        return self._receive(self, token);
+    }
+
+    /// Abort an asynchronous transmits or receive request.
+    pub fn cancel(self: *const Ip6, token: ?*CompletionToken) Status {
+        return self._cancel(self, token);
+    }
+
+    /// Polls for incoming data packets and processes outgoing data packets.
+    pub fn poll(self: *const Ip6) Status {
+        return self._poll(self);
+    }
+
+    pub const guid align(8) = Guid{
+        .time_low = 0x2c8759d5,
+        .time_mid = 0x5c2d,
+        .time_high_and_version = 0x66ef,
+        .clock_seq_high_and_reserved = 0x92,
+        .clock_seq_low = 0x5f,
+        .node = [_]u8{ 0xb6, 0x6c, 0x10, 0x19, 0x57, 0xe2 },
+    };
+
+    pub const Mode = extern struct {
+        is_started: bool,
+        max_packet_size: u32,
+        config_data: Config,
+        is_configured: bool,
+        address_count: u32,
+        address_list: [*]AddressInfo,
+        group_count: u32,
+        group_table: [*]Address,
+        route_count: u32,
+        route_table: [*]RouteTable,
+        neighbor_count: u32,
+        neighbor_cache: [*]NeighborCache,
+        prefix_count: u32,
+        prefix_table: [*]AddressInfo,
+        icmp_type_count: u32,
+        icmp_type_list: [*]IcmpType,
+    };
+
+    pub const Config = extern struct {
+        default_protocol: u8,
+        accept_any_protocol: bool,
+        accept_icmp_errors: bool,
+        accept_promiscuous: bool,
+        destination_address: Address,
+        station_address: Address,
+        traffic_class: u8,
+        hop_limit: u8,
+        flow_label: u32,
+        receive_timeout: u32,
+        transmit_timeout: u32,
+    };
+
+    pub const Address = [16]u8;
+
+    pub const AddressInfo = extern struct {
+        address: Address,
+        prefix_length: u8,
+    };
+
+    pub const RouteTable = extern struct {
+        gateway: Address,
+        destination: Address,
+        prefix_length: u8,
+    };
+
+    pub const NeighborState = enum(u32) {
+        Incomplete,
+        Reachable,
+        Stale,
+        Delay,
+        Probe,
+    };
+
+    pub const NeighborCache = extern struct {
+        neighbor: Address,
+        link_address: MacAddress,
+        state: NeighborState,
+    };
+
+    pub const IcmpType = extern struct {
+        type: u8,
+        code: u8,
+    };
+
+    pub const CompletionToken = extern struct {
+        event: Event,
+        status: Status,
+        packet: *anyopaque, // union TODO
+    };
+};

lib/std/os/uefi/protocol/managed_network.zig
@@ -1,0 +1,152 @@
+const std = @import("std");
+const uefi = std.os.uefi;
+const Guid = uefi.Guid;
+const Event = uefi.Event;
+const Handle = uefi.Handle;
+const Status = uefi.Status;
+const Time = uefi.Time;
+const SimpleNetworkMode = uefi.protocol.SimpleNetworkMode;
+const MacAddress = uefi.protocol.MacAddress;
+const cc = uefi.cc;
+
+pub const ManagedNetwork = extern struct {
+    _get_mode_data: *const fn (*const ManagedNetwork, ?*Config, ?*SimpleNetworkMode) callconv(cc) Status,
+    _configure: *const fn (*const ManagedNetwork, ?*const Config) callconv(cc) Status,
+    _mcast_ip_to_mac: *const fn (*const ManagedNetwork, bool, *const anyopaque, *MacAddress) callconv(cc) Status,
+    _groups: *const fn (*const ManagedNetwork, bool, ?*const MacAddress) callconv(cc) Status,
+    _transmit: *const fn (*const ManagedNetwork, *const CompletionToken) callconv(cc) Status,
+    _receive: *const fn (*const ManagedNetwork, *const CompletionToken) callconv(cc) Status,
+    _cancel: *const fn (*const ManagedNetwork, ?*const CompletionToken) callconv(cc) Status,
+    _poll: *const fn (*const ManagedNetwork) callconv(cc) Status,
+
+    /// Returns the operational parameters for the current MNP child driver.
+    /// May also support returning the underlying SNP driver mode data.
+    pub fn getModeData(self: *const ManagedNetwork, mnp_config_data: ?*Config, snp_mode_data: ?*SimpleNetworkMode) Status {
+        return self._get_mode_data(self, mnp_config_data, snp_mode_data);
+    }
+
+    /// Sets or clears the operational parameters for the MNP child driver.
+    pub fn configure(self: *const ManagedNetwork, mnp_config_data: ?*const Config) Status {
+        return self._configure(self, mnp_config_data);
+    }
+
+    /// Translates an IP multicast address to a hardware (MAC) multicast address.
+    /// This function may be unsupported in some MNP implementations.
+    pub fn mcastIpToMac(self: *const ManagedNetwork, ipv6flag: bool, ipaddress: *const anyopaque, mac_address: *MacAddress) Status {
+        return self._mcast_ip_to_mac(self, ipv6flag, ipaddress, mac_address);
+    }
+
+    /// Enables and disables receive filters for multicast address.
+    /// This function may be unsupported in some MNP implementations.
+    pub fn groups(self: *const ManagedNetwork, join_flag: bool, mac_address: ?*const MacAddress) Status {
+        return self._groups(self, join_flag, mac_address);
+    }
+
+    /// Places asynchronous outgoing data packets into the transmit queue.
+    pub fn transmit(self: *const ManagedNetwork, token: *const CompletionToken) Status {
+        return self._transmit(self, token);
+    }
+
+    /// Places an asynchronous receiving request into the receiving queue.
+    pub fn receive(self: *const ManagedNetwork, token: *const CompletionToken) Status {
+        return self._receive(self, token);
+    }
+
+    /// Aborts an asynchronous transmit or receive request.
+    pub fn cancel(self: *const ManagedNetwork, token: ?*const CompletionToken) Status {
+        return self._cancel(self, token);
+    }
+
+    /// Polls for incoming data packets and processes outgoing data packets.
+    pub fn poll(self: *const ManagedNetwork) Status {
+        return self._poll(self);
+    }
+
+    pub const guid align(8) = Guid{
+        .time_low = 0x7ab33a91,
+        .time_mid = 0xace5,
+        .time_high_and_version = 0x4326,
+        .clock_seq_high_and_reserved = 0xb5,
+        .clock_seq_low = 0x72,
+        .node = [_]u8{ 0xe7, 0xee, 0x33, 0xd3, 0x9f, 0x16 },
+    };
+
+    pub const ServiceBinding = extern struct {
+        _create_child: *const fn (*const ServiceBinding, *?Handle) callconv(cc) Status,
+        _destroy_child: *const fn (*const ServiceBinding, Handle) callconv(cc) Status,
+
+        pub fn createChild(self: *const ServiceBinding, handle: *?Handle) Status {
+            return self._create_child(self, handle);
+        }
+
+        pub fn destroyChild(self: *const ServiceBinding, handle: Handle) Status {
+            return self._destroy_child(self, handle);
+        }
+
+        pub const guid align(8) = Guid{
+            .time_low = 0xf36ff770,
+            .time_mid = 0xa7e1,
+            .time_high_and_version = 0x42cf,
+            .clock_seq_high_and_reserved = 0x9e,
+            .clock_seq_low = 0xd2,
+            .node = [_]u8{ 0x56, 0xf0, 0xf2, 0x71, 0xf4, 0x4c },
+        };
+    };
+
+    pub const Config = extern struct {
+        received_queue_timeout_value: u32,
+        transmit_queue_timeout_value: u32,
+        protocol_type_filter: u16,
+        enable_unicast_receive: bool,
+        enable_multicast_receive: bool,
+        enable_broadcast_receive: bool,
+        enable_promiscuous_receive: bool,
+        flush_queues_on_reset: bool,
+        enable_receive_timestamps: bool,
+        disable_background_polling: bool,
+    };
+
+    pub const CompletionToken = extern struct {
+        event: Event,
+        status: Status,
+        packet: extern union {
+            RxData: *ReceiveData,
+            TxData: *TransmitData,
+        },
+    };
+
+    pub const ReceiveData = extern struct {
+        timestamp: Time,
+        recycle_event: Event,
+        packet_length: u32,
+        header_length: u32,
+        address_length: u32,
+        data_length: u32,
+        broadcast_flag: bool,
+        multicast_flag: bool,
+        promiscuous_flag: bool,
+        protocol_type: u16,
+        destination_address: [*]u8,
+        source_address: [*]u8,
+        media_header: [*]u8,
+        packet_data: [*]u8,
+    };
+
+    pub const TransmitData = extern struct {
+        destination_address: ?*MacAddress,
+        source_address: ?*MacAddress,
+        protocol_type: u16,
+        data_length: u32,
+        header_length: u16,
+        fragment_count: u16,
+
+        pub fn getFragments(self: *TransmitData) []Fragment {
+            return @as([*]Fragment, @ptrCast(@alignCast(@as([*]u8, @ptrCast(self)) + @sizeOf(TransmitData))))[0..self.fragment_count];
+        }
+    };
+
+    pub const Fragment = extern struct {
+        fragment_length: u32,
+        fragment_buffer: [*]u8,
+    };
+};

lib/std/os/uefi/protocol/udp6.zig
@@ -1,0 +1,114 @@
+const std = @import("std");
+const uefi = std.os.uefi;
+const Guid = uefi.Guid;
+const Event = uefi.Event;
+const Status = uefi.Status;
+const Time = uefi.Time;
+const Ip6 = uefi.protocol.Ip6;
+const ManagedNetworkConfigData = uefi.protocol.ManagedNetworkConfigData;
+const SimpleNetworkMode = uefi.protocol.SimpleNetworkMode;
+const cc = uefi.cc;
+
+pub const Udp6 = extern struct {
+    _get_mode_data: *const fn (*const Udp6, ?*Config, ?*Ip6.ModeData, ?*ManagedNetworkConfigData, ?*SimpleNetworkMode) callconv(cc) Status,
+    _configure: *const fn (*const Udp6, ?*const Config) callconv(cc) Status,
+    _groups: *const fn (*const Udp6, bool, ?*const Ip6.Address) callconv(cc) Status,
+    _transmit: *const fn (*const Udp6, *CompletionToken) callconv(cc) Status,
+    _receive: *const fn (*const Udp6, *CompletionToken) callconv(cc) Status,
+    _cancel: *const fn (*const Udp6, ?*CompletionToken) callconv(cc) Status,
+    _poll: *const fn (*const Udp6) callconv(cc) Status,
+
+    pub fn getModeData(self: *const Udp6, udp6_config_data: ?*Config, ip6_mode_data: ?*Ip6.ModeData, mnp_config_data: ?*ManagedNetworkConfigData, snp_mode_data: ?*SimpleNetworkMode) Status {
+        return self._get_mode_data(self, udp6_config_data, ip6_mode_data, mnp_config_data, snp_mode_data);
+    }
+
+    pub fn configure(self: *const Udp6, udp6_config_data: ?*const Config) Status {
+        return self._configure(self, udp6_config_data);
+    }
+
+    pub fn groups(self: *const Udp6, join_flag: bool, multicast_address: ?*const Ip6.Address) Status {
+        return self._groups(self, join_flag, multicast_address);
+    }
+
+    pub fn transmit(self: *const Udp6, token: *CompletionToken) Status {
+        return self._transmit(self, token);
+    }
+
+    pub fn receive(self: *const Udp6, token: *CompletionToken) Status {
+        return self._receive(self, token);
+    }
+
+    pub fn cancel(self: *const Udp6, token: ?*CompletionToken) Status {
+        return self._cancel(self, token);
+    }
+
+    pub fn poll(self: *const Udp6) Status {
+        return self._poll(self);
+    }
+
+    pub const guid align(8) = uefi.Guid{
+        .time_low = 0x4f948815,
+        .time_mid = 0xb4b9,
+        .time_high_and_version = 0x43cb,
+        .clock_seq_high_and_reserved = 0x8a,
+        .clock_seq_low = 0x33,
+        .node = [_]u8{ 0x90, 0xe0, 0x60, 0xb3, 0x49, 0x55 },
+    };
+
+    pub const Config = extern struct {
+        accept_promiscuous: bool,
+        accept_any_port: bool,
+        allow_duplicate_port: bool,
+        traffic_class: u8,
+        hop_limit: u8,
+        receive_timeout: u32,
+        transmit_timeout: u32,
+        station_address: Ip6.Address,
+        station_port: u16,
+        remote_address: Ip6.Address,
+        remote_port: u16,
+    };
+
+    pub const CompletionToken = extern struct {
+        event: Event,
+        Status: usize,
+        packet: extern union {
+            RxData: *ReceiveData,
+            TxData: *TransmitData,
+        },
+    };
+
+    pub const ReceiveData = extern struct {
+        timestamp: Time,
+        recycle_signal: Event,
+        udp6_session: SessionData,
+        data_length: u32,
+        fragment_count: u32,
+
+        pub fn getFragments(self: *ReceiveData) []Fragment {
+            return @as([*]Fragment, @ptrCast(@alignCast(@as([*]u8, @ptrCast(self)) + @sizeOf(ReceiveData))))[0..self.fragment_count];
+        }
+    };
+
+    pub const TransmitData = extern struct {
+        udp6_session_data: ?*SessionData,
+        data_length: u32,
+        fragment_count: u32,
+
+        pub fn getFragments(self: *TransmitData) []Fragment {
+            return @as([*]Fragment, @ptrCast(@alignCast(@as([*]u8, @ptrCast(self)) + @sizeOf(TransmitData))))[0..self.fragment_count];
+        }
+    };
+
+    pub const SessionData = extern struct {
+        source_address: Ip6.Address,
+        source_port: u16,
+        destination_address: Ip6.Address,
+        destination_port: u16,
+    };
+
+    pub const Fragment = extern struct {
+        fragment_length: u32,
+        fragment_buffer: [*]u8,
+    };
+};

lib/std/builtin.zig
@@ -794,9 +794,9 @@ pub fn default_panic(msg: []const u8, error_return_trace: ?*StackTrace, ret_addr
 
             if (exit_data) |data| {
                 if (uefi.system_table.std_err) |out| {
-                    _ = out.setAttribute(uefi.protocols.SimpleTextOutputProtocol.red);
+                    _ = out.setAttribute(uefi.protocol.SimpleTextOutput.red);
                     _ = out.outputString(data);
-                    _ = out.setAttribute(uefi.protocols.SimpleTextOutputProtocol.white);
+                    _ = out.setAttribute(uefi.protocol.SimpleTextOutput.white);
                 }
             }
 

lib/std/os/uefi.zig
@@ -1,7 +1,9 @@
 const std = @import("../std.zig");
 
 /// A protocol is an interface identified by a GUID.
-pub const protocols = @import("uefi/protocols.zig");
+pub const protocol = @import("uefi/protocol.zig");
+pub const DevicePath = @import("uefi/device_path.zig").DevicePath;
+pub const hii = @import("uefi/hii.zig");
 
 /// Status codes returned by EFI interfaces
 pub const Status = @import("uefi/status.zig").Status;
@@ -157,7 +159,60 @@ test "GUID formatting" {
     try std.testing.expect(std.mem.eql(u8, str, "32cb3c89-8080-427c-ba13-5049873bc287"));
 }
 
+pub const FileInfo = extern struct {
+    size: u64,
+    file_size: u64,
+    physical_size: u64,
+    create_time: Time,
+    last_access_time: Time,
+    modification_time: Time,
+    attribute: u64,
+
+    pub fn getFileName(self: *const FileInfo) [*:0]const u16 {
+        return @ptrCast(@alignCast(@as([*]const u8, @ptrCast(self)) + @sizeOf(FileInfo)));
+    }
+
+    pub const efi_file_read_only: u64 = 0x0000000000000001;
+    pub const efi_file_hidden: u64 = 0x0000000000000002;
+    pub const efi_file_system: u64 = 0x0000000000000004;
+    pub const efi_file_reserved: u64 = 0x0000000000000008;
+    pub const efi_file_directory: u64 = 0x0000000000000010;
+    pub const efi_file_archive: u64 = 0x0000000000000020;
+    pub const efi_file_valid_attr: u64 = 0x0000000000000037;
+
+    pub const guid align(8) = Guid{
+        .time_low = 0x09576e92,
+        .time_mid = 0x6d3f,
+        .time_high_and_version = 0x11d2,
+        .clock_seq_high_and_reserved = 0x8e,
+        .clock_seq_low = 0x39,
+        .node = [_]u8{ 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b },
+    };
+};
+
+pub const FileSystemInfo = extern struct {
+    size: u64,
+    read_only: bool,
+    volume_size: u64,
+    free_space: u64,
+    block_size: u32,
+    _volume_label: u16,
+
+    pub fn getVolumeLabel(self: *const FileSystemInfo) [*:0]const u16 {
+        return @as([*:0]const u16, @ptrCast(&self._volume_label));
+    }
+
+    pub const guid align(8) = Guid{
+        .time_low = 0x09576e93,
+        .time_mid = 0x6d3f,
+        .time_high_and_version = 0x11d2,
+        .clock_seq_high_and_reserved = 0x8e,
+        .clock_seq_low = 0x39,
+        .node = [_]u8{ 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b },
+    };
+};
+
 test {
     _ = tables;
-    _ = protocols;
+    _ = protocol;
 }

commit e5e99d0ec45bc5bf08e93ba85f9dfb203f8ace01

fix compile errors introduced by #16953

lib/std/os/uefi/protocol.zig
@@ -18,7 +18,7 @@ pub const GraphicsOutput = @import("protocol/graphics_output.zig").GraphicsOutpu
 
 pub const edid = @import("protocol/edid.zig");
 
-pub const SimpleNetwork = @import("protocol/simple_network.zig").SimpleNetwork;
+pub const SimpleNetworkMode = @import("protocol/simple_network.zig").SimpleNetwork;
 pub const ManagedNetwork = @import("protocol/managed_network.zig").ManagedNetwork;
 
 pub const Ip6ServiceBinding = @import("protocol/ip6_service_binding.zig").Ip6ServiceBinding;
@@ -28,8 +28,8 @@ pub const Ip6Config = @import("protocol/ip6_config.zig").Ip6Config;
 pub const Udp6ServiceBinding = @import("protocol/udp6_service_binding.zig").Udp6ServiceBinding;
 pub const Udp6 = @import("protocol/udp6.zig").Udp6;
 
-pub const HiiDatabase = @import("protocol/hii_database.zig").HiiDatabase;
-pub const HiiPopup = @import("protocol/hii_popup.zig").HiiPopup;
+pub const HiiDatabase = @import("protocol/hii_database.zig").HIIDatabase;
+pub const HiiPopup = @import("protocol/hii_popup.zig").HIIPopup;
 
 test {
     @setEvalBranchQuota(2000);

lib/std/os/uefi/protocol/hii_popup.zig
@@ -8,10 +8,10 @@ const cc = uefi.cc;
 /// Display a popup window
 pub const HIIPopup = extern struct {
     revision: u64,
-    _create_popup: *const fn (*const HIIPopup, PopupStyle, PopupType, hii.HIIHandle, u16, ?*PopupSelection) callconv(cc) Status,
+    _create_popup: *const fn (*const HIIPopup, PopupStyle, PopupType, hii.Handle, u16, ?*PopupSelection) callconv(cc) Status,
 
     /// Displays a popup window.
-    pub fn createPopup(self: *const HIIPopup, style: PopupStyle, popup_type: PopupType, handle: hii.HIIHandle, msg: u16, user_selection: ?*PopupSelection) Status {
+    pub fn createPopup(self: *const HIIPopup, style: PopupStyle, popup_type: PopupType, handle: hii.Handle, msg: u16, user_selection: ?*PopupSelection) Status {
         return self._create_popup(self, style, popup_type, handle, msg, user_selection);
     }
 

lib/std/os/uefi/protocol/ip6.zig
@@ -3,8 +3,8 @@ const uefi = std.os.uefi;
 const Guid = uefi.Guid;
 const Event = uefi.Event;
 const Status = uefi.Status;
-const MacAddress = uefi.protocol.MacAddress;
-const ManagedNetworkConfigData = uefi.protocol.ManagedNetworkConfigData;
+const MacAddress = uefi.MacAddress;
+const ManagedNetworkConfigData = uefi.protocol.ManagedNetwork.Config;
 const SimpleNetworkMode = uefi.protocol.SimpleNetworkMode;
 const cc = uefi.cc;
 

lib/std/os/uefi/protocol/managed_network.zig
@@ -6,7 +6,7 @@ const Handle = uefi.Handle;
 const Status = uefi.Status;
 const Time = uefi.Time;
 const SimpleNetworkMode = uefi.protocol.SimpleNetworkMode;
-const MacAddress = uefi.protocol.MacAddress;
+const MacAddress = uefi.MacAddress;
 const cc = uefi.cc;
 
 pub const ManagedNetwork = extern struct {

lib/std/os/uefi/protocol/udp6.zig
@@ -5,12 +5,12 @@ const Event = uefi.Event;
 const Status = uefi.Status;
 const Time = uefi.Time;
 const Ip6 = uefi.protocol.Ip6;
-const ManagedNetworkConfigData = uefi.protocol.ManagedNetworkConfigData;
+const ManagedNetworkConfigData = uefi.protocol.ManagedNetwork.Config;
 const SimpleNetworkMode = uefi.protocol.SimpleNetworkMode;
 const cc = uefi.cc;
 
 pub const Udp6 = extern struct {
-    _get_mode_data: *const fn (*const Udp6, ?*Config, ?*Ip6.ModeData, ?*ManagedNetworkConfigData, ?*SimpleNetworkMode) callconv(cc) Status,
+    _get_mode_data: *const fn (*const Udp6, ?*Config, ?*Ip6.Mode, ?*ManagedNetworkConfigData, ?*SimpleNetworkMode) callconv(cc) Status,
     _configure: *const fn (*const Udp6, ?*const Config) callconv(cc) Status,
     _groups: *const fn (*const Udp6, bool, ?*const Ip6.Address) callconv(cc) Status,
     _transmit: *const fn (*const Udp6, *CompletionToken) callconv(cc) Status,
@@ -18,7 +18,7 @@ pub const Udp6 = extern struct {
     _cancel: *const fn (*const Udp6, ?*CompletionToken) callconv(cc) Status,
     _poll: *const fn (*const Udp6) callconv(cc) Status,
 
-    pub fn getModeData(self: *const Udp6, udp6_config_data: ?*Config, ip6_mode_data: ?*Ip6.ModeData, mnp_config_data: ?*ManagedNetworkConfigData, snp_mode_data: ?*SimpleNetworkMode) Status {
+    pub fn getModeData(self: *const Udp6, udp6_config_data: ?*Config, ip6_mode_data: ?*Ip6.Mode, mnp_config_data: ?*ManagedNetworkConfigData, snp_mode_data: ?*SimpleNetworkMode) Status {
         return self._get_mode_data(self, udp6_config_data, ip6_mode_data, mnp_config_data, snp_mode_data);
     }
 