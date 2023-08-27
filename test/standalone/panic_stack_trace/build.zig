284e7cad05e699c713be0e7d074d26c4374496e5
Wooster <wooster0@proton.me>
2023-08-27 23:39:49 +0200

    use a tmp dir
    This is kind of awful but it works for me locally and probably also on
    the CI.

--- test/standalone/panic_stack_trace/build.zig
+++ test/standalone/panic_stack_trace/build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const test_step = b.step("test", "Test");
     b.default_step = test_step;
 
@@ -25,7 +25,15 @@ pub fn build(b: *std.Build) void {
     exe.linkLibrary(lib);
     b.installArtifact(exe);
 
-    const dest_path = "test/standalone/panic_stack_trace";
+    // main.zig does the clean-up.
+    var tmp_dir = std.testing.tmpDir(.{});
+    const tmp_dir_path = try tmp_dir.dir.realpathAlloc(b.allocator, ".");
+    try tmp_dir.dir.writeFile(
+        "exe_path.zig",
+        try std.mem.join(b.allocator, "", &.{ "pub const exe_path = \"", tmp_dir_path, "\";" }),
+    );
+
+    const dest_path = tmp_dir_path;
     b.install_prefix = dest_path;
     b.install_path = dest_path;
     const install_exe = b.addInstallArtifact(exe, .{ .dest_dir = .{ .override = .{ .custom = "." } } });
@@ -37,6 +45,10 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
         .target = target,
     });
+    const exe_path_module = b.createModule(.{
+        .source_file = .{ .path = try std.mem.join(b.allocator, "", &.{ tmp_dir_path, "/", "exe_path.zig" }) },
+    });
+    main.addModule("exe_path", exe_path_module);
     const run = b.addRunArtifact(main);
     run.addArtifactArg(exe);
     run.expectExitCode(0);
--- test/standalone/panic_stack_trace/main.zig
+++ test/standalone/panic_stack_trace/main.zig
@@ -2,19 +2,25 @@
 //! of a library without libc linked that is linked against an executable without libc linked.
 
 const std = @import("std");
+const exe_path = @import("exe_path");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
+    const exe_path_exe = try std.mem.join(allocator, "", &.{ exe_path.exe_path, "/exe" });
+    const exe_path_exe_path_zig = try std.mem.join(allocator, "", &.{ exe_path.exe_path, "/exe_path.zig" });
+
     var result = try std.ChildProcess.exec(.{
         .allocator = allocator,
-        .argv = &[_][]const u8{"./exe"},
+        .argv = &[_][]const u8{exe_path_exe},
     });
     defer allocator.free(result.stdout);
     defer allocator.free(result.stderr);
 
     _ = std.mem.indexOf(u8, result.stderr, "@panic(\"hello\")").?;
 
-    try std.fs.cwd().deleteFile("exe");
+    try std.fs.deleteFileAbsolute(exe_path_exe);
+    try std.fs.deleteFileAbsolute(exe_path_exe_path_zig);
+    try std.fs.deleteDirAbsolute(exe_path.exe_path);
 }
