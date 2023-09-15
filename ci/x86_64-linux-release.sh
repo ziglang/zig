8278722e5705f331ff48a00f5972505b384552a1
Andrew Kelley <andrew@ziglang.org>
2023-09-14 19:45:13 -0700

    temporarily disable most ci stuff

--- ci/x86_64-linux-release.sh
+++ ci/x86_64-linux-release.sh
@@ -20,8 +20,8 @@ git config core.abbrev 9
 git fetch --unshallow || true
 git fetch --tags
 
-export CC="$ZIG cc -target $TARGET -mcpu=$MCPU"
-export CXX="$ZIG c++ -target $TARGET -mcpu=$MCPU"
+#export CC="$ZIG cc -target $TARGET -mcpu=$MCPU"
+#export CXX="$ZIG c++ -target $TARGET -mcpu=$MCPU"
 
 rm -rf build-release
 mkdir build-release
@@ -33,66 +33,66 @@ cd build-release
 export ZIG_GLOBAL_CACHE_DIR="$(pwd)/zig-global-cache"
 export ZIG_LOCAL_CACHE_DIR="$(pwd)/zig-local-cache"
 
-cmake .. \
-  -DCMAKE_INSTALL_PREFIX="stage3-release" \
-  -DCMAKE_PREFIX_PATH="$PREFIX" \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DZIG_TARGET_TRIPLE="$TARGET" \
-  -DZIG_TARGET_MCPU="$MCPU" \
-  -DZIG_STATIC=ON \
-  -DZIG_NO_LIB=ON \
-  -GNinja
-
-# Now cmake will use zig as the C/C++ compiler. We reset the environment variables
-# so that installation and testing do not get affected by them.
-unset CC
-unset CXX
-
-ninja install
-
-# TODO: move this to a build.zig step (check-fmt)
-echo "Looking for non-conforming code formatting..."
-stage3-release/bin/zig fmt --check .. \
-  --exclude ../test/cases/ \
-  --exclude ../build-debug \
-  --exclude ../build-release
-
-# simultaneously test building self-hosted without LLVM and with 32-bit arm
-stage3-release/bin/zig build \
-  -Dtarget=arm-linux-musleabihf \
-  -Dno-lib
-
-stage3-release/bin/zig build test docs \
-  --maxrss 21000000000 \
-  -fqemu \
-  -fwasmtime \
-  -Dstatic-llvm \
-  -Dtarget=native-native-musl \
-  --search-prefix "$PREFIX" \
-  --zig-lib-dir "$(pwd)/../lib"
-
-# Look for HTML errors.
-# TODO: move this to a build.zig flag (-Denable-tidy)
-tidy --drop-empty-elements no -qe "../zig-out/doc/langref.html"
-
-# Ensure that stage3 and stage4 are byte-for-byte identical.
-stage3-release/bin/zig build \
-  --prefix stage4-release \
-  -Denable-llvm \
-  -Dno-lib \
-  -Doptimize=ReleaseFast \
-  -Dstrip \
-  -Dtarget=$TARGET \
-  -Duse-zig-libcxx \
-  -Dversion-string="$(stage3-release/bin/zig version)"
-
-# diff returns an error code if the files differ.
-echo "If the following command fails, it means nondeterminism has been"
-echo "introduced, making stage3 and stage4 no longer byte-for-byte identical."
-diff stage3-release/bin/zig stage4-release/bin/zig
-
-# Ensure that updating the wasm binary from this commit will result in a viable build.
-stage3-release/bin/zig build update-zig1
+#cmake .. \
+#  -DCMAKE_INSTALL_PREFIX="stage3-release" \
+#  -DCMAKE_PREFIX_PATH="$PREFIX" \
+#  -DCMAKE_BUILD_TYPE=Release \
+#  -DZIG_TARGET_TRIPLE="$TARGET" \
+#  -DZIG_TARGET_MCPU="$MCPU" \
+#  -DZIG_STATIC=ON \
+#  -DZIG_NO_LIB=ON \
+#  -GNinja
+#
+## Now cmake will use zig as the C/C++ compiler. We reset the environment variables
+## so that installation and testing do not get affected by them.
+#unset CC
+#unset CXX
+#
+#ninja install
+#
+## TODO: move this to a build.zig step (check-fmt)
+#echo "Looking for non-conforming code formatting..."
+#stage3-release/bin/zig fmt --check .. \
+#  --exclude ../test/cases/ \
+#  --exclude ../build-debug \
+#  --exclude ../build-release
+#
+## simultaneously test building self-hosted without LLVM and with 32-bit arm
+#stage3-release/bin/zig build \
+#  -Dtarget=arm-linux-musleabihf \
+#  -Dno-lib
+#
+#stage3-release/bin/zig build test docs \
+#  --maxrss 21000000000 \
+#  -fqemu \
+#  -fwasmtime \
+#  -Dstatic-llvm \
+#  -Dtarget=native-native-musl \
+#  --search-prefix "$PREFIX" \
+#  --zig-lib-dir "$(pwd)/../lib"
+#
+## Look for HTML errors.
+## TODO: move this to a build.zig flag (-Denable-tidy)
+#tidy --drop-empty-elements no -qe "../zig-out/doc/langref.html"
+#
+## Ensure that stage3 and stage4 are byte-for-byte identical.
+#stage3-release/bin/zig build \
+#  --prefix stage4-release \
+#  -Denable-llvm \
+#  -Dno-lib \
+#  -Doptimize=ReleaseFast \
+#  -Dstrip \
+#  -Dtarget=$TARGET \
+#  -Duse-zig-libcxx \
+#  -Dversion-string="$(stage3-release/bin/zig version)"
+#
+## diff returns an error code if the files differ.
+#echo "If the following command fails, it means nondeterminism has been"
+#echo "introduced, making stage3 and stage4 no longer byte-for-byte identical."
+#diff stage3-release/bin/zig stage4-release/bin/zig
+#
+## Ensure that updating the wasm binary from this commit will result in a viable build.
+#stage3-release/bin/zig build update-zig1
 
 rm -rf ../build-new
 mkdir ../build-new
