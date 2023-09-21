70563aeac3e9efee7e8fb24744bc97197fc0ca9b
kcbanner <kcbanner@gmail.com>
2023-09-21 02:26:05 -0400

    windows: fix not finding system libs when compiling for *-windows-msvc
    When compiling for *-windows-msvc, find the native libc_installation and
    add the lib dirs to lib_dirs, so that system libs can be found.
    
    Previously, `version` and `ole32` were detected via the mingw.libExists logic,
    even on .msvc, which was a false positive. This detection logic for mingw doesn't
    find uuid.lib, which was the failure that triggered this bugfix.
    
    Only build the issue_5825 test if the native target is x86_64-windows-msvc,
    since it requires the .msvc abi.

--- test/standalone/issue_5825/build.zig
+++ test/standalone/issue_5825/build.zig
@@ -1,8 +1,12 @@
+const builtin = @import("builtin");
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Test it");
     b.default_step = test_step;
+
+    // Building for the msvc abi requires a native MSVC installation
+    if (builtin.os.tag != .windows or builtin.cpu.arch != .x86_64) return;
 
     const target = .{
         .cpu_arch = .x86_64,
