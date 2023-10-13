cbe9923c2220d7e3475c0adfe87a29142fef8e6d
Andrew Kelley <andrew@ziglang.org>
2023-10-12 21:19:43 -0700

    glibc patch: don't hardcode __GLIBC_MINOR__
    Instead Zig passes it based on the target.

--- lib/libc/include/generic-glibc/features.h
+++ lib/libc/include/generic-glibc/features.h
@@ -492,7 +492,7 @@
 /* Major and minor version number of the GNU C library package.  Use
    these macros to test for features in specific releases.  */
 #define	__GLIBC__	2
-#define	__GLIBC_MINOR__	39
+/* Zig patch: we pass `-D__GLIBC_MINOR__=XX` depending on the target. */
 
 #define __GLIBC_PREREQ(maj, min) \
 	((__GLIBC__ << 16) + __GLIBC_MINOR__ >= ((maj) << 16) + (min))
