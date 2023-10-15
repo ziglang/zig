e5d620b9d1af469c8fc5170ec9b646f8ac404343
David Rubin <daviru007@icloud.com>
2023-10-15 16:46:38 -0700

    add test to prevent regression

--- lib/std/compress/deflate/decompressor.zig
+++ lib/std/compress/deflate/decompressor.zig
@@ -899,6 +899,36 @@ const expectError = std.testing.expectError;
 const io = std.io;
 const testing = std.testing;
 
+test "confirm decompressor resets" {
+    var compressed = std.ArrayList(u8).init(std.testing.allocator);
+    defer compressed.deinit();
+
+    inline for (.{
+        &[_]u8{ 0x5d, 0xc0, 0x21, 0x01, 0x00, 0x00, 0x00, 0x80, 0x20, 0xff, 0xaf, 0xa6, 0x4b, 0x03 },
+        &[_]u8{ 0x55, 0xc1, 0x41, 0x0d, 0x00, 0x00, 0x00, 0x02, 0xa1, 0x94, 0x96, 0x34, 0x25, 0xef, 0x1b, 0x5f, 0x01 },
+    }) |data| {
+        try compressed.writer().writeAll(data);
+    }
+
+    var stream = std.io.fixedBufferStream(compressed.items);
+    var decomp = try decompressor(std.testing.allocator, stream.reader(), null);
+    defer decomp.deinit();
+
+    while (true) {
+        if (try stream.getPos() == try stream.getEndPos()) break;
+
+        const buf = try decomp.reader().readAllAlloc(std.testing.allocator, 1024 * 100);
+        defer std.testing.allocator.free(buf);
+
+        std.log.debug("{}", .{std.fmt.fmtSliceHexLower(buf)});
+        if (decomp.close()) |err| {
+            return err;
+        }
+
+        try decomp.reset(stream.reader(), null);
+    }
+}
+
 test "truncated input" {
     const TruncatedTest = struct {
         input: []const u8,
