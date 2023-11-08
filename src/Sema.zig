b1fc624041c2804061fff62de3d688d945054f0a
Bogdan Romanyuk <wrongnull@gmail.com>
2023-11-08 19:05:21 +0300

    propogate 'undefined' in comparison

--- src/Sema.zig
+++ src/Sema.zig
@@ -8907,10 +8907,13 @@ fn analyzeErrUnionCode(sema: *Sema, block: *Block, src: LazySrcLoc, operand: Air
     const result_ty = operand_ty.errorUnionSet(mod);
 
     if (try sema.resolveDefinedValue(block, src, operand)) |val| {
-        return Air.internedToRef((try mod.intern(.{ .err = .{
-            .ty = result_ty.toIntern(),
-            .name = mod.intern_pool.indexToKey(val.toIntern()).error_union.val.err_name,
-        } })));
+        switch (mod.intern_pool.indexToKey(val.toIntern()).error_union.val) {
+            .err_name => |err_name| return Air.internedToRef((try mod.intern(.{ .err = .{
+                .ty = result_ty.toIntern(),
+                .name = err_name,
+            } }))),
+            else => {},
+        }
     }
 
     try sema.requireRuntimeBlock(block, src, null);
@@ -16267,17 +16270,6 @@ fn zirCmpEq(
     const lhs = try sema.resolveInst(extra.lhs);
     const rhs = try sema.resolveInst(extra.rhs);
 
-    if (sema.resolveValue(lhs)) |lhs_maybe_undef| {
-        if (lhs_maybe_undef.isUndef(mod)) {
-            return Air.Inst.Ref.undef;
-         }
-    }
-    if (sema.resolveValue(rhs)) |rhs_maybe_undef| {
-        if (rhs_maybe_undef.isUndef(mod)) {
-            return Air.Inst.Ref.undef;
-        }
-    }
-
     const lhs_ty = sema.typeOf(lhs);
     const rhs_ty = sema.typeOf(rhs);
     const lhs_ty_tag = lhs_ty.zigTypeTag(mod);
@@ -16386,7 +16378,6 @@ fn zirCmp(
     const tracy = trace(@src());
     defer tracy.end();
 
-    const mod = sema.mod;
     const inst_data = sema.code.instructions.items(.data)[@intFromEnum(inst)].pl_node;
     const extra = sema.code.extraData(Zir.Inst.Bin, inst_data.payload_index).data;
     const src: LazySrcLoc = inst_data.src();
@@ -16394,17 +16385,6 @@ fn zirCmp(
     const rhs_src: LazySrcLoc = .{ .node_offset_bin_rhs = inst_data.src_node };
     const lhs = try sema.resolveInst(extra.lhs);
     const rhs = try sema.resolveInst(extra.rhs);
-
-    if (sema.resolveValue(lhs)) |lhs_maybe_undef| {
-        if (lhs_maybe_undef.isUndef(mod)) {
-            return Air.Inst.Ref.undef;
-         }
-    }
-    if (sema.resolveValue(rhs)) |rhs_maybe_undef| {
-        if (rhs_maybe_undef.isUndef(mod)) {
-            return Air.Inst.Ref.undef;
-        }
-    }
 
     return sema.analyzeCmp(block, src, lhs, rhs, op, lhs_src, rhs_src, false);
 }
@@ -16437,12 +16417,20 @@ fn analyzeCmp(
         return sema.cmpNumeric(block, src, lhs, rhs, op, lhs_src, rhs_src);
     }
     if (is_equality_cmp and lhs_ty.zigTypeTag(mod) == .ErrorUnion and rhs_ty.zigTypeTag(mod) == .ErrorSet) {
-        const casted_lhs = try sema.analyzeErrUnionCode(block, lhs_src, lhs);
-        return sema.cmpSelf(block, src, casted_lhs, rhs, op, lhs_src, rhs_src);
+        if (try sema.resolveValue(lhs)) |lhs_val| {
+            if (lhs_val.isUndef(mod)) return mod.undefRef(Type.bool);
+        } else {
+            const casted_lhs = try sema.analyzeErrUnionCode(block, lhs_src, lhs);
+            return sema.cmpSelf(block, src, casted_lhs, rhs, op, lhs_src, rhs_src);
+        }
     }
     if (is_equality_cmp and lhs_ty.zigTypeTag(mod) == .ErrorSet and rhs_ty.zigTypeTag(mod) == .ErrorUnion) {
-        const casted_rhs = try sema.analyzeErrUnionCode(block, rhs_src, rhs);
-        return sema.cmpSelf(block, src, lhs, casted_rhs, op, lhs_src, rhs_src);
+        if (try sema.resolveValue(rhs)) |rhs_val| {
+            if (rhs_val.isUndef(mod)) return mod.undefRef(Type.bool);
+        } else {
+            const casted_rhs = try sema.analyzeErrUnionCode(block, rhs_src, rhs);
+            return sema.cmpSelf(block, src, lhs, casted_rhs, op, lhs_src, rhs_src);
+        }
     }
     const instructions = &[_]Air.Inst.Ref{ lhs, rhs };
     const resolved_type = try sema.resolvePeerTypes(block, src, instructions, .{ .override = &[_]?LazySrcLoc{ lhs_src, rhs_src } });
@@ -16504,11 +16492,9 @@ fn cmpSelf(
         } else {
             // For bools, we still check the other operand, because we can lower
             // bool eq/neq more efficiently.
-            if (resolved_type.zigTypeTag(mod) == .Bool) {
-                if (try sema.resolveValue(casted_rhs)) |rhs_val| {
-                    if (rhs_val.isUndef(mod)) return mod.undefRef(Type.bool);
-                    return sema.runtimeBoolCmp(block, src, op, casted_lhs, rhs_val.toBool(), lhs_src);
-                }
+            if (try sema.resolveValue(casted_rhs)) |rhs_val| {
+                if (rhs_val.isUndef(mod)) return mod.undefRef(Type.bool);
+                if (resolved_type.zigTypeTag(mod) == .Bool) return sema.runtimeBoolCmp(block, src, op, casted_lhs, rhs_val.toBool(), lhs_src);
             }
             break :src lhs_src;
         }
@@ -32800,17 +32786,9 @@ fn cmpVector(
     const casted_lhs = try sema.coerce(block, resolved_ty, lhs, lhs_src);
     const casted_rhs = try sema.coerce(block, resolved_ty, rhs, rhs_src);
 
-    const result_ty = try mod.vectorType(.{
-        .len = lhs_ty.vectorLen(mod),
-        .child = .bool_type,
-    });
-
     const runtime_src: LazySrcLoc = src: {
         if (try sema.resolveValue(casted_lhs)) |lhs_val| {
             if (try sema.resolveValue(casted_rhs)) |rhs_val| {
-                if (lhs_val.isUndef(mod) or rhs_val.isUndef(mod)) {
-                    return mod.undefRef(result_ty);
-                }
                 const cmp_val = try sema.compareVector(lhs_val, op, rhs_val, resolved_ty);
                 return Air.internedToRef(cmp_val.toIntern());
             } else {
@@ -37777,7 +37755,10 @@ fn compareVector(
         const lhs_elem = try lhs.elemValue(sema.mod, i);
         const rhs_elem = try rhs.elemValue(sema.mod, i);
         const res_bool = try sema.compareScalar(lhs_elem, op, rhs_elem, ty.scalarType(mod));
-        scalar.* = try Value.makeBool(res_bool).intern(Type.bool, mod);
+        scalar.* = if (lhs_elem.isUndef(mod) or rhs_elem.isUndef(mod))
+            Air.refToInterned(try mod.undefRef(Type.bool)).?
+        else
+            try Value.makeBool(res_bool).intern(Type.bool, mod);
     }
     return (try mod.intern(.{ .aggregate = .{
         .ty = (try mod.vectorType(.{ .len = ty.vectorLen(mod), .child = .bool_type })).toIntern(),
