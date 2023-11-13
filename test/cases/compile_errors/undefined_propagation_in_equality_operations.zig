1ad4e6ece40d7cc08cffabbcb164565cc5d13da2
Bogdan Romanyuk <wrongnull@gmail.com>
2023-11-13 14:53:20 +0300

    add compile-error test

---
+++ test/cases/compile_errors/undefined_propagation_in_equality_operations.zig
@@ -0,0 +1,38 @@
+test "undefined propagation in equality operators for bool" {
+    var foo: bool = undefined;
+    _ = foo == undefined;
+    if (foo == undefined) {}
+}
+
+test "undefined propagation in equality operators for pointer" {
+    var foo: *i32 = undefined;
+    _ = foo == undefined;
+    if (foo == undefined) {}
+}
+
+test "undefined propagation in equality operators for optional" {
+    var foo: ?*i32 = undefined;
+    _ = foo == undefined;
+    if (foo == undefined) {}
+}
+
+test "undefined propagation in equality operators for error union" {
+    var foo: anyerror = undefined;
+    _ = foo == undefined;
+    if (foo == @as(anyerror!i32, undefined)) {}
+}
+
+test "undefined propagation in equality operators for integers" {
+    var foo: i32 = undefined;
+    _ = foo == undefined;
+    if (foo == undefined) {}
+}
+
+// error
+// is_test=true
+//
+// :4:13: error: use of undefined value here causes undefined behavior
+// :10:13: error: use of undefined value here causes undefined behavior
+// :16:13: error: use of undefined value here causes undefined behavior
+// :22:13: error: use of undefined value here causes undefined behavior
+// :28:13: error: use of undefined value here causes undefined behavior
