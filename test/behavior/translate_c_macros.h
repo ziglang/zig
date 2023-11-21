8c6ca1997be212226f2f28c7a74773c21d8c6484
Garrett <138411610+garrettlennoxbeck@users.noreply.github.com>
2023-11-21 07:54:46 -0600

    translate-c: skip blank macros when translating defines

--- test/behavior/translate_c_macros.h
+++ test/behavior/translate_c_macros.h
@@ -62,3 +62,9 @@ typedef _Bool uintptr_t;
 
 #define LONG(x) x##L
 #define X LONG(10)
+
+#define BLANK_MACRO
+#define BLANK_CHILD_MACRO BLANK_MACRO BLANK_MACRO BLANK_MACRO
+#define MACRO_VALUE 0
+typedef long def_type;
+#define BLANK_MACRO_CAST (BLANK_CHILD_MACRO def_type BLANK_CHILD_MACRO)MACRO_VALUE
