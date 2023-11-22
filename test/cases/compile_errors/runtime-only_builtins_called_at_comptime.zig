ed4534bf36ebd443ebc173123d0919267def66ff
Bogdan Romanyuk <wrongnull@gmail.com>
2023-11-22 17:33:17 +0300

    rework test

--- test/cases/compile_errors/runtime-only_builtins_called_at_comptime.zig
+++ test/cases/compile_errors/runtime-only_builtins_called_at_comptime.zig
@@ -1,25 +1,9 @@
-test "comptime @workGroupId call" {
-    _ = comptime @workGroupId(42);
-}
-
-test "comptime @workGroupSize call" {
-    _ = comptime @workGroupSize(42);
-}
-
-test "comptime @workItemId call" {
-    _ = comptime @workItemId(42);
-}
-
 test "comptime @panic call" {
     comptime @panic("amogus");
 }
 
 test "comptime @trap call" {
     comptime @trap();
-}
-
-test "comptime @setAlignStack call" {
-    comptime @setAlignStack(1);
 }
 
 test "comptime @breakpoint call" {
@@ -31,10 +15,6 @@ test "comptime @breakpoint call" {
 // target=native
 // is_test=true
 //
-// :2:18: error: encountered @workGroupId at comptime
-// :6:18: error: encountered @workGroupSize at comptime
-// :10:18: error: encountered @workItemId at comptime
-// :14:14: error: encountered @panic at comptime
-// :18:14: error: encountered @trap at comptime
-// :22:14: error: encountered @setAlignStack at comptime
-// :26:14: error: encountered @breakpoint at comptime
+// :2:14: error: encountered @panic at comptime
+// :6:14: error: encountered @trap at comptime
+// :10:14: error: encountered @breakpoint at comptime
