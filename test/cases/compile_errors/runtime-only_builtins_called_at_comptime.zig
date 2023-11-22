8ae70431ed2a71efcb33c2c1f5fdfdb0ba2c2218
Bogdan Romanyuk <wrongnull@gmail.com>
2023-11-22 14:16:35 +0300

    update test case

---
+++ test/cases/compile_errors/runtime-only_builtins_called_at_comptime.zig
@@ -0,0 +1,35 @@
+test "comptime @workGroupId call" {
+    _ = comptime @workGroupId(42);
+}
+
+test "comptime @workGroupSize call" {
+    _ = comptime @workGroupSize(42);
+}
+
+test "comptime @workItemId call" {
+    _ = comptime @workItemId(42);
+}
+
+test "comptime @panic call" {
+    comptime @panic("amogus");
+}
+
+test "comptime @trap call" {
+    comptime @trap();
+}
+
+test "comptime @setAlignStack call" {
+    comptime @setAlignStack(1);
+}
+
+// error
+// backend=stage2
+// target=native
+// is_test=true
+//
+// :2:18: error: encountered @workGroupId at comptime
+// :6:18: error: encountered @workGroupSize at comptime
+// :10:18: error: encountered @workItemId at comptime
+// :14:14: error: encountered @panic at comptime
+// :18:14: error: encountered @trap at comptime
+// :22:14: error: encountered @setAlignStack at comptime
