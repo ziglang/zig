0891ccd48fed69f88e9209525a9b50bfe4aa648c
Ryan Liptak <squeek502@hotmail.com>
2023-11-22 18:23:15 -0800

    Remove std.os.windows.ole32/shell32

--- lib/std/os/windows.zig
+++ lib/std/os/windows.zig
@@ -21,8 +21,6 @@ test {
 pub const advapi32 = @import("windows/advapi32.zig");
 pub const kernel32 = @import("windows/kernel32.zig");
 pub const ntdll = @import("windows/ntdll.zig");
-pub const ole32 = @import("windows/ole32.zig");
-pub const shell32 = @import("windows/shell32.zig");
 pub const ws2_32 = @import("windows/ws2_32.zig");
 pub const crypt32 = @import("windows/crypt32.zig");
 pub const nls = @import("windows/nls.zig");
