64138fb9bcf04b89412b331f30f6413ab9b68793
DISTREAT <99132213+DISTREAT@users.noreply.github.com>
2023-11-26 16:08:52 +0100

    docgen: add support for more ansi escape sequences

--- tools/docgen.zig
+++ tools/docgen.zig
@@ -875,7 +875,11 @@ fn termColor(allocator: Allocator, input: []const u8) ![]u8 {
             },
             .after_number => switch (c) {
                 ';' => state = .arg,
-                'D' => state = .start,
+                'A'...'G' => state = .start,
+                'J' => {
+                    buf.items.len = last_new_line;
+                    state = .start;
+                },
                 'K' => {
                     buf.items.len = last_new_line;
                     state = .start;
