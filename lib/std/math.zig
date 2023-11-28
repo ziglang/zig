5ebe551a51c2f16f606f0432d31154940272d261
expikr <77922942+expikr@users.noreply.github.com>
2023-11-28 19:27:23 +0800

    Update math.zig

--- lib/std/math.zig
+++ lib/std/math.zig
@@ -414,7 +414,7 @@ pub const ln = @compileError("deprecated; use @log instead");
 ///   /    /|   /
 ///  /    / |  /
 ///         |
-/// Limit x to the interval [-r, r).
+/// Limit x to the half-open interval [-r, r).
 pub fn wrap(x: anytype, r: anytype) @TypeOf(x) {
     return @mod(x + r, 2 * r) - r;
 }
