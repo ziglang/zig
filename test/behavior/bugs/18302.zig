ead15f437402f816e31d0eacd523deb5ff0c0326
Stefan Su <stefansu28@gmail.com>
2023-12-18 23:03:01 -0500

    skip test for stage2 backends

--- test/behavior/bugs/18302.zig
+++ test/behavior/bugs/18302.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 fn getIntShiftType(comptime T: type) type {
     const int = @typeInfo(T).Int;
@@ -32,6 +33,12 @@ pub fn FixedPoint(comptime value_type: type) type {
 }
 
 test "Segfault when compiling Saturating Shift Left where lhs is of a computed type" {
+    if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_sparc64) return error.SkipZigTest; // TODO
+    if (builtin.zig_backend == .stage2_spirv64) return error.SkipZigTest;
+
     const FP = FixedPoint(i32);
 
     _ = (FP {
