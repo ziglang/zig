97eae6af0c0e534d4f65b2afd3faf62b2fb206d6
amp-59 <114923809+amp-59@users.noreply.github.com>
2023-12-28 06:50:04 +0000

    Added tests for inheritance of `want_safety` by sub-blocks.

---
+++ test/cases/safety/inherit_want_safety_catch_block.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const general = switch (@import("builtin").mode) {
+    .ReleaseSafe, .Debug => true,
+    .ReleaseFast, .ReleaseSmall => false,
+};
+const want = !general;
+pub fn panic(_: []const u8, _: @TypeOf(@errorReturnTrace()), _: ?usize) noreturn {
+    std.process.exit(@intFromBool(general));
+}
+var i: usize = ~@as(usize, 0);
+pub fn main() void {
+    @setRuntimeSafety(want);
+    @as(error{E}, error.E) catch {
+        i += 1;
+    };
+    std.process.exit(@intFromBool(want));
+}
+// run
+// backend=llvm
+// target=native
