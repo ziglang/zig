c082f2bf300defb96b3d69e51e6a7b8e0edf734b
amp-59 <114923809+amp-59@users.noreply.github.com>
2024-01-03 13:30:03 +0000

    Updated test program.

--- test/cases/no_compile_panic_for_unchecked_arith.zig
+++ test/cases/no_compile_panic_for_unchecked_arith.zig
@@ -4,9 +4,13 @@ export fn entry() usize {
     var x: usize = 0;
     x +%= 1;
     x -%= 1;
-    x *%= 1;
+    x *%= 2;
+    x +|= 1;
+    x -|= 1;
+    x *|= 2;
     return x;
 }
 
 // compile
 // output_mode=Obj
+// backend=stage2,llvm
