d455794ef4c0ea853b732c44076c346f22061f02
february cozzocrea <february.cozzocrea@gmail.com>
2024-01-04 18:51:53 -0800

    failing test case added

---
+++ test/cases/run_translated_c/int_and_ptr_casts.c
@@ -0,0 +1,32 @@
+#include <stdint.h>
+
+void run() {
+  short s = -80;
+  short* s_ptr = &s;
+  unsigned short us = 160;
+  unsigned short* us_ptr = &us;
+  intptr_t i_ptr = 400;
+  uintptr_t u_ptr = 800;
+
+  s = (short)s_ptr;
+  s_ptr = (short*)s;
+
+  us = (unsigned short)us_ptr;
+  us_ptr = (unsigned short*)us;
+
+  s = (short)i_ptr;
+  i_ptr = (intptr_t)s;
+
+  s_ptr = (short*)i_ptr;
+  i_ptr = (intptr_t)s_ptr;
+
+  s = (short)u_ptr;
+  u_ptr = (uintptr_t)s;
+
+  s_ptr = (short*)u_ptr;
+  u_ptr = (uintptr_t)s_ptr;
+}
+
+// run-translated-c
+// c_frontends=aro,clang
+// link_libc=true
