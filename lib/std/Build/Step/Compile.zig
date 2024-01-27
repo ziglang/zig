71c0dc818df2dfd75aa3a6c89e19669bab7c42ff
Xavier Bouchoux <xavierb@gmail.com>
2024-01-27 14:08:34 +0100

    std.Build: add precompiled C header creation

--- lib/std/Build/Step/Compile.zig
+++ lib/std/Build/Step/Compile.zig
@@ -266,6 +266,7 @@ pub const Kind = enum {
     exe,
     lib,
     obj,
+    pch,
     @"test",
 };
 
@@ -349,6 +350,7 @@ pub fn create(owner: *std.Build, options: Options) *Compile {
             .exe => "zig build-exe",
             .lib => "zig build-lib",
             .obj => "zig build-obj",
+            .pch => "zig build-pch",
             .@"test" => "zig test",
         },
         name_adjusted,
@@ -356,17 +358,21 @@ pub fn create(owner: *std.Build, options: Options) *Compile {
         resolved_target.query.zigTriple(owner.allocator) catch @panic("OOM"),
     });
 
-    const out_filename = std.zig.binNameAlloc(owner.allocator, .{
-        .root_name = name,
-        .target = target,
-        .output_mode = switch (options.kind) {
-            .lib => .Lib,
-            .obj => .Obj,
-            .exe, .@"test" => .Exe,
-        },
-        .link_mode = options.linkage,
-        .version = options.version,
-    }) catch @panic("OOM");
+    const out_filename = if (options.kind == .pch)
+        std.fmt.allocPrint(owner.allocator, "{s}.pch", .{name}) catch @panic("OOM")
+    else
+        std.zig.binNameAlloc(owner.allocator, .{
+            .root_name = name,
+            .target = target,
+            .output_mode = switch (options.kind) {
+                .lib => .Lib,
+                .obj => .Obj,
+                .exe, .@"test" => .Exe,
+                .pch => unreachable,
+            },
+            .link_mode = options.linkage,
+            .version = options.version,
+        }) catch @panic("OOM");
 
     const compile = owner.allocator.create(Compile) catch @panic("OOM");
     compile.* = .{
@@ -795,10 +801,12 @@ pub fn linkFrameworkWeak(c: *Compile, name: []const u8) void {
 
 /// Handy when you have many C/C++ source files and want them all to have the same flags.
 pub fn addCSourceFiles(compile: *Compile, options: Module.AddCSourceFilesOptions) void {
+    assert(compile.kind != .pch); // pch can only be generated from a single C header file
     compile.root_module.addCSourceFiles(options);
 }
 
 pub fn addCSourceFile(compile: *Compile, source: Module.CSourceFile) void {
+    assert(compile.kind != .pch or compile.root_module.link_objects.items.len == 0); // pch can only be generated from a single C header file
     compile.root_module.addCSourceFile(source);
 }
 
@@ -806,6 +814,7 @@ pub fn addCSourceFile(compile: *Compile, source: Module.CSourceFile) void {
 /// Can be called regardless of target. The .rc file will be ignored
 /// if the target object format does not support embedded resources.
 pub fn addWin32ResourceFile(compile: *Compile, source: Module.RcSourceFile) void {
+    assert(compile.kind != .pch); // pch can only be generated from a single C header file
     compile.root_module.addWin32ResourceFile(source);
 }
 
@@ -886,14 +895,17 @@ pub fn getEmittedLlvmBc(compile: *Compile) LazyPath {
 }
 
 pub fn addAssemblyFile(compile: *Compile, source: Module.AsmSourceFile) void {
+    assert(compile.kind != .pch); // pch can only be generated from a single C header file
     compile.root_module.addAssemblyFile(source);
 }
 
 pub fn addObjectFile(compile: *Compile, source: LazyPath) void {
+    assert(compile.kind != .pch); // pch can only be generated from a single C header file
     compile.root_module.addObjectFile(source);
 }
 
 pub fn addObject(compile: *Compile, object: *Compile) void {
+    assert(compile.kind != .pch); // pch can only be generated from a single C header file
     compile.root_module.addObject(object);
 }
 
@@ -1018,6 +1030,7 @@ fn getZigArgs(compile: *Compile, fuzz: bool) ![][]const u8 {
         .lib => "build-lib",
         .exe => "build-exe",
         .obj => "build-obj",
+        .pch => "build-pch",
         .@"test" => "test",
     };
     try zig_args.append(cmd);
@@ -1083,6 +1096,14 @@ fn getZigArgs(compile: *Compile, fuzz: bool) ![][]const u8 {
                 if (key.module.link_libc == true) compile.is_linking_libc = true;
                 if (key.module.link_libcpp == true) compile.is_linking_libcpp = true;
             }
+        }
+
+        if (compile.kind == .pch) {
+            // precompiled headers must have a single input header file.
+            var it = compile.root_module.iterateDependencies(compile, false);
+            const link_objects = it.next().?.module.link_objects;
+            assert(link_objects.items.len == 1 and link_objects.items[0] == .c_source_file);
+            assert(it.next() == null);
         }
 
         var cli_named_modules = try CliNamedModules.init(arena, &compile.root_module);
@@ -1196,6 +1217,7 @@ fn getZigArgs(compile: *Compile, fuzz: bool) ![][]const u8 {
                         switch (other.kind) {
                             .exe => return step.fail("cannot link with an executable build artifact", .{}),
                             .@"test" => return step.fail("cannot link with a test", .{}),
+                            .pch => @panic("Cannot link with a precompiled header file"),
                             .obj => {
                                 const included_in_lib_or_obj = !my_responsibility and
                                     (dep.compile.?.kind == .lib or dep.compile.?.kind == .obj);
