f4391930ed196542457c06a1eb6e3b51b7d3030e
David Rubin <daviru007@icloud.com>
2024-02-04 03:21:58 -0800

    better test case

--- test/cases/compile_errors/comptime_keyword.zig
+++ test/cases/compile_errors/comptime_keyword.zig
@@ -1,11 +1,11 @@
 export fn a() void {
-    var x: u8 = 10;
+    var x: bool = false;
     _ = &x;
-    switch (comptime x) {}
+    if (comptime x) {}
 }
 
 // error
 // backend=stage2
 // target=native
 //
-// :4:22: error: unable to resolve comptime value
+// :4:18: error: unable to resolve comptime value
