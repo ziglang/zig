03f29c69d752726363b0db0597b5a5e75a94223c
Carl Åstholm <carl@astholm.se>
2024-02-17 17:23:08 +0100

    Turn "simple" standalone test cases into a package

--- test/standalone/build.zig
+++ test/standalone/build.zig
@@ -5,14 +5,58 @@ pub fn build(b: *std.Build) void {
     const step = b.step("test", "Run standalone test cases");
     b.default_step = step;
 
-    const enable_ios_sdk = b.option(bool, "enable-ios-sdk", "Run tests requiring presence of iOS SDK and frameworks") orelse false;
-    const enable_macos_sdk = b.option(bool, "enable-macos-sdk", "Run tests requiring presence of macOS SDK and frameworks") orelse enable_ios_sdk;
-    const enable_symlinks_windows = b.option(bool, "enable-symlinks-windows", "Run tests requiring presence of symlinks on Windows") orelse false;
-
+    const enable_ios_sdk = b.option(bool, "enable_ios_sdk", "Run tests requiring presence of iOS SDK and frameworks") orelse false;
+    const enable_macos_sdk = b.option(bool, "enable_macos_sdk", "Run tests requiring presence of macOS SDK and frameworks") orelse enable_ios_sdk;
+    const enable_symlinks_windows = b.option(bool, "enable_symlinks_windows", "Run tests requiring presence of symlinks on Windows") orelse false;
     const omit_symlinks = builtin.os.tag == .windows and !enable_symlinks_windows;
+
+    const simple_skip_debug = b.option(bool, "simple_skip_debug", "Simple tests skip debug builds") orelse false;
+    const simple_skip_release_safe = b.option(bool, "simple_skip_release_safe", "Simple tests skip release-safe builds") orelse false;
+    const simple_skip_release_fast = b.option(bool, "simple_skip_release_fast", "Simple tests skip release-fast builds") orelse false;
+    const simple_skip_release_small = b.option(bool, "simple_skip_release_small", "Simple tests skip release-small builds") orelse false;
+
+    const simple_dep = b.dependency("simple", .{
+        .skip_debug = simple_skip_debug,
+        .skip_release_safe = simple_skip_release_safe,
+        .skip_release_fast = simple_skip_release_fast,
+        .skip_release_small = simple_skip_release_small,
+    });
+    const simple_dep_step = simple_dep.builder.default_step;
+    simple_dep_step.name = "standalone_test_cases.simple";
+    step.dependOn(simple_dep_step);
+
+    // Ensure the development tools are buildable.
+    const tools_tests_step = b.step("standalone_test_cases.tools", "Test tools");
+    step.dependOn(tools_tests_step);
+    const tools_target = b.resolveTargetQuery(.{});
+    for ([_][]const u8{
+        // Alphabetically sorted. No need to build `tools/spirv/grammar.zig`.
+        "../../tools/gen_outline_atomics.zig",
+        "../../tools/gen_spirv_spec.zig",
+        "../../tools/gen_stubs.zig",
+        "../../tools/generate_linux_syscalls.zig",
+        "../../tools/process_headers.zig",
+        "../../tools/update-linux-headers.zig",
+        "../../tools/update_clang_options.zig",
+        "../../tools/update_cpu_features.zig",
+        "../../tools/update_glibc.zig",
+        "../../tools/update_spirv_features.zig",
+    }) |tool_src_path| {
+        const tool = b.addTest(.{
+            .name = std.fs.path.stem(tool_src_path),
+            .root_source_file = .{ .path = tool_src_path },
+            .optimize = .Debug,
+            .target = tools_target,
+        });
+        const run = b.addRunArtifact(tool);
+        tools_tests_step.dependOn(&run.step);
+    }
 
     add_dep_steps: for (b.available_deps) |available_dep| {
         const dep_name, const dep_hash = available_dep;
+
+        // The 'simple' dependency was already handled manually above.
+        if (std.mem.eql(u8, dep_name, "simple")) continue;
 
         const all_pkgs = @import("root").dependencies.packages;
         inline for (@typeInfo(all_pkgs).Struct.decls) |decl| {
@@ -20,8 +64,7 @@ pub fn build(b: *std.Build) void {
             if (std.mem.eql(u8, dep_hash, pkg_hash)) {
                 const pkg = @field(all_pkgs, pkg_hash);
                 if (!@hasDecl(pkg, "build_zig")) {
-                    std.debug.print("standalone test case '{s}' is missing a 'build.zig' file\n", .{dep_name});
-                    std.process.exit(1);
+                    std.debug.panic("standalone test case '{s}' is missing a 'build.zig' file", .{dep_name});
                 }
                 const requires_ios_sdk = @hasDecl(pkg.build_zig, "requires_ios_sdk") and
                     pkg.build_zig.requires_ios_sdk;
