e1d8187028ec6df7fbf420d3e4b56da8bf3dcf0a
mlugg <mlugg@mlugg.co.uk>
2024-03-05 09:45:58 +0000

    cases: correct after #18816
    I changed an error messages and fixed a minor bug while implementing
    this proposal, which led to a few compile error cases failing.

--- test/cases/compile_errors/reify_type_for_tagged_union_with_no_union_fields.zig
+++ test/cases/compile_errors/reify_type_for_tagged_union_with_no_union_fields.zig
@@ -26,7 +26,7 @@ export fn entry() void {
 // backend=stage2
 // target=native
 //
-// :12:16: error: enum field(s) missing in union
+// :12:16: error: enum fields missing in union
 // :1:13: note: field 'signed' missing, declared here
 // :1:13: note: field 'unsigned' missing, declared here
 // :1:13: note: enum declared here
