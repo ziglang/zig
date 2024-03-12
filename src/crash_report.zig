8c04e3ad316ea373854ae364943371c647a36d6a
amp-59 <114923809+amp-59@users.noreply.github.com>
2024-03-12 03:40:52 +0000

    crash_report: Added tentative definition for `compilerPanicNew`.

--- src/crash_report.zig
+++ src/crash_report.zig
@@ -15,7 +15,8 @@ const Decl = Module.Decl;
 /// To use these crash report diagnostics, publish this panic in your main file
 /// and add `pub const enable_segfault_handler = false;` to your `std_options`.
 /// You will also need to call initialize() on startup, preferably as the very first operation in your program.
-pub const panic = if (build_options.enable_debug_extensions) compilerPanic else std.builtin.default_panic;
+pub const panic = if (build_options.enable_debug_extensions) compilerPanic else std.debug.panicImpl;
+pub const panicNew = if (build_options.enable_debug_extensions) compilerPanicNew else std.builtin.panicNew;
 
 /// Install signal handlers to identify crashes and report diagnostics.
 pub fn initialize() void {
@@ -147,6 +148,17 @@ pub fn compilerPanic(msg: []const u8, error_return_trace: ?*std.builtin.StackTra
     PanicSwitch.preDispatch();
     @setCold(true);
     const ret_addr = maybe_ret_addr orelse @returnAddress();
+    const stack_ctx: StackContext = .{ .current = .{ .ret_addr = ret_addr } };
+    PanicSwitch.dispatch(error_return_trace, stack_ctx, msg);
+}
+pub fn compilerPanicNew(comptime panic_cause: std.builtin.PanicCause, data: std.builtin.PanicData(panic_cause)) noreturn {
+    PanicSwitch.preDispatch();
+    @setCold(true);
+    const ret_addr = @returnAddress();
+    const error_return_trace: ?std.builtin.StackTrace =
+        if (panic_cause != .unwrapped_error) null else data.st;
+    const msg: []const u8 =
+        if (panic_cause != .message) @tagName(panic_cause) else data;
     const stack_ctx: StackContext = .{ .current = .{ .ret_addr = ret_addr } };
     PanicSwitch.dispatch(error_return_trace, stack_ctx, msg);
 }
