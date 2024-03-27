03760d93b87cbfe7d91acdfc073ef6947cf83a04
bilaliscarioth <98892516+bilaliscarioth@users.noreply.github.com>
2024-03-27 23:31:34 +0100

    Update debug.zig

--- lib/std/debug.zig
+++ lib/std/debug.zig
@@ -690,7 +690,7 @@ pub const StackIterator = struct {
         } else if (@hasDecl(posix.system, "msync") and native_os != .wasi and native_os != .emscripten) {
             posix.msync(aligned_memory, posix.MSF.ASYNC) catch |err| {
                 switch (err) {
-                    error.AccessDenied => return true,
+                    error.PermissionDenied => return true,
                     error.UnmappedMemory => return false,
                     else => unreachable,
                 }
