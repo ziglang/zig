6b7e1a48970ca3be02047bff9d895894f8a3f2ab
Frank Denis <github@pureftpd.org>
2024-04-11 15:50:05 +0200

    Update wasi-libc to d03829489904d38c624f6de9983190f1e5e7c9c5

--- lib/libc/include/wasm-wasi-musl/__struct_sockaddr_in6.h
+++ lib/libc/include/wasm-wasi-musl/__struct_sockaddr_in6.h
@@ -1,15 +1,12 @@
 #ifndef __wasilibc___struct_sockaddr_in6_h
 #define __wasilibc___struct_sockaddr_in6_h
 
-#define __need_STDDEF_H_misc
-#include <stddef.h>
-
 #include <__typedef_sa_family_t.h>
 #include <__typedef_in_port_t.h>
 #include <__struct_in6_addr.h>
 
 struct sockaddr_in6 {
-    _Alignas(max_align_t) sa_family_t sin6_family;
+    __attribute__((aligned(__BIGGEST_ALIGNMENT__))) sa_family_t sin6_family;
     in_port_t sin6_port;
     unsigned sin6_flowinfo;
     struct in6_addr sin6_addr;
