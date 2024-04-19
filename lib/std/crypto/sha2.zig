7b0e08c9acc972da28738743a6ce909ce77d87df
Meghan Denny <hello@nektro.net>
2024-04-18 22:44:47 -0700

    add sha512-224 and sha512-256 tests

--- lib/std/crypto/sha2.zig
+++ lib/std/crypto/sha2.zig
@@ -890,3 +890,25 @@ test "sha512 aligned final" {
     h.update(&block);
     h.final(out[0..]);
 }
+
+test "sha512-224 single" {
+    const h1 = "6ed0dd02806fa89e25de060c19d3ac86cabb87d6a0ddd05c333b84f4";
+    try htest.assertEqualHash(Sha512224, h1, "");
+
+    const h2 = "4634270f707b6a54daae7530460842e20e37ed265ceee9a43e8924aa";
+    try htest.assertEqualHash(Sha512224, h2, "abc");
+
+    const h3 = "23fec5bb94d60b23308192640b0c453335d664734fe40e7268674af9";
+    try htest.assertEqualHash(Sha512224, h3, "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu");
+}
+
+test "sha512-256 single" {
+    const h1 = "c672b8d1ef56ed28ab87c3622c5114069bdd3ad7b8f9737498d0c01ecef0967a";
+    try htest.assertEqualHash(Sha512256, h1, "");
+
+    const h2 = "53048e2681941ef99b2e29b76b4c7dabe4c2d0c634fc6d46e0e2f13107e7af23";
+    try htest.assertEqualHash(Sha512256, h2, "abc");
+
+    const h3 = "3928e184fb8690f840da3988121d31be65cb9d3ef83ee6146feac861e19b563a";
+    try htest.assertEqualHash(Sha512256, h3, "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu");
+}
