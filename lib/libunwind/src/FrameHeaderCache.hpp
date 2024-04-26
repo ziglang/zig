60f8ea597bedc68f243b25cf80adf032fba70ae1
Andrew Kelley <andrew@ziglang.org>
2024-04-26 14:40:08 -0700

    libunwind: update to LLVM 18
    release/18.x branch, commit 78b99c73ee4b96fe9ce0e294d4632326afb2db42

--- lib/libunwind/src/FrameHeaderCache.hpp
+++ lib/libunwind/src/FrameHeaderCache.hpp
@@ -31,8 +31,8 @@
 
 class _LIBUNWIND_HIDDEN FrameHeaderCache {
   struct CacheEntry {
-    uintptr_t LowPC() { return Info.dso_base; };
-    uintptr_t HighPC() { return Info.dso_base + Info.text_segment_length; };
+    uintptr_t LowPC() { return Info.dso_base; }
+    uintptr_t HighPC() { return Info.dso_base + Info.text_segment_length; }
     UnwindInfoSections Info;
     CacheEntry *Next;
   };
@@ -41,7 +41,7 @@ class _LIBUNWIND_HIDDEN FrameHeaderCache {
 
   // Can't depend on the C++ standard library in libunwind, so use an array to
   // allocate the entries, and two linked lists for ordering unused and recently
-  // used entries.  FIXME: Would the the extra memory for a doubly-linked list
+  // used entries.  FIXME: Would the extra memory for a doubly-linked list
   // be better than the runtime cost of traversing a very short singly-linked
   // list on a cache miss? The entries themselves are all small and consecutive,
   // so unlikely to cause page faults when following the pointers. The memory
--- src/libunwind.zig
+++ src/libunwind.zig
@@ -164,6 +164,7 @@ const unwind_src_list = [_][]const u8{
     "libunwind" ++ path.sep_str ++ "src" ++ path.sep_str ++ "UnwindLevel1.c",
     "libunwind" ++ path.sep_str ++ "src" ++ path.sep_str ++ "UnwindLevel1-gcc-ext.c",
     "libunwind" ++ path.sep_str ++ "src" ++ path.sep_str ++ "Unwind-sjlj.c",
+    "libunwind" ++ path.sep_str ++ "src" ++ path.sep_str ++ "Unwind-wasm.c",
     "libunwind" ++ path.sep_str ++ "src" ++ path.sep_str ++ "UnwindRegistersRestore.S",
     "libunwind" ++ path.sep_str ++ "src" ++ path.sep_str ++ "UnwindRegistersSave.S",
     "libunwind" ++ path.sep_str ++ "src" ++ path.sep_str ++ "Unwind_AIXExtras.cpp",
