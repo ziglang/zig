d0d66f95c63fed2503259425decbca20dd0cb00b
amp-59 <114923809+amp-59@users.noreply.github.com>
2024-05-10 00:34:30 +0000

    test: Updated `panic_has_source_location` to define `panicNew`.

--- test/cases/compile_errors/panic_has_source_location.zig
+++ test/cases/compile_errors/panic_has_source_location.zig
@@ -6,7 +6,7 @@ export fn foo() void {
     @panic("oh no");
 }
 
-pub fn panic(_: []const u8, _: ?*std.builtin.StackTrace, _: ?usize) noreturn {
+pub fn panicNew(_: anytype, _: anytype) noreturn {
     @compileError("panic");
 }
 
