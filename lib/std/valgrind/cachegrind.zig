5e7255c71f46c0dfe6ca208d0ba3fcbd3d81adbb
Prokop Randacek <prokop@rdck.dev>
2024-05-10 22:29:20 +0200

    Avoid Redundant Names in Fully-Qualified Namespaces

--- lib/std/valgrind/cachegrind.zig
+++ lib/std/valgrind/cachegrind.zig
@@ -1,29 +1,29 @@
 const std = @import("../std.zig");
 const valgrind = std.valgrind;
 
-pub const CachegrindClientRequest = enum(usize) {
+pub const ClientRequest = enum(usize) {
     StartInstrumentation = valgrind.ToolBase("CG".*),
     StopInstrumentation,
 };
 
-fn doCachegrindClientRequestExpr(default: usize, request: CachegrindClientRequest, a1: usize, a2: usize, a3: usize, a4: usize, a5: usize) usize {
+fn doClientRequestExpr(default: usize, request: ClientRequest, a1: usize, a2: usize, a3: usize, a4: usize, a5: usize) usize {
     return valgrind.doClientRequest(default, @as(usize, @intCast(@intFromEnum(request))), a1, a2, a3, a4, a5);
 }
 
-fn doCachegrindClientRequestStmt(request: CachegrindClientRequest, a1: usize, a2: usize, a3: usize, a4: usize, a5: usize) void {
-    _ = doCachegrindClientRequestExpr(0, request, a1, a2, a3, a4, a5);
+fn doClientRequestStmt(request: ClientRequest, a1: usize, a2: usize, a3: usize, a4: usize, a5: usize) void {
+    _ = doClientRequestExpr(0, request, a1, a2, a3, a4, a5);
 }
 
 /// Start Cachegrind instrumentation if not already enabled. Use this in
 /// combination with `std.valgrind.cachegrind.stopInstrumentation` and
 /// `--instr-at-start` to measure only part of a client program's execution.
 pub fn startInstrumentation() void {
-    doCachegrindClientRequestStmt(.StartInstrumentation, 0, 0, 0, 0, 0);
+    doClientRequestStmt(.StartInstrumentation, 0, 0, 0, 0, 0);
 }
 
 /// Stop Cachegrind instrumentation if not already disabled. Use this in
 /// combination with `std.valgrind.cachegrind.startInstrumentation` and
 /// `--instr-at-start` to measure only part of a client program's execution.
 pub fn stopInstrumentation() void {
-    doCachegrindClientRequestStmt(.StopInstrumentation, 0, 0, 0, 0, 0);
+    doClientRequestStmt(.StopInstrumentation, 0, 0, 0, 0, 0);
 }
