340c58be99872d31c5a4d46bbdb14600812b24cf
freakmangd <53349189+freakmangd@users.noreply.github.com>
2024-05-31 18:07:24 -0400

    update test case

--- test/cases/translate_c/macro_referencing_var.c
+++ test/cases/translate_c/macro_referencing_var.c
@@ -1,14 +1,20 @@
-extern float number;
-#define number_twice number * 2.0f
-#define number_negative -number
+extern float foo;
+#define FOO_TWICE foo * 2.0f
+#define FOO_NEGATIVE -foo
+
+#define BAR 10.0f
+#define BAR_TWICE BAR * 2.0f
 
 // translate-c
 // c_frontend=clang
 //
-// pub inline fn number_twice() @TypeOf(number * @as(f32, 2.0)) {
-//     return number * @as(f32, 2.0);
-// }
+// pub extern var foo: f32;
 //
-// pub inline fn number_negative() @TypeOf(-number) {
-//     return -number;
+// pub inline fn FOO_TWICE() @TypeOf(foo * @as(f32, 2.0)) {
+//     return foo * @as(f32, 2.0);
 // }
+// pub inline fn FOO_NEGATIVE() @TypeOf(-foo) {
+//     return -foo;
+// }
+// pub const BAR = @as(f32, 10.0);
+// pub const BAR_TWICE = BAR * @as(f32, 2.0);
