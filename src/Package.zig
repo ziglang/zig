a0d201a34a8d861ed6dab81566ed382ce13f0596
dweiller <4678790+dweiller@users.noreply.github.com>
2024-06-13 19:34:07 +1000

    move introspect, Manifest, findBuildRoot, findTemplates to std.zig

--- src/Package.zig
+++ src/Package.zig
@@ -1,7 +1,9 @@
 pub const Module = @import("Package/Module.zig");
 pub const Fetch = @import("Package/Fetch.zig");
-pub const build_zig_basename = "build.zig";
-pub const Manifest = @import("Package/Manifest.zig");
+pub const build_zig_basename = std.zig.build_file_basename;
+pub const Manifest = std.zig.Manifest;
+
+const std = @import("std");
 
 test {
     _ = Fetch;
