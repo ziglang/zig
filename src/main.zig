ec263122106c5f2f7202d597d61daee8b28da099
dweiller <4678790+dweiller@users.noreply.github.com>
2024-06-19 00:28:29 +1000

    package manager: allow overriding deps with local cache

--- src/main.zig
+++ src/main.zig
@@ -5025,6 +5025,7 @@ fn cmdBuild(gpa: Allocator, arena: Allocator, args: []const []const u8) !void {
                     .http_client = &http_client,
                     .thread_pool = &thread_pool,
                     .global_cache = global_cache_directory,
+                    .local_cache = local_cache_directory,
                     .read_only = false,
                     .recursive = true,
                     .debug_hash = false,
@@ -5067,6 +5068,7 @@ fn cmdBuild(gpa: Allocator, arena: Allocator, args: []const []const u8) !void {
                     .use_latest_commit = false,
 
                     .package_root = undefined,
+                    .local_override = false,
                     .error_bundle = undefined,
                     .manifest = null,
                     .manifest_ast = undefined,
@@ -6933,6 +6935,7 @@ fn cmdFetch(
         .http_client = &http_client,
         .thread_pool = &thread_pool,
         .global_cache = global_cache_directory,
+        .local_cache = null,
         .recursive = false,
         .read_only = false,
         .debug_hash = debug_hash,
@@ -6956,6 +6959,7 @@ fn cmdFetch(
         .use_latest_commit = true,
 
         .package_root = undefined,
+        .local_override = false,
         .error_bundle = undefined,
         .manifest = null,
         .manifest_ast = undefined,
