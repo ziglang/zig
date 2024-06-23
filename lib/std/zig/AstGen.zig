786fb34bddf43a9d510ea3089fccdac8904bb4ef
wooster0 <wooster0@proton.me>
2024-06-23 21:37:11 +0900

    Make 0e.0 and 0xp0 not crash
    This fixes those sequences of characters crashing.

--- lib/std/zig/AstGen.zig
+++ lib/std/zig/AstGen.zig
@@ -8637,6 +8637,7 @@ fn failWithNumberError(astgen: *AstGen, err: std.zig.number_literal.Error, token
             assert(bytes.len >= 2 and bytes[0] == '0' and bytes[1] == 'x'); // Validated by tokenizer
             return astgen.failOff(token, @intCast(i), "sign '{c}' cannot follow digit '{c}' in hex base", .{ bytes[i], bytes[i - 1] });
         },
+        .period_after_exponent => |i| return astgen.failOff(token, @intCast(i), "unexpected period after exponent", .{}),
     }
 }
 
