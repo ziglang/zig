9e6f0bf81644534256c4e99ca40cdaa92350a129
Alex Rønne Petersen <alex@alexrp.com>
2024-06-23 17:44:56 +0200

    std.Target: Remove `nacl` OS specifier and `le32`/`le64` arch specifiers.
    Native Client is dead.
    
    https://developer.chrome.com/docs/native-client

--- src/target.zig
+++ src/target.zig
@@ -153,8 +153,6 @@ pub fn hasLlvmSupport(target: std.Target, ofmt: std.Target.ObjectFormat) bool {
         .xtensa,
         .nvptx,
         .nvptx64,
-        .le32,
-        .le64,
         .amdil,
         .amdil64,
         .hsail,
