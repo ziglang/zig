8db1490b8a36155d85a2d5741f97984ff583cfde
Andrew Kelley <andrew@ziglang.org>
2024-07-01 16:38:02 -0700

    update test build script to latest API

--- test/standalone/dep_duplicate_module/build.zig
+++ test/standalone/dep_duplicate_module/build.zig
@@ -5,14 +5,14 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const mod = b.addModule("mod", .{
-        .root_source_file = .{ .path = "mod.zig" },
+        .root_source_file = b.path("mod.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     const lib = b.addStaticLibrary(.{
         .name = "lib",
-        .root_source_file = .{ .path = "lib.zig" },
+        .root_source_file = b.path("lib.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -20,7 +20,7 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "app",
-        .root_source_file = .{ .path = "main.zig" },
+        .root_source_file = b.path("main.zig"),
         .target = target,
         .optimize = optimize,
     });
