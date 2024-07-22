a69d403cb2c82ce6257bfa1ee7eba52f895c14e7
YANG Xudong <yangxudong@ymatrix.cn>
2024-07-22 10:56:14 +0800

    std: fix long double size for loongarch.

--- lib/std/Target.zig
+++ lib/std/Target.zig
@@ -2089,6 +2089,8 @@ pub fn c_type_bit_size(target: Target, c_type: CType) u16 {
                     .sparcel,
                     .wasm32,
                     .wasm64,
+                    .loongarch32,
+                    .loongarch64,
                     => return 128,
 
                     else => return 64,
@@ -2194,6 +2196,8 @@ pub fn c_type_bit_size(target: Target, c_type: CType) u16 {
                     .sparcel,
                     .wasm32,
                     .wasm64,
+                    .loongarch32,
+                    .loongarch64,
                     => return 128,
 
                     else => return 64,
