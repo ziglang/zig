761337d8a84e184ff15829204fa36ce950eab37e
Nameless <truemedian@gmail.com>
2024-08-02 15:59:13 -0500

    os: fix missing and incorrect msghdr definitions
    Macos uses the BSD definition of msghdr
    
    All linux architectures share a single msghdr definition. Many
    architectures had manually inserted padding fields that were endian
    specific and some had fields with different integers. This unifies all
    architectures to use a single correct msghdr definition.

--- lib/std/c.zig
+++ lib/std/c.zig
@@ -3446,7 +3446,7 @@ pub const itimerspec = switch (native_os) {
 };
 pub const msghdr = switch (native_os) {
     .linux => linux.msghdr,
-    .openbsd, .emscripten, .dragonfly, .freebsd, .netbsd, .haiku, .solaris, .illumos => extern struct {
+    .openbsd, .emscripten, .dragonfly, .freebsd, .netbsd, .haiku, .solaris, .illumos, .macos => extern struct {
         /// optional address
         name: ?*sockaddr,
         /// size of address
@@ -3466,7 +3466,7 @@ pub const msghdr = switch (native_os) {
 };
 pub const msghdr_const = switch (native_os) {
     .linux => linux.msghdr_const,
-    .openbsd, .emscripten, .dragonfly, .freebsd, .netbsd, .haiku, .solaris, .illumos => extern struct {
+    .openbsd, .emscripten, .dragonfly, .freebsd, .netbsd, .haiku, .solaris, .illumos, .macos => extern struct {
         /// optional address
         name: ?*const sockaddr,
         /// size of address
