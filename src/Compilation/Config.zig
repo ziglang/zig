944644bdc3a5d3c786a2f96e9dda35c2e82d32bd
YANG Xudong <yangxudong@ymatrix.cn>
2024-08-09 21:55:50 +0800

    disable lto for loongarch

--- src/Compilation/Config.zig
+++ src/Compilation/Config.zig
@@ -302,6 +302,12 @@ pub fn resolve(options: Options) ResolveError!Config {
             break :b false;
         }
 
+        if (target.cpu.arch.isLoongArch()) {
+            // FIXME:
+            // loongarch LTO might have problem on LLVM 18.
+            break :b false;
+        }
+
         break :b switch (options.output_mode) {
             .Lib, .Obj => false,
             .Exe => switch (root_optimize_mode) {
