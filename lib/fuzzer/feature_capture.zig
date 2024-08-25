3465e099044a81c97a277fac592e64eac060b447
Prokop Randacek <prokop@rdck.dev>
2024-08-25 10:15:31 +0200

    simplify feature capture

--- lib/fuzzer/feature_capture.zig
+++ lib/fuzzer/feature_capture.zig
@@ -1,11 +1,10 @@
 const std = @import("std");
 
-var buffer: [*]u32 = undefined;
+var buffer: []u32 = undefined;
 var top: usize = 0;
-var last_valid_index: usize = 0;
 
 /// Primitive prevention of wasting space in the buffer on duplicite features.
-/// TODO: is the size right?
+/// TODO: is the size right? libfuzzer uses 1 << 21
 const TableSize = 16 * 4096;
 var epoch: u8 = 0;
 var table: [TableSize]u8 = [1]u8{0} ** TableSize;
@@ -16,25 +15,20 @@ var table: [TableSize]u8 = [1]u8{0} ** TableSize;
 pub fn newFeature(f: u32) void {
     const hash = std.hash.uint32(f);
     const entry: *u8 = &table[hash % table.len];
-    const new_top = @min(b: {
-        @setRuntimeSafety(false); // we will never overflow this
-        break :b top + 1;
-    }, last_valid_index);
 
-    if (entry.* != epoch) {
+    if (entry.* != epoch and top < buffer.len) {
         entry.* = epoch;
         buffer[top] = f;
-        top = new_top;
+        top += 1;
     }
 }
 
 /// Run before calling the instrumented function
 pub fn prepare(b: []u32) void {
-    buffer = b.ptr;
-    last_valid_index = b.len - 1;
+    buffer = b;
     top = 0;
     if (epoch == 255) {
-        // TODO mark unlikely
+        @setCold(true);
         @memset(&table, 0);
         epoch = 1;
     } else {
@@ -42,11 +36,11 @@ pub fn prepare(b: []u32) void {
     }
 }
 
-/// Get the features after the instrumented function completed
+/// Get the captured features
 pub fn values() []u32 {
     return buffer[0..top];
 }
 
 pub fn is_full() bool {
-    return top == last_valid_index;
+    return top == buffer.len;
 }
