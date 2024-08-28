fc4b98fea48ba418ecb6c173d9f5f46c615b281d
Hila Friedman <red.black.liquorice@gmail.com>
2024-08-28 10:30:32 +0300

    Improve implementation performance

--- lib/std/math/gcd.zig
+++ lib/std/math/gcd.zig
@@ -5,44 +5,39 @@ const std = @import("std");
 /// For example, the GCD of `8` and `12` is `4`, that is, `gcd(8, 12) == 4`.
 pub fn gcd(a: anytype, b: anytype) @TypeOf(a, b) {
     const N = switch (@TypeOf(a, b)) {
-        // convert comptime_int to some sized int so we can @ctz on it.
-        // type coercion takes care of the conversion back to comptime_int
-        // at function's return
+        // convert comptime_int to some sized int type for @ctz
         comptime_int => std.math.IntFittingRange(@min(a, b), @max(a, b)),
         else => |T| T,
     };
-    // integers are unsigned, at least one is nonzero
     comptime std.debug.assert(@typeInfo(N).Int.signedness == .unsigned);
+
+    // using an optimised form of Stein's algorithm:
+    // https://en.wikipedia.org/wiki/Binary_GCD_algorithm
     std.debug.assert(a != 0 or b != 0);
 
-    // using Stein's algorithm (https://en.wikipedia.org/wiki/Binary_GCD_algorithm)
     if (a == 0) return b;
     if (b == 0) return a;
 
     var x: N = a;
     var y: N = b;
 
-    const i = @ctz(x);
-    const j = @ctz(y);
-    // x, y are nonzero, @intCast(@ctz(self)) does not overflow
-    x >>= @intCast(i);
-    y >>= @intCast(j);
+    const xz = @ctz(x);
+    const yz = @ctz(y);
+    const shift = @min(xz, yz);
+    x >>= @intCast(xz);
+    y >>= @intCast(yz);
 
-    // invariants: x, y are odd
-    while (true) {
-        // ensure x ≥ y
-        if (y > x) std.mem.swap(N, &x, &y);
-
-        // odd - odd is even
-        x -= y;
-
-        // gcd(0, y) == y, remultiply by the common power of 2
-        if (x == 0) return y << @intCast(@min(i, j));
-
-        // x is nonzero, @intCast(@ctz(self)) does not overflow
-        // x is even, its value decreases
-        x >>= @intCast(@ctz(x));
+    var diff = y -% x;
+    while (diff != 0) : (diff = y -% x) {
+        // ctz is invariant under negation, we
+        // put it here to ease data dependencies,
+        // makes the CPU happy.
+        const zeros = @ctz(diff);
+        if (x > y) diff = -%diff;
+        y = @min(x, y);
+        x = diff >> @intCast(zeros);
     }
+    return y << @intCast(shift);
 }
 
 test gcd {
@@ -58,8 +53,5 @@ test gcd {
     try expectEqual(gcd(300_000, 2_300_000), 100_000);
     try expectEqual(gcd(90000000_000_000_000_000_000, 2), 2);
     try expectEqual(gcd(@as(u80, 90000000_000_000_000_000_000), 2), 2);
-
-    // an important test case! @ctz's returned type is different from the
-    // type of >>'s rhs - an @intCast is required!
     try expectEqual(gcd(300_000, @as(u32, 2_300_000)), 100_000);
 }
