6cfff7931fe796461c9fd241894b950782512778
Prokop Randacek <prokop@rdck.dev>
2024-08-29 19:59:42 +0200

    dump corpus tool

--- lib/fuzzer/dump_corpus.zig
+++ lib/fuzzer/dump_corpus.zig
@@ -1,3 +1,44 @@
+// This is a standalone tool that can print the string stored inside a corpus
+// (buffer + meta file pair in the .zig-cache/v/ directory)
+
+const std = @import("std");
+
+const InputPool = @import("input_pool.zig").InputPool;
+
 pub fn main() void {
-    st
+    var args = std.process.args();
+    const bin = args.next();
+    const cache_dir_path = args.next();
+    const pc_digest_str = args.next();
+
+    if (cache_dir_path == null or pc_digest_str == null or args.next() != null) {
+        std.process.fatal("usage: {s} CACHE_DIR PC_DIGEST\n", .{bin.?});
+    }
+
+    // std.fmt.hex actually produces the hex number in the opposite order than
+    // parseInt reads...
+    const pc_digest = @byteSwap(std.fmt.parseInt(u64, pc_digest_str.?, 16) catch |e|
+        std.process.fatal("invalid pc digest: {}", .{e}));
+
+    const cache_dir = std.fs.cwd().makeOpenPath(cache_dir_path.?, .{}) catch |e|
+        std.process.fatal("invalid cache dir: {}", .{e});
+
+    std.log.info("cache_dir: {s}", .{cache_dir_path.?});
+    std.log.info("pc_digest: {x}", .{@byteSwap(pc_digest)});
+
+    var input_pool = InputPool.init(cache_dir, pc_digest);
+
+    const len = input_pool.len();
+
+    std.log.info("There are {} strings in the corpus:", .{len});
+
+    for (0..len) |i| {
+        const str = input_pool.getString(@intCast(i));
+
+        // the optimizer has nothing smart to do here to mess up what the
+        // volatile was trying to achieve in the first place
+        const str2: []const u8 = @volatileCast(str);
+
+        std.log.info("\"{s}\"", .{str2});
+    }
 }
