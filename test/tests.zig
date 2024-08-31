8c0902b7aec230b8149cc560ebcc3dc0bd7e2328
Alex Rønne Petersen <alex@alexrp.com>
2024-08-31 22:39:22 +0200

    Revert "tests: skip native CPU std tests on Windows"
    This reverts commit 234693bcbba6f55ff6e975ddbedf0fad4dfaa8f1.

--- test/tests.zig
+++ test/tests.zig
@@ -1198,14 +1198,6 @@ pub fn addModuleTests(b: *std.Build, options: ModuleTestOptions) *Step {
             test_target.use_llvm == false and mem.eql(u8, options.name, "c-import"))
             continue;
 
-        if (target.cpu.arch == .x86_64 and target.os.tag == .windows and
-            test_target.target.cpu_arch == null and test_target.optimize_mode != .Debug and
-            mem.eql(u8, options.name, "std"))
-        {
-            // https://github.com/ziglang/zig/issues/17902
-            continue;
-        }
-
         const want_this_mode = for (options.optimize_modes) |m| {
             if (m == test_target.optimize_mode) break true;
         } else false;
