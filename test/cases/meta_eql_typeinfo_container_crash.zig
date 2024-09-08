89fa3d5668e962580b6dbc56a77278e3b106de8c
RetroDev256 <ericpetersen3.14@gmail.com>
2024-09-08 00:33:59 -0600

    implement feedback

--- test/cases/meta_eql_typeinfo_container_crash.zig
+++ test/cases/meta_eql_typeinfo_container_crash.zig
@@ -1,10 +1,29 @@
-// from #7789 - crashed stage1 at ../src/stage1/analyze.cpp:7168
+comptime {
+    if (!eql(@typeInfo(opaque {}), @typeInfo(opaque {}))) unreachable;
+}
 
-const std = @import("std");
-
-test "basic add functionality" {
-    // fails
-    try std.testing.expect(comptime std.meta.eql(@typeInfo(opaque {}), @typeInfo(opaque {})));
+pub fn eql(a: anytype, b: @TypeOf(a)) bool {
+    switch (@typeInfo(@TypeOf(a))) {
+        .@"struct" => |info| {
+            inline for (info.fields) |field_info| {
+                if (!eql(@field(a, field_info.name), @field(b, field_info.name))) return false;
+            }
+            return true;
+        },
+        .@"union" => |info| {
+            if (info.tag_type) |UnionTag| {
+                inline for (info.fields) |field_info| {
+                    if (@field(UnionTag, field_info.name) == @as(UnionTag, a)) {
+                        return eql(@field(a, field_info.name), @field(b, field_info.name));
+                    }
+                }
+                return false;
+            }
+            unreachable;
+        },
+        .pointer => return a.ptr == b.ptr and a.len == b.len,
+        else => unreachable,
+    }
 }
 
 // compile
