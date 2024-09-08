d981f2d16fd77b42c4d4a78bf15d8277fca6033e
RetroDev256 <ericpetersen3.14@gmail.com>
2024-09-08 00:33:59 -0600

    implement feedback

--- test/cases/compile_errors/sentinel_hiding_const_error.zig
+++ test/cases/compile_errors/sentinel_hiding_const_error.zig
@@ -1,13 +1,11 @@
-// from #7816 - in stage1 the wrong error would be displayed
-
 export fn b() void {
     const ptr: [*:0]u8 = "foo";
-    _ = &ptr;
+    _ = ptr;
 }
 
 // error
 // target=native
 // backend=stage2
 //
-// :4:26: error: expected type '[*:0]u8', found '*const [3:0]u8'
-// :4:26: note: cast discards const qualifier
+// :2:26: error: expected type '[*:0]u8', found '*const [3:0]u8'
+// :2:26: note: cast discards const qualifier
--- test/cases/compile_errors/unneeded_declaration_reference_from_invalid_import.zig
+++ test/cases/compile_errors/unneeded_declaration_reference_from_invalid_import.zig
@@ -1,12 +1,8 @@
-// from #8146 - the error used to reference 'd', of all things
-
 const Compilation = @import("This file must not exist.zig");
-
 d: Compilation.Directory,
-
-test "thing" {}
 
 // error
 // target=native
+// backend=stage2
 //
 // This file must not exist.zig': FileNotFound
