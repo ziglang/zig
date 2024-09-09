d6ee754e9a6132d63555e5e03bf588a45c27793d
RetroDev256 <ericpetersen3.14@gmail.com>
2024-09-08 18:58:02 -0600

    procure test for #9021

---
+++ test/cases/compile_errors/empty_inferred_errorset_assigned.zig
@@ -0,0 +1,24 @@
+pub const A = struct {
+    a: i32,
+    pub fn init() !A {
+        return A{ .a = 5 };
+    }
+};
+
+pub const B = struct {
+    a: A = A.init(),
+};
+
+pub fn main() void {
+    const b = B{};
+    _ = b;
+}
+
+// error
+// target=native
+// backend=stage2
+//
+// :9:18: error: expected type 'example.A', found 'error{}!example.A'
+// :9:18: note: cannot convert error union to payload type
+// :9:18: note: consider using 'try', 'catch', or 'if'
+// :1:15: note: struct declared here
