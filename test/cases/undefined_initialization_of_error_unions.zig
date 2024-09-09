638cc9fc19be0a923013a16c428d0a3ac3a8c968
RetroDev256 <ericpetersen3.14@gmail.com>
2024-09-08 22:20:39 -0600

    one last mistake of mine

--- test/cases/undefined_initialization_of_error_unions.zig
+++ test/cases/undefined_initialization_of_error_unions.zig
@@ -1,11 +1,4 @@
-const builtin = @import("builtin");
-
 pub fn main() !void {
-    if (builtin.mode == .Debug) {
-        var foo: ?u32 = undefined;
-        if (foo == null) return error.Miscompilation;
-        foo = undefined;
-    }
     var a: anyerror!u32 = undefined;
     _ = a catch return error.Miscompilation;
     a = undefined;
