98881fc873d023248dd1fefbd6c516691ccddecd
RetroDev256 <ericpetersen3.14@gmail.com>
2024-09-08 19:05:42 -0600

    one oops, also procure test for #9085

--- test/cases/compile_errors/empty_inferred_errorset_assigned.zig
+++ test/cases/compile_errors/empty_inferred_errorset_assigned.zig
@@ -18,7 +18,7 @@ pub fn main() void {
 // target=native
 // backend=stage2
 //
-// :9:18: error: expected type 'example.A', found 'error{}!example.A'
+// :9:18: error: expected type 'tmp.A', found 'error{}!tmp.A'
 // :9:18: note: cannot convert error union to payload type
 // :9:18: note: consider using 'try', 'catch', or 'if'
 // :1:15: note: struct declared here
