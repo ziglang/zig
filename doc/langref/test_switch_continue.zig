b069396ada495188eb8b68ccaf7ff43aa8b91309
Literally Void <literallyvoid@gmail.com>
2024-09-10 22:09:16 -0700

    Fix inaccurate comment
    After testing, `break` does not target an unlabeled `switch`. However,
    it *can* target a labeled switch, even if the `break` is unlabeled. This
    behavior seems surprising and worth a callout, although this may not be
    the place for it.

--- doc/langref/test_switch_continue.zig
+++ doc/langref/test_switch_continue.zig
@@ -3,14 +3,14 @@ const expectEqual = std.testing.expectEqual;
 
 test "switch continue" {
     const value: i32 = 54;
+
     const result = sw: switch (value) {
         10...60 => |v| continue :sw v - 10,
         4 => continue :sw 3,
         3 => continue :sw 2,
         2 => continue :sw 1,
 
-        // A switch statement can be targeted by a break, even if the switch and
-        // the break are unlabeled.
+        // A labeled switch statement can be targeted by an unlabeled break.
         1 => break -6,
 
         else => unreachable,
