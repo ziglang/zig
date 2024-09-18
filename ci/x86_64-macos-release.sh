38af1b7ed1cd6d4a71bd2f22e09b90cf186571d5
Andrew Kelley <andrew@ziglang.org>
2024-09-18 15:07:02 -0700

    CI: update tarballs to 0.14.0-dev.1622+2ac543388

--- ci/x86_64-macos-release.sh
+++ ci/x86_64-macos-release.sh
@@ -6,7 +6,7 @@ set -e
 ZIGDIR="$PWD"
 TARGET="$ARCH-macos-none"
 MCPU="baseline"
-CACHE_BASENAME="zig+llvm+lld+clang-$TARGET-0.14.0-dev.1504+3750468a4"
+CACHE_BASENAME="zig+llvm+lld+clang-$TARGET-0.14.0-dev.1622+2ac543388"
 PREFIX="$HOME/$CACHE_BASENAME"
 JOBS="-j3"
 ZIG="$PREFIX/bin/zig"
