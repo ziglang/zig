2857ca1edc1c3fa83298d2e8f6d505a3776f1ae3
Andrew Kelley <andrew@ziglang.org>
2024-09-28 12:16:41 -0700

    revert safety test cases to rely on deprecated old API
    in case someone wants to pursue the idea of making the panic interface a
    struct, this will reduce churn.

--- test/cases/safety/noreturn returned.zig
+++ test/cases/safety/noreturn returned.zig
@@ -1,16 +1,6 @@
 const std = @import("std");
 
-pub const Panic = struct {
-    pub const call = panic;
-    pub const unwrapError = std.debug.FormattedPanic.unwrapError;
-    pub const outOfBounds = std.debug.FormattedPanic.outOfBounds;
-    pub const startGreaterThanEnd = std.debug.FormattedPanic.startGreaterThanEnd;
-    pub const sentinelMismatch = std.debug.FormattedPanic.sentinelMismatch;
-    pub const inactiveUnionField = std.debug.FormattedPanic.inactiveUnionField;
-    pub const messages = std.debug.FormattedPanic.messages;
-};
-
-fn panic(message: []const u8, stack_trace: ?*std.builtin.StackTrace, _: ?usize) noreturn {
+pub fn panic(message: []const u8, stack_trace: ?*std.builtin.StackTrace, _: ?usize) noreturn {
     _ = stack_trace;
     if (std.mem.eql(u8, message, "'noreturn' function returned")) {
         std.process.exit(0);
