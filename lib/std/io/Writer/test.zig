d2d0b8f6916412aa5d2306576eb41e6afd147f6e
kj4tmp@gmail.com <kj4tmp@gmail.com>
2024-10-05 17:20:31 -0700

    add tests for endianness-affected types

--- lib/std/io/Writer/test.zig
+++ lib/std/io/Writer/test.zig
@@ -33,3 +33,31 @@ test "writeStructEndian writes packed structs without padding and in correct fie
     try writer.writeStructEndian(PackedStruct{ .a = 11, .b = 12, .c = 13 }, .big);
     try testing.expectEqualSlices(u8, &.{ 13, 12, 11 }, &buf);
 }
+
+test "writeStruct a packed struct with endianness-affected types" {
+    var buf: [4]u8 = undefined;
+    var fis = std.io.fixedBufferStream(&buf);
+    const writer = fis.writer();
+
+    const PackedStruct = packed struct(u32) { a: u16, b: u16 };
+
+    try writer.writeStruct(PackedStruct{ .a = 0x1234, .b = 0x5678 });
+    switch (native_endian) {
+        .little => try testing.expectEqualSlices(u8, &.{ 0x34, 0x12, 0x78, 0x56 }, &buf),
+        .big => try testing.expectEqualSlices(u8, &.{ 0x56, 0x78, 0x12, 0x34 }, &buf),
+    }
+}
+
+test "writeStructEndian a packed struct with endianness-affected types" {
+    var buf: [4]u8 = undefined;
+    var fis = std.io.fixedBufferStream(&buf);
+    const writer = fis.writer();
+
+    const PackedStruct = packed struct(u32) { a: u16, b: u16 };
+
+    try writer.writeStructEndian(PackedStruct{ .a = 0x1234, .b = 0x5678 }, .little);
+    try testing.expectEqualSlices(u8, &.{ 0x34, 0x12, 0x78, 0x56 }, &buf);
+    fis.reset();
+    try writer.writeStructEndian(PackedStruct{ .a = 0x1234, .b = 0x5678 }, .big);
+    try testing.expectEqualSlices(u8, &.{ 0x56, 0x78, 0x12, 0x34 }, &buf);
+}
