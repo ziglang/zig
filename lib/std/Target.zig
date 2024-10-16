73b4cb6314690b179800a3959a5e94ae469fcc0b
Alex Rønne Petersen <alex@alexrp.com>
2024-10-16 05:59:13 +0200

    std.Target: Use ck810 as the baseline CPU model for csky.

--- lib/std/Target.zig
+++ lib/std/Target.zig
@@ -1856,6 +1856,7 @@ pub const Cpu = struct {
                     .watchos => &aarch64.cpu.apple_s4,
                     else => generic(arch),
                 },
+                .csky => &csky.cpu.ck810, // gcc/clang do not have a generic csky model.
                 .hexagon => &hexagon.cpu.hexagonv60, // gcc/clang do not have a generic hexagon model.
                 .riscv32 => &riscv.cpu.baseline_rv32,
                 .riscv64 => &riscv.cpu.baseline_rv64,
