e9ae9a5fc45a6ee85d988372a4d150032f52ef4c
Alex Rønne Petersen <alex@alexrp.com>
2024-10-31 09:00:46 +0100

    llvm: Don't set nonnull attribute on allowzero slices.

--- src/codegen/llvm.zig
+++ src/codegen/llvm.zig
@@ -1341,7 +1341,7 @@ pub const Object = struct {
                                 try attributes.addParamAttr(llvm_arg_i, .@"noalias", &o.builder);
                             }
                         }
-                        if (param_ty.zigTypeTag(zcu) != .optional) {
+                        if (param_ty.zigTypeTag(zcu) != .optional and !ptr_info.flags.is_allowzero) {
                             try attributes.addParamAttr(llvm_arg_i, .nonnull, &o.builder);
                         }
                         if (ptr_info.flags.is_const) {
@@ -5410,7 +5410,7 @@ pub const FuncGen = struct {
                             try attributes.addParamAttr(llvm_arg_i, .@"noalias", &o.builder);
                         }
                     }
-                    if (param_ty.zigTypeTag(zcu) != .optional) {
+                    if (param_ty.zigTypeTag(zcu) != .optional and !ptr_info.flags.is_allowzero) {
                         try attributes.addParamAttr(llvm_arg_i, .nonnull, &o.builder);
                     }
                     if (ptr_info.flags.is_const) {
