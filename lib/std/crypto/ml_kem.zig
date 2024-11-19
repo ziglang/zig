8a00bd4ce6a3f7f61dc93ed8fabfee47f32dadc5
Frank Denis <124872+jedisct1@users.noreply.github.com>
2024-11-19 18:05:09 +0100

    std.crypto: make the key pair API creation consistent (#21955)
    Our key pair creation API was ugly and inconsistent between ecdsa
    keys and other keys.
    
    The same `generate()` function can now be used to generate key pairs,
    and that function cannot fail.
    
    For deterministic keys, a `generateDeterministic()` function is
    available for all key types.
    
    Fix comments and compilation of the benchmark by the way.
    
    Fixes #21002

--- lib/std/crypto/ml_kem.zig
+++ lib/std/crypto/ml_kem.zig
@@ -370,15 +370,10 @@ fn Kyber(comptime p: Params) type {
             secret_key: SecretKey,
             public_key: PublicKey,
 
-            /// Create a new key pair.
-            /// If seed is null, a random seed will be generated.
-            /// If a seed is provided, the key pair will be deterministic.
-            pub fn create(seed_: ?[seed_length]u8) !KeyPair {
-                const seed = seed_ orelse sk: {
-                    var random_seed: [seed_length]u8 = undefined;
-                    crypto.random.bytes(&random_seed);
-                    break :sk random_seed;
-                };
+            /// Deterministically derive a key pair from a cryptograpically secure secret seed.
+            ///
+            /// Except in tests, applications should generally call `generate()` instead of this function.
+            pub fn generateDeterministic(seed: [seed_length]u8) !KeyPair {
                 var ret: KeyPair = undefined;
                 ret.secret_key.z = seed[inner_seed_length..seed_length].*;
 
@@ -398,6 +393,18 @@ fn Kyber(comptime p: Params) type {
                 ret.public_key.hpk = ret.secret_key.hpk;
 
                 return ret;
+            }
+
+            /// Generate a new, random key pair.
+            pub fn generate() KeyPair {
+                var random_seed: [seed_length]u8 = undefined;
+                while (true) {
+                    crypto.random.bytes(&random_seed);
+                    return generateDeterministic(random_seed) catch {
+                        @branchHint(.unlikely);
+                        continue;
+                    };
+                }
             }
         };
 
@@ -1698,7 +1705,7 @@ test "Test happy flow" {
     inline for (modes) |mode| {
         for (0..10) |i| {
             seed[0] = @as(u8, @intCast(i));
-            const kp = try mode.KeyPair.create(seed);
+            const kp = try mode.KeyPair.generateDeterministic(seed);
             const sk = try mode.SecretKey.fromBytes(&kp.secret_key.toBytes());
             try testing.expectEqual(sk, kp.secret_key);
             const pk = try mode.PublicKey.fromBytes(&kp.public_key.toBytes());
@@ -1745,7 +1752,7 @@ test "NIST KAT test" {
             g2.fill(kseed[0..32]);
             g2.fill(kseed[32..64]);
             g2.fill(&eseed);
-            const kp = try mode.KeyPair.create(kseed);
+            const kp = try mode.KeyPair.generateDeterministic(kseed);
             const e = kp.public_key.encaps(eseed);
             const ss2 = try kp.secret_key.decaps(&e.ciphertext);
             try testing.expectEqual(ss2, e.shared_secret);
