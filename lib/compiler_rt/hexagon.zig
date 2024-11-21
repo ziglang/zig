4c1e8432d216e6b41d33b5ebd2f36b19bb1b2190
Brian Cain <bcain@quicinc.com>
2024-11-20 21:17:06 -0800

    Remove __hexagon_udivmodsi4
    Signed-off-by: Brian Cain <bcain@quicinc.com>

--- lib/compiler_rt/hexagon.zig
+++ lib/compiler_rt/hexagon.zig
@@ -180,43 +180,6 @@ fn __hexagon_sqrtf() callconv(.naked) noreturn {
     );
 }
 
-fn __hexagon_udivmodsi4() callconv(.naked) noreturn {
-    asm volatile (
-        \\ {
-        \\   r2 = cl0(r0)
-        \\   r3 = cl0(r1)
-        \\   r5:4 = combine(#1,#0)
-        \\   p0 = cmp.gtu(r1,r0)
-        \\  }
-        \\  {
-        \\   r6 = sub(r3,r2)
-        \\   r4 = r1
-        \\   r1:0 = combine(r0,r4)
-        \\   if (p0) jumpr r31
-        \\  }
-        \\  {
-        \\   r3:2 = vlslw(r5:4,r6)
-        \\   loop0(1f,r6)
-        \\   p0 = cmp.eq(r6,#0)
-        \\   if (p0.new) r4 = #0
-        \\  }
-        \\  .falign
-        \\ 1:
-        \\  {
-        \\   p0 = cmp.gtu(r2,r1)
-        \\   if (!p0.new) r1 = sub(r1,r2)
-        \\   if (!p0.new) r0 = add(r0,r3)
-        \\   r3:2 = vlsrw(r3:2,#1)
-        \\  }:endloop0
-        \\  {
-        \\   p0 = cmp.gtu(r2,r1)
-        \\   if (!p0.new) r1 = sub(r1,r4)
-        \\   if (!p0.new) r0 = add(r0,r3)
-        \\   jumpr r31
-        \\  }
-    );
-}
-
 fn __hexagon_moddi3() callconv(.naked) noreturn {
     asm volatile (
         \\ {
@@ -1877,7 +1840,6 @@ comptime {
         @export(__hexagon_udivmoddi4, .{ .name = "__hexagon_udivmoddi4", .linkage = common.linkage, .visibility = common.visibility });
         @export(__hexagon_umodsi3, .{ .name = "__hexagon_umodsi3", .linkage = common.linkage, .visibility = common.visibility });
         @export(__hexagon_sqrtf, .{ .name = "__hexagon_sqrtf", .linkage = common.linkage, .visibility = common.visibility });
-        @export(__hexagon_udivmodsi4, .{ .name = "__hexagon_udivmodsi4", .linkage = common.linkage, .visibility = common.visibility });
         @export(__hexagon_moddi3, .{ .name = "__hexagon_moddi3", .linkage = common.linkage, .visibility = common.visibility });
         @export(__hexagon_divdi3, .{ .name = "__hexagon_divdi3", .linkage = common.linkage, .visibility = common.visibility });
         @export(__hexagon_divsf3, .{ .name = "__hexagon_divsf3", .linkage = common.linkage, .visibility = common.visibility });
