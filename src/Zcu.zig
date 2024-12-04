d2119bb5b25873ce0bf9723bbd0b12b7a878fbf2
dweiller <4678790+dweiller@users.noreply.github.com>
2024-12-04 18:48:18 +1100

    compiler: add @loopHint builtin

--- src/Zcu.zig
+++ src/Zcu.zig
@@ -252,6 +252,7 @@ pub const BuiltinDecl = enum {
     ExportOptions,
     ExternOptions,
     BranchHint,
+    LoopHint,
 
     Type,
     @"Type.Fn",
@@ -326,6 +327,7 @@ pub const BuiltinDecl = enum {
             .ExportOptions,
             .ExternOptions,
             .BranchHint,
+            .LoopHint,
             => .type,
 
             .Type,
