c5a4dde6f49998241cd218865314042ea2997b98
geemili <opensource@geemili.xyz>
2024-12-03 22:59:48 -0700

    check that debug formats work in multiple optimize modes

--- test/debug_format_stack_traces.zig
+++ test/debug_format_stack_traces.zig
@@ -8,34 +8,46 @@ pub fn addCases(cases: *tests.DebugFormatStackTraceContext) void {
         .source =
         \\ const std = @import("std");
         \\
-        \\ noinline fn foo() void {
-        \\     std.debug.dumpCurrentStackTrace(@returnAddress());
+        \\ noinline fn foo(x: u32) u32 {
+        \\     return x * x;
         \\ }
         \\
-        \\ noinline fn bar() void {
-        \\     return foo();
+        \\ noinline fn bar() u32 {
+        \\     return foo(std.math.maxInt(u32));
         \\ }
         \\
         \\ pub fn main() !void {
-        \\     std.mem.doNotOptimizeAway(bar());
+        \\     std.debug.print("{}", .{bar()});
         \\ }
         ,
         .symbols = .{
             .exclude_os = &.{ .macos, .windows },
+            // release modes won't check for overflow, so no error occurs
+            .exclude_optimize_mode = &.{ .ReleaseFast, .ReleaseSmall },
+            .expect_panic = true,
             .expect =
+            \\thread [thread_id] panic: integer overflow
+            \\???:?:?: [address] in source.foo (???)
             \\???:?:?: [address] in source.bar (???)
             \\???:?:?: [address] in source.main (???)
             \\
             ,
         },
         .dwarf32 = .{
+            // release modes won't check for overflow, so no error occurs
+            .exclude_optimize_mode = &.{ .ReleaseFast, .ReleaseSmall },
+            .expect_panic = true,
             .expect =
+            \\thread [thread_id] panic: integer overflow
+            \\source.zig:4:15: [address] in foo (test)
+            \\     return x * x;
+            \\              ^
             \\source.zig:8:16: [address] in bar (test)
-            \\     return foo();
+            \\     return foo(std.math.maxInt(u32));
             \\               ^
-            \\source.zig:12:35: [address] in main (test)
-            \\     std.mem.doNotOptimizeAway(bar());
-            \\                                  ^
+            \\source.zig:12:33: [address] in main (test)
+            \\     std.debug.print("{}", .{bar()});
+            \\                                ^
             \\
             ,
         },
