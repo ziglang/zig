a879c3ea0b6cf46c879b0c92925fd6024c2d4ca4
Alex Rønne Petersen <alex@alexrp.com>
2024-12-06 14:41:34 +0100

    compiler: Classify libgcc as an alias for compiler-rt.
    This is GCC's equivalent to compiler-rt. The two libraries have a huge overlap
    in exported symbols, so we may as well satisfy it this way to increase
    compatibility with build systems in the wild.

--- src/target.zig
+++ src/target.zig
@@ -268,6 +268,7 @@ pub fn classifyCompilerRtLibName(name: []const u8) CompilerRtClassification {
         return .both;
     }
     if (std.mem.eql(u8, name, "compiler_rt") or
+        std.mem.eql(u8, name, "gcc") or
         std.mem.eql(u8, name, "atomic"))
     {
         return .only_compiler_rt;
