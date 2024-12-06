4abc3401d28ad22d2ff94f723b683c1bd8855231
Tw <tw19881113@gmail.com>
2024-12-06 11:42:09 +0800

    Sema: make source location in checkCallConvSupportsVarArgs more meaningful
    As calling convention may not be specified explicitly in the source,
    so use va_arg's location instead.
    
    Signed-off-by: Tw <tw19881113@gmail.com>

--- test/cases/compile_errors/invalid_variadic_function.zig
+++ test/cases/compile_errors/invalid_variadic_function.zig
@@ -11,7 +11,7 @@ comptime {
 // error
 // target=x86_64-linux
 //
-// :1:1: error: variadic function does not support 'auto' calling convention
-// :1:1: note: supported calling conventions: 'x86_64_sysv', 'x86_64_win'
-// :1:1: error: variadic function does not support 'inline' calling convention
-// :1:1: note: supported calling conventions: 'x86_64_sysv', 'x86_64_win'
+// :1:8: error: variadic function does not support 'auto' calling convention
+// :1:8: note: supported calling conventions: 'x86_64_sysv', 'x86_64_win'
+// :2:16: error: variadic function does not support 'inline' calling convention
+// :2:16: note: supported calling conventions: 'x86_64_sysv', 'x86_64_win'
