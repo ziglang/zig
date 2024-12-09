d8be3fad099af2dd7e1dee5bf77ff3099df71689
seanthegleaming <srob08100@gmail.com>
2024-12-09 18:45:43 -0500

    Disabled problemativ usage of the mmap hint in windows.

--- lib/std/heap/PageAllocator.zig
+++ lib/std/heap/PageAllocator.zig
@@ -8,6 +8,7 @@ const native_os = builtin.os.tag;
 const windows = std.os.windows;
 const posix = std.posix;
 
+/// TODO: utilize this on windows
 pub var next_mmap_addr_hint = std.atomic.Value(?[*]align(mem.page_size) u8).init(null);
 
 pub const vtable = Allocator.VTable{
@@ -19,85 +20,19 @@ pub const vtable = Allocator.VTable{
 /// Whether `posix.mremap` may be used
 const use_mremap = @hasDecl(posix.system, "REMAP") and posix.system.REMAP != void;
 
-/// Whether an invalid `next_mmap_addr_hint` will cause `mapGet` to fail
-const invalid_hint_fails = switch (native_os) {
-    .windows => true,
-    else => false,
-};
-
-/// Allocated pages of memory. The size of the allocation is rounded up to the page.
-/// `hint` is a hint for where the allocation should be mapped to.
-/// If `invalid_hint_fails`, then this function fails when an allocation cannot be made exactly at `hint`.
-/// Otherwise, this function may return an address other than `hint`.
-fn mapGet(bytes: usize, hint: ?[*]align(mem.page_size) u8) ![*]align(mem.page_size) u8 {
-    return switch (native_os) {
-        .windows => @ptrCast(@alignCast(try windows.VirtualAlloc(
-            @ptrCast(hint),
-            bytes,
-            windows.MEM_COMMIT | windows.MEM_RESERVE,
-            windows.PAGE_READWRITE,
-        ))),
-        else => (try posix.mmap(
-            hint,
-            bytes,
-            posix.PROT.READ | posix.PROT.WRITE,
-            .{ .TYPE = .PRIVATE, .ANONYMOUS = true },
-            -1,
-            0,
-        )).ptr,
-    };
+fn mmapAlloc(bytes: usize, hint: ?[*]align(mem.page_size) u8) ![]align(mem.page_size) u8 {
+    return posix.mmap(
+        hint,
+        bytes,
+        posix.PROT.READ | posix.PROT.WRITE,
+        .{ .TYPE = .PRIVATE, .ANONYMOUS = true },
+        -1,
+        0,
+    );
 }
 
-/// Unmaps allocated memory. The size of the allocation is rounded up to the page.
-/// Using `free` frees the allocation entirely,
-/// while using `shrink` may simply decommit memory for later use.
-fn mapFree(memory: []align(mem.page_size) u8, kind: enum { free, shrink }) void {
-    switch (native_os) {
-        .windows => windows.VirtualFree(
-            @ptrCast(memory.ptr),
-            memory.len,
-            switch (kind) {
-                .free => windows.MEM_RELEASE,
-                .shrink => windows.MEM_DECOMMIT,
-            },
-        ),
-        else => posix.munmap(memory),
-    }
-}
-
-/// Attempts to shrink memory acquired for `mapGet` to `new_size` bytes.
-/// Returns true of success, and false on failure.
-fn mapShrink(memory: []align(mem.page_size) u8, new_size: usize) bool {
-    const new_size_aligned = mem.alignForward(usize, new_size, mem.page_size);
-    const old_size_aligned = mem.alignForward(usize, memory.len, mem.page_size);
-
-    assert(new_size_aligned < old_size_aligned);
-
-    mapFree(@alignCast(memory.ptr[new_size_aligned..old_size_aligned]), .shrink);
-    return true;
-}
-
-/// Attempts to grow memory acquired for `mapGet` to `new_size` bytes.
-/// Returns true of success, and false on failure.
-fn mapGrow(memory: []align(mem.page_size) u8, new_size: usize) bool {
-    assert(new_size > memory.len);
-
-    const new_size_aligned = mem.alignForward(usize, new_size, mem.page_size);
-    const old_size_aligned = mem.alignForward(usize, memory.len, mem.page_size);
-
-    assert(new_size_aligned > old_size_aligned);
-
-    if (use_mremap) {
-        const slice = posix.mremap(
-            memory.ptr[0..old_size_aligned],
-            new_size_aligned,
-            false,
-        ) catch return false;
-        assert(slice.ptr == memory.ptr);
-        return true;
-    } else {
-        return false;
-    }
+fn mapUnget(memory: []align(mem.page_size) u8) void {
+    std.posix.munmap(memory);
 }
 
 fn alloc(_: *anyopaque, n: usize, log2_align: u8, ra: usize) ?[*]u8 {
@@ -106,20 +41,40 @@ fn alloc(_: *anyopaque, n: usize, log2_align: u8, ra: usize) ?[*]u8 {
     assert(n > 0);
     if (n > maxInt(usize) - (mem.page_size - 1)) return null;
 
+    if (native_os == .windows) {
+        const addr = windows.VirtualAlloc(
+            null,
+
+            // VirtualAlloc will round the length to a multiple of page size.
+            // VirtualAlloc docs: If the lpAddress parameter is NULL, this value is rounded up to the next page boundary
+            n,
+
+            windows.MEM_COMMIT | windows.MEM_RESERVE,
+            windows.PAGE_READWRITE,
+        ) catch return null;
+        return @ptrCast(addr);
+    }
+
     const aligned_len = mem.alignForward(usize, n, mem.page_size);
     const hint = next_mmap_addr_hint.load(.unordered);
-    const head = mapGet(aligned_len, hint) catch blk: {
-        if (invalid_hint_fails and next_mmap_addr_hint.rmw(.Xchg, null, .monotonic) != null) {
-            // for systems where an invalid hint causes allocation failure,
-            // if we encounter an error, first attempt to retry without a hint
-            break :blk mapGet(aligned_len, null) catch return null;
-        } else {
-            return null;
-        }
+
+    const slice = posix.mmap(
+        hint,
+        aligned_len,
+        posix.PROT.READ | posix.PROT.WRITE,
+        .{ .TYPE = .PRIVATE, .ANONYMOUS = true },
+        -1,
+        0,
+    ) catch {
+        _ = next_mmap_addr_hint.cmpxchgStrong(hint, null, .monotonic, .monotonic);
+        return null;
     };
-    assert(mem.isAligned(@intFromPtr(head), mem.page_size));
-    _ = next_mmap_addr_hint.cmpxchgStrong(hint, @alignCast(head + aligned_len), .monotonic, .monotonic);
-    return head;
+
+    assert(mem.isAligned(@intFromPtr(slice.ptr), mem.page_size));
+    const new_hint: [*]align(mem.page_size) u8 = @alignCast(slice.ptr + aligned_len);
+    _ = next_mmap_addr_hint.cmpxchgStrong(hint, new_hint, .monotonic, .monotonic);
+
+    return slice.ptr;
 }
 
 fn resize(
@@ -131,16 +86,51 @@ fn resize(
 ) bool {
     _ = log2_buf_align;
     _ = return_address;
+
     const new_size_aligned = mem.alignForward(usize, new_size, mem.page_size);
     const old_size_aligned = mem.alignForward(usize, buf_unaligned.len, mem.page_size);
+
+    if (native_os == .windows) {
+        if (new_size <= buf_unaligned.len) {
+            const base_addr = @intFromPtr(buf_unaligned.ptr);
+            const old_addr_end = base_addr + buf_unaligned.len;
+            const new_addr_end = mem.alignForward(usize, base_addr + new_size, mem.page_size);
+            if (old_addr_end > new_addr_end) {
+                // For shrinking that is not releasing, we will only
+                // decommit the pages not needed anymore.
+                windows.VirtualFree(
+                    @as(*anyopaque, @ptrFromInt(new_addr_end)),
+                    old_addr_end - new_addr_end,
+                    windows.MEM_DECOMMIT,
+                );
+            }
+            return true;
+        }
+        return new_size_aligned <= old_size_aligned;
+    }
+
     const buf: []align(mem.page_size) u8 = @alignCast(buf_unaligned.ptr[0..old_size_aligned]);
-    const ordering = std.math.order(old_size_aligned, new_size_aligned);
-    const result = switch (ordering) {
-        .lt => mapGrow(buf, new_size_aligned),
-        .eq => true,
-        .gt => mapShrink(buf, new_size_aligned),
+    const result = switch (std.math.order(old_size_aligned, new_size_aligned)) {
+        .lt => grow: {
+            if (use_mremap) {
+                const slice = posix.mremap(
+                    buf.ptr[0..old_size_aligned],
+                    new_size_aligned,
+                    false,
+                ) catch break :grow false;
+                assert(slice.ptr == buf.ptr);
+                break :grow true;
+            } else {
+                break :grow false;
+            }
+        },
+        .eq => return true, // return now and don't set the hint
+        .gt => shrink: {
+            posix.munmap(@alignCast(buf.ptr[new_size_aligned..old_size_aligned]));
+            break :shrink true;
+        },
     };
-    if (result and ordering != .eq) {
+    if (result) {
         const old_end: [*]align(mem.page_size) u8 = @alignCast(buf.ptr + old_size_aligned);
         const new_end: [*]align(mem.page_size) u8 = @alignCast(buf.ptr + new_size_aligned);
         _ = next_mmap_addr_hint.cmpxchgStrong(old_end, new_end, .monotonic, .monotonic);
@@ -152,9 +142,13 @@ fn free(_: *anyopaque, slice: []u8, log2_buf_align: u8, return_address: usize) v
     _ = log2_buf_align;
     _ = return_address;
 
-    const aligned_len = mem.alignForward(usize, slice.len, mem.page_size);
-    const head: []align(mem.page_size) u8 = @alignCast(slice.ptr[0..aligned_len]);
-    mapFree(head, .free);
-    const tail: [*]align(mem.page_size) u8 = @alignCast(head.ptr + head.len);
-    _ = next_mmap_addr_hint.cmpxchgStrong(tail, head.ptr, .monotonic, .monotonic);
+    if (native_os == .windows) {
+        windows.VirtualFree(slice.ptr, 0, windows.MEM_RELEASE);
+    } else {
+        const buf_aligned_len = mem.alignForward(usize, slice.len, mem.page_size);
+        const head: []align(mem.page_size) u8 = @alignCast(slice.ptr[0..buf_aligned_len]);
+        posix.munmap(head);
+        const tail: [*]align(mem.page_size) u8 = @alignCast(head.ptr + head.len);
+        _ = next_mmap_addr_hint.cmpxchgStrong(tail, head.ptr, .monotonic, .monotonic);
+    }
 }
