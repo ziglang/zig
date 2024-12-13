130310972d41eed50d7f033bb2a71c3eb2467439
Alex Rønne Petersen <alex@alexrp.com>
2024-12-13 07:58:13 +0100

    compiler: Print more information when failing to provide libc.

--- src/main.zig
+++ src/main.zig
@@ -3550,21 +3550,28 @@ fn buildOutputType(
 
             for (std.zig.target.available_libcs) |t| {
                 if (t.arch == target.cpu.arch and t.os == target.os.tag) {
-                    if (t.os_ver) |os_ver| {
-                        std.log.info("zig can provide libc for related target {s}-{s}.{d}-{s}", .{
-                            @tagName(t.arch), @tagName(t.os), os_ver.major, @tagName(t.abi),
-                        });
-                    } else if (t.glibc_min) |glibc_min| {
-                        std.log.info("zig can provide libc for related target {s}-{s}-{s}.{d}.{d}", .{
+                    // If there's a `glibc_min`, there's also an `os_ver`.
+                    if (t.glibc_min) |glibc_min| {
+                        std.log.info("zig can provide libc for related target {s}-{s}.{}-{s}.{d}.{d}", .{
                             @tagName(t.arch),
                             @tagName(t.os),
+                            t.os_ver.?,
                             @tagName(t.abi),
                             glibc_min.major,
                             glibc_min.minor,
                         });
+                    } else if (t.os_ver) |os_ver| {
+                        std.log.info("zig can provide libc for related target {s}-{s}.{}-{s}", .{
+                            @tagName(t.arch),
+                            @tagName(t.os),
+                            os_ver,
+                            @tagName(t.abi),
+                        });
                     } else {
                         std.log.info("zig can provide libc for related target {s}-{s}-{s}", .{
-                            @tagName(t.arch), @tagName(t.os), @tagName(t.abi),
+                            @tagName(t.arch),
+                            @tagName(t.os),
+                            @tagName(t.abi),
                         });
                     }
                 }
