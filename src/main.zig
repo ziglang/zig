08ce367422c768cfc4e0a50879021de23e402deb
Chan Lee <Leetimemp@gmail.com>
2025-01-03 22:03:43 +0800

    windows-gnu: Add support for `-mwindows` option.

--- src/main.zig
+++ src/main.zig
@@ -468,6 +468,7 @@ const usage_build_generic =
     \\  -fno-structured-cfg       (SPIR-V) force SPIR-V kernels to not use structured control flow
     \\  -mexec-model=[value]      (WASI) Execution model
     \\  -municode                 (Windows) Use wmain/wWinMain as entry point
+    \\  -mwindows                 (Windows) Specify that a GUI application is to be generated
     \\
     \\Per-Module Compile Options:
     \\  -target [name]            <arch><sub>-<os>-<abi> see the targets command
@@ -1773,6 +1774,8 @@ fn buildOutputType(
                         create_module.opts.wasi_exec_model = parseWasiExecModel(arg["-mexec-model=".len..]);
                     } else if (mem.eql(u8, arg, "-municode")) {
                         mingw_unicode_entry_point = true;
+                    } else if (mem.eql(u8, arg, "-mwindows")) {
+                        subsystem = .Windows;
                     } else {
                         fatal("unrecognized parameter: '{s}'", .{arg});
                     }
@@ -2327,6 +2330,7 @@ fn buildOutputType(
                     },
                     .force_load_objc => force_load_objc = true,
                     .mingw_unicode_entry_point => mingw_unicode_entry_point = true,
+                    .mingw_subsystem_windows => subsystem = .Windows,
                     .weak_library => try create_module.cli_link_inputs.append(arena, .{ .name_query = .{
                         .name = it.only_arg,
                         .query = .{
@@ -2658,6 +2662,8 @@ fn buildOutputType(
                             minor, @errorName(err),
                         });
                     };
+                } else if (mem.eql(u8, arg, "-mwindows")) {
+                    subsystem = .Windows;
                 } else if (mem.eql(u8, arg, "-framework")) {
                     try create_module.frameworks.put(arena, linker_args_it.nextOrFatal(), .{});
                 } else if (mem.eql(u8, arg, "-weak_framework")) {
@@ -5775,6 +5781,7 @@ pub const ClangArgIterator = struct {
         undefined,
         force_load_objc,
         mingw_unicode_entry_point,
+        mingw_subsystem_windows,
         san_cov_trace_pc_guard,
         san_cov,
         no_san_cov,
