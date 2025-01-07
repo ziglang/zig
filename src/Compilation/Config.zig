274a21b43a8b374eb9523d5f72ce93632d8d0024
Chan Lee <Leetimemp@gmail.com>
2025-01-07 13:05:11 +0800

    Debug: Move `Compilation.Config.DebugFormat` to `std.builtin` for broader usage

--- src/Compilation/Config.zig
+++ src/Compilation/Config.zig
@@ -56,7 +56,7 @@ import_memory: bool,
 export_memory: bool,
 shared_memory: bool,
 is_test: bool,
-debug_format: DebugFormat,
+debug_format: std.builtin.DebugFormat,
 root_optimize_mode: std.builtin.OptimizeMode,
 root_strip: bool,
 root_error_tracing: bool,
@@ -68,11 +68,6 @@ pub const CFrontend = enum { clang, aro };
 
 pub const LtoMode = enum { none, full, thin };
 
-pub const DebugFormat = union(enum) {
-    strip,
-    dwarf: std.dwarf.Format,
-    code_view,
-};
 
 pub const Options = struct {
     output_mode: std.builtin.OutputMode,
@@ -111,7 +106,7 @@ pub const Options = struct {
     import_memory: ?bool = null,
     export_memory: ?bool = null,
     shared_memory: ?bool = null,
-    debug_format: ?DebugFormat = null,
+    debug_format: ?std.builtin.DebugFormat = null,
     dll_export_fns: ?bool = null,
     rdynamic: ?bool = null,
     san_cov_trace_pc_guard: bool = false,
@@ -418,7 +413,7 @@ pub fn resolve(options: Options) ResolveError!Config {
         break :b false;
     };
 
-    const debug_format: DebugFormat = b: {
+    const debug_format: std.builtin.DebugFormat = b: {
         if (root_strip and !options.any_non_stripped) break :b .strip;
         if (options.debug_format) |x| break :b x;
         break :b switch (target.ofmt) {
