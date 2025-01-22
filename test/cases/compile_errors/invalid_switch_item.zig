8470b6ea37a82330be2c5ab41460fe58b8cfd4f6
mlugg <mlugg@mlugg.co.uk>
2025-01-22 02:22:39 +0000

    Zcu: fix switch prong source location resolution
    Resolves: #22343

---
+++ test/cases/compile_errors/invalid_switch_item.zig
@@ -0,0 +1,46 @@
+const E = enum { a, b, c };
+var my_e: E = .a;
+
+export fn f0() void {
+    switch (my_e) {
+        .a => {},
+        .b => {},
+        .x => {},
+        .c => {},
+    }
+}
+
+export fn f1() void {
+    switch (my_e) {
+        else => {},
+        .x, .y => {},
+    }
+}
+
+export fn f2() void {
+    switch (my_e) {
+        else => {},
+        .a => {},
+        .x, .y => {},
+        .b => {},
+    }
+}
+
+export fn f3() void {
+    switch (my_e) {
+        .a, .b => {},
+        .x, .y => {},
+        else => {},
+    }
+}
+
+// error
+//
+// :8:10: error: no field named 'x' in enum 'tmp.E'
+// :1:11: note: enum declared here
+// :16:10: error: no field named 'x' in enum 'tmp.E'
+// :1:11: note: enum declared here
+// :24:10: error: no field named 'x' in enum 'tmp.E'
+// :1:11: note: enum declared here
+// :32:10: error: no field named 'x' in enum 'tmp.E'
+// :1:11: note: enum declared here
