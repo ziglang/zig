898658e3a0c4fdc4f352033d62921a8c6bfa2baa
David Rubin <daviru007@icloud.com>
2025-01-28 00:08:28 -0800

    std.c: add `msghdr` and `msghdr_const` definitions for macos

--- lib/std/c.zig
+++ lib/std/c.zig
@@ -3528,7 +3528,21 @@ pub const itimerspec = switch (native_os) {
 };
 pub const msghdr = switch (native_os) {
     .linux => linux.msghdr,
-    .openbsd, .emscripten, .dragonfly, .freebsd, .netbsd, .haiku, .solaris, .illumos => extern struct {
+    .openbsd,
+    .emscripten,
+    .dragonfly,
+    .freebsd,
+    .netbsd,
+    .haiku,
+    .solaris,
+    .illumos,
+    .macos,
+    .driverkit,
+    .ios,
+    .tvos,
+    .visionos,
+    .watchos,
+    => extern struct {
         /// optional address
         name: ?*sockaddr,
         /// size of address
@@ -3548,7 +3562,21 @@ pub const msghdr = switch (native_os) {
 };
 pub const msghdr_const = switch (native_os) {
     .linux => linux.msghdr_const,
-    .openbsd, .emscripten, .dragonfly, .freebsd, .netbsd, .haiku, .solaris, .illumos => extern struct {
+    .openbsd,
+    .emscripten,
+    .dragonfly,
+    .freebsd,
+    .netbsd,
+    .haiku,
+    .solaris,
+    .illumos,
+    .macos,
+    .driverkit,
+    .ios,
+    .tvos,
+    .visionos,
+    .watchos,
+    => extern struct {
         /// optional address
         name: ?*const sockaddr,
         /// size of address
