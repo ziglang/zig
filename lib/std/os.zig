064ef7ab26f9b1f326610124a9e1fb81a016db15
Andrew Kelley <andrew@ziglang.org>
2025-02-03 20:53:22 -0800

    WIP - new main function API

--- lib/std/os.zig
+++ lib/std/os.zig
@@ -41,14 +41,10 @@ test {
     _ = windows;
 }
 
-/// See also `getenv`. Populated by startup code before main().
-/// TODO this is a footgun because the value will be undefined when using `zig build-lib`.
-/// https://github.com/ziglang/zig/issues/4524
+/// Deprecated in favor of accepting a `std.process.Init` argument to main.
 pub var environ: [][*:0]u8 = undefined;
 
-/// Populated by startup code before main().
-/// Not available on WASI or Windows without libc. See `std.process.argsAlloc`
-/// or `std.process.argsWithAllocator` for a cross-platform alternative.
+/// Deprecated in favor of accepting a `std.process.Init` argument to main.
 pub var argv: [][*:0]u8 = if (builtin.link_libc) undefined else switch (native_os) {
     .windows => @compileError("argv isn't supported on Windows: use std.process.argsAlloc instead"),
     .wasi => @compileError("argv isn't supported on WASI: use std.process.argsAlloc instead"),
