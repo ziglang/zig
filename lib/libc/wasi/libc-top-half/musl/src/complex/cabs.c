05d8b565ad1a1d6c0c1e93dc47f1d828043fcafc
Alex Rønne Petersen <alex@alexrp.com>
2025-02-16 03:35:35 +0100

    wasi-libc: Deduplicate sources and headers with regards to upstream musl.
    Unfortunately some duplicate files must remain in lib/libc/wasi/libc-top-half
    because they include internal headers *in the same directory* which have edits
    relative to upstream musl. Because C is an amazing language, there is no way to
    make it so that e.g. upstream musl's src/stdio/fputc.c includes wasi-libc's
    src/stdio/putc.h instead of the upstream putc.h. The preprocessor always
    searches the current directory first for quote includes.
    
    Anyway, this still takes us from 2.9M to 1.4M for the combination of
    lib/libc/wasi and lib/libc/include/wasm-wasi-musl, so I still call it a win.

--- lib/libc/wasi/libc-top-half/musl/src/complex/cabs.c
+++
@@ -1,6 +0,0 @@
-#include "complex_impl.h"
-
-double cabs(double complex z)
-{
-	return hypot(creal(z), cimag(z));
-}
--- lib/libc/wasi/libc-top-half/musl/src/complex/cexpl.c
+++
@@ -1,7 +0,0 @@
-#include "complex_impl.h"
-
-//FIXME
-long double complex cexpl(long double complex z)
-{
-	return cexp(z);
-}
