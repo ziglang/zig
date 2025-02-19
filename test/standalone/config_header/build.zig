c390f55e727c8f4ace2eb7992403bf8332a22126
Techatrix <techatrix@mailbox.org>
2025-02-19 09:34:25 +0100

    add a standalone test for autoconf style addConfigHeader

---
+++ test/standalone/config_header/build.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const config_header = b.addConfigHeader(
+        .{ .style = .{ .autoconf = b.path("config.h.in") } },
+        .{
+            .SOME_NO = null,
+            .SOME_TRUE = true,
+            .SOME_FALSE = false,
+            .SOME_ZERO = 0,
+            .SOME_ONE = 1,
+            .SOME_TEN = 10,
+            .SOME_ENUM_LITERAL = .@"test",
+            .SOME_STRING = "test",
+        },
+    );
+
+    const check_config_header = b.addCheckFile(config_header.getOutput(), .{ .expected_exact = @embedFile("config.h") });
+
+    const test_step = b.step("test", "Test it");
+    test_step.dependOn(&check_config_header.step);
+}
