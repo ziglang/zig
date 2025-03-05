fac882e7c18228d69320b1eab639943facf879a7
Linus Groh <mail@linusgroh.de>
2025-03-05 03:17:54 +0000

    Remove numerous things deprecated during the 0.14 release cycle
    Basically everything that has a direct replacement or no uses left.
    
    Notable omissions:
    
    - std.ArrayHashMap: Too much fallout, needs a separate cleanup.
    - std.Build top level fields moved to root_module: Too much fallout,
      needs a separate cleanup.
    - std.debug.runtime_safety: Too much fallout.
    - std.heap.GeneralPurposeAllocator: Lots of references to it remain, not
      a simple find and replace as "debug allocator" is not equivalent to
      "general purpose allocator".
    - std.io.Reader: Is being reworked at the moment.
    - std.unicode.utf8Decode(): No replacement, needs a new API first.
    - Manifest backwards compat options: Removal would break test data used
      by TestFetchBuilder.
    - panic handler needs to be a namespace: Many tests still rely on it
      being a function, needs a separate cleanup.

--- lib/std/posix/test.zig
+++ lib/std/posix/test.zig
@@ -1161,7 +1161,7 @@ test "POSIX file locking with fcntl" {
         posix.exit(0);
     } else {
         // parent waits for child to get shared lock:
-        std.time.sleep(1 * std.time.ns_per_ms);
+        std.Thread.sleep(1 * std.time.ns_per_ms);
         // parent expects deadlock when attempting to upgrade the shared lock to exclusive:
         struct_flock.start = 1;
         struct_flock.type = posix.F.WRLCK;
