61a95ab6629f9eb6e9aeff4f409c68539e117f18
Andrew Kelley <andrew@ziglang.org>
2025-03-05 12:41:31 -0800

    start the 0.14.1 release cycle

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ const assert = std.debug.assert;
 const DevEnv = @import("src/dev.zig").Env;
 const ValueInterpretMode = enum { direct, by_name };
 
-const zig_version: std.SemanticVersion = .{ .major = 0, .minor = 14, .patch = 0 };
+const zig_version: std.SemanticVersion = .{ .major = 0, .minor = 14, .patch = 1 };
 const stack_size = 46 * 1024 * 1024;
 
 pub fn build(b: *std.Build) !void {
