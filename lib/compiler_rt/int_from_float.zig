a06628de294ed66ddf167b1a9ed876157b8ab8e5
Jacob Young <jacobly0@users.noreply.github.com>
2025-03-08 01:42:03 -0500

    compiler-rt: fix signed min int from float

--- lib/compiler_rt/int_from_float.zig
+++ lib/compiler_rt/int_from_float.zig
@@ -72,10 +72,12 @@ pub inline fn bigIntFromFloat(comptime signedness: std.builtin.Signedness, resul
     } });
 
     const parts = math.frexp(a);
-    const exponent = @max(parts.exponent - significand_bits, 0);
+    const significand_bits_adjusted_to_handle_smin = @as(i32, significand_bits) +
+        @intFromBool(signedness == .signed and parts.exponent == 32 * result.len);
+    const exponent = @max(parts.exponent - significand_bits_adjusted_to_handle_smin, 0);
     const int: I = @intFromFloat(switch (exponent) {
         0 => a,
-        else => math.ldexp(parts.significand, significand_bits),
+        else => math.ldexp(parts.significand, significand_bits_adjusted_to_handle_smin),
     });
     switch (signedness) {
         .signed => {
