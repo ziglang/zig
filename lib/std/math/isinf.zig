7290112ead41ca6d9237393a0a9007cf860ba643
Pat Tullmann <pat.github@tullmann.org>
2025-03-16 18:23:16 -0700

    std.math: allow comptime_floats in isInf and isNan
    Currently constants passed to these functions trigger an obtuse:
    `error: access of union field 'float' while field 'comptime_float' is active`
    where the work-around is to cast the constant into a runtime float.
    
    From https://github.com/ziglang/zig/issues/21205, I'm leveraging the fact
    that `comptime_float` won't (eventually) support NaN or infinity, so the
    `isNan` and `isInf` methods can unilaterally return false for
    `comptime_float` parameters.
    
    Add tests for comptime floats passed to isNan, isInf*.
    
    See https://github.com/ziglang/zig/pull/23258 for the motivation.
    
    Fixes #22107

--- lib/std/math/isinf.zig
+++ lib/std/math/isinf.zig
@@ -2,22 +2,38 @@ const std = @import("../std.zig");
 const math = std.math;
 const expect = std.testing.expect;
 
-/// Returns whether x is an infinity, ignoring sign.
+/// Returns whether float argument is an infinity, ignoring sign.
 pub inline fn isInf(x: anytype) bool {
     const T = @TypeOf(x);
-    const TBits = std.meta.Int(.unsigned, @typeInfo(T).float.bits);
-    const remove_sign = ~@as(TBits, 0) >> 1;
-    return @as(TBits, @bitCast(x)) & remove_sign == @as(TBits, @bitCast(math.inf(T)));
+    switch (@typeInfo(T)) {
+        .comptime_float => return false, // comptime does not allow inf
+        .float => {
+            const TBits = std.meta.Int(.unsigned, @typeInfo(T).float.bits);
+            const remove_sign = ~@as(TBits, 0) >> 1;
+            return @as(TBits, @bitCast(x)) & remove_sign == @as(TBits, @bitCast(math.inf(T)));
+        },
+        else => @compileError("isInf unsupported for " ++ @typeName(T)),
+    }
 }
 
-/// Returns whether x is an infinity with a positive sign.
+/// Returns whether float argument is an infinity with a positive sign.
 pub inline fn isPositiveInf(x: anytype) bool {
-    return x == math.inf(@TypeOf(x));
+    const T = @TypeOf(x);
+    switch (@typeInfo(T)) {
+        .comptime_float => return false, // comptime does not allow inf
+        .float => return x == math.inf(T),
+        else => @compileError("isPositiveInf unsupported for " ++ @typeName(T)),
+    }
 }
 
-/// Returns whether x is an infinity with a negative sign.
+/// Returns whether float argument is an infinity with a negative sign.
 pub inline fn isNegativeInf(x: anytype) bool {
-    return x == -math.inf(@TypeOf(x));
+    const T = @TypeOf(x);
+    switch (@typeInfo(T)) {
+        .comptime_float => return false, // comptime does not allow inf
+        .float => return x == -math.inf(T),
+        else => @compileError("isNegativeInf unsupported for " ++ @typeName(T)),
+    }
 }
 
 test isInf {
@@ -29,6 +45,7 @@ test isInf {
         try expect(!isInf(math.nan(T)));
         try expect(!isInf(-math.nan(T)));
     }
+    try expect(!isInf(0.0));
 }
 
 test isPositiveInf {
@@ -37,9 +54,10 @@ test isPositiveInf {
         try expect(!isPositiveInf(@as(T, -0.0)));
         try expect(isPositiveInf(math.inf(T)));
         try expect(!isPositiveInf(-math.inf(T)));
-        try expect(!isInf(math.nan(T)));
-        try expect(!isInf(-math.nan(T)));
+        try expect(!isPositiveInf(math.nan(T)));
+        try expect(!isPositiveInf(-math.nan(T)));
     }
+    try expect(!isPositiveInf(0.0));
 }
 
 test isNegativeInf {
@@ -48,7 +66,8 @@ test isNegativeInf {
         try expect(!isNegativeInf(@as(T, -0.0)));
         try expect(!isNegativeInf(math.inf(T)));
         try expect(isNegativeInf(-math.inf(T)));
-        try expect(!isInf(math.nan(T)));
-        try expect(!isInf(-math.nan(T)));
+        try expect(!isNegativeInf(math.nan(T)));
+        try expect(!isNegativeInf(-math.nan(T)));
     }
+    try expect(!isNegativeInf(0.0));
 }
