063f431e7e9b3960f7154fc976e8358b70a7e35b
Carmen <carmen@dotcarmen.dev>
2025-03-18 10:34:00 +0900

    move file info stuff to file.zig

--- lib/std/os/uefi/protocol/file.zig
+++ lib/std/os/uefi/protocol/file.zig
@@ -278,15 +278,51 @@ pub const File = extern struct {
         directory: bool = false,
         // 0x0000000000000020
         archive: bool = false,
-        // used exclusively for `valid_attr` as far as i can tell...
-        _flag: bool = false,
-        _pad: u57 = 0,
+        _pad: u58 = 0,
+    };
 
-        // 0x0000000000000037
-        pub const valid_attr: Attributes = .{
-            .read_only = true,
-            .system = true,
-            ._flag = true,
+    pub const FileInfo = extern struct {
+        size: u64,
+        file_size: u64,
+        physical_size: u64,
+        create_time: Time,
+        last_access_time: Time,
+        modification_time: Time,
+        attribute: Attributes,
+
+        pub fn getFileName(self: *const FileInfo) [*:0]const u16 {
+            return @ptrCast(@alignCast(@as([*]const u8, @ptrCast(self)) + @sizeOf(FileInfo)));
+        }
+
+        pub const guid align(8) = Guid{
+            .time_low = 0x09576e92,
+            .time_mid = 0x6d3f,
+            .time_high_and_version = 0x11d2,
+            .clock_seq_high_and_reserved = 0x8e,
+            .clock_seq_low = 0x39,
+            .node = [_]u8{ 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b },
+        };
+    };
+
+    pub const FileSystemInfo = extern struct {
+        size: u64,
+        read_only: bool,
+        volume_size: u64,
+        free_space: u64,
+        block_size: u32,
+        _volume_label: u16,
+
+        pub fn getVolumeLabel(self: *const FileSystemInfo) [*:0]const u16 {
+            return @as([*:0]const u16, @ptrCast(&self._volume_label));
+        }
+
+        pub const guid align(8) = Guid{
+            .time_low = 0x09576e93,
+            .time_mid = 0x6d3f,
+            .time_high_and_version = 0x11d2,
+            .clock_seq_high_and_reserved = 0x8e,
+            .clock_seq_low = 0x39,
+            .node = [_]u8{ 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b },
         };
     };
 
