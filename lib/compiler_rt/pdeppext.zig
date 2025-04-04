f431c1ee2da9fce9ac52a9cdc7ee0dbbd57a6484
Clara <clara@bnuuycoded.uk>
2025-04-04 19:44:22 +0100

    x86-backend: @{deposit,extract}Bits: Use new API
    This replaces the old (and broken) use of the old API with the new
    `select` API. Windows is not supported at all by this API, however the
    original old API implementation never worked properly anyway, and the
    correct course of action is to wait for the COFF linker to be fixed.
    
    TODO: This currently has a hacky copy-paste in compiler-rt. Wait for
    alexrp's PR.
    
    Co-authored-by: David Rubin <daviru007@icloud.com>

--- lib/compiler_rt/pdeppext.zig
+++ lib/compiler_rt/pdeppext.zig
@@ -37,9 +37,8 @@ inline fn limb_set(x: []Limb, i: usize, v: Limb) void {
 
 // Code for bigint pdep and pext largely taken from std.math.big.int.depositBits and extractBits
 
+// Assumes that `result` is zeroed.
 inline fn pdep_bigint(result: []Limb, source: []const Limb, mask: []const Limb) void {
-    @memset(result, 0);
-
     var mask_limb: Limb = limb(mask, 0);
     var mask_limb_index: usize = 0;
     var i: usize = 0;
@@ -72,6 +71,9 @@ inline fn pdep_bigint(result: []Limb, source: []const Limb, mask: []const Limb) 
 }
 
 pub fn __pdep_bigint(r: [*]Limb, s: [*]const Limb, m: [*]const Limb, bits: usize) callconv(.c) void {
+    const result_full = r[0 .. std.math.divCeil(usize, @intCast(intAbiSize(@intCast(bits), builtin.target)), 4) catch unreachable];
+    @memset(result_full, 0);
+
     const result = r[0 .. std.math.divCeil(usize, bits, 32) catch unreachable];
     const source = s[0 .. std.math.divCeil(usize, bits, 32) catch unreachable];
     const mask = m[0 .. std.math.divCeil(usize, bits, 32) catch unreachable];
@@ -79,9 +81,8 @@ pub fn __pdep_bigint(r: [*]Limb, s: [*]const Limb, m: [*]const Limb, bits: usize
     pdep_bigint(result, source, mask);
 }
 
+// Assumes that `result` is zeroed.
 inline fn pext_bigint(result: []Limb, source: []const Limb, mask: []const Limb) void {
-    @memset(result, 0);
-
     var mask_limb: Limb = limb(mask, 0);
     var mask_limb_index: usize = 0;
     var i: usize = 0;
@@ -113,6 +114,9 @@ inline fn pext_bigint(result: []Limb, source: []const Limb, mask: []const Limb) 
 }
 
 pub fn __pext_bigint(r: [*]Limb, s: [*]const Limb, m: [*]const Limb, bits: usize) callconv(.c) void {
+    const result_full = r[0 .. std.math.divCeil(usize, @intCast(intAbiSize(@intCast(bits), builtin.target)), 4) catch unreachable];
+    @memset(result_full, 0);
+
     const result = r[0 .. std.math.divCeil(usize, bits, 32) catch unreachable];
     const source = s[0 .. std.math.divCeil(usize, bits, 32) catch unreachable];
     const mask = m[0 .. std.math.divCeil(usize, bits, 32) catch unreachable];
@@ -174,4 +178,137 @@ pub fn __pext_u64(source: u64, mask: u64) callconv(.c) u64 {
 
 pub fn __pext_u128(source: u128, mask: u128) callconv(.c) u128 {
     return pext_uX(u128, source, mask);
+}
+
+// BEGIN HACKY CODE COPY WAIT FOR ALEXRP PR
+
+const Target = std.Target;
+const assert = std.debug.assert;
+
+pub const Alignment = enum(u6) {
+    @"1" = 0,
+    @"2" = 1,
+    @"4" = 2,
+    @"8" = 3,
+    @"16" = 4,
+    @"32" = 5,
+    @"64" = 6,
+    none = std.math.maxInt(u6),
+    _,
+
+    pub fn fromByteUnits(n: u64) Alignment {
+        if (n == 0) return .none;
+        assert(std.math.isPowerOfTwo(n));
+        return @enumFromInt(@ctz(n));
+    }
+
+    /// Align an address forwards to this alignment.
+    pub fn forward(a: Alignment, addr: u64) u64 {
+        assert(a != .none);
+        const x = (@as(u64, 1) << @intFromEnum(a)) - 1;
+        return (addr + x) & ~x;
+    }
+};
+
+pub fn intAbiSize(bits: u16, target: Target) u64 {
+    return intAbiAlignment(bits, target).forward(@as(u16, @intCast((@as(u17, bits) + 7) / 8)));
+}
+
+pub fn intAbiAlignment(bits: u16, target: Target) Alignment {
+    return switch (target.cpu.arch) {
+        .x86 => switch (bits) {
+            0 => .none,
+            1...8 => .@"1",
+            9...16 => .@"2",
+            17...32 => .@"4",
+            33...64 => switch (target.os.tag) {
+                .uefi, .windows => .@"8",
+                else => .@"4",
+            },
+            else => .@"16",
+        },
+        .x86_64 => switch (bits) {
+            0 => .none,
+            1...8 => .@"1",
+            9...16 => .@"2",
+            17...32 => .@"4",
+            33...64 => .@"8",
+            else => .@"16",
+        },
+        else => return Alignment.fromByteUnits(@min(
+            std.math.ceilPowerOfTwoPromote(u16, @as(u16, @intCast((@as(u17, bits) + 7) / 8))),
+            maxIntAlignment(target),
+        )),
+    };
+}
+
+pub fn maxIntAlignment(target: std.Target) u16 {
+    return switch (target.cpu.arch) {
+        .avr => 1,
+        .msp430 => 2,
+        .xcore => 4,
+        .propeller => 4,
+
+        .arm,
+        .armeb,
+        .thumb,
+        .thumbeb,
+        .hexagon,
+        .mips,
+        .mipsel,
+        .powerpc,
+        .powerpcle,
+        .amdgcn,
+        .riscv32,
+        .sparc,
+        .s390x,
+        .lanai,
+        .wasm32,
+        .wasm64,
+        => 8,
+
+        // For these, LLVMABIAlignmentOfType(i128) reports 8. Note that 16
+        // is a relevant number in three cases:
+        // 1. Different machine code instruction when loading into SIMD register.
+        // 2. The C ABI wants 16 for extern structs.
+        // 3. 16-byte cmpxchg needs 16-byte alignment.
+        // Same logic for powerpc64, mips64, sparc64.
+        .powerpc64,
+        .powerpc64le,
+        .mips64,
+        .mips64el,
+        .sparc64,
+        => switch (target.ofmt) {
+            .c => 16,
+            else => 8,
+        },
+
+        .x86_64 => 16,
+
+        // Even LLVMABIAlignmentOfType(i128) agrees on these targets.
+        .x86,
+        .aarch64,
+        .aarch64_be,
+        .riscv64,
+        .bpfel,
+        .bpfeb,
+        .nvptx,
+        .nvptx64,
+        => 16,
+
+        // Below this comment are unverified but based on the fact that C requires
+        // int128_t to be 16 bytes aligned, it's a safe default.
+        .csky,
+        .arc,
+        .m68k,
+        .kalimba,
+        .spirv,
+        .spirv32,
+        .ve,
+        .spirv64,
+        .loongarch32,
+        .loongarch64,
+        .xtensa,
+        => 16,
+    };
 }
