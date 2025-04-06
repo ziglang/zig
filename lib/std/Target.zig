c8631ec523bb9ae9ff04f00401cb7e0fe0ae3bf4
Alex Rønne Petersen <alex@alexrp.com>
2025-04-06 08:01:27 +0200

    std.Target: Bump minimum Dragonfly BSD version to 6.0.0.

--- lib/std/Target.zig
+++ lib/std/Target.zig
@@ -500,7 +500,7 @@ pub const Os = struct {
 
                 .dragonfly => .{
                     .semver = .{
-                        .min = .{ .major = 5, .minor = 8, .patch = 0 },
+                        .min = .{ .major = 6, .minor = 0, .patch = 0 },
                         .max = .{ .major = 6, .minor = 4, .patch = 0 },
                     },
                 },
