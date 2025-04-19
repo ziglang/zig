7b0206f16d004f65f619c4045908356086b52f8e
Ali Cheraghi <alichraghi@proton.me>
2025-04-19 20:33:03 +0330

    Module: ignore `xnack` and `sramecc` features on some gpu models

--- src/Compilation.zig
+++ src/Compilation.zig
@@ -6720,6 +6720,10 @@ pub fn addCCArgs(
                     // See https://github.com/ziglang/zig/issues/23539
                     if (target_util.isDynamicAMDGCNFeature(target, feature)) continue;
 
+                    // Ignore these until we figure out how to handle the concept of omitting features.
+                    // See https://github.com/ziglang/zig/issues/23539
+                    if (target_util.isDynamicAMDGCNFeature(target, feature)) continue;
+
                     argv.appendSliceAssumeCapacity(&[_][]const u8{ "-Xclang", "-target-feature", "-Xclang" });
                     const plus_or_minus = "-+"[@intFromBool(is_enabled)];
                     const arg = try std.fmt.allocPrint(arena, "{c}{s}", .{ plus_or_minus, llvm_name });
