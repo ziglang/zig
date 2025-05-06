8ec86e45a68c9e0c9b71f351fb0dcdc25e62aba1
David <45010661+kada49@users.noreply.github.com>
2025-05-06 12:10:05 +0200

    Update test name according to review
    Co-authored-by: Linus Groh <mail@linusgroh.de>

--- lib/c/strings.zig
+++ lib/c/strings.zig
@@ -9,7 +9,7 @@ fn bzero(s: *anyopaque, n: usize) void {
     _ = std.zig.c_builtins.__builtin_memset(s, 0, n);
 }
 
-test "bzero" {
+test bzero {
     var array: [10]u8 = [_]u8{ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
     var a = std.mem.zeroes([array.len]u8);
     a[9] = '0';
