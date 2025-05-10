f90a588fd53bca2e7a67e77329e65cecac655689
devindotzip <devinprothero@gmail.com>
2025-05-09 21:02:37 -0700

    Implement Several More Functions - add memccpy, memmem, mempcpy, stpcpy, stpncpy, strcat, strcpy, strncpy, strnlen

--- lib/c/string.zig
+++ lib/c/string.zig
@@ -5,11 +5,19 @@ const common = @import("common.zig");
 comptime {
     @export(&strcmp, .{ .name = "strcmp", .linkage = common.linkage, .visibility = common.visibility });
     @export(&strlen, .{ .name = "strlen", .linkage = common.linkage, .visibility = common.visibility });
+    @export(&strnlen, .{ .name = "strnlen", .linkage = common.linkage, .visibility = common.visibility });
     @export(&strncmp, .{ .name = "strncmp", .linkage = common.linkage, .visibility = common.visibility });
-    @export(&strchr, .{ .name = "index", .linkage = common.linkage, .visibility = common.visibility });
     @export(&strchr, .{ .name = "strchr", .linkage = common.linkage, .visibility = common.visibility });
+    @export(&strchr, .{ .name = "index", .linkage = common.linkage, .visibility = common.visibility });
+    @export(&strrchr, .{ .name = "strrchr", .linkage = common.linkage, .visibility = common.visibility });
     @export(&strrchr, .{ .name = "rindex", .linkage = common.linkage, .visibility = common.visibility });
+    @export(&strcpy, .{ .name = "strcpy", .linkage = common.linkage, .visibility = common.visibility });
+    @export(&strcat, .{ .name = "strcat", .linkage = common.linkage, .visibility = common.visibility });
+    @export(&strncpy, .{ .name = "strncpy", .linkage = common.linkage, .visibility = common.visibility });
+    @export(&memccpy, .{ .name = "memccpy", .linkage = common.linkage, .visibility = common.visibility });
+    @export(&mempcpy, .{ .name = "mempcpy", .linkage = common.linkage, .visibility = common.visibility });
     @export(&memset, .{ .name = "memset", .linkage = common.linkage, .visibility = common.visibility });
+    @export(&memmem, .{ .name = "memmem", .linkage = common.linkage, .visibility = common.visibility });
     { // TODO: Conditional export for armeabi?
         @export(&__aeabi_memclr, .{ .name = "__aeabi_memclr8", .linkage = common.linkage, .visibility = common.visibility });
         @export(&__aeabi_memclr, .{ .name = "__aeabi_memclr4", .linkage = common.linkage, .visibility = common.visibility });
@@ -19,11 +27,14 @@ comptime {
         @export(&memset, .{ .name = "__aeabi_memset", .linkage = common.linkage, .visibility = common.visibility });
     }
     @export(&memchr, .{ .name = "memchr", .linkage = common.linkage, .visibility = common.visibility });
-    @export(&strrchr, .{ .name = "strrchr", .linkage = common.linkage, .visibility = common.visibility });
     @export(&strchrnul, .{ .name = "__strchrnul", .linkage = .weak, .visibility = .hidden });
     @export(&strchrnul, .{ .name = "strchrnul", .linkage = .weak, .visibility = common.visibility });
     @export(&memrchr, .{ .name = "memrchr", .linkage = .weak, .visibility = common.visibility });
     @export(&memrchr, .{ .name = "__memrchr", .linkage = .weak, .visibility = .hidden });
+    @export(&stpcpy, .{ .name = "stpcpy", .linkage = .weak, .visibility = common.visibility });
+    @export(&stpcpy, .{ .name = "__stpcpy", .linkage = .weak, .visibility = .hidden });
+    @export(&stpncpy, .{ .name = "stpncpy", .linkage = .weak, .visibility = common.visibility });
+    @export(&stpncpy, .{ .name = "__stpncpy", .linkage = .weak, .visibility = .hidden });
 }
 
 fn strcmp(s1: [*:0]const c_char, s2: [*:0]const c_char) callconv(.c) c_int {
@@ -60,6 +71,10 @@ test strncmp {
 
 fn strlen(s: [*:0]const c_char) callconv(.c) usize {
     return std.mem.len(s);
+}
+
+fn strnlen(s: [*:0]const c_char, n: usize) callconv(.c) usize {
+    return if (std.mem.indexOfScalar(c_char, s[0..n], 0)) |idx| idx else n;
 }
 
 fn strchrnul(s: [*:0]const c_char, c: c_int) callconv(.c) [*:0]const c_char {
@@ -154,3 +169,145 @@ fn __aeabi_memclr(dest: *anyopaque, n: usize) callconv(.c) *anyopaque {
     return memset(dest, 0, n);
 }
 
+fn memccpy(noalias dest: *anyopaque, noalias src: *const anyopaque, c: c_int, n: usize) callconv(.c) ?*anyopaque {
+    @setRuntimeSafety(false);
+    const d: [*]u8 = @ptrCast(dest);
+    const s: [*]const u8 = @ptrCast(src);
+    const needle: u8 = @intCast(c);
+    var idx: usize = 0;
+    while (idx < n) : (idx += 1) {
+        d[idx] = s[idx];
+        if (d[idx] == needle) return d + idx + 1;
+    }
+    return null;
+}
+
+test memccpy {
+    const src: []const u8 = "supercalifragilisticexpialidocious";
+    var dst: [src.len]u8 = @splat(0);
+    const dOffset = std.mem.indexOfScalar(u8, src, 'd').?;
+    const endPtr: *anyopaque = @ptrCast(@as([*]u8, &dst) + dOffset + 1);
+    try std.testing.expect(memccpy(@ptrCast(&dst), @ptrCast(src.ptr), 'd', src.len) == endPtr);
+    try std.testing.expectEqualStrings("supercalifragilisticexpialid", std.mem.trimRight(u8, dst[0..], &.{0}));
+
+    dst = @splat(0);
+    try std.testing.expect(memccpy(@constCast(@ptrCast(&dst)), (@ptrCast(src.ptr)), 'z', src.len) == null);
+    try std.testing.expectEqualStrings(src, dst[0..]);
+}
+
+fn mempcpy(noalias dest: *anyopaque, noalias src: *const anyopaque, n: usize) callconv(.c) *anyopaque {
+    @setRuntimeSafety(false);
+    const d: [*]u8 = @ptrCast(dest);
+    const s: [*]const u8 = @ptrCast(src);
+    @memcpy(d[0..n], s[0..n]);
+    return @ptrCast(d + n);
+}
+
+test mempcpy {
+    const bytesToWrite = 3;
+    const src: []const u8 = "testing";
+    var dst: [src.len]u8 = @splat('z');
+    const endPtr: *anyopaque = @ptrCast(@as([*]u8, &dst) + bytesToWrite);
+    try std.testing.expect(mempcpy(@ptrCast(&dst), @ptrCast(src.ptr), bytesToWrite) == endPtr);
+    try std.testing.expect(@as(*u8, @ptrCast(endPtr)).* == 'z');
+}
+
+fn memmem(haystack: *const anyopaque, haystack_len: usize, needle: *const anyopaque, needle_len: usize) callconv(.c) ?*const anyopaque {
+    const h: [*]const u8 = @ptrCast(haystack);
+    const n: [*]const u8 = @ptrCast(needle);
+
+    if (std.mem.indexOf(u8, h[0..haystack_len], n[0..needle_len])) |idx| {
+        return @ptrCast(h + idx);
+    } else {
+        return null;
+    }
+}
+
+fn stpcpy(noalias dest: [*:0]c_char, noalias src: [*:0]const c_char) callconv(.c) [*:0]c_char {
+    var d: [*]u8 = @ptrCast(dest);
+    var s: [*]const u8 = @ptrCast(src);
+    // QUESTION: is std.mem.span -> @memset more efficient here?
+    while (true) {
+        d[0] = s[0];
+        if (s[0] == 0) return @ptrCast(d);
+        d += 1;
+        s += 1;
+    }
+}
+
+test stpcpy {
+    const src: [:0]const c_char = @ptrCast("bananas");
+    var dst: [src.len]c_char = undefined;
+    const endPtr: [*:0]c_char = @ptrCast(@as([*]c_char, &dst) + src.len);
+    try std.testing.expect(stpcpy(@ptrCast(&dst), @ptrCast(src.ptr)) == endPtr);
+    try std.testing.expect(endPtr[0] == 0);
+    try std.testing.expectEqualSentinel(c_char, 0, src, @ptrCast(&dst));
+}
+
+fn strcpy(noalias dest: [*:0]c_char, noalias src: [*:0]const c_char) callconv(.c) [*:0]c_char {
+    _ = stpcpy(dest, src);
+    return dest;
+}
+
+test strcpy {
+    const src: [:0]const c_char = @ptrCast("bananas");
+    var dst: [src.len]c_char = undefined;
+    try std.testing.expect(strcpy(@ptrCast(&dst), @ptrCast(src.ptr)) == @as([*:0]c_char, @ptrCast(&dst)));
+    try std.testing.expectEqualSentinel(c_char, 0, src, @ptrCast(&dst));
+}
+
+fn stpncpy(noalias dest: [*:0]c_char, noalias src: [*:0]const c_char, n: usize) callconv(.c) [*:0]c_char {
+    const dlen = strnlen(src, n);
+    const end = mempcpy(@ptrCast(dest), @ptrCast(src), dlen);
+    return @ptrCast(memset(end, 0, n - dlen));
+}
+
+test stpncpy {
+    var buf: [5]c_char = undefined;
+    const b: [*:0]c_char = @ptrCast(&buf);
+    try std.testing.expect(stpncpy(@ptrCast(&buf), @ptrCast("1"), buf.len) == b + 1);
+    try std.testing.expectEqualSlices(c_char, &.{ '1', 0, 0, 0, 0 }, buf[0..]);
+    try std.testing.expect(stpncpy(@ptrCast(&buf), @ptrCast("12"), buf.len) == b + 2);
+    try std.testing.expectEqualSlices(c_char, &.{ '1', '2', 0, 0, 0 }, buf[0..]);
+    try std.testing.expect(stpncpy(@ptrCast(&buf), @ptrCast("123"), buf.len) == b + 3);
+    try std.testing.expectEqualSlices(c_char, &.{ '1', '2', '3', 0, 0 }, buf[0..]);
+    try std.testing.expect(stpncpy(@ptrCast(&buf), @ptrCast("1234"), buf.len) == b + 4);
+    try std.testing.expectEqualSlices(c_char, &.{ '1', '2', '3', '4', 0 }, buf[0..]);
+    try std.testing.expect(stpncpy(@ptrCast(&buf), @ptrCast("12345"), buf.len) == b + 5);
+    try std.testing.expectEqualSlices(c_char, &.{ '1', '2', '3', '4', '5' }, buf[0..]);
+}
+
+fn strncpy(noalias dest: [*:0]c_char, noalias src: [*:0]const c_char, n: usize) callconv(.c) [*:0]c_char {
+    _ = stpncpy(dest, src, n);
+    return dest;
+}
+
+test strncpy {
+    var buf: [5]c_char = undefined;
+    const b: [*:0]c_char = @ptrCast(&buf);
+    try std.testing.expect(strncpy(@ptrCast(&buf), @ptrCast("1"), buf.len) == b);
+    try std.testing.expectEqualSlices(c_char, &.{ '1', 0, 0, 0, 0 }, buf[0..]);
+    try std.testing.expect(strncpy(@ptrCast(&buf), @ptrCast("12"), buf.len) == b);
+    try std.testing.expectEqualSlices(c_char, &.{ '1', '2', 0, 0, 0 }, buf[0..]);
+    try std.testing.expect(strncpy(@ptrCast(&buf), @ptrCast("123"), buf.len) == b);
+    try std.testing.expectEqualSlices(c_char, &.{ '1', '2', '3', 0, 0 }, buf[0..]);
+    try std.testing.expect(strncpy(@ptrCast(&buf), @ptrCast("1234"), buf.len) == b);
+    try std.testing.expectEqualSlices(c_char, &.{ '1', '2', '3', '4', 0 }, buf[0..]);
+    try std.testing.expect(strncpy(@ptrCast(&buf), @ptrCast("12345"), buf.len) == b);
+    try std.testing.expectEqualSlices(c_char, &.{ '1', '2', '3', '4', '5' }, buf[0..]);
+}
+
+fn strcat(noalias dest: [*:0]c_char, noalias src: [*:0]const c_char) callconv(.c) [*:0]c_char {
+    const start = dest + strlen(dest);
+    _ = stpcpy(start, src);
+    return dest;
+}
+
+test strcat {
+    const start = "Hello";
+    const end = " World!\n";
+    var buf: [start.len + end.len:0]c_char = undefined;
+    @memcpy(buf[0 .. start.len + 1], @as([*:0]const c_char, @ptrCast(start)));
+    try std.testing.expect(strcat(&buf, @as([*:0]const c_char, @ptrCast(end))) == &buf);
+    try std.testing.expectEqualStrings("Hello World!\n", @as([]u8, @ptrCast(buf[0..])));
+}
--- lib/libc/musl/src/string/strcat.c
+++
@@ -1,7 +0,0 @@
-#include <string.h>
-
-char *strcat(char *restrict dest, const char *restrict src)
-{
-	strcpy(dest + strlen(dest), src);
-	return dest;
-}
