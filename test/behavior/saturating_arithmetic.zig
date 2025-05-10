6a47d31725b1a882caa39d52e2afeaccddf19630
Jacob Young <jacobly0@users.noreply.github.com>
2025-05-10 01:55:25 -0400

    x86_64: rewrite scalar `+|`

--- test/behavior/saturating_arithmetic.zig
+++ test/behavior/saturating_arithmetic.zig
@@ -53,12 +53,12 @@ test "saturating add" {
 
 test "saturating add 128bit" {
     if (builtin.zig_backend == .stage2_wasm) return error.SkipZigTest; // TODO
-    if (builtin.zig_backend == .stage2_x86_64) return error.SkipZigTest; // TODO
     if (builtin.zig_backend == .stage2_aarch64) return error.SkipZigTest; // TODO
     if (builtin.zig_backend == .stage2_arm) return error.SkipZigTest; // TODO
     if (builtin.zig_backend == .stage2_sparc64) return error.SkipZigTest; // TODO
     if (builtin.zig_backend == .stage2_spirv64) return error.SkipZigTest;
     if (builtin.zig_backend == .stage2_riscv64) return error.SkipZigTest;
+    if (builtin.zig_backend == .stage2_x86_64 and builtin.target.ofmt != .elf and builtin.target.ofmt != .macho) return error.SkipZigTest;
 
     const S = struct {
         fn doTheTest() !void {
