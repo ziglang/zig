c702da372074d3ccdce044f21286fd9dae9bd304
tjog <28024277+tjog@users.noreply.github.com>
2025-05-18 16:31:52 +0200

    libfuzzer test: use proper cache dir logic

--- test/standalone/libfuzzer/build.zig
+++ test/standalone/libfuzzer/build.zig
@@ -22,5 +22,6 @@ pub fn build(b: *std.Build) void {
     b.default_step = run_step;
 
     const run_artifact = b.addRunArtifact(exe);
+    run_artifact.addArg(b.cache_root.path orelse "");
     run_step.dependOn(&run_artifact.step);
 }
