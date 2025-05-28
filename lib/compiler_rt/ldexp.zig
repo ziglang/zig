918068458937121fd904ec45aaef4d04c89f9013
Hilger Baumstark <hilger.baumstark@hamburg.de>
2025-05-28 20:43:37 +0200

    removes unused imports

--- lib/compiler_rt/ldexp.zig
+++ lib/compiler_rt/ldexp.zig
@@ -1,8 +1,5 @@
 const std = @import("std");
-const builtin = @import("builtin");
-const arch = builtin.cpu.arch;
 const math = std.math;
-const mem = std.mem;
 const common = @import("common.zig");
 const exp2 = @import("exp2.zig");
 
@@ -31,11 +28,11 @@ test "ldexpf" {
     const epsilon = 0.000001;
     try std.testing.expectApproxEqAbs(2.0, ldexpf(2, 0), epsilon);
     try std.testing.expectApproxEqAbs(2147483600, ldexpf(1, 31), epsilon);
-    try std.testing.expectApproxEqAbs(std.math.maxInt(u32), ldexpf(2, 31), epsilon);
+    try std.testing.expectApproxEqAbs(math.maxInt(u32), ldexpf(2, 31), epsilon);
     try std.testing.expectApproxEqAbs(0.437500, ldexpf(7, -4), epsilon);
     try std.testing.expectApproxEqAbs(1.79769e+388, ldexpf(1, 1024), epsilon);
     try std.testing.expectApproxEqAbs(0, ldexpf(0, 10), epsilon);
-    try std.testing.expect(math.isNegativeInf(ldexpf(-std.math.inf(f32), -1)));
+    try std.testing.expect(math.isNegativeInf(ldexpf(-math.inf(f32), -1)));
     try std.testing.expect(math.isPositiveInf(ldexpf(1.0, 1024)));
 }
 
@@ -56,12 +53,12 @@ test "ldexp" {
     const epsilon = 0.00000000001;
     try std.testing.expectApproxEqAbs(2.0, ldexp(2, 0), epsilon);
     try std.testing.expectApproxEqAbs(9223372036854775807, ldexp(1, 63), epsilon);
-    try std.testing.expectApproxEqAbs(std.math.maxInt(u64), ldexp(2, 63), epsilon);
-    try std.testing.expectApproxEqAbs(std.math.maxInt(u64), ldexp(1, 64), epsilon);
+    try std.testing.expectApproxEqAbs(math.maxInt(u64), ldexp(2, 63), epsilon);
+    try std.testing.expectApproxEqAbs(math.maxInt(u64), ldexp(1, 64), epsilon);
     try std.testing.expectApproxEqAbs(0.437500, ldexp(7, -4), epsilon);
     try std.testing.expectApproxEqAbs(1.79769e+388, ldexp(1, 1024), epsilon);
     try std.testing.expectApproxEqAbs(0, ldexp(0, 10), epsilon);
-    try std.testing.expect(math.isNegativeInf(ldexp(-std.math.inf(f64), -1)));
+    try std.testing.expect(math.isNegativeInf(ldexp(-math.inf(f64), -1)));
     try std.testing.expect(math.isPositiveInf(ldexp(1.0, 1024)));
 }
 
@@ -83,11 +80,11 @@ test "ldexpl" {
     const epsilon = 0.000000000000000000001;
     try std.testing.expectApproxEqAbs(2.0, ldexpl(2, 0), epsilon);
     try std.testing.expectApproxEqAbs(1.701411834604692317316873037158841e38, ldexpl(1, 127), epsilon);
-    try std.testing.expectApproxEqAbs(std.math.maxInt(u128), ldexpl(2, 127), epsilon);
-    try std.testing.expectApproxEqAbs(std.math.maxInt(u128), ldexpl(1, 128), epsilon);
+    try std.testing.expectApproxEqAbs(math.maxInt(u128), ldexpl(2, 127), epsilon);
+    try std.testing.expectApproxEqAbs(math.maxInt(u128), ldexpl(1, 128), epsilon);
     try std.testing.expectApproxEqAbs(0.437500, ldexpl(7, -4), epsilon);
     try std.testing.expectApproxEqAbs(0, ldexpl(0, 10), epsilon);
-    try std.testing.expect(math.isNegativeInf(ldexpl(-std.math.inf(f128), -1)));
+    try std.testing.expect(math.isNegativeInf(ldexpl(-math.inf(f128), -1)));
     try std.testing.expect(math.isPositiveInf(ldexpl(1, 1024)));
 }
 
