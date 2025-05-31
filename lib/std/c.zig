2139eb75f9e1e6fa766797b60566b254efce2f8f
Elaine Gibson <ypsvlq@gmail.com>
2025-05-31 03:04:58 +0100

    std.c: getcontext is not supported on haiku

--- lib/std/c.zig
+++ lib/std/c.zig
@@ -10827,8 +10827,8 @@ pub const LC = enum(c_int) {
 
 pub extern "c" fn setlocale(category: LC, locale: ?[*:0]const u8) ?[*:0]const u8;
 
-pub const getcontext = if (builtin.target.abi.isAndroid() or builtin.target.os.tag == .openbsd)
-{} // android bionic and openbsd libc does not implement getcontext
+pub const getcontext = if (builtin.target.abi.isAndroid() or builtin.target.os.tag == .openbsd or builtin.target.os.tag == .haiku)
+{} // libc does not implement getcontext
     else if (native_os == .linux and builtin.target.abi.isMusl())
         linux.getcontext
     else
