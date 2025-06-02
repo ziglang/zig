1116d881965ed8c0009f2d5bb4f97eed605c4ec0
Loris Cro <kappaloris@gmail.com>
2025-06-02 13:02:44 +0200

    zig init: add new --strip flag and improve template files
    This commit introduces a new flag to generate a new Zig project using
    `zig init` without comments for users who are already familiar with the
    Zig build system.
    
    Additionally, the generated files are now different. Previously we would
    generate a set of files that defined a static library and an executable,
    which real-life experience has shown to cause confusion to newcomers.
    
    The new template generates one Zig module and one executable both in
    order to accommodate the two most common use cases, but also to suggest
    that a library could use a CLI tool (e.g. a parser library could use a
    CLI tool that provides syntax checking) and vice-versa a CLI tool might
    want to expose its core functionality as a Zig module.
    
    All references to C interoperability are removed from the template under
    the assumption that if you're tall enough to do C interop, you're also
    tall enough to find your way around the build system. Experienced users
    will still be able to use the current template and adapt it with minimal
    changes in order to perform more advanced operations. As an example, one
    only needs to change `b.addExecutable` to `b.addLibrary` to switch from
    generating an executable to a dynamic (or static) library.

--- lib/init/src/main.zig
+++ lib/init/src/main.zig
@@ -1,21 +1,10 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
+const std = @import("std");
+const .NAME = @import(".NAME");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    // Prints to stderr, ignoring potential errors.
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
+    try .NAME.advancedPrint();
 }
 
 test "simple test" {
@@ -23,10 +12,6 @@ test "simple test" {
     defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
     try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
 }
 
 test "fuzz example" {
@@ -39,8 +24,3 @@ test "fuzz example" {
     };
     try std.testing.fuzz(Context{}, Context.testOne, .{});
 }
-
-const std = @import("std");
-
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import(".NAME_lib");
