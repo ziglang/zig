180e8442af0c29924e021aed327bb4070665af65
Loris Cro <kappaloris@gmail.com>
2025-06-14 00:31:29 +0200

    zig init: simplify templating logic (#24170)
    and also rename `advancedPrint` to `bufferedPrint` in the zig init templates
    
    These are left overs from my previous changes to zig init.
    
    The new templating system removes LITNAME because the new restrictions on package names make it redundant with NAME, and the use of underscores for marking templated identifiers lets us template variable names while still keeping zig fmt happy.

--- lib/init/build.zig
+++ lib/init/build.zig
@@ -29,7 +29,7 @@ pub fn build(b: *std.Build) void {
     // to our consumers. We must give it a name because a Zig package can expose
     // multiple modules and consumers will need to be able to specify which
     // module they want to access.
-    const mod = b.addModule(".NAME", .{
+    const mod = b.addModule("_NAME", .{
         // The root source file is the "entry point" of this module. Users of
         // this module will only be able to access public declarations contained
         // in this file, which means that if you have declarations that you
@@ -59,7 +59,7 @@ pub fn build(b: *std.Build) void {
     // If neither case applies to you, feel free to delete the declaration you
     // don't need and to put everything under a single module.
     const exe = b.addExecutable(.{
-        .name = ".NAME",
+        .name = "_NAME",
         .root_module = b.createModule(.{
             // b.createModule defines a new module just like b.addModule but,
             // unlike b.addModule, it does not expose the module to consumers of
@@ -74,12 +74,12 @@ pub fn build(b: *std.Build) void {
             // List of modules available for import in source files part of the
             // root module.
             .imports = &.{
-                // Here ".NAME" is the name you will use in your source code to
-                // import this module (e.g. `@import(".NAME")`). The name is
+                // Here "_NAME" is the name you will use in your source code to
+                // import this module (e.g. `@import("_NAME")`). The name is
                 // repeated because you are allowed to rename your imports, which
                 // can be extremely useful in case of collisions (which can happen
                 // importing modules from different packages).
-                .{ .name = ".NAME", .module = mod },
+                .{ .name = "_NAME", .module = mod },
             },
         }),
     });
--- lib/init/src/root.zig
+++ lib/init/src/root.zig
@@ -1,11 +1,12 @@
 //! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
 
-pub fn advancedPrint() !void {
+pub fn bufferedPrint() !void {
     // Stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
     // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
+    // Buffering can improve performance significantly in print-heavy programs.
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
