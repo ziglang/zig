ff06de4c897e0b51c4d1f044746d43397500be69
Bingwu Zhang <xtex@aosc.io>
2025-06-14 18:05:57 +0800

    riscv64: enable test summary printing

--- lib/compiler/test_runner.zig
+++ lib/compiler/test_runner.zig
@@ -292,6 +292,7 @@ pub fn mainSimple() anyerror!void {
     };
     // is the backend capable of using std.fmt.format to print a summary at the end?
     const print_summary = switch (builtin.zig_backend) {
+        .stage2_riscv64 => true,
         else => false,
     };
 
