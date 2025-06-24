75d6d4c3f20423eede8ecaf7bcc4eaa501c1af9d
Marc <m@pyc.ac>
2025-06-24 23:14:00 +0700

    add glibc versioning for a number of headers (#24237)
    * getrandom was added in glibc 2.25
    https://sourceware.org/bugzilla/show_bug.cgi?id=17252
    
    * copy_file_range was added in glibc 2.27
    https://sourceware.org/git/?p=glibc.git;a=commit;h=bad7a0c81f501fbbcc79af9eaa4b8254441c4a1f
    
    * threads.h should not exist for glibc < 2.28
    
    * single_threaded.h should not exist for glibc < 2.35
    
    * Apply suggestions from code review
    
    Co-authored-by: Alex Rønne Petersen <alex@alexrp.com>
    
    * 2.35 instead of 35
    
    * before 2.35 instead of 2.34 and before
    
    ---------
    
    Co-authored-by: Alex Rønne Petersen <alex@alexrp.com>

--- lib/libc/include/generic-glibc/threads.h
+++ lib/libc/include/generic-glibc/threads.h
@@ -16,6 +16,12 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
+
+// zig patch: threads header was added in glibc 2.28
+#if __GLIBC__ == 2 && __GLIBC_MINOR__ < 28
+   #error "threads.h did not exist before glibc 2.28"
+#endif /* error for glibc before 2.28 */
+
 #ifndef _THREADS_H
 #define _THREADS_H	1
 
