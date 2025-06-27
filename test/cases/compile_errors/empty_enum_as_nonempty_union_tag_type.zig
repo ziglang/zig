ziglang/zig

commit 7b8fc18c666ba6952ad1571fbed4cc48e81f647d

Sema: fail if analyzing return in `noreturn`-declared function before coercing `undefined`
Just switches logic around in coerceExtra to check for returning in a noreturn function before coercing undefined to anything

src/Sema.zig
@@ -30346,11 +30346,6 @@ fn coerceExtra(
         else => {},
     }
 
-    // undefined to anything. We do this after the big switch above so that
-    // special logic has a chance to run first, such as `*[N]T` to `[]T` which
-    // should initialize the length field of the slice.
-    if (maybe_inst_val) |val| if (val.toIntern() == .undef) return pt.undefRef(dest_ty);
-
     if (!opts.report_err) return error.NotCoercible;
 
     if (opts.is_ret and dest_ty.zigTypeTag(zcu) == .noreturn) {
@@ -30367,6 +30362,11 @@ fn coerceExtra(
         };
         return sema.failWithOwnedErrorMsg(block, msg);
     }
+
+    // undefined to anything. We do this after the big switch above so that
+    // special logic has a chance to run first, such as `*[N]T` to `[]T` which
+    // should initialize the length field of the slice.
+    if (maybe_inst_val) |val| if (val.toIntern() == .undef) return pt.undefRef(dest_ty);
 
     const msg = msg: {
         const msg = try sema.errMsg(inst_src, "expected type '{}', found '{}'", .{ dest_ty.fmt(pt), inst_ty.fmt(pt) });

test/cases/compile_errors/return_undefined_from_noreturn.zig
@@ -1,0 +1,10 @@
+export fn entry() noreturn {
+    return undefined;
+}
+
+// error
+// backend=stage2
+// target=native
+//
+// :2:12: error: function declared 'noreturn' returns
+// :1:19: note: 'noreturn' declared here

commit f90bb09b11277bd5d28e4e6430656855f362ac90

Sema: fail if analyzing a nonempty union with an empty enum tag
Fixes #24262; as a bonus, adresses a potential issue I introduced in #21665

src/Sema.zig
@@ -29442,8 +29442,6 @@ fn coerceExtra(
         if (maybe_inst_val) |val| if (val.toIntern() == .undef) return pt.undefRef(dest_ty);
     }
 
-    if (!opts.report_err) return error.NotCoercible;
-
     if (opts.is_ret and dest_ty.zigTypeTag(zcu) == .noreturn) {
         const msg = msg: {
             const msg = try sema.errMsg(inst_src, "function declared 'noreturn' returns", .{});
@@ -29458,6 +29456,8 @@ fn coerceExtra(
         };
         return sema.failWithOwnedErrorMsg(block, msg);
     }
+
+    if (!opts.report_err) return error.NotCoercible;
 
     const msg = msg: {
         const msg = try sema.errMsg(inst_src, "expected type '{}', found '{}'", .{ dest_ty.fmt(pt), inst_ty.fmt(pt) });
@@ -36141,6 +36141,11 @@ fn unionFields(
                 };
                 return sema.failWithOwnedErrorMsg(&block_scope, msg);
             }
+        } else if (!small.auto_enum_tag and tag_type_ref != .none) {
+            const tag_ty = union_type.tagTypeUnordered(ip);
+            return sema.fail(&block_scope, name_src, "no field named '{}' in enum '{}'", .{
+                field_name.fmt(ip), Type.fromInterned(tag_ty).fmt(pt),
+            });
         }
 
         if (field_ty.zigTypeTag(zcu) == .@"opaque") {

test/cases/compile_errors/empty_enum_as_nonempty_union_tag_type.zig
@@ -1,0 +1,16 @@
+const E = enum {};
+const U = union(E) {
+    one,
+    two,
+};
+
+pub fn main() void {
+    _ = U.one;
+}
+
+// error
+// backend=stage2
+// target=native
+//
+// :4:5: error: no field named 'one' in enum 'tmp.E'
+// :2:11: note: enum declared here