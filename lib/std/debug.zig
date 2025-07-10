88e50b30c3c929761f2ae1a924b96f8bc7548b84
Andrew Kelley <andrew@ziglang.org>
2025-07-10 12:58:47 -0700

    std.debug.print: provide a small buffer

--- lib/std/debug.zig
+++ lib/std/debug.zig
@@ -219,10 +219,14 @@ pub fn unlockStderrWriter() void {
     std.Progress.unlockStderrWriter();
 }
 
-/// Print to stderr, unbuffered, and silently returning on failure. Intended
-/// for use in "printf debugging". Use `std.log` functions for proper logging.
+/// Print to stderr, silently returning on failure. Intended for use in "printf
+/// debugging". Use `std.log` functions for proper logging.
+///
+/// Uses a 64-byte buffer for formatted printing which is flushed before this
+/// function returns.
 pub fn print(comptime fmt: []const u8, args: anytype) void {
-    const bw = lockStderrWriter(&.{});
+    var buffer: [64]u8 = undefined;
+    const bw = lockStderrWriter(&buffer);
     defer unlockStderrWriter();
     nosuspend bw.print(fmt, args) catch return;
 }
