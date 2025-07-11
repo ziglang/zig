f521616ff14107c7b9c3c7069b01b09e06f441b0
mochalins <117967760+mochalins@users.noreply.github.com>
2025-07-11 09:18:19 +0900

    std: File reader `seekTo` pos->offset cast

--- lib/std/fs/File.zig
+++ lib/std/fs/File.zig
@@ -1368,7 +1368,10 @@ pub const Reader = struct {
                 r.pos = offset;
             },
             .streaming, .streaming_reading => {
-                if (offset >= r.pos) return Reader.seekBy(r, offset - r.pos);
+                if (offset >= r.pos) return Reader.seekBy(
+                    r,
+                    std.math.cast(i64, offset - r.pos) orelse return Reader.SeekError.Unexpected,
+                );
                 if (r.seek_err) |err| return err;
                 posix.lseek_SET(r.file.handle, offset) catch |err| {
                     r.seek_err = err;
