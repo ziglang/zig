23f624b3c5fa0af152401c934febf8d061409a77
Alex Rønne Petersen <alex@alexrp.com>
2025-07-13 00:59:44 +0200

    ci: target baseline instead of spacemit_x60 on riscv64-linux

--- ci/riscv64-linux-debug.sh
+++ ci/riscv64-linux-debug.sh
@@ -7,7 +7,7 @@ set -e
 
 ARCH="$(uname -m)"
 TARGET="$ARCH-linux-musl"
-MCPU="spacemit_x60"
+MCPU="baseline"
 CACHE_BASENAME="zig+llvm+lld+clang-riscv64-linux-musl-0.15.0-dev.929+31e46be74"
 PREFIX="$HOME/deps/$CACHE_BASENAME"
 ZIG="$PREFIX/bin/zig"
