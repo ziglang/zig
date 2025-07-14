6f6fa4e9c1865fc1f91423ab4c466c70eeda8a4b
Alex Rønne Petersen <alex@alexrp.com>
2025-07-15 00:15:35 +0200

    ci: make changes to riscv64-linux to try to avoid timeouts
    The idea is to have 2 runners per machine, since a lot of time is spent building
    stage3 and stage4, both of which are largely single-core affairs. This will make
    the test steps take longer, however, so the timeouts have been bumped a bit, and
    max RSS for the test step has been lowered from 64G to 32G to prevent OOM.
    Finally, we now only run a single ReleaseSafe job on PRs; Debug and Release jobs
    are limited to pushes.

--- ci/riscv64-linux-release.sh
+++ ci/riscv64-linux-release.sh
@@ -50,7 +50,7 @@ ninja install
 
 # No -fqemu and -fwasmtime here as they're covered by the x86_64-linux scripts.
 stage3-release/bin/zig build test docs \
-  --maxrss 68719476736 \
+  --maxrss 34359738368 \
   -Dstatic-llvm \
   -Dskip-non-native \
   -Dtarget=native-native-musl \
