33041fdbe5fb0e10a0f18e1953343bf1fc2c4fc4
Alex Rønne Petersen <alex@alexrp.com>
2025-07-17 14:12:05 +0200

    ci: increase max rss for riscv64-linux back to 64G

--- ci/riscv64-linux-release.sh
+++ ci/riscv64-linux-release.sh
@@ -50,7 +50,7 @@ ninja install
 
 # No -fqemu and -fwasmtime here as they're covered by the x86_64-linux scripts.
 stage3-release/bin/zig build test-cases test-modules test-unit test-standalone test-c-abi test-link test-stack-traces test-asm-link test-llvm-ir docs \
-  --maxrss 34359738368 \
+  --maxrss 68719476736 \
   -Dstatic-llvm \
   -Dskip-non-native \
   -Dtarget=native-native-musl \
