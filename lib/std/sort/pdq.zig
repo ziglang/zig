ec05d519ee022bb2b07e183508d9a0b99817ded6
Oittaa <eero@oittaa.net>
2025-07-21 03:42:34 +0200

    Also avoid overflow when `was_balanced`, add overflow test

--- lib/std/sort/pdq.zig
+++ lib/std/sort/pdq.zig
@@ -114,13 +114,13 @@ pub fn pdqContext(a: usize, b: usize, context: anytype) void {
             const left_len = mid - range.a;
             const right_len = range.b - mid;
             const balanced_threshold = len / 8;
-            if (left_len < right_len) {
-                was_balanced = left_len >= balanced_threshold;
+            if (right_len < left_len) {
+                was_balanced = right_len >= balanced_threshold;
                 stack[top] = .{ .a = range.a, .b = mid, .limit = range.limit };
                 top += 1;
                 range.a = mid + 1;
             } else {
-                was_balanced = right_len >= balanced_threshold;
+                was_balanced = left_len >= balanced_threshold;
                 stack[top] = .{ .a = mid + 1, .b = range.b, .limit = range.limit };
                 top += 1;
                 range.b = mid;
@@ -328,4 +328,33 @@ fn reverseRange(a: usize, b: usize, context: anytype) void {
         i += 1;
         j -= 1;
     }
+}
+
+test "stack overflow" {
+    var array = [_]u64{std.math.maxInt(u64)} ** 3789730;
+    var idx: usize = 0;
+    var a: usize = 0;
+    var d: usize = 0;
+    while (d < 64) : (d += 1) {
+        const cur_len = array.len - a;
+        const left_len = cur_len / 8;
+        const small_val = 10 * d + 1;
+        const pivot_val = 10 * d + 2;
+        const i_pos = a + (cur_len / 4);
+        const j_pos = a + (cur_len / 4) * 2;
+        array[i_pos - 1] = small_val;
+        array[i_pos] = small_val;
+        array[i_pos + 1] = small_val;
+        array[j_pos - 1] = pivot_val;
+        array[j_pos] = small_val;
+        var fill_len = left_len - 4;
+        while (fill_len > 0) : (fill_len -= 1) {
+            while (array[idx] != std.math.maxInt(u64)) idx += 1;
+            array[idx] = small_val;
+            idx += 1;
+        }
+        a = a + left_len + 1;
+    }
+    pdq(u64, &array, {}, std.sort.asc(u64));
+    try testing.expect(std.sort.isSorted(u64, &array, {}, std.sort.asc(u64)));
 }
