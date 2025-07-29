5c87b7220fdea2b7faa592b8f6c3d798c21c1ed2
mlugg <mlugg@mlugg.co.uk>
2025-07-30 00:07:47 +0100

    std.Build.Watch: add macOS implementation based on FSEventStream
    Resolves: #21905

--- lib/compiler/build_runner.zig
+++ lib/compiler/build_runner.zig
@@ -488,7 +488,7 @@ pub fn main() !void {
         // recursive dependants.
         var caption_buf: [std.Progress.Node.max_name_len]u8 = undefined;
         const caption = std.fmt.bufPrint(&caption_buf, "watching {d} directories, {d} processes", .{
-            w.dir_table.entries.len, countSubProcesses(run.step_stack.keys()),
+            w.dir_count, countSubProcesses(run.step_stack.keys()),
         }) catch &caption_buf;
         var debouncing_node = main_progress_node.start(caption, 0);
         var debounce_timeout: Watch.Timeout = .none;
