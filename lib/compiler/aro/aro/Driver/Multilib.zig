0f88f9c66444406879bf133895f3c21ab3b85418
Andrew Kelley <andrew@ziglang.org>
2025-08-11 13:52:52 -0700

    aro: avoid BoundedArray

--- lib/compiler/aro/aro/Driver/Multilib.zig
+++ lib/compiler/aro/aro/Driver/Multilib.zig
@@ -1,12 +1,12 @@
 const std = @import("std");
 const Filesystem = @import("Filesystem.zig").Filesystem;
 
-pub const Flags = std.BoundedArray([]const u8, 6);
+pub const Flags = std.ArrayListUnmanaged([]const u8);
 
 /// Large enough for GCCDetector for Linux; may need to be increased to support other toolchains.
 const max_multilibs = 4;
 
-const MultilibArray = std.BoundedArray(Multilib, max_multilibs);
+const MultilibArray = std.ArrayListUnmanaged(Multilib);
 
 pub const Detected = struct {
     multilibs: MultilibArray = .{},
@@ -15,20 +15,20 @@ pub const Detected = struct {
 
     pub fn filter(self: *Detected, multilib_filter: Filter, fs: Filesystem) void {
         var found_count: usize = 0;
-        for (self.multilibs.constSlice()) |multilib| {
+        for (self.multilibs.items) |multilib| {
             if (multilib_filter.exists(multilib, fs)) {
-                self.multilibs.set(found_count, multilib);
+                self.multilibs.items[found_count] = multilib;
                 found_count += 1;
             }
         }
         self.multilibs.resize(found_count) catch unreachable;
     }
 
-    pub fn select(self: *Detected, flags: Flags) !bool {
+    pub fn select(self: *Detected, flags: []const []const Flags) !bool {
         var filtered: MultilibArray = .{};
-        for (self.multilibs.constSlice()) |multilib| {
-            for (multilib.flags.constSlice()) |multilib_flag| {
-                const matched = for (flags.constSlice()) |arg_flag| {
+        for (self.multilibs.items) |multilib| {
+            for (flags) |multilib_flag| {
+                const matched = for (flags) |arg_flag| {
                     if (std.mem.eql(u8, arg_flag[1..], multilib_flag[1..])) break arg_flag;
                 } else multilib_flag;
                 if (matched[0] != multilib_flag[0]) break;
