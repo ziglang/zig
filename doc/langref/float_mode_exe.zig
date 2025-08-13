d1742dcfbdc3a8e8ce8b4c27034fe3ae8bfd3cc4
IOKG04 <iokg04@gmail.com>
2025-08-13 12:05:16 +0200

    float mode demo should conform with style guide
    I can see an argument why it shouldnt, readability,
    but I'd argue that consistency is about as important in documentation
    and camelCase is still very much readable.
    
    also did a small fix, I forgot a *safety-checked* in `@shrExact()` langreft

--- doc/langref/float_mode_exe.zig
+++ doc/langref/float_mode_exe.zig
@@ -1,12 +1,12 @@
 const print = @import("std").debug.print;
 
-extern fn foo_strict(x: f64) f64;
-extern fn foo_optimized(x: f64) f64;
+extern fn fooStrict(x: f64) f64;
+extern fn fooOptimized(x: f64) f64;
 
 pub fn main() void {
     const x = 0.001;
-    print("optimized = {}\n", .{foo_optimized(x)});
-    print("strict = {}\n", .{foo_strict(x)});
+    print("optimized = {}\n", .{fooOptimized(x)});
+    print("strict = {}\n", .{fooStrict(x)});
 }
 
 // syntax
