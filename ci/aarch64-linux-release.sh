e17b6c35083ed01d1b71bddb5bc43a553c7ca726
mlugg <mlugg@mlugg.co.uk>
2025-08-26 22:40:26 +0100

    ci: set unit test timeouts

--- ci/aarch64-linux-release.sh
+++ ci/aarch64-linux-release.sh
@@ -50,7 +50,8 @@ stage3-release/bin/zig build test docs \
   -Dtarget=native-native-musl \
   --search-prefix "$PREFIX" \
   --zig-lib-dir "$PWD/../lib" \
-  -Denable-superhtml
+  -Denable-superhtml \
+  --test-timeout-ms 60_000
 
 # Ensure that stage3 and stage4 are byte-for-byte identical.
 stage3-release/bin/zig build \
