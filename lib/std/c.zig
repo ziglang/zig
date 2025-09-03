79f60302d659c7719f8b9b6b35629d7450459030
Linus Groh <mail@linusgroh.de>
2025-09-03 02:24:47 +0100

    std.c: Enable arc4random_buf for serenity

--- lib/std/c.zig
+++ lib/std/c.zig
@@ -10353,7 +10353,7 @@ pub extern "c" fn setrlimit64(resource: rlimit_resource, rlim: *const rlimit) c_
 
 pub const arc4random_buf = switch (native_os) {
     .linux => if (builtin.abi.isAndroid()) private.arc4random_buf else {},
-    .dragonfly, .netbsd, .freebsd, .solaris, .openbsd, .macos, .ios, .tvos, .watchos, .visionos => private.arc4random_buf,
+    .dragonfly, .netbsd, .freebsd, .solaris, .openbsd, .serenity, .macos, .ios, .tvos, .watchos, .visionos => private.arc4random_buf,
     else => {},
 };
 pub const getentropy = switch (native_os) {
