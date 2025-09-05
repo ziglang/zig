b9766f0a36ce101e6ecd18e154de917bca1a973e
mlugg <mlugg@mlugg.co.uk>
2025-09-05 20:40:11 +0100

    replace usages of old std.debug APIs
    src/crash_handler.zig is still TODO though, i am planning bigger changes there

--- lib/std/posix.zig
+++ lib/std/posix.zig
@@ -7514,7 +7514,7 @@ pub const UnexpectedError = error{
 pub fn unexpectedErrno(err: E) UnexpectedError {
     if (unexpected_error_tracing) {
         std.debug.print("unexpected errno: {d}\n", .{@intFromEnum(err)});
-        std.debug.dumpCurrentStackTrace(null);
+        std.debug.dumpCurrentStackTrace(.{});
     }
     return error.Unexpected;
 }
