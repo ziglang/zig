8a3737260fd4d037e0465a8c2d1b4ab88deb12a9
Sardorbek Imomaliev <imomaliev@hey.com>
2025-09-08 18:42:23 +0100

    Make cat in test/standalone/simple working again
    - Fixes:
        zig/0.15.1/lib/zig/std/Io/Writer.zig:939:11: 0x1049aef63 in sendFileAll (nclip)
            assert(w.buffer.len > 0);
    - because we are no using non zero buffers for stdout - "do not forget to flush"

--- test/standalone/simple/cat/main.zig
+++ test/standalone/simple/cat/main.zig
@@ -13,7 +13,8 @@ pub fn main() !void {
 
     const exe = args[0];
     var catted_anything = false;
-    var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
+    var stdout_buffer: [4096]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writerStreaming(&stdout_buffer);
     const stdout = &stdout_writer.interface;
     var stdin_reader = std.fs.File.stdin().readerStreaming(&.{});
 
@@ -23,6 +24,7 @@ pub fn main() !void {
         if (mem.eql(u8, arg, "-")) {
             catted_anything = true;
             _ = try stdout.sendFileAll(&stdin_reader, .unlimited);
+            try stdout.flush();
         } else if (mem.startsWith(u8, arg, "-")) {
             return usage(exe);
         } else {
@@ -32,10 +34,12 @@ pub fn main() !void {
             catted_anything = true;
             var file_reader = file.reader(&.{});
             _ = try stdout.sendFileAll(&file_reader, .unlimited);
+            try stdout.flush();
         }
     }
     if (!catted_anything) {
         _ = try stdout.sendFileAll(&stdin_reader, .unlimited);
+        try stdout.flush();
     }
 }
 
