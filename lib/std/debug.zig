0f56d7afe298ae8f2b49d6c3b14fd935c971f1bb
Alex Rønne Petersen <alex@alexrp.com>
2025-10-03 03:24:21 +0200

    std.debug: use correct return address offset for s390x
    Makes FP-based unwinding work.

--- lib/std/debug.zig
+++ lib/std/debug.zig
@@ -952,6 +952,9 @@ const StackIterator = union(enum) {
     /// Offset of the saved return address wrt the frame pointer.
     const ra_offset = off: {
         if (native_arch == .powerpc64le) break :off 2 * @sizeOf(usize);
+        // On s390x, r14 is the link register and we need to grab it from its customary slot in the
+        // register save area (ELF ABI s390x Supplement §1.2.2.2).
+        if (native_arch == .s390x) break :off 14 * @sizeOf(usize);
         break :off @sizeOf(usize);
     };
 
