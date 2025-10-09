b4a5720134c6bd70de8298c2de7d85cb817e49e7
bnuuydev <clara@bnuuycoded.uk>
2025-10-09 22:47:27 +0100

    @{deposit,extract}Bits: More behaviour tests
    Adds randomly-generated test cases for the builtins. These cases have a number
    of different integer sizes. This has helped to uncover a few issues with the
    current implementation, which will be fixed in subsequent commits. Additionally
    skips tests for Windows with the x86 backend, as we are not providing support
    until progress is made on the self-hosted COFF linker.

--- test/behavior/deposit_extract_bits.zig
+++ test/behavior/deposit_extract_bits.zig
@@ -5,13 +5,12 @@ const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 
 const supports_pext_pdep = switch (builtin.zig_backend) {
-    .stage2_llvm => true,
-    .stage2_x86_64 => true,
-    .stage2_c => true,
+    .stage2_llvm, .stage2_c => true,
+    .stage2_x86_64 => builtin.target.os.tag != .windows,
     else => false,
 };
 
-test "@depositBits" {
+test "@depositBits u64" {
     if (!supports_pext_pdep) return error.SkipZigTest; // TODO
 
     const S = struct {
@@ -42,8 +41,7 @@ test "@depositBits" {
 
 test "@depositBits u128" {
     if (!supports_pext_pdep) return error.SkipZigTest; // TODO
-    if (builtin.zig_backend == .stage2_c) return error.SkipZigTest;
-    if (builtin.zig_backend == .stage2_x86_64 and builtin.target.os.tag == .windows) return error.SkipZigTest; // TODO #19498
+    if (builtin.zig_backend == .stage2_c) return error.SkipZigTest; // TODO #19991
 
     const S = struct {
         pub fn doTheTest() !void {
@@ -63,7 +61,7 @@ test "@depositBits u128" {
 
 test "@depositBits u256" {
     if (!supports_pext_pdep) return error.SkipZigTest; // TODO
-    if (builtin.zig_backend == .stage2_c) return error.SkipZigTest;
+    if (builtin.zig_backend == .stage2_c) return error.SkipZigTest; // TODO #19991
 
     const S = struct {
         pub fn doTheTest() !void {
@@ -81,7 +79,7 @@ test "@depositBits u256" {
     try comptime S.doTheTest();
 }
 
-test "@extractBits" {
+test "@extractBits u64" {
     if (!supports_pext_pdep) return error.SkipZigTest; // TODO
 
     const S = struct {
@@ -112,8 +110,7 @@ test "@extractBits" {
 
 test "@extractBits u128" {
     if (!supports_pext_pdep) return error.SkipZigTest; // TODO
-    if (builtin.zig_backend == .stage2_c) return error.SkipZigTest;
-    if (builtin.zig_backend == .stage2_x86_64 and builtin.target.os.tag == .windows) return error.SkipZigTest; // TODO #19498
+    if (builtin.zig_backend == .stage2_c) return error.SkipZigTest; // TODO #19991
 
     const S = struct {
         pub fn doTheTest() !void {
@@ -133,7 +130,7 @@ test "@extractBits u128" {
 
 test "@extractBits u256" {
     if (!supports_pext_pdep) return error.SkipZigTest; // TODO
-    if (builtin.zig_backend == .stage2_c) return error.SkipZigTest;
+    if (builtin.zig_backend == .stage2_c) return error.SkipZigTest; // TODO #19991
 
     const S = struct {
         pub fn doTheTest() !void {
@@ -149,4 +146,66 @@ test "@extractBits u256" {
 
     try S.doTheTest();
     try comptime S.doTheTest();
+}
+
+test "@depositBits" {
+    if (!supports_pext_pdep) return error.SkipZigTest; // TODO
+
+    const S = struct {
+        pub fn doTheTest() !void {
+            try expectDepositBits(u5, 0xc, 0x0, 0x0);
+            try expectDepositBits(u8, 0x34, 0x3e, 0x28);
+            try expectDepositBits(u12, 0x8d1, 0x3ff, 0xd1);
+            try expectDepositBits(u16, 0x71bf, 0x3af1, 0x32f1);
+            try expectDepositBits(u32, 0x3bae5063, 0x7b17b132, 0x1200a012);
+            try expectDepositBits(u48, 0x434aa15ff2fa, 0xce370a6c311, 0xce34086c210);
+            try expectDepositBits(u64, 0x8361fc9b827793a6, 0xe67fcd567987eee6, 0x425c041639026a24);
+
+            if (builtin.zig_backend == .stage2_c) return error.SkipZigTest; // TODO #19991
+
+            try expectDepositBits(u97, 0x171f755a01d485c4c34c18c81, 0xac06c853b200585f371570eb, 0x80044800a200084030142001);
+            try expectDepositBits(u128, 0xb7be70a644ee77116f7265b2a4b95a8b, 0x6c3396ebe8de95f9eaf62d08b2c3cb56, 0x80292e38818856148442c0090c14046);
+            try expectDepositBits(u185, 0x4a0774246e045222bb0ed34d184b1bbde1fc99c9ca0e89, 0x1b91d49bb592ec503cce5e517e87137fff828329d15be8f, 0x11811410a0104c0018ca5a510687104ce4828021001a809);
+            try expectDepositBits(u256, 0x43837440edafe142bd5b2f022f8a05d596c98b3c4be1ba19f4df4f9cbaadbda2, 0x86942d4fa0882cfeea9b45ad11334e0877b81e6c3e9c8b01a38c673778c8a1d3, 0x8280214120800cc44a98018d0100480875180e640e908a008384223338888102);
+            try expectDepositBits(u479, 0x4b9850b7dacb9a133557b25750455b9aead11be92175443d26db30bdd39a81e5a9a3a106d679f35067f76e832f15e13af81b56400bbe0ac9dff4cb06, 0x2c318fa22f8ae1373baa74eed5b70b1c7b7ab0bd6ea4804f88f87b21464ad5ee017cacd69a8c82bdcb68fe0b71e787eeda6d770d3c80f03a5b805dcc, 0x203084000f026024320a500805320a0c6370a0900c24804e88b0720002409584013c0086888c8200c320b200000585e8084c23093c80e02242804048);
+        }
+    };
+
+    try S.doTheTest();
+    try comptime S.doTheTest();
+}
+
+test "@extractBits" {
+    if (!supports_pext_pdep) return error.SkipZigTest; // TODO
+
+    const S = struct {
+        pub fn doTheTest() !void {
+            try expectExtractBits(u5, 0x1c, 0xe, 0x6);
+            try expectExtractBits(u8, 0xc1, 0xbe, 0x20);
+            try expectExtractBits(u12, 0x8fd, 0x910, 0x5);
+            try expectExtractBits(u16, 0x694c, 0xaaea, 0xca);
+            try expectExtractBits(u32, 0xa9f97bcf, 0x64f207c2, 0x179f);
+            try expectExtractBits(u48, 0x32901c841c2a, 0x3721b7ff376d, 0x6832118c);
+            try expectExtractBits(u64, 0xbc1ba402eaabd49b, 0x8324f9742e70d227, 0x21406ae3);
+
+            if (builtin.zig_backend == .stage2_c) return error.SkipZigTest; // TODO #19991
+
+            try expectExtractBits(u97, 0x12c7ffc54e5772313dae0aa7a, 0x144e7728badaa0c5edee2f2ab, 0x15fe0da89f7aae);
+            try expectExtractBits(u128, 0x18eb4eaa5e93441fa28d2860de22961b, 0x3b89eec7dd369bb8634b8da908272721, 0xcebc9e501b2a2699);
+            try expectExtractBits(u185, 0x1fad06e744cee4f42aa80057dd1fb8b86a2281d124e389e, 0x1a5f25ae5516369fd211e040df64b5fb97ca12d189474b8, 0x1cd5ba75a083f4f2e084f16b);
+            try expectExtractBits(u256, 0xb5db52469100b3796a6981ed441d685ede3c39e423d91ff5dc33d0ae3696067c, 0x2b03ad2a509cc14a8cfc71b9cfbadee93ab976d6335c3897d5188cec3c89081a, 0x4e0285c9a1da86514ee66af9f7d4bec6);
+            try expectExtractBits(u479, 0x7c44ec50c139a0fb34d51fa28a9f63f9940e578df33e21792c25b4a4e931df79bcbe45eb5cce05b0e73b5d01d0bc9bd4677e2217285c390012de90cf, 0x3facb493aa8150da7350b5f7ef349addba0fc293a258319cb61c1b224f07f0e096cf117bdb0e2338a7eae3e88e8e392161be97b90e23b879c8c51333, 0x3c1d484fb33d294c7da739eeb28c593afae77739df3a4239cef88b0380743);
+        }
+    };
+
+    try S.doTheTest();
+    try comptime S.doTheTest();
+}
+
+fn expectDepositBits(comptime T: type, src: T, dst: T, exp: T) !void {
+    return expectEqual(@depositBits(src, dst), exp);
+}
+
+fn expectExtractBits(comptime T: type, src: T, dst: T, exp: T) !void {
+    return expectEqual(@extractBits(src, dst), exp);
 }
