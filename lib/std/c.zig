ac3e4f45194ecf01d697d0cdc0c7af76f56f103c
Ryan Zezeski <ryan@zinascii.com>
2025-10-25 10:36:17 +0200

    std.c: define arc4random_buf() for illumos

--- lib/std/c.zig
+++ lib/std/c.zig
@@ -10298,7 +10298,7 @@ pub extern "c" fn setrlimit64(resource: rlimit_resource, rlim: *const rlimit) c_
 
 pub const arc4random_buf = switch (native_os) {
     .linux => if (builtin.abi.isAndroid()) private.arc4random_buf else {},
-    .dragonfly, .netbsd, .freebsd, .solaris, .openbsd, .serenity, .macos, .ios, .tvos, .watchos, .visionos => private.arc4random_buf,
+    .dragonfly, .netbsd, .freebsd, .solaris, .illumos, .openbsd, .serenity, .macos, .ios, .tvos, .watchos, .visionos => private.arc4random_buf,
     else => {},
 };
 pub const getentropy = switch (native_os) {
