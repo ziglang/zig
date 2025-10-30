51a229c596b010396c64ef8769cde7018b05ab17
qilme <89187439+qilme@users.noreply.github.com>
2025-10-30 17:51:28 +0800

    std.os.windows: eliminate forwarder function in kernel32 #1840

--- lib/std/os/windows/kernel32.zig
+++ lib/std/os/windows/kernel32.zig
@@ -1,18 +1,16 @@
 const std = @import("../../std.zig");
 const windows = std.os.windows;
+const ntdll = windows.ntdll;
 
 const BOOL = windows.BOOL;
-const BOOLEAN = windows.BOOLEAN;
 const CONDITION_VARIABLE = windows.CONDITION_VARIABLE;
 const CONSOLE_SCREEN_BUFFER_INFO = windows.CONSOLE_SCREEN_BUFFER_INFO;
 const COORD = windows.COORD;
-const CRITICAL_SECTION = windows.CRITICAL_SECTION;
 const DWORD = windows.DWORD;
 const FARPROC = windows.FARPROC;
 const FILETIME = windows.FILETIME;
 const HANDLE = windows.HANDLE;
 const HANDLER_ROUTINE = windows.HANDLER_ROUTINE;
-const HLOCAL = windows.HLOCAL;
 const HMODULE = windows.HMODULE;
 const INIT_ONCE = windows.INIT_ONCE;
 const INIT_ONCE_FN = windows.INIT_ONCE_FN;
@@ -32,30 +30,29 @@ const SECURITY_ATTRIBUTES = windows.SECURITY_ATTRIBUTES;
 const SIZE_T = windows.SIZE_T;
 const SRWLOCK = windows.SRWLOCK;
 const STARTUPINFOW = windows.STARTUPINFOW;
+const SYSTEM_INFO = windows.SYSTEM_INFO;
 const UCHAR = windows.UCHAR;
 const UINT = windows.UINT;
 const ULONG = windows.ULONG;
 const ULONG_PTR = windows.ULONG_PTR;
 const va_list = windows.va_list;
-const VECTORED_EXCEPTION_HANDLER = windows.VECTORED_EXCEPTION_HANDLER;
 const WCHAR = windows.WCHAR;
 const WIN32_FIND_DATAW = windows.WIN32_FIND_DATAW;
 const Win32Error = windows.Win32Error;
 const WORD = windows.WORD;
-const SYSTEM_INFO = windows.SYSTEM_INFO;
 
 // I/O - Filesystem
 
 pub extern "kernel32" fn ReadDirectoryChangesW(
-    hDirectory: windows.HANDLE,
+    hDirectory: HANDLE,
     lpBuffer: [*]align(@alignOf(windows.FILE_NOTIFY_INFORMATION)) u8,
-    nBufferLength: windows.DWORD,
-    bWatchSubtree: windows.BOOL,
+    nBufferLength: DWORD,
+    bWatchSubtree: BOOL,
     dwNotifyFilter: windows.FileNotifyChangeFilter,
-    lpBytesReturned: ?*windows.DWORD,
-    lpOverlapped: ?*windows.OVERLAPPED,
+    lpBytesReturned: ?*DWORD,
+    lpOverlapped: ?*OVERLAPPED,
     lpCompletionRoutine: windows.LPOVERLAPPED_COMPLETION_ROUTINE,
-) callconv(.winapi) windows.BOOL;
+) callconv(.winapi) BOOL;
 
 // TODO: Wrapper around NtCancelIoFile.
 pub extern "kernel32" fn CancelIo(
@@ -258,16 +255,8 @@ pub extern "kernel32" fn CreateIoCompletionPort(
     NumberOfConcurrentThreads: DWORD,
 ) callconv(.winapi) ?HANDLE;
 
-// TODO: Forwarder to NtAddVectoredExceptionHandler.
-pub extern "kernel32" fn AddVectoredExceptionHandler(
-    First: ULONG,
-    Handler: ?VECTORED_EXCEPTION_HANDLER,
-) callconv(.winapi) ?LPVOID;
-
-// TODO: Forwarder to NtRemoveVectoredExceptionHandler.
-pub extern "kernel32" fn RemoveVectoredExceptionHandler(
-    Handle: HANDLE,
-) callconv(.winapi) ULONG;
+pub const AddVectoredExceptionHandler = ntdll.RtlAddVectoredExceptionHandler;
+pub const RemoveVectoredExceptionHandler = ntdll.RtlRemoveVectoredExceptionHandler;
 
 // TODO: Wrapper around RtlReportSilentProcessExit + NtTerminateProcess.
 pub extern "kernel32" fn TerminateProcess(
@@ -321,10 +310,7 @@ pub extern "kernel32" fn CreateProcessW(
     lpProcessInformation: *PROCESS_INFORMATION,
 ) callconv(.winapi) BOOL;
 
-// TODO: Fowarder to RtlExitUserProcess.
-pub extern "kernel32" fn ExitProcess(
-    exit_code: UINT,
-) callconv(.winapi) noreturn;
+pub const ExitProcess = ntdll.RtlExitUserProcess;
 
 // TODO: implement via ntdll instead
 pub extern "kernel32" fn SleepEx(
@@ -372,40 +358,14 @@ pub extern "kernel32" fn SwitchToThread() callconv(.winapi) BOOL;
 
 // Locks, critical sections, initializers
 
-// TODO: Forwarder to RtlInitializeCriticalSection
-pub extern "kernel32" fn InitializeCriticalSection(
-    lpCriticalSection: *CRITICAL_SECTION,
-) callconv(.winapi) void;
+pub const InitializeCriticalSection = ntdll.RtlInitializeCriticalSection;
+pub const EnterCriticalSection = ntdll.RtlEnterCriticalSection;
+pub const LeaveCriticalSection = ntdll.RtlLeaveCriticalSection;
+pub const DeleteCriticalSection = ntdll.RtlDeleteCriticalSection;
 
-// TODO: Forwarder to RtlEnterCriticalSection
-pub extern "kernel32" fn EnterCriticalSection(
-    lpCriticalSection: *CRITICAL_SECTION,
-) callconv(.winapi) void;
-
-// TODO: Forwarder to RtlLeaveCriticalSection
-pub extern "kernel32" fn LeaveCriticalSection(
-    lpCriticalSection: *CRITICAL_SECTION,
-) callconv(.winapi) void;
-
-// TODO: Forwarder to RtlDeleteCriticalSection
-pub extern "kernel32" fn DeleteCriticalSection(
-    lpCriticalSection: *CRITICAL_SECTION,
-) callconv(.winapi) void;
-
-// TODO: Forwarder to RtlTryAcquireSRWLockExclusive
-pub extern "kernel32" fn TryAcquireSRWLockExclusive(
-    SRWLock: *SRWLOCK,
-) callconv(.winapi) BOOLEAN;
-
-// TODO: Forwarder to RtlAcquireSRWLockExclusive
-pub extern "kernel32" fn AcquireSRWLockExclusive(
-    SRWLock: *SRWLOCK,
-) callconv(.winapi) void;
-
-// TODO: Forwarder to RtlReleaseSRWLockExclusive
-pub extern "kernel32" fn ReleaseSRWLockExclusive(
-    SRWLock: *SRWLOCK,
-) callconv(.winapi) void;
+pub const TryAcquireSRWLockExclusive = ntdll.RtlTryAcquireSRWLockExclusive;
+pub const AcquireSRWLockExclusive = ntdll.RtlAcquireSRWLockExclusive;
+pub const ReleaseSRWLockExclusive = ntdll.RtlReleaseSRWLockExclusive;
 
 pub extern "kernel32" fn InitOnceExecuteOnce(
     InitOnce: *INIT_ONCE,
@@ -414,15 +374,8 @@ pub extern "kernel32" fn InitOnceExecuteOnce(
     Context: ?*anyopaque,
 ) callconv(.winapi) BOOL;
 
-// TODO: Forwarder to RtlWakeConditionVariable
-pub extern "kernel32" fn WakeConditionVariable(
-    ConditionVariable: *CONDITION_VARIABLE,
-) callconv(.winapi) void;
-
-// TODO: Forwarder to RtlWakeAllConditionVariable
-pub extern "kernel32" fn WakeAllConditionVariable(
-    ConditionVariable: *CONDITION_VARIABLE,
-) callconv(.winapi) void;
+pub const WakeConditionVariable = ntdll.RtlWakeConditionVariable;
+pub const WakeAllConditionVariable = ntdll.RtlWakeAllConditionVariable;
 
 // TODO:
 //  - dwMilliseconds -> LARGE_INTEGER.
@@ -514,27 +467,9 @@ pub extern "kernel32" fn HeapCreate(
     dwMaximumSize: SIZE_T,
 ) callconv(.winapi) ?HANDLE;
 
-// TODO: Forwarder to RtlReAllocateHeap.
-pub extern "kernel32" fn HeapReAlloc(
-    hHeap: HANDLE,
-    dwFlags: DWORD,
-    lpMem: *anyopaque,
-    dwBytes: SIZE_T,
-) callconv(.winapi) ?*anyopaque;
-
-// TODO: Fowrarder to RtlAllocateHeap.
-pub extern "kernel32" fn HeapAlloc(
-    hHeap: HANDLE,
-    dwFlags: DWORD,
-    dwBytes: SIZE_T,
-) callconv(.winapi) ?*anyopaque;
-
-// TODO: Fowrarder to RtlFreeHeap.
-pub extern "kernel32" fn HeapFree(
-    hHeap: HANDLE,
-    dwFlags: DWORD,
-    lpMem: LPVOID,
-) callconv(.winapi) BOOL;
+pub const HeapReAlloc = ntdll.RtlReAllocateHeap;
+pub const HeapAlloc = ntdll.RtlAllocateHeap;
+pub const HeapFree = ntdll.RtlFreeHeap;
 
 // TODO: Wrapper around RtlValidateHeap (BOOLEAN -> BOOL)
 pub extern "kernel32" fn HeapValidate(
@@ -642,4 +577,6 @@ pub extern "kernel32" fn SetLastError(
 
 // Everything Else
 
-pub extern "kernel32" fn GetSystemInfo(lpSystemInfo: *SYSTEM_INFO) callconv(.winapi) void;
+pub extern "kernel32" fn GetSystemInfo(
+    lpSystemInfo: *SYSTEM_INFO,
+) callconv(.winapi) void;
