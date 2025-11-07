cf77fcbb4f8ce383e9b723a9652b8c81035454bc
Danielkonge <dakongsgaard@gmail.com>
2025-11-07 01:06:31 +0100

    Add comptime_float support to std.math.isFinite

--- lib/std/math/isfinite.zig
+++ lib/std/math/isfinite.zig
@@ -4,14 +4,14 @@ const expect = std.testing.expect;
 
 /// Returns whether x is a finite value.
 pub fn isFinite(x: anytype) bool {
-    const T = @TypeOf(x);
+    const T = if (@TypeOf(x) == comptime_float) f128 else @TypeOf(x);
     const TBits = std.meta.Int(.unsigned, @typeInfo(T).float.bits);
     const remove_sign = ~@as(TBits, 0) >> 1;
-    return @as(TBits, @bitCast(x)) & remove_sign < @as(TBits, @bitCast(math.inf(T)));
+    return @as(TBits, @bitCast(@as(T, x))) & remove_sign < @as(TBits, @bitCast(math.inf(T)));
 }
 
 test isFinite {
-    inline for ([_]type{ f16, f32, f64, f80, f128 }) |T| {
+    inline for ([_]type{ f16, f32, f64, f80, f128, comptime_float }) |T| {
         // normals
         try expect(isFinite(@as(T, 1.0)));
         try expect(isFinite(-@as(T, 1.0)));
@@ -24,6 +24,8 @@ test isFinite {
         // other float limits
         try expect(isFinite(math.floatMin(T)));
         try expect(isFinite(math.floatMax(T)));
+
+        if (T == comptime_float) return;
 
         // inf & nan
         try expect(!isFinite(math.inf(T)));
