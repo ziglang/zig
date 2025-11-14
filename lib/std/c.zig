3a08d2f162de86e2613392c78e75df3b3a19aaf8
Lukas Lalinsky <lukas@lalinsky.com>
2025-11-14 17:48:19 +0100

    Define std.c.MADV for NetBSD
    The `.netbsd` branch was completely missing. Validated against the
    actual system headers.

--- lib/std/c.zig
+++ lib/std/c.zig
@@ -1599,6 +1599,15 @@ pub const MADV = switch (native_os) {
         pub const SEQUENTIAL = 0x5;
         pub const RANDOM = 0x6;
     },
+    .netbsd => struct {
+        pub const NORMAL = 0;
+        pub const RANDOM = 1;
+        pub const SEQUENTIAL = 2;
+        pub const WILLNEED = 3;
+        pub const DONTNEED = 4;
+        pub const SPACEAVAIL = 5;
+        pub const FREE = 6;
+    },
     else => void,
 };
 pub const MCL = switch (native_os) {
