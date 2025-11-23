6bcf0066b1a0b972b215f2ebb49c94aaaa3a151a
meowjesty <43983236+meowjesty@users.noreply.github.com>
2025-11-22 22:18:27 -0300

    Use `.empty` for initialization.
    Co-authored-by: Rue <78876133+IOKG04@users.noreply.github.com>

--- doc/langref/testing_detect_leak.zig
+++ doc/langref/testing_detect_leak.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 test "detect leak" {
     const allocator = std.testing.allocator;
-    var list = try std.ArrayList(u21).initCapacity(allocator, 1);
+    var list: std.ArrayList(u21) = .empty;
     // missing `defer list.deinit(allocator);`
     try list.append(allocator, '☔');
 
